2a8c58f6144b4a82d151c7429eec8045e7bab5ad
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 19:03:53 +0000

    c3a5269 perf(animations): reduce size of bundle by removing AST classes (#19539)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-b1ca5d4
+ * @license Angular v5.0.0-rc.1-c3a5269
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.1-b1ca5d4
+ * @license Angular v5.0.0-rc.1-c3a5269
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
