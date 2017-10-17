049c3b479291c6346db2540b7a4e13ef3ac60f37
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 22:18:39 +0000

    64b3619 build: don’t make BrowserStack required as it fails with “Access denied”

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-507290d
+ * @license Angular v5.0.0-rc.2-64b3619
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.2-507290d
+ * @license Angular v5.0.0-rc.2-64b3619
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
