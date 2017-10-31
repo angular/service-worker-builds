40e73b3ba04fb3d051b9f7663ef07e6d407f7dd8
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:39:45 +0000

    420852e fix(compiler): reexport less symbols in `.ngfactory.ts` files (#19884)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-04eb80c
+ * @license Angular v5.0.0-rc.8-420852e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.8-04eb80c
+ * @license Angular v5.0.0-rc.8-420852e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
