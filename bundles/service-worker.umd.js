272876d7c6b6434c94c743b5baab2245832003a1
Victor Berchet <victor@suumit.com>
2017-11-02 22:40:27 +0000

    067e926 build(aio): do not fail the initial doc-gen when running docs-watch

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-8639995
+ * @license Angular v5.0.0-067e926
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-8639995
+ * @license Angular v5.0.0-067e926
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
