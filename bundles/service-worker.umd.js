c62d0eb1e4e16c86974126779f950892c4a3562b
Victor Berchet <victor@suumit.com>
2017-11-03 05:27:24 +0000

    dcf8840 feat(elements): implement `registerAsCustomElements()`

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-a28d616
+ * @license Angular v5.1.0-beta.0-dcf8840
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-beta.0-a28d616
+ * @license Angular v5.1.0-beta.0-dcf8840
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
