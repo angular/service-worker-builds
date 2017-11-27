cf81428947270645ebded52f4ec95d1b6b08f85d
Miško Hevery <misko@hevery.com>
2017-11-27 01:28:57 +0000

    330bb2a release: cut the 5.1.0-beta.2 release

---
+++ service-worker.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { NgswCommChannel as ɵe } from './src/low_level';
+export { OPTS as ɵb, SCRIPT as ɵa, ngswAppInitializer as ɵc, ngswCommChannelFactory as ɵd } from './src/module';
