e83e0205be768ec2ab4a76d343d92fde5982fa84
Igor Minar <igor@angularjs.org>
2017-12-06 15:31:30 +0000

    eb57bc3 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-d824cc2
+ * @license Angular v5.0.5-eb57bc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.5-d824cc2
+ * @license Angular v5.0.5-eb57bc3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
