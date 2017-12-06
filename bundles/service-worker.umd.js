b60b15b7a482d63bf543cc900ce33755f808cac1
Igor Minar <igor@angular.io>
2017-12-06 21:00:12 +0000

    c2dbc55 release: cut the 5.1.0 release

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-b78ada1
+ * @license Angular v5.1.0-c2dbc55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-rc.1-b78ada1
+ * @license Angular v5.1.0-c2dbc55
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
