004afa64267b5f0e22355c8accccac485eceb2cb
Jason Aden <jasonaden@google.com>
2017-12-08 23:26:17 +0000

    d09d497 fix(animations): ensure multi-level route leave animations are queryable (#20787)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-d713225
+ * @license Angular v5.1.0-d09d497
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.1.0-d713225
+ * @license Angular v5.1.0-d09d497
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
