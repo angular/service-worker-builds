46f376b1a28c53f4cdf71b614293db6e8794d435
Kara Erickson <karakara@google.com>
2018-01-05 22:45:15 +0000

    2fc4cf6 build(core): update payload size of hello world (#21340)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-f9436f7
+ * @license Angular v5.2.0-rc.0-2fc4cf6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-f9436f7
+ * @license Angular v5.2.0-rc.0-2fc4cf6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
