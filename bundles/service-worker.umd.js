63f58244b4b95ccf18c085f4af53e7f3bea668f4
Alex Eagle <alexeagle@google.com>
2018-01-10 04:35:04 +0000

    a0166bd docs(bazel): document an installation issue

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-0d1611f
+ * @license Angular v5.2.0-a0166bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-rc.0-0d1611f
+ * @license Angular v5.2.0-a0166bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
