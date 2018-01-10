712ef0ae5f94c438d7e716ceb27d1c11c9b37640
Kara Erickson <karakara@google.com>
2018-01-10 18:28:31 +0000

    04dd971 docs: add changelog for 5.2.0

---
+++ service-worker.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { NgswCommChannel as ɵe } from './src/low_level';
+export { RegistrationOptions as ɵa, SCRIPT as ɵb, ngswAppInitializer as ɵc, ngswCommChannelFactory as ɵd } from './src/module';
