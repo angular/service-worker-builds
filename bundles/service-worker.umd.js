fe57ce1dc08aede7d35c8e5e8023d05c66a4412a
Misko Hevery <misko@hevery.com>
2018-01-26 06:44:48 +0000

    56f3e18 fix(forms): allow FormBuilder to create controls with any formState type (#20917)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-27ecd07
+ * @license Angular v5.2.2-56f3e18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-27ecd07
+ * @license Angular v5.2.2-56f3e18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
