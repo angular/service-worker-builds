330c9f58513a1dae37f0ed05b9e9b3f82a30a307
Jason Aden <jasonaden@google.com>
2018-01-26 23:57:20 +0000

    81d64d6 fix(core): fix retrieving the binding name when an expression changes (#21814)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-7410941
+ * @license Angular v5.2.2-81d64d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.2-7410941
+ * @license Angular v5.2.2-81d64d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
