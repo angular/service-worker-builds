4781ec9b1fead711b0350a3131acab59fa199d3a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:47:12 +0000

    973607f ci: mark PRs with rejection as not green (#21922) PR Close #21922

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-b155ae1
+ * @license Angular v5.2.3-973607f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.3-b155ae1
+ * @license Angular v5.2.3-973607f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
