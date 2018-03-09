7c0c492c75fdbf78c8d323ab4d957fe50fd9d056
Kara Erickson <karakara@google.com>
2018-03-09 01:05:13 +0000

    489fec1 feat: update tslib to 1.9.0 (#22667)

--- bundles/service-worker-config.umd.js
+++ bundles/service-worker-config.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-2fee5cc
+ * @license Angular v6.0.0-beta.7-489fec1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -40,7 +40,7 @@ and limitations under the License.
 function __awaiter(thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
@@ -75,7 +75,7 @@ function __generator(thisArg, body) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-2fee5cc
+ * @license Angular v6.0.0-beta.7-489fec1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
