b73e296bf4eee8ef4cb8e94387bad3a70c69335c
Kara Erickson <karakara@google.com>
2018-03-14 17:45:50 +0000

    ad674da docs: testing - highlight dispatchEvent (#22726)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-86517f2
+ * @license Angular v5.2.8-ad674da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.8-86517f2
+ * @license Angular v5.2.8-ad674da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
