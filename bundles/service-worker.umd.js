c8453aaa33d1715f51a58108356da1eabbcef880
Igor Minar <igor@angular.io>
2018-03-21 17:18:23 +0000

    4f2c51f docs: switch over to the preview url for the rxjs upgrade guide in changelog

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-7cd8e8d
+ * @license Angular v6.0.0-rc.0-4f2c51f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-rc.0-7cd8e8d
+ * @license Angular v6.0.0-rc.0-4f2c51f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
