00feaedd9dcd11e1eedc2e7398fe3d126972ad31
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 17:29:17 +0000

    8d1e640 docs(aio): fix TS warning error  - filter expects a boolean function param (#22954)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-49f6d1d
+ * @license Angular v5.2.9-8d1e640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-49f6d1d
+ * @license Angular v5.2.9-8d1e640
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
