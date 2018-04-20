67a56fb0c553791fb0a2d9ce5a0e84e48f62342c
GitHub <noreply@github.com>
2018-04-20 13:19:23 +0000

    ab41920 fix(compiler): avoid a crash in ngc-wrapped.

---
+++ esm5/src/module.js
@@ -0,0 +1,147 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { isPlatformBrowser } from '@angular/common';
+import { APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
+import { filter, take } from 'rxjs/operators';
+import { NgswCommChannel } from './low_level';
+import { SwPush } from './push';
+import { SwUpdate } from './update';
+/**
+ * @abstract
+ */
+var /**
+ * @abstract
+ */
+RegistrationOptions = /** @class */ (function () {
+    function RegistrationOptions() {
+    }
+    return RegistrationOptions;
+}());
+/**
+ * @abstract
+ */
+export { RegistrationOptions };
+function RegistrationOptions_tsickle_Closure_declarations() {
+    /** @type {?} */
+    RegistrationOptions.prototype.scope;
+    /** @type {?} */
+    RegistrationOptions.prototype.enabled;
+}
+export var /** @type {?} */ SCRIPT = new InjectionToken('NGSW_REGISTER_SCRIPT');
+/**
+ * @param {?} injector
+ * @param {?} script
+ * @param {?} options
+ * @param {?} platformId
+ * @return {?}
+ */
+export function ngswAppInitializer(injector, script, options, platformId) {
+    var /** @type {?} */ initializer = function () {
+        var /** @type {?} */ app = injector.get(ApplicationRef);
+        if (!(isPlatformBrowser(platformId) && ('serviceWorker' in navigator) &&
+            options.enabled !== false)) {
+            return;
+        }
+        var /** @type {?} */ whenStable = app.isStable.pipe(filter(function (stable) { return !!stable; }), take(1)).toPromise();
+        // Wait for service worker controller changes, and fire an INITIALIZE action when a new SW
+        // becomes active. This allows the SW to initialize itself even if there is no application
+        // traffic.
+        navigator.serviceWorker.addEventListener('controllerchange', function () {
+            if (navigator.serviceWorker.controller !== null) {
+                navigator.serviceWorker.controller.postMessage({ action: 'INITIALIZE' });
+            }
+        });
+        // Don't return the Promise, as that will block the application until the SW is registered, and
+        // cause a crash if the SW registration fails.
+        whenStable.then(function () { return navigator.serviceWorker.register(script, { scope: options.scope }); });
+    };
+    return initializer;
+}
+/**
+ * @param {?} opts
+ * @param {?} platformId
+ * @return {?}
+ */
+export function ngswCommChannelFactory(opts, platformId) {
+    return new NgswCommChannel(opts.enabled !== false ? navigator.serviceWorker : undefined, platformId);
+}
+/**
+ * \@experimental
+ */
+var ServiceWorkerModule = /** @class */ (function () {
+    function ServiceWorkerModule() {
+    }
+    /**
+     * Register the given Angular Service Worker script.
+     *
+     * If `enabled` is set to `false` in the given options, the module will behave as if service
+     * workers are not supported by the browser, and the service worker will not be registered.
+     */
+    /**
+     * Register the given Angular Service Worker script.
+     *
+     * If `enabled` is set to `false` in the given options, the module will behave as if service
+     * workers are not supported by the browser, and the service worker will not be registered.
+     * @param {?} script
+     * @param {?=} opts
+     * @return {?}
+     */
+    ServiceWorkerModule.register = /**
+     * Register the given Angular Service Worker script.
+     *
+     * If `enabled` is set to `false` in the given options, the module will behave as if service
+     * workers are not supported by the browser, and the service worker will not be registered.
+     * @param {?} script
+     * @param {?=} opts
+     * @return {?}
+     */
+    function (script, opts) {
+        if (opts === void 0) { opts = {}; }
+        return {
+            ngModule: ServiceWorkerModule,
+            providers: [
+                { provide: SCRIPT, useValue: script },
+                { provide: RegistrationOptions, useValue: opts },
+                {
+                    provide: NgswCommChannel,
+                    useFactory: ngswCommChannelFactory,
+                    deps: [RegistrationOptions, PLATFORM_ID]
+                },
+                {
+                    provide: APP_INITIALIZER,
+                    useFactory: ngswAppInitializer,
+                    deps: [Injector, SCRIPT, RegistrationOptions, PLATFORM_ID],
+                    multi: true,
+                },
+            ],
+        };
+    };
+    ServiceWorkerModule.decorators = [
+        { type: NgModule, args: [{
+                    providers: [SwPush, SwUpdate],
+                },] },
+    ];
+    /** @nocollapse */
+    ServiceWorkerModule.ctorParameters = function () { return []; };
+    return ServiceWorkerModule;
+}());
+export { ServiceWorkerModule };
+function ServiceWorkerModule_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    ServiceWorkerModule.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    ServiceWorkerModule.ctorParameters;
+}
+//# sourceMappingURL=module.js.map
