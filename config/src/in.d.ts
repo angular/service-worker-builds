0f3148d0383641329999fb8c62e7fba7f7aac7a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:39:25 +0000

    1d378e2 fix(service-worker): deprecate `versionedFiles` in asset-group resources (#23584)

--- config/src/in.d.ts
+++ config/src/in.d.ts
@@ -37,6 +37,8 @@ export interface AssetGroup {
     updateMode?: 'prefetch' | 'lazy';
     resources: {
         files?: Glob[];
+        /** @deprecated As of v6 `versionedFiles` and `files` options have the same behavior. Use
+           `files` instead. */
         versionedFiles?: Glob[];
         urls?: Glob[];
     };
