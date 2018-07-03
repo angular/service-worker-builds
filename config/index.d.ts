900fba568f4e589a01adcd5c2c02e71c7276dfdd
Miško Hevery <misko@hevery.com>
2018-07-03 21:05:07 +0000

    0c3738a feat(ivy): support templateUrl for ngtsc (#24704)

--- config/index.d.ts
+++ config/index.d.ts
@@ -1,3 +1,4 @@
+/// <amd-module name="@angular/service-worker/config" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -5,5 +6,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="@angular/service-worker/config" />
 export * from './public_api';
--- config/public_api.d.ts
+++ config/public_api.d.ts
@@ -1,3 +1,4 @@
+/// <amd-module name="@angular/service-worker/config/public_api" />
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -5,7 +6,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <amd-module name="@angular/service-worker/config/public_api" />
 export { Filesystem } from './src/filesystem';
 export { Generator } from './src/generator';
 export { AssetGroup, Config, DataGroup, Duration, Glob } from './src/in';
