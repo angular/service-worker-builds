9b6b92c7cd248ff02a375f6dc528feb8fcf6113d
Ben Lesh <ben@benlesh.com>
2018-08-16 00:22:16 +0000

    1f11039 refactor(ivy): cleanup the public API for core/testing (#25492)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -458,8 +458,9 @@
                     // Lowercase all the directive names.
                     cacheDirectives.forEach(v => v[0] = v[0].toLowerCase());
                     // Find the max-age directive, if one exists.
-                    const cacheAge = cacheDirectives.filter(v => v[0] === 'max-age').map(v => v[1])[0];
-                    if (cacheAge.length === 0) {
+                    const maxAgeDirective = cacheDirectives.find(v => v[0] === 'max-age');
+                    const cacheAge = maxAgeDirective ? maxAgeDirective[1] : undefined;
+                    if (!cacheAge) {
                         // No usable TTL defined. Must assume that the response is stale.
                         return true;
                     }
