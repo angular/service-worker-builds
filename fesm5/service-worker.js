aaa9a25e7a941d951c8eb2406033cdbc8d988538
Igor Minar <igor@angular.io>
2018-09-06 23:03:23 +0000

    d6cd041 docs(service-worker): update http-server command (#25845) PR Close #25845

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.5+1.sha-34ec924
+ * @license Angular v7.0.0-beta.5+4.sha-d6cd041
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
