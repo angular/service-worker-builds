e314aa687e11ecbc29fccae02ede04d73e57e617
Kara Erickson <karakara@google.com>
2018-09-20 18:31:38 +0000

    549de1e fix(core): add missing `peerDependency ` to `@angular/compiler` (#26033)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+13.sha-48e73c1
+ * @license Angular v7.0.0-beta.6+14.sha-549de1e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __assign, __metadata } from 'tslib';
+import { __assign, __decorate, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
