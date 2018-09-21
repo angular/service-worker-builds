86e7280bd39423be2d782114b8d608560212eff0
Kara Erickson <karakara@google.com>
2018-09-21 18:03:37 +0000

    e42c1b0 ci: correct github robot size check configuration (#26057)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.6+27.sha-e7ade38
+ * @license Angular v7.0.0-beta.6+28.sha-e42c1b0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
