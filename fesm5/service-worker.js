5683a4b3504b2d08bcea6a9767b43b95678dc8fe
Kara Erickson <karakara@google.com>
2018-09-24 17:41:28 +0000

    77d2cbd docs: fix Sajee info in contributors (#26063)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.8+36.sha-b907e5a
+ * @license Angular v6.1.8+35.sha-77d2cbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __assign, __metadata } from 'tslib';
+import { __assign, __decorate, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
