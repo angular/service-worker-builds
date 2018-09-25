f93c5513a1ea765449e9552cadfe1a56d36a3f9e
Kara Erickson <karakara@google.com>
2018-09-25 18:43:24 +0000

    0c34471 feat(ivy): expose a series of helpful application inspection tools (#25919)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -158,6 +158,14 @@
             this.isCritical = true;
         }
     }
+    function errorToString(error) {
+        if (error instanceof Error) {
+            return `${error.message}\n${error.stack}`;
+        }
+        else {
+            return `${error}`;
+        }
+    }
 
     /**
      * @license
@@ -580,23 +588,32 @@
                     if (!res.ok) {
                         throw new Error(`Response not Ok (fetchAndCacheOnce): request for ${req.url} returned response ${res.status} ${res.statusText}`);
                     }
-                    // This response is safe to cache (as long as it's cloned). Wait until the cache operation
-                    // is complete.
-                    const cache = yield this.scope.caches.open(`${this.prefix}:${this.config.name}:cache`);
-                    yield cache.put(req, res.clone());
-                    // If the request is not hashed, update its metadata, especially the timestamp. This is needed
-                    // for future determination of whether this cached response is stale or not.
-                    if (!this.hashes.has(req.url)) {
-                        // Metadata is tracked for requests that are unhashed.
-                        const meta = { ts: this.adapter.time, used };
-                        const metaTable = yield this.metadata;
-                        yield metaTable.write(req.url, meta);
+                    try {
+                        // This response is safe to cache (as long as it's cloned). Wait until the cache operation
+                        // is complete.
+                        const cache = yield this.scope.caches.open(`${this.prefix}:${this.config.name}:cache`);
+                        yield cache.put(req, res.clone());
+                        // If the request is not hashed, update its metadata, especially the timestamp. This is
+                        // needed for future determination of whether this cached response is stale or not.
+                        if (!this.hashes.has(req.url)) {
+                            // Metadata is tracked for requests that are unhashed.
+                            const meta = { ts: this.adapter.time, used };
+                            const metaTable = yield this.metadata;
+                            yield metaTable.write(req.url, meta);
+                        }
+                        return res;
                     }
-                    return res;
+                    catch (err) {
+                        // Among other cases, this can happen when the user clears all data through the DevTools,
+                        // but the SW is still running and serving another tab. In that case, trying to write to the
+                        // caches throws an `Entry was not found` error.
+                        // If this happens the SW can no longer work correctly. This situation is unrecoverable.
+                        throw new SwCriticalError(`Failed to update the caches for request to '${req.url}' (fetchAndCacheOnce): ${errorToString(err)}`);
+                    }
                 }
                 finally {
                     // Finally, it can be removed from `inFlightRequests`. This might result in a double-remove
-                    // if some other  chain was already making this request too, but that won't hurt anything.
+                    // if some other chain was already making this request too, but that won't hurt anything.
                     this.inFlightRequests.delete(req.url);
                 }
             });
@@ -2356,7 +2373,7 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                     // network, but caches continue to be valid for previous versions. This is
                     // unfortunate but unavoidable.
                     this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;
-                    this.stateMessage = `Degraded due to failed initialization: ${errorToString(err)}`;
+                    this.stateMessage = `Degraded due to: ${errorToString(err)}`;
                     // Cancel the binding for these clients.
                     Array.from(this.clientVersionMap.keys())
                         .forEach(clientId => this.clientVersionMap.delete(clientId));
@@ -2370,7 +2387,14 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                     // Push the affected clients onto the latest version.
                     affectedClients.forEach(clientId => this.clientVersionMap.set(clientId, this.latestHash));
                 }
-                yield this.sync();
+                try {
+                    yield this.sync();
+                }
+                catch (err2) {
+                    // We are already in a bad state. No need to make things worse.
+                    // Just log the error and move on.
+                    this.debugger.log(err2, `Driver.versionFailed(${err.message || err})`);
+                }
             });
         }
         setupUpdate(manifest, hash) {
@@ -2620,14 +2644,6 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                     });
                 }
             });
-        }
-    }
-    function errorToString(error) {
-        if (error instanceof Error) {
-            return `${error.message}\n${error.stack}`;
-        }
-        else {
-            return `${error}`;
         }
     }
 
