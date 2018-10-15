35e47ad338bec0053432bc81ecef38155b6a9b95
George Kalpakas <kalpakas.g@gmail.com>
2018-10-15 22:57:53 +0000

    6ad85ae build(docs-infra): upgrade cli command docs sources to 301dfa6e5

---
+++ config.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license Angular v7.0.0-rc.1+45.sha-6ad85ae
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+  export * from './config/config';
+  
---
+++ config/config.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+/// <amd-module name="@angular/service-worker/config/config" />
+export * from './index';
