d708427c11b111bfbf0c38190b8b229dca4b46b5
Miško Hevery <misko@hevery.com>
2018-10-16 22:14:34 +0000

    2326b9c fix(service-worker): clean up caches from old SW versions (#26319)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-rc.1+54.sha-41de0e0
+ * @license Angular v7.0.0-rc.1+55.sha-2326b9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __metadata, __assign } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
