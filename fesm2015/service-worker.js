a0c358cf6ba4163ae85b2826a431222a14d64d17
Matias Niemelä <matias@yearofmoo.com>
2018-10-24 23:04:36 +0000

    f6c2db8 fix(ivy): ensure styling pipes are allocated before used in bindings (#26593)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.1.0-beta.0
+ * @license Angular v7.1.0-beta.0+1.sha-f6c2db8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector, defineInjectable, inject } from '@angular/core';
+import { Injectable, defineInjectable, inject, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
