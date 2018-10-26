7fdf0790af39cd4b0adaecca67aac24889e98602
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 22:46:04 +0000

    d52d82d fix(ivy): injecting optional TemplateRef on element should not throw (#26664)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.1.0-beta.0+41.sha-da4a28e
+ * @license Angular v7.1.0-beta.0+43.sha-d52d82d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector, defineInjectable, inject } from '@angular/core';
+import { Injectable, defineInjectable, inject, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
