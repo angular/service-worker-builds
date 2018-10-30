b3ba68a04a93e4186298a5be9bf7582942bed37c
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:55:15 +0000

    634e9e9 docs: Removing duplicate lines (#26754)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
+ * @license Angular v7.1.0-beta.0+72.sha-634e9e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
