def153881b0d6fb87cccfdc074fc9138a34d8e0d
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:56:27 +0000

    634e9e9 docs: Removing duplicate lines (#26754)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
+ * @license Angular v7.1.0-beta.0+72.sha-634e9e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, defineInjectable, inject, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector } from '@angular/core';
+import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector, defineInjectable, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
