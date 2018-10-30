cb9284807e3dacb1246bef9e4dab3a099c4e60a2
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:55:37 +0000

    87a7f2d docs: fix typo (#26773) PR Close #26773

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+69.sha-8fc4ae5
+ * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7,7 +7,7 @@
 import { __assign } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, ɵdefineNgModule, defineInjector, defineInjectable, inject } from '@angular/core';
+import { defineInjectable, inject, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
