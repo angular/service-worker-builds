430e87dbe01af7feb88b5563988482ea05e29a8b
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:26:10 +0000

    1810cdf fix(ivy): compiler should generate restoreView() for local refs in listeners (#27034)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+21.sha-e5c9f7a
+ * @license Angular v7.1.0-beta.2+24.sha-1810cdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __assign, __metadata } from 'tslib';
+import { __assign, __decorate, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
