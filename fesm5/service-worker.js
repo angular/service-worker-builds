d1baaa1d75bc8040e80fedb7ed438b1ce5c9688b
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:23:16 +0000

    1b97971 docs: fixed typo on Github issue template (#27070) PR Close #27070

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+31.sha-efa443b
+ * @license Angular v7.1.0-beta.2+35.sha-1b97971
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
