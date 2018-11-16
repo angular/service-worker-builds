718208fd7f45acd85280261d2a696230a7c4c08f
Misko Hevery <misko@hevery.com>
2018-11-16 20:45:42 +0000

    a02eda5 docs: fix code examples style (#26573)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.4+3.sha-41b5ea1
+ * @license Angular v7.0.4+4.sha-a02eda5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __assign, __metadata } from 'tslib';
+import { __assign, __decorate, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
