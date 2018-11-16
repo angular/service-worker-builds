49cd8109829a4b755daa5237b77578f47c73333c
Misko Hevery <misko@hevery.com>
2018-11-16 20:46:08 +0000

    4f965ad test(docs-infra): remove reflect-metadata polyfill from aio tests (#27130)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+6.sha-848f414
+ * @license Angular v7.1.0-rc.0+7.sha-4f965ad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
