b5b8930297ff2e0aa20844daec285a3364dec666
Filipe Silva <filipematossilva@gmail.com>
2018-11-22 15:32:38 +0000

    d3ddd31 ci: test ts-api-guardian on windows

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+17.sha-83b1c63.with-local-changes
+ * @license Angular v7.1.0-rc.0+17.sha-d3ddd31.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
