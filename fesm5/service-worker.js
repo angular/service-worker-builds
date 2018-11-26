db0149838fc01e781efc248b9a555b1dc5b827d8
Jason Aden <jasonaden@google.com>
2018-11-26 20:39:58 +0000

    b37a2fb build(docs-infra): upgrade `npm-run-all` to latest version for security (#27274)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.4+12.sha-106b85d
+ * @license Angular v7.0.4+16.sha-b37a2fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
