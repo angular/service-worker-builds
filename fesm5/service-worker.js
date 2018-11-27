f1a1c2a0be58762659735ee763e96b04a3ccbb3c
Jason Aden <jasonaden@google.com>
2018-11-27 18:45:56 +0000

    d767e0b fix(ivy): consider providers for view/content queries (#27156)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+11.sha-f45aedc
+ * @license Angular v7.1.0+13.sha-d767e0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
