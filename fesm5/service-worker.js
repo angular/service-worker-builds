595b84f5b2b8094a50671959fbc870a478678fa3
Igor Minar <igor@angular.io>
2018-11-30 06:11:26 +0000

    4354fce build: Replace iteration over depsets with an explicit .to_list() call. (#27336)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+55.sha-816ec0b
+ * @license Angular v7.1.0+54.sha-4354fce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
