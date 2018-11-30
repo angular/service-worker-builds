6b39a210bdf2526691592f6484a0c0511606675a
Igor Minar <igor@angular.io>
2018-11-30 21:27:45 +0000

    1b84b11 Revert "fix(forms): apply unicode flag to pattern attribute when supported (#20819)" (#27375)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+69.sha-412e47d
+ * @license Angular v7.1.0+75.sha-1b84b11
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
