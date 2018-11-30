34c4574f296f51b0b44f8b2c48ef3b245e03b957
Igor Minar <igor@angular.io>
2018-11-30 22:07:50 +0000

    eb17502 fix(bazel): ng_package should correctly map to source maps in secondary entry-points (#27313)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+81.sha-c8a8dcf
+ * @license Angular v7.1.0+82.sha-eb17502
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7,7 +7,7 @@
 import { __assign } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, ɵdefineNgModule, defineInjector, ɵsetClassMetadata, NgModule, defineInjectable, inject } from '@angular/core';
+import { Injectable, defineInjectable, inject, ɵsetClassMetadata, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, ɵdefineNgModule, defineInjector, NgModule } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
