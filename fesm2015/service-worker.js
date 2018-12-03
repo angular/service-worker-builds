4f3a2bf4922d53ebd80b0963b2da12b1db75e2c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:08:09 +0000

    7ec05b4 fix(ivy): run initialializers in TestBed on init (#27355)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.1.0+110.sha-2a39425
+ * @license Angular v7.1.0+112.sha-7ec05b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector, ɵsetClassMetadata, defineInjectable, inject } from '@angular/core';
+import { Injectable, defineInjectable, inject, ɵsetClassMetadata, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
