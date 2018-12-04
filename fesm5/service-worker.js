c7d609e64d8952aa189887d12e8a69af6682f30b
Igor Minar <igor@angular.io>
2018-12-04 22:33:56 +0000

    8087b6b fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.1+18.sha-0378792
+ * @license Angular v7.1.1+23.sha-8087b6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
