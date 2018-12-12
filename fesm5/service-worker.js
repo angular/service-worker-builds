91617c37a9ba6d7268964dca2037706c715184fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 00:53:23 +0000

    93078e3 fix(bazel): Read latest versions from latest-versions.ts & use semver check (#27591)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+1.sha-29ab729
+ * @license Angular v7.1.3+2.sha-93078e3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
