251744f0673ff8f8b0e819bb35cfebb408700eef
Miško Hevery <misko@hevery.com>
2018-12-14 23:56:50 +0000

    cd1e206 fix(animations): do not truncate decimals for delay (#24455)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+25.sha-c220328
+ * @license Angular v7.1.3+28.sha-cd1e206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
