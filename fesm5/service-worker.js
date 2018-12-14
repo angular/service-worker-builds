42d27d03e2ede2c9b4ef03b4a96385f7383989ba
Miško Hevery <misko@hevery.com>
2018-12-14 18:50:31 +0000

    f5e7208 build: re-enable saucelabs non-verbose logging (#27657)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.3+15.sha-52fd4a2
+ * @license Angular v7.1.3+20.sha-f5e7208
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __assign, __metadata } from 'tslib';
+import { __assign, __decorate, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
