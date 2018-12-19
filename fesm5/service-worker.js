65b1d177e017a04236383cd11192af706734b74d
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:44:03 +0000

    8f8572f fix(ivy): @Host should behave as in View Engine (#27646)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-e8f7241
+ * @license Angular v7.2.0-rc.0+3.sha-8f8572f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
