442ffa6f10cfa650f1bff1ba86b5b9fac20e235c
Ben Lesh <ben@benlesh.com>
2019-01-04 00:36:46 +0000

    51a0bd2 test(ivy): correct attribute marker in broken test (#27919)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+46.sha-1e6c9be
+ * @license Angular v7.2.0-rc.0+52.sha-51a0bd2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7,7 +7,7 @@
 import { __assign } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, defineInjectable, inject, ɵsetClassMetadata, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, ɵdefineNgModule, defineInjector, NgModule } from '@angular/core';
+import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, ɵdefineNgModule, defineInjector, ɵsetClassMetadata, NgModule, defineInjectable, inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
