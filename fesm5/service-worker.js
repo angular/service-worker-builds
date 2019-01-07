898ef9f228a0489fc262cc23ab34dae655ec441b
Kara Erickson <karakara@google.com>
2019-01-07 23:10:10 +0000

    e775313 fix(ivy): ngOnDestroy hooks should be called on providers (#27955)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+7.sha-4588053
+ * @license Angular v7.2.0+9.sha-e775313
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __decorate, __metadata } from 'tslib';
+import { __decorate, __assign, __metadata } from 'tslib';
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
 import { Injectable, APP_INITIALIZER, ApplicationRef, InjectionToken, Injector, NgModule, PLATFORM_ID } from '@angular/core';
