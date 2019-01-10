2413528a26d9241ef6d2f3dd3e263f8abbc4cd6c
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:31:07 +0000

    c5ab3e8 fix(ivy): proper resolution of Enums in Component decorator (#27971)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.2.0+88.sha-142553a
+ * @license Angular v7.2.0+89.sha-c5ab3e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector, ɵsetClassMetadata, defineInjectable, inject } from '@angular/core';
+import { Injectable, defineInjectable, inject, ɵsetClassMetadata, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
