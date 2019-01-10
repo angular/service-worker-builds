e42a5f3c1eb23d5ac9540db47290a3b8d5971a66
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:23:46 +0000

    6003145 fix(ivy): properly rewrite imports in generated factory shims (#27998)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,12 +1,12 @@
 /**
- * @license Angular v7.2.0+85.sha-5a0deb8
+ * @license Angular v7.2.0+87.sha-6003145
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { concat, defer, fromEvent, of, throwError, NEVER, Subject, merge } from 'rxjs';
 import { filter, map, publish, switchMap, take, tap } from 'rxjs/operators';
-import { Injectable, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector, ɵsetClassMetadata, defineInjectable, inject } from '@angular/core';
+import { Injectable, defineInjectable, inject, ɵsetClassMetadata, InjectionToken, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule, ɵdefineNgModule, defineInjector } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 /**
