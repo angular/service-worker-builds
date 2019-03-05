38f28e941b2191b312102023a52ed9db8f127164
Andrew Kushnir <akushnir@google.com>
2019-03-05 20:22:38 +0000

    3063547 docs(router): align examples in interfaces docs (#29093)

--- fesm2015/config.js
+++ fesm2015/config.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+68.sha-866d500.with-local-changes
+ * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -171,6 +171,7 @@ class Generator {
             const assetGroups = yield this.processAssetGroups(config, unorderedHashTable);
             return {
                 configVersion: 1,
+                timestamp: Date.now(),
                 appData: config.appData,
                 index: joinUrls(this.baseHref, config.index), assetGroups,
                 dataGroups: this.processDataGroups(config),
