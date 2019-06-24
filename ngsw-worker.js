a0581b5f42a8ae59d4979efeb437b0cd28c93d7f
Kara Erickson <karakara@google.com>
2019-06-24 22:20:22 +0000

    980bcaf17 fix(bazel): remove unsupported Css pre-processors from ng new (#31234)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -47,9 +47,7 @@
          * Extract the pathname of a URL.
          */
         parseUrl(url, relativeTo) {
-            // Workaround a Safari bug, see
-            // https://github.com/angular/angular/issues/31061#issuecomment-503637978
-            const parsed = !relativeTo ? new URL(url) : new URL(url, relativeTo);
+            const parsed = new URL(url, relativeTo);
             return { origin: parsed.origin, path: parsed.pathname, search: parsed.search };
         }
         /**
