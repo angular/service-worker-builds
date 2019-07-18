10739f584ffb75194e9c8f09f8163dfc6215dccc
Miško Hevery <misko@hevery.com>
2019-07-18 21:44:25 +0000

    60f58bf05 refactor: ensure zone.js can be built with typescript strict flag (#30993)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+8.sha-78e7fdd.with-local-changes
+ * @license Angular v8.2.0-next.2+22.sha-60f58bf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -56,7 +56,7 @@ class NgswCommChannel {
              * @param {?} c
              * @return {?}
              */
-            c => !!c)));
+            (c) => !!c)));
             this.registration = (/** @type {?} */ ((this.worker.pipe(switchMap((/**
              * @return {?}
              */
