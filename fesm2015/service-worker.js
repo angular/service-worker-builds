254b5d5e8055c75f187b6a7ea88bae2287f31c90
Miško Hevery <misko@hevery.com>
2019-07-25 20:28:55 +0000

    85d051f8d perf(ivy): introduce new benchmark for view create, destroy, traverse (#31797)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+78.sha-3d7303e.with-local-changes
+ * @license Angular v8.2.0-next.2+86.sha-85d051f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -373,7 +373,7 @@ SwUpdate.ctorParameters = () => [
  * for example via a function call:
  *
  * {\@example service-worker/registration-options/module.ts region="registration-options"
- *     header="app.module.ts" linenums="false"}
+ *     header="app.module.ts"}
  *
  * \@publicApi
  * @abstract
