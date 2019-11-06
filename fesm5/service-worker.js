721f262a8a0fae23ac8aeff6d06003711b470afd
atscott <atscott01@gmail.com>
2019-11-06 20:24:10 +0000

    2ed04ffcdc build: update to latest material commit to pick up https://github.com/angular/components/pull/17620 (#33607)

--- fesm5/service-worker.js
+++ fesm5/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
+ * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -436,5 +436,5 @@ var ServiceWorkerModule = /** @class */ (function () {
  * found in the LICENSE file at https://angular.io/license
  */
 
-export { ServiceWorkerModule, SwRegistrationOptions, SwPush, SwUpdate };
+export { ServiceWorkerModule, SwPush, SwRegistrationOptions, SwUpdate };
 //# sourceMappingURL=service-worker.js.map
