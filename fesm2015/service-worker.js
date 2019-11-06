cee59dbbca62a8aabab93bae2f286e253cdf9f63
atscott <atscott01@gmail.com>
2019-11-06 20:54:28 +0000

    41560b47c4 refactor(ivy): move all styling configurations into `TNodeFlags` (#33540)

--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
+ * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -806,5 +806,5 @@ ServiceWorkerModule.decorators = [
  * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 
-export { ServiceWorkerModule, SwPush, SwRegistrationOptions, SwUpdate };
+export { ServiceWorkerModule, SwRegistrationOptions, SwPush, SwUpdate };
 //# sourceMappingURL=service-worker.js.map
