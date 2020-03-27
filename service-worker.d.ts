d1c759c289579ed20d574eed11dd320e66b704af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:23:20 +0000

    bfa7b1a494 docs: correct a misleading sentence (#36155) (#36158)

--- service-worker.d.ts
+++ service-worker.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -188,8 +188,12 @@ export declare abstract class SwRegistrationOptions {
      * registered (e.g. there might be a long-running timeout or polling interval, preventing the app
      * to stabilize). The available option are:
      *
-     * - `registerWhenStable`: Register as soon as the application stabilizes (no pending
-     *      micro-/macro-tasks).
+     * - `registerWhenStable:<timeout>`: Register as soon as the application stabilizes (no pending
+     *     micro-/macro-tasks) but no later than `<timeout>` milliseconds. If the app hasn't
+     *     stabilized after `<timeout>` milliseconds (for example, due to a recurrent asynchronous
+     *     task), the ServiceWorker will be registered anyway.
+     *     If `<timeout>` is omitted, the ServiceWorker will only be registered once the app
+     *     stabilizes.
      * - `registerImmediately`: Register immediately.
      * - `registerWithDelay:<timeout>`: Register with a delay of `<timeout>` milliseconds. For
      *     example, use `registerWithDelay:5000` to register the ServiceWorker after 5 seconds. If
