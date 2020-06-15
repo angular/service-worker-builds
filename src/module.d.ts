fce658431bef1e001fc5492c74339a7ccd1b2dc5
Misko Hevery <misko@hevery.com>
2020-06-15 17:16:36 +0000

    a7faa6bb65 docs(service-worker): minor fixes/improvements in the SW Communication guide (#37555)

--- src/module.d.ts
+++ src/module.d.ts
@@ -55,7 +55,7 @@ export declare abstract class SwRegistrationOptions {
      *     The function will be used at runtime to obtain and subscribe to the `Observable` and the
      *     ServiceWorker will be registered as soon as the first value is emitted.
      *
-     * Default: 'registerWhenStable'
+     * Default: 'registerWhenStable:30000'
      */
     registrationStrategy?: string | (() => Observable<unknown>);
 }
