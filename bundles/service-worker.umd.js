cf0b271b683349723ff8c69666e3469a7b24636f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:39:13 +0000

    21bc16d4d8 fix(service-worker): handle error with console.error (#40236)

--- bundles/service-worker.umd.js
+++ bundles/service-worker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+160.sha-fc64fa8
+ * @license Angular v11.1.0-next.4+164.sha-21bc16d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -683,10 +683,8 @@
             // given that some registration strategies wait for the app to stabilize).
             // Catch and log the error if SW registration fails to avoid uncaught rejection warning.
             var ngZone = injector.get(core.NgZone);
-            ngZone.runOutsideAngular(function () { return readyToRegister$.pipe(operators.take(1)).subscribe(function () { return navigator.serviceWorker.register(script, { scope: options.scope }).catch(function (err) {
-                var errorHandler = injector.get(core.ErrorHandler);
-                errorHandler.handleError(err);
-            }); }); });
+            ngZone.runOutsideAngular(function () { return readyToRegister$.pipe(operators.take(1)).subscribe(function () { return navigator.serviceWorker.register(script, { scope: options.scope })
+                .catch(function (err) { return console.error('Service worker registration failed with:', err); }); }); });
         };
         return initializer;
     }
--- fesm2015/service-worker.js
+++ fesm2015/service-worker.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v11.1.0-next.4+160.sha-fc64fa8
+ * @license Angular v11.1.0-next.4+164.sha-21bc16d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
 
 import { isPlatformBrowser } from '@angular/common';
-import { Injectable, InjectionToken, NgZone, ErrorHandler, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule } from '@angular/core';
+import { Injectable, InjectionToken, NgZone, ApplicationRef, PLATFORM_ID, APP_INITIALIZER, Injector, NgModule } from '@angular/core';
 import { defer, throwError, fromEvent, of, concat, Subject, NEVER, merge } from 'rxjs';
 import { map, filter, switchMap, publish, take, tap, delay } from 'rxjs/operators';
 
@@ -368,10 +368,8 @@ function ngswAppInitializer(injector, script, options, platformId) {
         // given that some registration strategies wait for the app to stabilize).
         // Catch and log the error if SW registration fails to avoid uncaught rejection warning.
         const ngZone = injector.get(NgZone);
-        ngZone.runOutsideAngular(() => readyToRegister$.pipe(take(1)).subscribe(() => navigator.serviceWorker.register(script, { scope: options.scope }).catch(err => {
-            const errorHandler = injector.get(ErrorHandler);
-            errorHandler.handleError(err);
-        })));
+        ngZone.runOutsideAngular(() => readyToRegister$.pipe(take(1)).subscribe(() => navigator.serviceWorker.register(script, { scope: options.scope })
+            .catch(err => console.error('Service worker registration failed with:', err))));
     };
     return initializer;
 }
