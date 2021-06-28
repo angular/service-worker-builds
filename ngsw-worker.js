9e2df9020644db633987d0efdf98b7aa689aa6cd
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 19:19:54 +0000

    c93df7dc6d docs: correct literal string example (#42623)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.0+13.sha-b54e8ae';
+    const SW_VERSION = '12.2.0-next.0+12.sha-c93df7d';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
