06002aad3013b22cfea01b285e05fa9ee3e11371
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:52:23 +0000

    02742552f3 refactor(docs-infra): make angular.io and docs examples compatible with RxJS v7 (#42660)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.0+15.sha-2c490d5';
+    const SW_VERSION = '12.2.0-next.0+17.sha-0274255';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
