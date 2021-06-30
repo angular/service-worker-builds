1493fc2c30fbf3587ca3490ca1bb8e0ca332501c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:04:20 +0000

    aab41ca42f build: update API goldens to reflect new tool (#42688)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.0+18.sha-d19ddd1';
+    const SW_VERSION = '12.1.0+22.sha-aab41ca';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
