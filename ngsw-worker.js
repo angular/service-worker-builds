459982a268ebabe4cb5ceb21c05e172f457e123a
atscott <atscott01@gmail.com>
2021-07-01 22:44:02 +0000

    ffeea63f43 fix(language-service): Do not override TS LS methods not supported by VE NgLS (#42727)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+4.sha-74350a5';
+    const SW_VERSION = '12.2.0-next.1+5.sha-ffeea63';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
