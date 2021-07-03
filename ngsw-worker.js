71c452a3f7a051f1cc5a4ed8c11d0d8f51ab10e1
atscott <atscott01@gmail.com>
2021-07-03 00:35:23 +0000

    c9b47f2182 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+15.sha-4c78984';
+    const SW_VERSION = '12.2.0-next.1+16.sha-c9b47f2';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
