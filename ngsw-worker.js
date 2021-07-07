a8c799f2a37e65528b5ca676f87dacd75096fef8
atscott <atscott01@gmail.com>
2021-07-07 22:40:10 +0000

    0a17e98ae2 fix(compiler-cli): inline type checking instructions no longer prevent incremental reuse (#42759)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+25.sha-fe602c9';
+    const SW_VERSION = '12.1.1+26.sha-0a17e98';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
