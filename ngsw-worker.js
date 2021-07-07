540e4f167172437b68c0f7244fbe8d4e3c62c822
Andrew Scott <atscott@google.com>
2021-07-07 21:01:31 +0000

    fe602c9c4e fix(docs-infra): ensure the sidenav backdrop covers the floating ToC (#42787)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+18.sha-eca4801';
+    const SW_VERSION = '12.1.1+25.sha-fe602c9';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
