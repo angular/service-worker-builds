cdf809f57f74dc48542495ed196fb20ef653db29
Andrew Scott <atscott@google.com>
2021-07-07 20:56:23 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1737,7 +1737,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+18.sha-e064f17';
+    const SW_VERSION = '12.2.0-next.1+19.sha-24ac378';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
