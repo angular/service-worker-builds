d16bc6c9311afb3588a001ac973ee39edccae13d
atscott <atscott01@gmail.com>
2021-07-08 21:27:54 +0000

    c30502ee1b ci: use issue forms for issue creation (#42099)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+40.sha-e26bfc1';
+    const SW_VERSION = '12.2.0-next.1+42.sha-c30502e';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
