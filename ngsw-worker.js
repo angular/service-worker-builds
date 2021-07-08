223e8cd6bf30133e895ff3c9a68523465a42868e
atscott <atscott01@gmail.com>
2021-07-08 22:12:11 +0000

    89084ffcf9 docs(core): initializeApp method should return function (#42743)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+43.sha-e7832a6';
+    const SW_VERSION = '12.2.0-next.1+44.sha-89084ff';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
