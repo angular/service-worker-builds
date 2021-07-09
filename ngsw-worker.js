6b5cb8cba65fb4a9aa5485ab27120fd6f2729574
Andrew Kushnir <akushnir@google.com>
2021-07-09 19:37:03 +0000

    9d58ebfcee test(bazel): update `example_package.golden` (#42804)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+50.sha-3791ae0';
+    const SW_VERSION = '12.2.0-next.1+53.sha-9d58ebf';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
