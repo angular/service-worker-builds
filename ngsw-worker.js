aacfc4e596d394647b6107c3b0c63b86f3b84e64
Andrew Kushnir <akushnir@google.com>
2021-07-09 17:36:11 +0000

    5356796250 fix(docs-infra): fix margin of social icon on small screens (#42790)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+47.sha-9ec2419';
+    const SW_VERSION = '12.2.0-next.1+49.sha-5356796';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
