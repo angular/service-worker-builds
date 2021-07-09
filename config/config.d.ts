cf5554d4b1216f1c59640ce495dc610166f3a9bf
Andrew Kushnir <akushnir@google.com>
2021-07-09 19:35:43 +0000

    9d58ebfcee test(bazel): update `example_package.golden` (#42804)

--- config/config.d.ts
+++ config/config.d.ts
@@ -1,9 +1,8 @@
 /**
- * @license Angular v12.2.0-next.1+50.sha-3791ae0
+ * @license Angular v12.2.0-next.1+53.sha-9d58ebf
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
-
 
 /**
  * Configuration for a particular group of assets.
