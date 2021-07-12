2f6a25da3524068ce17aa27ae36cc219cfe7a0bf
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:10:22 +0000

    2ed5a2db79 docs(core): fix `ViewChildren` code examples to avoid TS error (#42816)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+46.sha-1c31828';
+    const SW_VERSION = '12.1.1+55.sha-2ed5a2d';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
