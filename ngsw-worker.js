4ce83b7737c4441b5d7fbdbf3cb6a12840480019
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:45:59 +0000

    88b15d572f docs: clarify voting phase for feature requests (#42663)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+98.sha-c1c1cda';
+    const SW_VERSION = '12.2.0-next.1+99.sha-88b15d5';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
