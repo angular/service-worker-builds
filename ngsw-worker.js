8354909f00c6ae3350239c9d3cdb5869098877ea
Andrew Kushnir <akushnir@google.com>
2021-07-12 16:58:49 +0000

    ef15b979cf docs(core): edit and add docs for ng-template (#42704)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+58.sha-762e057';
+    const SW_VERSION = '12.2.0-next.1+59.sha-ef15b97';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
