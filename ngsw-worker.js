95ecf250a36c439b4c199584d81965df35e10983
Andrew Kushnir <akushnir@google.com>
2021-07-13 21:34:57 +0000

    9ebd41e39c fix(core): allow proper type inference when `ngFor` is used with a `trackBy` function (#42692)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+93.sha-d654c79';
+    const SW_VERSION = '12.1.1+95.sha-9ebd41e';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
