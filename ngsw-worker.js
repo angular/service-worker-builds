ef57f9eecbcf6d4ebfbc339fcc7198d5fef1b8e2
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:34:21 +0000

    e6f01d777d revert: docs: remove references to Trusted Types due to incompatibility with Webpack 5 (#42796)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+97.sha-2a61728';
+    const SW_VERSION = '12.1.2+3.sha-e6f01d7';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
