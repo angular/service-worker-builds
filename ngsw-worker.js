f7d5c180179d692072acc802ff32d8bbdedba588
GitHub <noreply@github.com>
2021-07-14 18:11:56 +0000

    2a61728137 release: cut the v12.1.2 release (#42856)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+96.sha-6301354';
+    const SW_VERSION = '12.1.1+97.sha-2a61728';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
