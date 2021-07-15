3da94523b6ff2412543d144ce9b7638742a955d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:46:26 +0000

    f5baa55b81 docs: remove reference to an obsolete design doc (#42842)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+11.sha-4e97514';
+    const SW_VERSION = '12.2.0-next.2+12.sha-f5baa55';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
