c7a87b1f79ac699fb0cc7c695e3e73696cc228e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:57:16 +0000

    642362a9c7 build: add configuration for caretaker handoff (#42859)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+12.sha-f5baa55';
+    const SW_VERSION = '12.2.0-next.2+15.sha-642362a';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
