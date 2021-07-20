4a6ef0142c60190bebee025881561fcdaa393049
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:32:29 +0000

    0ce8f6e62d refactor(animations): remove unused animation trigger imports (#42763)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+24.sha-3b2f607';
+    const SW_VERSION = '12.2.0-next.2+28.sha-0ce8f6e';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
