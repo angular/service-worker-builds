5e8013305ec0a9d45b0a88d1b063b630ebec472d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 18:52:55 +0000

    445fee4174 feat(dev-infra): update package version verification to allow for experimental version (#42898)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+42.sha-282e86a';
+    const SW_VERSION = '12.2.0-next.2+41.sha-445fee4';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
