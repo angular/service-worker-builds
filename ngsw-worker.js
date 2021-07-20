469b786d3719d13df7449cb9e7e3f9ea85f48de5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:54:15 +0000

    7eddd12bf0 feat(dev-infra): add option to setup global types in API golden test (#42876)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+34.sha-30165f9';
+    const SW_VERSION = '12.2.0-next.2+36.sha-7eddd12';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
