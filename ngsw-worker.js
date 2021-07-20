813341247408d59c4abb6eb459bab9408d7e5dcb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:53:09 +0000

    4286b08b5d ci: exempt bazel scope from merge tool target label checks (#42869)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+32.sha-96d31d8';
+    const SW_VERSION = '12.2.0-next.2+33.sha-4286b08';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
