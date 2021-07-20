de5603d22f972122c77bd0e3363f7f8e91b4ae6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:55:35 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+35.sha-7271ad1';
+    const SW_VERSION = '12.2.0-next.2+38.sha-722eb5d';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
