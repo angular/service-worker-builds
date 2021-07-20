28cba5be200bbce0a339ac79d21b7c99adf310b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:57:40 +0000

    52dc65dd1b build(docs-infra): improve @usageNotes message (#42888)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+39.sha-307dac6';
+    const SW_VERSION = '12.2.0-next.2+40.sha-52dc65d';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
