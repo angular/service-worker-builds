0c2d0084da8865e35d261534c30affb428064374
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:54:45 +0000

    992dc93ea3 refactor(dev-infra): remove usages and dependency on shelljs (#42911)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.3+3.sha-4d86ea6.with-local-changes';
+    const SW_VERSION = '12.2.0-next.3+2.sha-992dc93.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
