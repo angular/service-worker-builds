6227fa8aa0a3ed9db18a3ec84125fc2e62040547
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:04:25 +0000

    f589b01672 perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.2+34.sha-b0854e2';
+    const SW_VERSION = '12.1.3+5.sha-f589b01.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
