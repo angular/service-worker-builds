3f2c4423519702d00d4c61d3f04572243f80f21e
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:46:19 +0000

    7e9673d14a ci: fix typo in docs-bug issue template (#42908)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.3+6.sha-5fb23ec.with-local-changes';
+    const SW_VERSION = '12.2.0-next.3+9.sha-7e9673d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
