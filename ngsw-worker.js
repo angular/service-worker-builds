8170b60fcceafdfb1411e92adb5234e4c9f3e142
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 18:23:50 +0000

    e462743969 refactor(dev-infra): move semver utils into the utils directory (#42917)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.3+10.sha-1d9d026.with-local-changes';
+    const SW_VERSION = '12.2.0-next.3+13.sha-e462743.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
