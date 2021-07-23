0be36f831d142b938f9b224a29d00d3b4bc01ee1
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:31:16 +0000

    8fc08c5ab1 ci: update pullapprove config to properly handle global approvals (#42929)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+71.sha-9073633.with-local-changes';
+    const SW_VERSION = '12.2.0-next.1+73.sha-c74927d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
