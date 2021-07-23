6af09b6a4787bde3e8017236752f8ab5abac5a01
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 17:59:54 +0000

    772dbe8a8e feat(dev-infra): include experimental version stamp in env stamps (#42934)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.3+11.sha-c20103c.with-local-changes';
+    const SW_VERSION = '12.1.1+47.sha-12a9d21.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
