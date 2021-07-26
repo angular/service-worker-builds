3ed21a33bb5bb457969ed2ee98a9e2bcb1d6483f
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 20:45:30 +0000

    77ae4459d3 fix(compiler-cli): use correct module resolution context for absolute imports in .d.ts files (#42879)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+54.sha-7dad7df.with-local-changes';
+    const SW_VERSION = '12.1.1+55.sha-2ed5a2d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
