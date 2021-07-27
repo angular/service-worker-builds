4572dd0454e6db862c61997d3c8d541d3e13757d
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 16:46:46 +0000

    85e93c3833 fix(docs-infra): notify `ErrorHandler` of `UnrecoverableState` errors (#42941)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+83.sha-388496c.with-local-changes';
+    const SW_VERSION = '12.2.0-next.1+85.sha-bfc4c3c.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
