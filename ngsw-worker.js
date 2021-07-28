18acd9b34d4d453a507b7345b6f00c0ed0aa5b6c
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 20:22:09 +0000

    b075721a49 fix(dev-infra): ensure that building environment stamp fails silently (#42985)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+92.sha-867000e.with-local-changes';
+    const SW_VERSION = '12.2.0-next.1+93.sha-e6593ad.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
