1810ade4d437a4ef76e777e45b076593438fb390
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:10:48 +0000

    047994b048 feat(dev-infra): introduce release action for directly branching-off into RC (#42973)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+86.sha-5b5868d.with-local-changes';
+    const SW_VERSION = '12.2.0-next.1+87.sha-1dffa51.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
