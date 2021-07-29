d45539a99a49f817628020a055c8a16895d0b18b
atscott <atscott01@gmail.com>
2021-07-29 22:25:59 +0000

    67a25b7410 docs: Fix wrongly provided/Injected AnimalService (#42988)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+65.sha-c1f9d80.with-local-changes';
+    const SW_VERSION = '12.1.1+66.sha-dc661eb.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
