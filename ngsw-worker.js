498087230649823144a818a08924c31106413ae9
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:59:55 +0000

    f31436da3c build: lock file maintenance (#42890)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.1+100.sha-81dce5c.with-local-changes';
+    const SW_VERSION = '12.2.0-next.1+97.sha-3d66816.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
