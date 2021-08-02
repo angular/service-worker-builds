500156851ada60646c666007a93d8f0584e89477
atscott <atscott01@gmail.com>
2021-08-02 17:16:27 +0000

    3574e564ad test(compiler): add a test for parsing multiline expressions in attributes (#42062)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.1.1+68.sha-a926402.with-local-changes';
+    const SW_VERSION = '12.1.1+82.sha-04472d2.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
