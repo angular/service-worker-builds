d36e29bc489f2d2355f328aac2e9ee7e36ab51d2
atscott <atscott01@gmail.com>
2021-08-03 00:30:47 +0000

    f0c5ba08f6 fix(language-service): global autocomplete doesn't work when the user tries to modify the symbol (#42923)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+13.sha-7422821.with-local-changes';
+    const SW_VERSION = '12.2.0-next.2+14.sha-bc1da1f.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
