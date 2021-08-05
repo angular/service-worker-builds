95867cc0fa90365a3111dbc3e38fbcc9d3698ffa
Andrew Kushnir <akushnir@google.com>
2021-08-05 22:52:58 +0000

    30c5b9c144 docs: fix interval type in the example (#43042)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0-next.2+26.sha-f12c533.with-local-changes';
+    const SW_VERSION = '12.2.0-next.2+27.sha-b41a2b3.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
