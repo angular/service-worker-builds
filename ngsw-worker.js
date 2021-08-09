3f31a4994bd16b562739fc593de56d47fa089639
Andrew Kushnir <akushnir@google.com>
2021-08-09 19:46:18 +0000

    8a3d2aad30 fix(docs-infra): correctly detect chunk load errors (#43088)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0+8.sha-dd3abdb.with-local-changes';
+    const SW_VERSION = '12.2.0+9.sha-8a3d2aa.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
