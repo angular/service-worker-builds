b9d5529803b773acd8cfd8ae9c1c6310a2885c14
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:02:29 +0000

    04879a1007 docs: fix cli link (#43085) PR Close #43085

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.0+6.sha-7846715.with-local-changes';
+    const SW_VERSION = '13.0.0-next.0+9.sha-04879a1.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
