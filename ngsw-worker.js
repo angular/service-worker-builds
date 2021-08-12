e8c8ee747973af1f48d427a7bff33569e7582045
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:59:13 +0000

    7a67597d01 docs: fix description: "property decorator" (not parameter) (#43068) (#43128)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.0+25.sha-9136dbd.with-local-changes';
+    const SW_VERSION = '12.2.1+3.sha-7a67597.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
