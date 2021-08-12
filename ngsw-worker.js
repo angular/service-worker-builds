7de5bff8222539c48f48eba7910b424517a520f1
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:52:52 +0000

    a2a9b917b5 docs: adjust description to code example (#43127)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.1+2.sha-d5f9890.with-local-changes';
+    const SW_VERSION = '13.0.0-next.1+3.sha-a2a9b91.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
