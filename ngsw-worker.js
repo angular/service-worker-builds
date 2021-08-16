324c091c4ba1d5b94323429d152c64be72a786c8
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:05:39 +0000

    83fa3ee581 docs: add tmdavis to contributors (#43135)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.1+9.sha-e0a8ca7.with-local-changes';
+    const SW_VERSION = '12.2.1+12.sha-83fa3ee.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
