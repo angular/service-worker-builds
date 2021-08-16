7ce7e534705e1ae6e962d9cc6549750b8932fd1a
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:02:06 +0000

    0c84f6fd52 refactor(compiler): fix lateron typo in i18n ExtractorHost (#43160)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.1+8.sha-b0b7ef2.with-local-changes';
+    const SW_VERSION = '13.0.0-next.1+9.sha-0c84f6f.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
