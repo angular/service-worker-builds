02f822395324f6bb856c962ca8c49a3b5cfcefba
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:27:05 +0000

    c91cecddaa ci: update circle cache key (#43177)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.1+17.sha-d743dd9.with-local-changes';
+    const SW_VERSION = '13.0.0-next.1+20.sha-c91cecd.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
