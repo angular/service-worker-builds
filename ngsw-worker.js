b5b44d81e0e5592c8750e5f8e1f92672d462e0d1
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:46:27 +0000

    ec4dcdfbd7 build: lock file maintenance (#43015)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.1+34.sha-7e71370.with-local-changes';
+    const SW_VERSION = '13.0.0-next.1+35.sha-ec4dcdf.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
