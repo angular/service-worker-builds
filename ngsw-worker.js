60427cef0cca2aaa230e23b59b13dd606175452c
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 03:20:10 +0000

    e31ed3d0dc docs: update links and formatting (#43066)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.1+29.sha-545972e.with-local-changes';
+    const SW_VERSION = '12.2.1+30.sha-e31ed3d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
