c195738b16e5008b91b12645bb95e81e286a3157
GitHub <noreply@github.com>
2021-08-18 21:58:49 +0000

    e3b6e26565 release: cut the v13.0.0-next.2 release (#43200)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.1+43.sha-3e756f0.with-local-changes';
+    const SW_VERSION = '13.0.0-next.1+44.sha-e3b6e26.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
