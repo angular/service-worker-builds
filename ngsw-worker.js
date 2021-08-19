36f7b87234d49bef1e684920b6056f7d9fbbec16
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 19:18:42 +0000

    cb8e51f8cf test(language-service): add extended diagnostics test (#43107)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.2+2.sha-19fa033.with-local-changes';
+    const SW_VERSION = '12.2.2+7.sha-cb8e51f.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
