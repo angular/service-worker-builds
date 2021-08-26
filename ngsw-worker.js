3c1f065fc7d529ffa45976a87546c1aa3f1323c6
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:06:37 +0000

    030ff0d7a1 test(ngcc): use @angular npm packages in integration test (#43234)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.3+3.sha-298d8a4.with-local-changes';
+    const SW_VERSION = '13.0.0-next.3+5.sha-030ff0d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
