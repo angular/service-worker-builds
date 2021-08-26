fc906658aab78a440fb843156bde3151c244c13c
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 20:04:30 +0000

    c33ba8d4de build: update issue template to include core package (#43272)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.3+8.sha-78690ee.with-local-changes';
+    const SW_VERSION = '13.0.0-next.3+9.sha-c33ba8d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
