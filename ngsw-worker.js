092ad781592e6322ba5dc606a7a204b33e7553b5
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:12:32 +0000

    fc3b50e427 fix(language-service): exclude the `SafePropertyRead` when applying the optional chaining (#43321)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.4+2.sha-3c863c5.with-local-changes';
+    const SW_VERSION = '13.0.0-next.4+11.sha-fc3b50e.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
