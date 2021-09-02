edb8f5d71c70c6f5eccb4209ef82fb3581cc01ee
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:06:24 +0000

    c7a93e632b docs: enhance spacings in dynamic component loader section examples (#43241)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.4+1.sha-95f3aec.with-local-changes';
+    const SW_VERSION = '13.0.0-next.4+10.sha-c7a93e6.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
