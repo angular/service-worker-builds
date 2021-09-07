20f8f15063aea10ad3d9d6374effeae5923e2fe6
Andrew Scott <atscott01@gmail.com>
2021-09-07 23:36:31 +0000

    2efc18e675 fix(migrations): migration failed finding tsconfig file (#43343)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.4+25.sha-9c7d18e.with-local-changes';
+    const SW_VERSION = '13.0.0-next.4+26.sha-2efc18e.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
