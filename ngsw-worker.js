d67727b8657ca5b3f873e7a097cba347b3532f0b
Andrew Scott <atscott01@gmail.com>
2021-09-07 23:25:16 +0000

    c02e5e15b6 refactor(common): removed TODO no longer considered necessary (#43378)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.4+18.sha-01d2f03.with-local-changes';
+    const SW_VERSION = '12.2.4+19.sha-c02e5e1.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
