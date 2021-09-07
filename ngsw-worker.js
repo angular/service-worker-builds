15415b91d110fac719a1f94478c3b767e2147a07
Andrew Scott <atscott01@gmail.com>
2021-09-07 21:51:20 +0000

    d647491679 docs: remove Angular 9 from support table (#43350)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.4+22.sha-2e11be8.with-local-changes';
+    const SW_VERSION = '13.0.0-next.4+24.sha-d647491.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
