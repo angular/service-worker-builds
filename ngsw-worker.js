8bac8bf93a8ea9e3f4699a7912588d695e264eac
Andrew Scott <atscott01@gmail.com>
2021-09-08 17:14:52 +0000

    9e039ca68b fix(router): Only trigger router navigation on `popstate` events from `Location` subscription (#43328)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.4+29.sha-93c9932.with-local-changes';
+    const SW_VERSION = '13.0.0-next.5+1.sha-9e039ca.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
