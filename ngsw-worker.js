89db9b775bf8d4b6208d58d377a1b41cf69a6989
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:38:31 +0000

    99f63dc13b fix(docs-infra): bump aio size limit (#42800)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.5+9.sha-f57f0ed.with-local-changes';
+    const SW_VERSION = '13.0.0-next.5+19.sha-99f63dc.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
