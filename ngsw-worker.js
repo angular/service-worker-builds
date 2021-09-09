021e066e6973ac23e40b32afe5c9ed930eeecd28
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:55:08 +0000

    b31adaf6cc docs: add Angular Projects book in resources (#43130)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.5+22.sha-a0fe899.with-local-changes';
+    const SW_VERSION = '13.0.0-next.5+23.sha-b31adaf.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
