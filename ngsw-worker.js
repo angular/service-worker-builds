35c51fe5088584e5a803a248e041a51daaf3f94f
Andrew Kushnir <akushnir@google.com>
2021-09-13 21:55:10 +0000

    59353c6305 fix(service-worker): clear service worker cache in safety worker (#43324)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.5+31.sha-de682eb.with-local-changes';
+    const SW_VERSION = '13.0.0-next.5+32.sha-59353c6.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
