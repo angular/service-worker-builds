517a9a585623c3ced64f100a6419e5ed257474fa
Andrew Kushnir <akushnir@google.com>
2021-09-13 16:45:22 +0000

    6d8a711f5c docs: remove unnecessary "https://angular.io/" from aio links (#43425)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.5+29.sha-75a3c77.with-local-changes';
+    const SW_VERSION = '13.0.0-next.5+30.sha-6d8a711.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
