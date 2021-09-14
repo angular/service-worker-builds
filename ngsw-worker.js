5fff3cf184ba13bc9a042cfc20449a8f48acbad8
Andrew Kushnir <akushnir@google.com>
2021-09-14 15:54:18 +0000

    2f94ac374a docs: add guidelines for localizing angular documentation (#43314)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.5+28.sha-4034f25.with-local-changes';
+    const SW_VERSION = '12.2.5+32.sha-2f94ac3.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
