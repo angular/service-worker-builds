1f35b34bb4a48d936f040c73ff7d7c5a12d0ba0e
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:09:48 +0000

    e5d08c1902 docs: update reviewed tag (#43452)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.6+13.sha-66c6dbd.with-local-changes';
+    const SW_VERSION = '13.0.0-next.6+14.sha-e5d08c1.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
