8f37e9f48a0aa0ec51a44b94727ad07306c0f36b
Andrew Kushnir <akushnir@google.com>
2021-09-15 18:08:39 +0000

    1986407ab5 docs: move i18n angular.json snippets to external file (#43216)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.5+56.sha-5e9eb7a.with-local-changes';
+    const SW_VERSION = '13.0.0-next.6+11.sha-1986407.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
