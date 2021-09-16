6d8a963c8402c204bfbea839ac6ea97ad95f6779
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 17:11:54 +0000

    b43f21bb15 refactor(compiler): improve regular expression for stripping comments (#43456)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.6+15.sha-def6315.with-local-changes';
+    const SW_VERSION = '12.2.6+21.sha-b43f21b.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
