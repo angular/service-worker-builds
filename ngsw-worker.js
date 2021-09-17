d6703b1ca1949e4e997ed7610ed7aabfca70be47
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 18:29:50 +0000

    33708f256d docs: Edits to remove jargon (#43017)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.6+34.sha-cabbff6.with-local-changes';
+    const SW_VERSION = '13.0.0-next.6+35.sha-33708f2.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
