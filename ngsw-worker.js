3569060568fe5a0d818d4bd2e3afeb6be1bcb7bd
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 16:22:56 +0000

    020f0d3858 fix(docs-infra): improve aio contributors page responsiveness (#43355)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.6+37.sha-909c706.with-local-changes';
+    const SW_VERSION = '12.2.6+38.sha-020f0d3.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
