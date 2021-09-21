2ca0382e3774f307306a93ad5bed5c3cd5ed7f53
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-21 18:08:07 +0000

    6ce6beb1d6 build: lock file maintenance (#43513)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.6+53.sha-d1baf7c.with-local-changes';
+    const SW_VERSION = '13.0.0-next.6+54.sha-6ce6beb.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
