40b4084bba10625dce27bdac1024ed0c743b0fa0
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 16:38:17 +0000

    6ff0eef35a docs(http): get rid of not needed space at the end of the sentence (#43528)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.6+59.sha-ce406e3.with-local-changes';
+    const SW_VERSION = '13.0.0-next.6+61.sha-6ff0eef.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
