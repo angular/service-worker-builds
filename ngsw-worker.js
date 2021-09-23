1d11dda9bc1420cc515a6a0f9d291589e78deddf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 22:05:31 +0000

    2306a3250f docs(router): fix typo in router event API documentation (#43555)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.6+58.sha-dad213e.with-local-changes';
+    const SW_VERSION = '12.2.7+9.sha-2306a32.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
