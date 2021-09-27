51a90adb259c70e575392dd436d6945d49788a82
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:04:21 +0000

    353cad283d docs: Edits to remove jargon (#43000)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.7+41.sha-22eebba.with-local-changes';
+    const SW_VERSION = '13.0.0-next.8+1.sha-353cad2.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
