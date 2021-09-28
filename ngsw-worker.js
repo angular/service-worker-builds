2e40fedda4899ee42628c466abeab854a38eaefd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:38:47 +0000

    4f3beffdbf feat(router): emit activate/deactivate events when an outlet gets attached/detached (#43333)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.8+13.sha-8d2b6af.with-local-changes';
+    const SW_VERSION = '13.0.0-next.8+14.sha-4f3beff.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
