1806bae4a2434eb600cdbccda553a1c4b61686c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:20:40 +0000

    d1589604a8 build(docs-infra): ensure all overloads are shown in interfaces (#43614)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.8+25.sha-e0a7285.with-local-changes';
+    const SW_VERSION = '13.0.0-next.8+26.sha-d158960.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
