c3bc5db59dfc5925de58b1234fcb2c83ed84d109
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:36:06 +0000

    09d325a9e6 test(router): refactor tests to not use deprecated loadChildren (#43578)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.8+27.sha-6a3492b.with-local-changes';
+    const SW_VERSION = '13.0.0-next.8+29.sha-09d325a.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
