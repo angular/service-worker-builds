4d75b00979c1212cb0ab4bd29079ae2b04ffe9ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:14:57 +0000

    51149ab530 docs: deprecate `aotSummaries` usage in TestBed (#43395)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.8+21.sha-c008e0f.with-local-changes';
+    const SW_VERSION = '13.0.0-next.8+22.sha-51149ab.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
