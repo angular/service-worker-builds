863b1cde0bc5093a39d2efe6a304385b0f46b7df
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 00:16:41 +0000

    81c7eb813c feat(core): add migration to opt out existing apps from new test module teardown behavior (#43353)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.10+11.sha-6d1c2f7.with-local-changes';
+    const SW_VERSION = '13.0.0-next.10+22.sha-81c7eb8.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
