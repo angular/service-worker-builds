b304783cf863ced15ba13d9fec6cff41b8b49a96
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 00:48:52 +0000

    c14085e434 feat(core): drop support for TypeScript 4.2 and 4.3 (#43642)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.11+10.sha-fddb50b.with-local-changes';
+    const SW_VERSION = '13.0.0-next.11+14.sha-c14085e.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
