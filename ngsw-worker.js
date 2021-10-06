cb5fbad3bb8c7e53f0b1a6f932f1ee7b066502f0
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 16:42:40 +0000

    6ac35d0660 fix(docs-infra): convert button-like elements to actual buttons (#43601)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.12+2.sha-fa6aa1b.with-local-changes';
+    const SW_VERSION = '13.0.0-next.12+4.sha-6ac35d0.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
