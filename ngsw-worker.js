a67cca037acfe550339f13de5c2b107e24fc1b7e
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 02:11:26 +0000

    e3d6e97050 docs: deprecate ViewEngine-based JIT Compiler symbols (#43710)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.11+16.sha-7acf3be.with-local-changes';
+    const SW_VERSION = '13.0.0-next.12+1.sha-e3d6e97.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
