0403c4be4ccaed40be0a3c77c455f2ac1e3b2857
Joey Perrott <josephperrott@gmail.com>
2021-10-07 01:45:44 +0000

    e1c2fbd2af release: bump the next branch to v13.1.0-next.0 (#43752)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.13+6.sha-0a0cc67.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+1.sha-e1c2fbd.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
