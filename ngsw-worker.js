fe669f1dfd4b807bb4e5db312d1d7294b00c201e
Andrew Scott <atscott01@gmail.com>
2021-10-07 18:45:48 +0000

    e0667efa6e feat(bazel): allow for custom conditions to be set in `ng_package` targets (#43764)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+1.sha-e1c2fbd.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+2.sha-e0667ef.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
