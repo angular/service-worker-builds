96be5a1e869f91db4ffa02a69f6014bdb99b183a
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:59:51 +0000

    1869474386 refactor(docs-infra): remove linting from i18n example angular.json (#43747)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.15+8.sha-a64adab.with-local-changes';
+    const SW_VERSION = '13.0.0-next.15+12.sha-1869474.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
