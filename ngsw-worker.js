a5a719b556180bcc0336fa33ad689979a1aa5638
Andrew Scott <atscott01@gmail.com>
2021-10-12 21:55:34 +0000

    85c0ce793b docs: update internationalization topics (#43722)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+39.sha-657ce1d.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+40.sha-85c0ce7.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
