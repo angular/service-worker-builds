1369ba7962b0ddb4e325d5b3b9cc7a15a529d018
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:02:57 +0000

    62494c9f2a docs: update how to remove a configuration item (#43795)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+27.sha-baf8da0.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+32.sha-62494c9.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
