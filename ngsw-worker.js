2204b6d604b59af5309b6bf27b3c1f2dfe077a2d
Andrew Scott <atscott01@gmail.com>
2021-10-13 17:45:29 +0000

    7ba9591a1d ci: remove rollup from renovate ignoreDeps (#43737)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.15+34.sha-ad83290.with-local-changes';
+    const SW_VERSION = '13.0.0-next.15+41.sha-7ba9591.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
@@ -2830,4 +2830,4 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
     const adapter = new Adapter(scope.registration.scope, self.caches);
     new Driver(scope, adapter, new CacheDatabase(adapter));
 
-}());
+})();
