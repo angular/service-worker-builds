a59dfc0a577f9be9850b7f9e1f8c8f1f4407b05d
Andrew Kushnir <akushnir@google.com>
2021-10-14 17:33:25 +0000

    c0a439927b build: update dependency @typescript-eslint/parser to v5 (#43807)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+54.sha-5cc5188.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+55.sha-c0a4399.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
