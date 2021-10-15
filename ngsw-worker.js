2fe4cdeb25095a7bf703045980a3986e6c922939
Andrew Kushnir <akushnir@google.com>
2021-10-15 17:35:22 +0000

    426a3ecae7 fix(compiler-cli): updates `ngc` to pass the build when only warnings are emitted (#43673)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-rc.0+8.sha-91e73c4.with-local-changes';
+    const SW_VERSION = '13.0.0-rc.0+11.sha-426a3ec.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
