5107e78cd239947ed3f8315e7dc35f7ee4e7b644
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:02:58 +0000

    6783977d21 build: update bazel integration test to `rules_nodejs` v4.4.1 (#43838)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+60.sha-b2b5972.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+62.sha-6783977.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
