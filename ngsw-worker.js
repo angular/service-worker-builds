7247b266003f9e33f5af01fb398ac1842000af38
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:55:26 +0000

    db57e391cc docs: update getting started links to provide a more consistent user experience (#43872)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-rc.0+16.sha-ee2678c.with-local-changes';
+    const SW_VERSION = '13.0.0-rc.0+20.sha-db57e39.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
