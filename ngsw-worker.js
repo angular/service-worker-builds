990e8b706b69fb71e11dab842f098c715527179b
Andrew Kushnir <akushnir@google.com>
2021-10-19 18:01:47 +0000

    222c5a845c docs: add docs about build cache (#43843)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+90.sha-fbd2e4f.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+91.sha-222c5a8.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
