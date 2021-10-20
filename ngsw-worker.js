2e3df582954014c1525a23997e5279be8c432b35
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:31:52 +0000

    b8f1f82fe2 docs: clarify meaning of ellipsis in bazel command (#43890)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+96.sha-4c9f35b.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+97.sha-b8f1f82.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
