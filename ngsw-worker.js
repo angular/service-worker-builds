49ef6b925fcd5b7043e7ff1d68c2204f0090c6e2
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 18:31:46 +0000

    4ed8dc858a docs: remove Ivy-specific guides (#43860)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+103.sha-45e4e60.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+104.sha-4ed8dc8.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
