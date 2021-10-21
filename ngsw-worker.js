13c8363d3037372d0fcc15e051501a837781b358
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 20:29:05 +0000

    7817892684 refactor(core): made comment structure consistent in index.ts (#43684)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.11+3.sha-cef01b8.with-local-changes';
+    const SW_VERSION = '12.2.11+6.sha-7817892.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
