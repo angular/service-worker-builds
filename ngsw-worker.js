facdad0a64322c95b99040840aa6c410f20811b8
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 19:49:28 +0000

    087fae5ea9 refactor(migrations): remove no longer accessible migrations (#43942)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-rc.1+17.sha-8277c38.with-local-changes';
+    const SW_VERSION = '13.0.0-rc.1+18.sha-087fae5.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
