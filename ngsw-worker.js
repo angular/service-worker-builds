ed4bc5ca12b701aa968d74cc6d20a9f0e7d82afb
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 23:10:39 +0000

    0e05f64bf4 docs(localize): fix spelling of documentation link (#43941)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+116.sha-0873ae2.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+117.sha-0e05f64.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
