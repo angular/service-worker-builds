a9ff6df58672dcba704a8c3daa1209eb384a1962
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 19:54:01 +0000

    bf59401473 refactor(compiler-cli): extract error documentation base url into separate file (#43527)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.11+10.sha-5f32863.with-local-changes';
+    const SW_VERSION = '12.2.11+12.sha-bf59401.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
