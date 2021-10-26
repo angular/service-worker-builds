6b3776627efac4ce15ffa4d6dd2c6d4e48b35de8
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 00:23:42 +0000

    59e56e79d0 docs(docs-infra): change deprecated keyboardEvent.keyCode example to use keyboardEvent.code (#43934)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-rc.1+20.sha-940eafa.with-local-changes';
+    const SW_VERSION = '13.0.0-rc.1+21.sha-59e56e7.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
