c4695782e8e8027683a9e47e9b1c3a50d8611a81
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-30 00:09:50 +0000

    0382f863db docs: update left-nav (#43964)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-rc.2+10.sha-96b99c4.with-local-changes';
+    const SW_VERSION = '13.0.0-rc.2+50.sha-0382f86.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
