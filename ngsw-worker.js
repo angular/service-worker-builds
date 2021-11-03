63efebbe308fdc929f914d3eaeb7555881bbeeac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 21:03:25 +0000

    cbf96c1407 release: cut the v13.1.0-next.0 release (#44040)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0-next.14+199.sha-1cbe864.with-local-changes';
+    const SW_VERSION = '13.0.0-next.14+200.sha-cbf96c1.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
