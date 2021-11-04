0ad260fcb0953940864d425ec1c8fb62836364e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-04 00:43:32 +0000

    ef15db0e80 docs: add blog post link to CHANGELOG (#44050)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0+6.sha-0744075.with-local-changes';
+    const SW_VERSION = '13.0.0+7.sha-ef15db0.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
