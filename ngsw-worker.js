f6164e3c87a4554ce47c2d9e5163727e0cd36bee
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:39:17 +0000

    bac03a95d7 ci: do not capture nested bazel integration test workspace within Bazel (#44061)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.0+22.sha-523e8fd.with-local-changes';
+    const SW_VERSION = '13.1.0-next.0+26.sha-bac03a9.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
