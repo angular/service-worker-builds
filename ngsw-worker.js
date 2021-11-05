fd369a13bb0cfb2b561b5b4854fec179730826be
Andrew Scott <atscott01@gmail.com>
2021-11-05 18:26:58 +0000

    544c26d779 docs: fix typescript errors for the schematics-for-libraries example (#30966)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.0+29.sha-64af4b9.with-local-changes';
+    const SW_VERSION = '13.1.0-next.0+31.sha-544c26d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
