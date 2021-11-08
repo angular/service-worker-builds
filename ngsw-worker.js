f50b0b0682fa8af493d3251c6ffe9bd788a88f2c
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:41:49 +0000

    49964c89e6 docs: add Angular 13 support information (#44098)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.0+46.sha-baab456.with-local-changes';
+    const SW_VERSION = '13.1.0-next.0+47.sha-49964c8.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
