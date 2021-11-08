b999fb745cd0af676481ae6e4214b2066cba3879
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:52:24 +0000

    e5a960b159 fix(compiler): generate correct code for safe method calls (#44088)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0+29.sha-58f453d.with-local-changes';
+    const SW_VERSION = '13.0.0+49.sha-e5a960b.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
