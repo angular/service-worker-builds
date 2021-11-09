b1097a356a8bb06df141606cdb4f1b6ffd5d7ee2
Andrew Scott <atscott01@gmail.com>
2021-11-09 21:14:33 +0000

    247c18dee0 fix(compiler-cli): handle pre-release versions when checking version (#44109)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.0+62.sha-28ef5af.with-local-changes';
+    const SW_VERSION = '13.1.0-next.0+64.sha-247c18d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
