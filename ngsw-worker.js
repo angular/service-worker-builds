eb90de5d7fe354a20d48d05b9bfd1cd65966f957
Andrew Scott <atscott01@gmail.com>
2021-11-10 18:45:21 +0000

    ee2031d9f4 fix(compiler): ensure that partially compiled queries can handle forward references (#44113)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0+68.sha-30a27ad.with-local-changes';
+    const SW_VERSION = '13.0.0+73.sha-ee2031d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
