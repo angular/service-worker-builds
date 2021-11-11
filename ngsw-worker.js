14705570642829344f6a4db55bb3adac834168f8
Andrew Scott <atscott01@gmail.com>
2021-11-11 00:03:14 +0000

    ca0873fca9 build: update to latest @angular/dev-infra-private to address issues related to verifying environment (#44138)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0+73.sha-ee2031d.with-local-changes';
+    const SW_VERSION = '13.0.0+78.sha-ca0873f.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
