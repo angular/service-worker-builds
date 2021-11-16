df5b20e2ec4b68aa14f030d513d4a535f0bc7601
Andrew Kushnir <akushnir@google.com>
2021-11-16 03:19:43 +0000

    7115e838fa ci: remove `cla: yes` from required labels (#44179)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.0+78.sha-ca0873f.with-local-changes';
+    const SW_VERSION = '13.0.1+3.sha-7115e83.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
