21877927b23ab4d6fb37d524196106597ece8168
Andrew Kushnir <akushnir@google.com>
2021-11-17 18:56:12 +0000

    38c02e13a5 fix(forms): make the `FormControlStatus` available as a public API (#44183)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.2+6.sha-65597d6.with-local-changes';
+    const SW_VERSION = '13.1.0-next.2+7.sha-38c02e1.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
