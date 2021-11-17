695ac355209cb0f524c517a5a56b5cee2da40162
Andrew Kushnir <akushnir@google.com>
2021-11-17 18:53:26 +0000

    6474c3da8e ci: remove `cla/google` from required statues (#44197)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.1+14.sha-90a4ae1.with-local-changes';
+    const SW_VERSION = '13.1.0-next.2+5.sha-6474c3d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
