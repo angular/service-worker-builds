6ddb330c6ecd0a906ccc41606ed9013f6360e331
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 17:31:48 +0000

    d265d0d241 fix(router): prevent componentless routes from being detached (#44240)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.2+27.sha-d9c99f7.with-local-changes';
+    const SW_VERSION = '13.0.2+32.sha-d265d0d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
