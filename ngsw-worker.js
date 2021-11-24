f796c05f1240fd4529bfde9acefdd5735d26095d
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 17:21:11 +0000

    2bf131a8a0 fix(docs-infra): move aio footer inside mat-sidenav-container (#44236)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.2+40.sha-414d5fe.with-local-changes';
+    const SW_VERSION = '13.1.0-next.2+43.sha-2bf131a.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
