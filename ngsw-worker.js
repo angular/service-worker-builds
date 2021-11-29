ac44da400fe08298b9b5aae81e6aea8b468c3356
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:40:14 +0000

    3aafa765ee fix(language-service): Correctly parse inputs and selectors with dollar signs (#44268)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.2+65.sha-ef140a7.with-local-changes';
+    const SW_VERSION = '13.1.0-next.2+67.sha-3aafa76.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
