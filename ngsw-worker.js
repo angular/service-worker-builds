1e708a140a55617dae0274e8f7940b63ff53ddd2
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 17:39:31 +0000

    f702c92916 build: fix lint errors in build-saucelabs legacy script (#44311)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.2+62.sha-cabc178.with-local-changes';
+    const SW_VERSION = '13.0.2+70.sha-f702c92.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
