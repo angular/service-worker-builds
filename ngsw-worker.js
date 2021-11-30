ac61a3c6b64e8f4095a1f2058eef3ef2fec310f0
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:07:11 +0000

    c92095c201 refactor(docs-infra): clean up entryComponents usages (#44257)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.2+70.sha-f702c92.with-local-changes';
+    const SW_VERSION = '13.0.2+72.sha-c92095c.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
