da804e202f0e6be3e77cfcedc0b6cc595224f0a8
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 20:23:09 +0000

    e65a245a0b feat(core): add migration to remove entryComponents (#44322)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.2+92.sha-f7ea524.with-local-changes';
+    const SW_VERSION = '13.1.0-next.2+93.sha-e65a245.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
