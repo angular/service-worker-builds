0c9e55913bc27bd3ee80c21fe4ec07a5e51b067b
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 21:22:19 +0000

    257a30d7b3 release: cut the v13.0.3 release (#44332)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.2+81.sha-0c9d33c.with-local-changes';
+    const SW_VERSION = '13.0.2+82.sha-257a30d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
