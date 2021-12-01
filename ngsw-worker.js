0b45d3b2c6cb5c7cbb4ebf30b8144e4232302b3a
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 20:16:48 +0000

    f7ea5249de refactor(docs-infra): replace use of deprecated `Compiler` with `createNgModuleRef()` (#44293)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.2+90.sha-4192ab4.with-local-changes';
+    const SW_VERSION = '13.1.0-next.2+92.sha-f7ea524.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
