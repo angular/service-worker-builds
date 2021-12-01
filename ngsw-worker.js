c2cceedf5a586633b6690c4958dff58fe0faa4d1
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 23:38:39 +0000

    f3898f8d84 fix(docs-infra): prevent automatic linking of 'json' (#44294)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.2+95.sha-6769df7.with-local-changes';
+    const SW_VERSION = '13.1.0-next.3+1.sha-f3898f8.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
