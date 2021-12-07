0672c53e47e67933ce6b81b99973620fa095e703
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:33 +0000

    763212d5c6 refactor(animations): reuse existing Version class (#44375)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.0.3+11.sha-d06e546.with-local-changes';
+    const SW_VERSION = '13.0.3+15.sha-763212d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
