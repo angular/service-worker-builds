6d19906c8962eb0e8c1fc546b89a61e98a9e86c8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 22:29:10 +0000

    a0316ab0c0 refactor(forms): Fix weak helper types in the Forms package, mostly `Function` types that can be strengthened to specific callbacks. This was originally proposed as part of Typed Forms, but it may be possible to submit at least some of these changes separately and unconditionally (i.e. without relying on `TypedOrUntyped`.) (#44370)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0+6.sha-6c0ac51.with-local-changes';
+    const SW_VERSION = '13.1.0+7.sha-a0316ab.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
