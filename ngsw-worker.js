8b76665677424d6b691cb53df892efd6b3fe8e58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 21:47:20 +0000

    944b6705f8 build: remove unused starlark file that is no longer used (#44430)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0-next.3+28.sha-a7c117a.with-local-changes';
+    const SW_VERSION = '13.2.0-next.0+2.sha-944b670.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
