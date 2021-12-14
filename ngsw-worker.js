c35839dc639fa86fbdb13c34f3b2dd96482a717e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:49 +0000

    d640fa7062 docs: Fixed typo 'test' => 'text' (#44464) PR Close #44464

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.2.0-next.0+18.sha-f56ac0f.with-local-changes';
+    const SW_VERSION = '13.2.0-next.0+20.sha-d640fa7.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
