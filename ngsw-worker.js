68e86f9c49bf79a919f201bd4a78e4a44e17c049
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:36:52 +0000

    240edcbc4e test(docs-infra): Correct an invalid assumption in aio tests. (#44479)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.1.0+7.sha-a0316ab.with-local-changes';
+    const SW_VERSION = '13.1.0+24.sha-240edcb.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
