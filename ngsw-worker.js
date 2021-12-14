b631beee06dca14a97ebed05cefa3ae412ed3ce3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:26:39 +0000

    65717999c8 refactor(forms): Make the minimum changes to the forms unit tests in order to support the typed forms PR (#43834). (#44451)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.2.0-next.0+13.sha-ab2248e.with-local-changes';
+    const SW_VERSION = '13.2.0-next.0+16.sha-6571799.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
