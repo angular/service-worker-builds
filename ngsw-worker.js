48a208f1a50ca03a5e10160cab559abf902aa065
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:20:51 +0000

    6c05c3fd2d build: update all non-major dependencies (#44152)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.2.0-next.0+6.sha-7df9127.with-local-changes';
+    const SW_VERSION = '13.2.0-next.0+7.sha-6c05c3f.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
