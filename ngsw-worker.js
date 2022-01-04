875465e7d8b790dc2952030c38217db25622c77d
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:30:10 +0000

    67c81ccef0 refactor(docs-infra): improve aio-footer's layout (#44532)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.2.0-next.1+14.sha-f7aa937.with-local-changes';
+    const SW_VERSION = '13.2.0-next.1+15.sha-67c81cc.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
