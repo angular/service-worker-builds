5f13f3dda8f270a06f54b7c6f62875aeeebe6174
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:20:23 +0000

    af512deeed docs: fix invalid link of library support for npm packages guide (#44612)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.2.0-next.1+3.sha-c6c75c9.with-local-changes';
+    const SW_VERSION = '13.2.0-next.1+4.sha-af512de.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
