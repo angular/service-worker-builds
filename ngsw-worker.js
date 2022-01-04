deca27be80e1e02bec07a2e9b1b37dbe43cd9975
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:38:23 +0000

    a864d1c926 fix(docs-infra): adjust code link styling height (#44376)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.1+27.sha-5ae1030.with-local-changes";
+  var SW_VERSION = "13.2.0-next.1+29.sha-a864d1c.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
