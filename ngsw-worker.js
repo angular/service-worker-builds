9f5413c206b04bb1ad97f36ab3d6c8759119fb27
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:26:45 +0000

    756761d534 fix(language-service): revert the test files for Ivy (#44528)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1633,7 +1633,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '13.2.0-next.1+11.sha-4f7a838.with-local-changes';
+    const SW_VERSION = '13.2.0-next.1+12.sha-756761d.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
