23cf3b813e291f996d733abea0dc09889a6613c5
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:14:30 +0000

    0ac4c0b406 refactor(compiler): remove output AST functions (#44411)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.1.1+26.sha-25aef62.with-local-changes";
+  var SW_VERSION = "13.1.1+46.sha-0ac4c0b.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
