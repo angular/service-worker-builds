765b80a20ecf4191c79de3bb343d743b507220f0
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:52:49 +0000

    7f51f1dcae docs(docs-infra): improve the content of attribute directives (#44628)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.1+62.sha-b69a514.with-local-changes";
+  var SW_VERSION = "13.2.0-next.1+65.sha-7f51f1d.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
