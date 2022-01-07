f9aab7dd7498568fb1e7392918a1014d2bf5c275
Andrew Kushnir <akushnir@google.com>
2022-01-07 00:58:45 +0000

    57d5f9529d build: use correct target name (#44651)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.1.1+62.sha-5017bf9.with-local-changes";
+  var SW_VERSION = "13.1.1+70.sha-57d5f95.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
