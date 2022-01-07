a05e7b1ecd979419dcb279d40cd924f531e43759
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:26:08 +0000

    21f8d12c3c release: cherry-pick the v12.2.15 release notes (#44619)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.1+77.sha-0cf5501.with-local-changes";
+  var SW_VERSION = "13.2.0-next.1+79.sha-21f8d12.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
