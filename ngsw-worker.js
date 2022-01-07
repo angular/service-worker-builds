064fa5669080e0d51079d133b514d80f03c7cec8
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:33:04 +0000

    abc217b28e fix(animations): retain triggers values for moved tracked list items (#44578)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.1.1+79.sha-919f8ae.with-local-changes";
+  var SW_VERSION = "13.1.1+80.sha-abc217b.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
