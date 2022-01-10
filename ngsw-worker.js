185529cf4f6455ed6edd9f13c3ad4a088a48d068
Andrew Scott <atscott01@gmail.com>
2022-01-10 20:16:17 +0000

    ebf8446981 docs: Updated described version number  (#44365)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.1+90.sha-dff497f.with-local-changes";
+  var SW_VERSION = "13.2.0-next.1+91.sha-ebf8446.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
