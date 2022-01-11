3d68c4c988779c930252c2998ba5e6370626c3db
Andrew Scott <atscott01@gmail.com>
2022-01-11 17:49:26 +0000

    a4ab6d6b72 feat(compiler): add support for safe calls in templates (#44580)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.1+94.sha-af2a131.with-local-changes";
+  var SW_VERSION = "13.2.0-next.1+95.sha-a4ab6d6.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
