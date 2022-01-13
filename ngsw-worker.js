ce0542fe566bd81afd731480d1aed56d53dff7ab
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:44:22 +0000

    3c0446cd2d build: update shelljs dependencies to "^0.8.5" (#44697)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+5.sha-432e518.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+7.sha-3c0446c.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
