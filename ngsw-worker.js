51b7bd69a5c8cd636be9876b3a6897e6de9a0d1b
Dylan Hunn <dylhunn@gmail.com>
2022-01-15 02:04:11 +0000

    b66e479cdb fix(ngcc): support element accesses for export declarations (#44669)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+14.sha-6e04815.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+15.sha-b66e479.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
