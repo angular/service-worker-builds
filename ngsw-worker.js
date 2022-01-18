f0a3fff06defc03b6e022476edc370afc3e43ec3
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 23:10:58 +0000

    e9bd16adbe refactor(compiler): remove directive matching from template compiler (#44731)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.1.2+16.sha-cab81ba.with-local-changes";
+  var SW_VERSION = "13.1.2+19.sha-e9bd16a.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
