f486f064f46f745547203e2093e2d9e5b02dc919
Andrew Kushnir <akushnir@google.com>
2022-01-20 19:33:30 +0000

    b7ff6f06de refactor(compiler): pass rootDir to tsickle (#44768)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-rc.0+1.sha-6b75263.with-local-changes";
+  var SW_VERSION = "13.2.0-rc.0+2.sha-b7ff6f0.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
