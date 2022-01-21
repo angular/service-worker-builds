43e33dd3957b2eca2ef2264715f08a82daf80240
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:38:15 +0000

    d8bff1a1b3 fix(compiler-cli): skip `ExtendedTemplateCheckerImpl` construction if there were configuration errors (#44778)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+41.sha-9d4af65.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+44.sha-d8bff1a.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
