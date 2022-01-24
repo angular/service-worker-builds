79e6bbb75d179b758d44895e3e0048cf4b682190
Andrew Kushnir <akushnir@google.com>
2022-01-24 20:40:06 +0000

    a534a78e67 Revert "feat(core): allow for injector to be specified when creating an embedded view (#44666)" (#44807)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+56.sha-215db7f.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+58.sha-a534a78.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
