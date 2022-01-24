8cf9ca3401f3b39a9eb82de06073ba8626b90d0f
Andrew Kushnir <akushnir@google.com>
2022-01-24 23:10:52 +0000

    6a68bab58e docs: add link to AbstractControlOptions reference (#44764) PR Close #44764

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+63.sha-c9fcc4f.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+74.sha-6a68bab.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
