e3ae17ea038d628844bb9baf9c63220e0cb08651
Andrew Kushnir <akushnir@google.com>
2022-01-24 19:01:52 +0000

    215db7fbe6 fix(core): error if NgZone.isInAngularZone is called with a noop zone (#44800)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+55.sha-8363db4.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+56.sha-215db7f.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
