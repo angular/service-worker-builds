c41a120f48bd175e0c2b679d402e5e615dd12735
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:35:55 +0000

    460befd693 fix(ngcc): support element accesses for export declarations (#44824)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.14+3.sha-9616b08.with-local-changes';
+    const SW_VERSION = '12.2.15+1.sha-460befd.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
