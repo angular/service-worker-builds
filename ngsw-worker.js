7ad7c68543e4d2ec7c9ac23b10b377e56c71c055
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:31:28 +0000

    fe3e4d6865 fix(compiler-cli): Handle `ng-template` with structural directive in indexer (#44788)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes";
+  var SW_VERSION = "13.2.0-rc.1+25.sha-fe3e4d6.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
