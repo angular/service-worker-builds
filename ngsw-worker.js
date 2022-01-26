4f170125365b7d32b72c6fe84a4936f5b59a4bfa
Andrew Kushnir <akushnir@google.com>
2022-01-26 01:20:04 +0000

    894fa759f1 refactor(forms): Temporarily disable the typed forms migration. (#44828)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0-next.2+77.sha-1bce51c.with-local-changes";
+  var SW_VERSION = "13.2.0-next.2+78.sha-894fa75.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
