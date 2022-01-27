8ac35b123375b0f7cdd4a6a4ee8c28c150f5bd95
Andrew Kushnir <akushnir@google.com>
2022-01-27 17:41:06 +0000

    df50af2bc5 ci: only build test targets for components repo unit test job (#44832)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.0+1020.sha-e9124b4.with-local-changes";
+  var SW_VERSION = "14.0.0-next.0+1022.sha-df50af2.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
