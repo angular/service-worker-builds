e0928516e9c3bda30fd82e8408a15b35f41dffc4
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:24:09 +0000

    3d07d1f772 fix(docs-infra): make it possible to see whole notification text on smaller screens (#44900)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.0+1049.sha-390a653.with-local-changes";
+  var SW_VERSION = "14.0.0-next.0+1051.sha-3d07d1f.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
