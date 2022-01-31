c0034425675e945b9647a2dcc1f88366e51c12e7
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:27:03 +0000

    b51d111d72 docs: update Nx headline (#44910) PR Close #44910

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0+21.sha-47c02ca.with-local-changes";
+  var SW_VERSION = "13.2.0+25.sha-b51d111.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
