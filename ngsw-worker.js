1cf9189037211225bb744abd618f2892b23ccafe
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 00:32:56 +0000

    2916ac9e1a refactor(core): the `RuntimeError` class should support more compact syntax (#44783)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.0+40.sha-4644886.with-local-changes";
+  var SW_VERSION = "13.2.0+41.sha-2916ac9.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
