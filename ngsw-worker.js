f04890273e38ddc763a9b9929c489d69871e3340
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 00:23:26 +0000

    d82c957a26 fix(compiler-cli): ensure casing of logical paths is preserved (#44798)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.0+1086.sha-6763967.with-local-changes";
+  var SW_VERSION = "14.0.0-next.0+1088.sha-d82c957.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
