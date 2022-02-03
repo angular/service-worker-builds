ef5c039524defdb2c76b8a7b361c8ec80e4f75b6
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:57:38 +0000

    f7ea275dc4 docs: improve punctuation in animation sequence summary (#44567)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.1+13.sha-db27677.with-local-changes";
+  var SW_VERSION = "14.0.0-next.1+18.sha-f7ea275.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
