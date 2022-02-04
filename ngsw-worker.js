4c610429adda05e744659ba3a1c12fe90f98295c
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:59:15 +0000

    d183e7d736 docs: correct typo (#44793)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.1+31.sha-459cbed.with-local-changes";
+  var SW_VERSION = "14.0.0-next.1+32.sha-d183e7d.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
