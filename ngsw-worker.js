3a36bc16c0a1b6f12099648a3428ae7328ecc7eb
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:01:57 +0000

    3fae6637e7 perf(http): remove IE special status handling (#44354)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.1+19.sha-30d1c34.with-local-changes";
+  var SW_VERSION = "13.2.1+20.sha-3fae663.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
