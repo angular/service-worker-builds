aa5484abfa3fcf231f11cb8bcf4cd5838effa3de
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 18:03:16 +0000

    638a2d56dc ci: group all scorecard action dependencies (#44998)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.1+38.sha-4708cd0.with-local-changes";
+  var SW_VERSION = "14.0.0-next.1+39.sha-638a2d5.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
