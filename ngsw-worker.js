7cb67b7cfb78fd43b35ce503e4f4f611560c40b9
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 19:33:32 +0000

    a01ae487d7 build: update dependency google-closure-compiler to v20220202 (#45013)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.2+5.sha-835b326.with-local-changes";
+  var SW_VERSION = "13.2.2+7.sha-a01ae48.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
