68a9101fd9f313b274b3fd64a22003668c037757
Andrew Scott <atscott01@gmail.com>
2022-02-22 18:37:51 +0000

    c02e4d72bc ci: update SHA for components-repo-unit-tests job to fix flakiness (#45166)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.3+9.sha-4d2ef0f.with-local-changes";
+  var SW_VERSION = "13.2.3+10.sha-c02e4d7.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
