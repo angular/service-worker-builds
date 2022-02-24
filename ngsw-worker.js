1a0684320658838468c7139fa4fa0ab055ef089d
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 17:56:52 +0000

    909b21aa29 refactor(http): change <script>'s ownerDocument in jsonp teardown (#36807) handler

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.4+4.sha-ca5603b.with-local-changes";
+  var SW_VERSION = "14.0.0-next.4+6.sha-909b21a.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
