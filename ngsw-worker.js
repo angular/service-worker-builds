4a00507a59c1392b56192d0e1b3a19b06a2b1804
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 01:12:25 +0000

    4f1870362e docs: remove activatedRouteData backward accesses in aio guides (#45140)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.3+14.sha-e914da1.with-local-changes";
+  var SW_VERSION = "13.2.4+3.sha-4f18703.with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
