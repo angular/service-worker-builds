b63f45d30159ea47b9fc383e768673e637cbbd1a
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 01:21:44 +0000

    4b936a1014 Revert "feat(core): allow for injector to be specified when creating an embedded view (#45156)" (#45220)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.4+34.sha-b817f3c";
+  var SW_VERSION = "14.0.0-next.4+35.sha-4b936a1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
