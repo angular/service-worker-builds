f16b1b858f69fe596286c72efc09fe94dd2c2cf6
Andrew Kushnir <akushnir@google.com>
2022-03-03 17:30:52 +0000

    30c52ac126 build: add retry logic to postinstall script (#45252)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.4+45.sha-6459b25";
+  var SW_VERSION = "14.0.0-next.5+1.sha-30c52ac";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
