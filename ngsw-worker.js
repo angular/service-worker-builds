bec2269de3815229965ce9dab1b0cfb00ed6c809
Andrew Kushnir <akushnir@google.com>
2022-03-03 20:46:53 +0000

    4e1d825d83 docs: change link for devtools to angular repo (#45207) PR Close #45207

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.5+6.sha-e16dab8";
+  var SW_VERSION = "13.2.5+9.sha-4e1d825";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
