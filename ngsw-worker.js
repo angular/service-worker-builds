a78813d86185965779cdd81deac83390d88b903b
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:10:24 +0000

    2e2c80acd5 docs: update <div> to <p> in lifecycle-hooks.md (#45063)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.6+1.sha-89d71b5";
+  var SW_VERSION = "14.0.0-next.6+4.sha-2e2c80a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
