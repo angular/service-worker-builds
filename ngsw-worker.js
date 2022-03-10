ae4de4a419950bf7eb00423c94a7e5ceb1dbb59a
GitHub <noreply@github.com>
2022-03-10 19:49:55 +0000

    79580bba4c docs: release notes for the v13.3.0-rc.0 release (#45322)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.6+3.sha-c7cab69";
+  var SW_VERSION = "14.0.0-next.6+6.sha-79580bb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
