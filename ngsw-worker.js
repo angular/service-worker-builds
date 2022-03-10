6ed8a737b58749b8b612056cafc56c02d69798b9
GitHub <noreply@github.com>
2022-03-10 18:54:28 +0000

    895c5417e3 Set up 13.3.0 release for TypeScript 4.6 support (#45319)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.6+2.sha-6336b8e";
+  var SW_VERSION = "13.2.6+3.sha-895c541";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
