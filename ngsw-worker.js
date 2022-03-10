62ed8ca34e178698a5937037e0739b51acc794bd
Andrew Scott <atscott01@gmail.com>
2022-03-10 21:08:41 +0000

    b839aeb2c1 refactor(core): Switch over to new closure LOCALE vs getLocale() (#45302)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.2.6+4.sha-0f8e748";
+  var SW_VERSION = "13.3.0-rc.0+1.sha-b839aeb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
