e81060c73b75ab28defe433e187d1164146f9cd7
Andrew Scott <atscott01@gmail.com>
2022-03-14 16:52:08 +0000

    7eddf582a1 docs: Fix typo in issue template (#45327)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.0-rc.0+1.sha-b839aeb";
+  var SW_VERSION = "13.3.0-rc.0+4.sha-7eddf58";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
