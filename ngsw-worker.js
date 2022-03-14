095b217c6a484b4167a45568d562829e7fcd9f06
Andrew Scott <atscott01@gmail.com>
2022-03-14 20:28:51 +0000

    bb9b33053c docs: fix small English typo (#45340)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.0-rc.0+4.sha-7eddf58";
+  var SW_VERSION = "13.3.0-rc.0+5.sha-bb9b330";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
