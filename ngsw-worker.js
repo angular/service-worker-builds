4b5b09717e99cdb5d31fe7ca08c1d8c1facfe724
Andrew Scott <atscott01@gmail.com>
2022-03-15 23:46:04 +0000

    3148ab93a9 docs: add ngx-gantt to "EXPLORE ANGULAR RESOURCES" page (#45351)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.0-rc.0+7.sha-7ff4c0b";
+  var SW_VERSION = "13.3.0-rc.0+8.sha-3148ab9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
