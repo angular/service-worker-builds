25ac2c7ffa2d8a3fd1e709e29005b24e091beae4
Joey Perrott <josephperrott.github@gmail.com>
2022-03-23 22:29:58 +0000

    68901bf0f3 fix(docs-infra): prevent framing of AIO with X-Frame-Options

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1735,7 +1735,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    const SW_VERSION = '12.2.15+3.sha-b2a081a.with-local-changes';
+    const SW_VERSION = '12.2.16+1.sha-68901bf.with-local-changes';
     const DEBUG_LOG_BUFFER_SIZE = 100;
     class DebugHandler {
         constructor(driver, adapter) {
