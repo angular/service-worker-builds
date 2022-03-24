3ce4c478c0d22a42e07ea15c0576d84960f54bdc
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 21:49:30 +0000

    7a9c6f56dd build: update dependency send to ^0.18.0 (#45428)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.7+36.sha-dc0e39a";
+  var SW_VERSION = "14.0.0-next.7+37.sha-7a9c6f5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
