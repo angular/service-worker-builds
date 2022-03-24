0d8209f7ec9819062d0b80e63004de9cc2d09d9e
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:08:39 +0000

    85782e607d docs(router): Fix typo on segments (#45411)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.7+20.sha-fe0e42a";
+  var SW_VERSION = "14.0.0-next.7+21.sha-85782e6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
