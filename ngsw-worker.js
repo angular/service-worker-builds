d965eea1c35e595cce6911a3f23bcd6f50da1f0e
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:42:29 +0000

    c996b477a9 build: replace bazel devserver with shared dev-infra implementation (#45452)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.9+5.sha-d36fa11";
+  var SW_VERSION = "14.0.0-next.9+7.sha-c996b47";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
