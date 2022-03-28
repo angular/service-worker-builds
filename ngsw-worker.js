b1ce99541da5555467f3efd0f56d7b61ee4bd860
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:56:42 +0000

    d43c0e973f fix(http): Throw error when headers are supplied in JSONP request (#45210)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.9+9.sha-b36dec6";
+  var SW_VERSION = "14.0.0-next.9+10.sha-d43c0e9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
