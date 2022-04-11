25272857f37bf823c1035967432f9beb08a5cd9a
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 21:23:14 +0000

    3fceba4bd1 refactor(animations): include pushUnrecognizedPropertiesWarning in  ngDevMode check (#45591)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.11+30.sha-0facba5";
+  var SW_VERSION = "14.0.0-next.11+31.sha-3fceba4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
