0298b6e3956d36701681f0f4a0c090038c10c93e
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 19:56:52 +0000

    c7bf75dd5e fix(platform-browser): remove obsolete shim for Map comparison in Jasmine (#45521)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.11+45.sha-b5f39c5";
+  var SW_VERSION = "14.0.0-next.11+46.sha-c7bf75d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
