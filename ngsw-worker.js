50f5e3352f9b49f6e79b3f7c0ed4d40c870b33a7
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 17:11:21 +0000

    598b759d33 fix(compiler): avoid errors with extremely long instruction chains (#45574)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.12+17.sha-7bf1cf4";
+  var SW_VERSION = "14.0.0-next.12+18.sha-598b759";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
