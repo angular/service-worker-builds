76c0742106456bd089a7e1cdada0c5b94ffe9c61
GitHub <noreply@github.com>
2022-04-13 18:45:47 +0000

    cbfe6284a2 release: cut the v13.3.3 release (#45614)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.2+24.sha-e50fd9e";
+  var SW_VERSION = "13.3.2+25.sha-cbfe628";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
