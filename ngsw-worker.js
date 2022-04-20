ccd5a85a577dc1e2159cf11f815a3d9604df2df2
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 20:15:46 +0000

    1d2f5c1101 refactor(router): deprecate no longer needed resolver fields (#45597)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.13+60.sha-4e0957a";
+  var SW_VERSION = "14.0.0-next.13+62.sha-1d2f5c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
