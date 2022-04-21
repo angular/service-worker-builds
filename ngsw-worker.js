8c8f0899f7cb81a64341833edc63f382bdc3914e
Andrew Scott <atscott01@gmail.com>
2022-04-21 02:27:44 +0000

    9e4c4bc42a docs(core): add API documentation to standalone-related classes (#45687)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.13+62.sha-1d2f5c1";
+  var SW_VERSION = "14.0.0-next.14+7.sha-9e4c4bc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
