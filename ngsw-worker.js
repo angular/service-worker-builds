86c25498963a9002e79603a171215dcdee2124ca
Andrew Scott <atscott01@gmail.com>
2022-04-25 18:11:03 +0000

    31d7c3bd71 feat(common): add getState method to LocationStrategy interface (#45648)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.14+20.sha-970a3b5";
+  var SW_VERSION = "14.0.0-next.14+22.sha-31d7c3b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
