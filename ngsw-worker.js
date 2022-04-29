c6957a714b410e5f62ba88af1a5930b70b7835c1
Dylan Hunn <dylhunn@gmail.com>
2022-04-29 21:39:12 +0000

    a88bf2090e refactor(compiler-cli): increase minimum version of partial declarations (#45782)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.15+sha-dff5586";
+  var SW_VERSION = "14.0.0-next.15+sha-a88bf20";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
