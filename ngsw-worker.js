fa4189c1289f26c71cad2d447f106995a8b74d36
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 23:27:04 +0000

    fa755b2a54 fix(core): prevent `BrowserModule` providers from being loaded twice (#45826)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-next.15+sha-7005da9";
+  var SW_VERSION = "14.0.0-next.15+sha-fa755b2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
