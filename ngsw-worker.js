d08c5235e1ed623ce1df39c942e3c133902bb73a
Andrew Kushnir <akushnir@google.com>
2022-05-06 17:10:51 +0000

    6644165e13 docs: remove redundant word (#45910) PR Close #45910

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.6+6.sha-f9f1f92";
+  var SW_VERSION = "13.3.6+8.sha-6644165";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
