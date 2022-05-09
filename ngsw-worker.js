d606a0a19bdece1d719a214c93486855ff50cfeb
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:42:10 +0000

    5056cfe839 docs(docs-infra): clarify toh-2 error message (#45878)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.6+8.sha-6644165";
+  var SW_VERSION = "13.3.6+12.sha-5056cfe";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
