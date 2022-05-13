9cb41782eb0b2c4ca81e087d9b42b3baea8fe25c
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 16:26:43 +0000

    dfc4852d6c build: manually unshallow the repo for builds publishing, use absolute paths (#45979)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.7+1.sha-e223649";
+  var SW_VERSION = "13.3.8+1.sha-dfc4852";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
