792d108cdf12f58ca9a3f5ef0435f8cb3a2c313d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 20:24:53 +0000

    c224adaa21 docs: adds ngx-cookie-service library to resources list (#45955) PR Close #45955

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.8+2.sha-9a31c22";
+  var SW_VERSION = "13.3.8+3.sha-c224ada";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
