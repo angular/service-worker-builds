07e5bc84f6b048d4a92445c22c73bd0f5c08f7ac
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 18:24:22 +0000

    db15a0882e docs(devtools): add firefox extension references (#45985)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.8+6.sha-c85bff1";
+  var SW_VERSION = "13.3.8+7.sha-db15a08";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
