ce441fc79ff18c413f34da4a03b699bc0f40a9ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 18:43:11 +0000

    a12372a66d docs: update the Angular roadmap as of May 2022 (#46117)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.9+17.sha-4a7e724";
+  var SW_VERSION = "13.3.9+18.sha-a12372a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
