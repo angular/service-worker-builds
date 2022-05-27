ac04b84a9c5ce5132720b79f46548b9d71f35efe
Andrew Kushnir <akushnir@google.com>
2022-05-27 05:56:31 +0000

    872282cdb7 refactor(common): use host element to access underlying document in `NgOptimizedImage

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.9+22.sha-6dbb148";
+  var SW_VERSION = "13.3.9+23.sha-872282c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
