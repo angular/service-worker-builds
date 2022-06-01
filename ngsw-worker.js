8742bdb00ef7d35c5676a714fb77b8a172d1d9e8
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:27:17 +0000

    536939e138 docs(core): improve standalone-related docs and add more usage examples (#46202)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.0-rc.3+sha-763c5f8";
+  var SW_VERSION = "14.0.0-rc.3+sha-536939e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
