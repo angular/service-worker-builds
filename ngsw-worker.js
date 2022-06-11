ce4c61ef1ba05ffc4ce43a479ae3805916b3013f
Andrew Kushnir <akushnir@google.com>
2022-06-11 00:11:54 +0000

    9dd43f222d refactor(common): update the NgOptimizedImage code to the latest version

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1010,7 +1010,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "13.3.9+23.sha-872282c";
+  var SW_VERSION = "13.3.11+sha-9dd43f2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
