fda81824c5192548d0b772f16313dd2f5c6a7e2a
Andrew Kushnir <akushnir@google.com>
2022-06-15 21:24:41 +0000

    ba4f3aabee docs: add standalone details to NG0302 (#46090)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.0-next.1+sha-d1e5ef5";
+  var SW_VERSION = "14.1.0-next.1+sha-ba4f3aa";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
