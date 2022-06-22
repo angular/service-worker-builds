49839efec52b7a2befd26daec8244b2dcda94227
Andrew Kushnir <akushnir@google.com>
2022-06-22 21:54:20 +0000

    8722eddd1d build: avoid use of `ts-node` wrapper script as it breaks child process forking (#46456)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.2+sha-2d8ad6c";
+  var SW_VERSION = "14.0.2+sha-8722edd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
