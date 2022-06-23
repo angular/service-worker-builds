aee8b37a2cc425451f2338437186141ae72583b2
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:12:33 +0000

    51be9bbe29 fix(animations): cleanup DOM elements when the root view is removed (#45143)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.3+sha-a4c9e8d";
+  var SW_VERSION = "14.0.3+sha-51be9bb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
