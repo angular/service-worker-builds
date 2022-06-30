3de75ed84c0ddda2dd490478e33516087f5f3a21
Andrew Scott <atscott01@gmail.com>
2022-06-30 21:27:03 +0000

    6f11a58040 feat(compiler): Add extended diagnostic to warn when text attributes are intended to be bindings (#46161)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.0-next.3+sha-5dd9d4a";
+  var SW_VERSION = "14.1.0-next.3+sha-6f11a58";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
