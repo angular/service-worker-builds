462447f7a8f29ef7a44de4453374dff1e96a8409
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 18:51:21 +0000

    9e836c232f feat(compiler): warn when style suffixes are used with attribute bindings (#46651)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.0-next.4+sha-ea0f07a";
+  var SW_VERSION = "14.1.0-next.4+sha-9e836c2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
