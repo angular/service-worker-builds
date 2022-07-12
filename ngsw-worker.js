bb5f6dec6e41e5871e91e8452a20e2a3ac77ec95
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 16:12:00 +0000

    5ba7503603 test: update payload size goldens for AIO after analytics change (#46716)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.0-next.4+sha-23e7f7f";
+  var SW_VERSION = "14.1.0-next.4+sha-5ba7503";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
