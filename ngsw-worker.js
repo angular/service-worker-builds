bfce932b6e3329233fefc3d1fe2e7a47416c08f4
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 23:11:46 +0000

    211c35358a docs: correct debugging task step (#46832)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.2.0-next.0+sha-66e4122";
+  var SW_VERSION = "14.2.0-next.0+sha-211c353";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
