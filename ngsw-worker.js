e50997ec4ecdbd075f491572486590d03ec54d9e
GitHub <noreply@github.com>
2022-07-13 19:53:21 +0000

    442b16c551 release: cut the v14.1.0-rc.0 release (#46830)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.0-next.4+sha-5308634";
+  var SW_VERSION = "14.1.0-rc.0+sha-442b16c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
