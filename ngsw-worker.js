451935fcc3b2e2fb0ecb530f40d570d6a393cbec
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 18:10:45 +0000

    97bff3cec8 Revert "build: update cross-repo angular dependencies to 84dd092 (#46848)" (#46849)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.0.6+sha-e6bcdb9";
+  var SW_VERSION = "14.0.6+sha-97bff3c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
