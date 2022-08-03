f1dbab0596b537ed8994c506e5aa9e560189a891
Andrew Kushnir <akushnir@google.com>
2022-08-03 18:22:27 +0000

    3ee883e417 fix(docs-infra): do  not require `@ngModule` tag on standalone types (#47024)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.0+sha-efb8e3e";
+  var SW_VERSION = "14.1.1+sha-3ee883e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
