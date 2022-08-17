30f0ee8cae35cf48f0c689fa5b82b940a69b0444
Pawel Kozlowski <pkozlowski@google.com>
2022-08-17 07:51:37 +0000

    0583227708 fix(compiler): infinite loop in parser assignment expression with invalid left-hand expression (#47151)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.2+sha-3d9455e";
+  var SW_VERSION = "14.1.2+sha-0583227";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
