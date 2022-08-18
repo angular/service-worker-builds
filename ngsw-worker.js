17235b25f72b56483761e48e42378345132ee371
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-18 20:59:41 +0000

    9b0fd078aa refactor(core): remove obsolete `data:` url validation (#45860)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.1.3+sha-1096ad2";
+  var SW_VERSION = "14.1.3+sha-9b0fd07";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
