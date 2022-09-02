ade38d7dbf8e75937b20ae6797b026c95313b34c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-02 02:33:39 +0000

    5dd72b7610 docs(http): replace http method return description (#47322)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1014,7 +1014,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-next.0+sha-0bb3aff";
+  var SW_VERSION = "15.0.0-next.0+sha-5dd72b7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
