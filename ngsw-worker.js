ebd61b6846f97ecbbc6b539b36420b265c83cdb6
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:23:23 +0000

    710d1da4ed refactor(common): create an `NgFor` alias for `NgForOf` directive (#47309)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-next.1+sha-7e0de5b";
+  var SW_VERSION = "15.0.0-next.1+sha-710d1da";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
