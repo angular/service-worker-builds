786156975fa4a454abb7c2a5a8f5ed8081a0dde2
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 16:39:52 +0000

    d961f1fb9b docs(docs-infra): add documentation on debugging the events generation process (#47449)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.2.2+sha-cc2d32a";
+  var SW_VERSION = "14.2.2+sha-d961f1f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
