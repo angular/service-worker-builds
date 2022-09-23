7df85625dc8853ec5d84e5e506ee170d41153619
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:18:24 +0000

    85330f3fd9 fix(core): update `isDevMode` to rely on `ngDevMode` (#47475)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-next.3+sha-3fe21a6";
+  var SW_VERSION = "15.0.0-next.3+sha-85330f3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
