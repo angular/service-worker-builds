64c3170d90407041660b7ba88c4198dba28c013c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-26 21:20:11 +0000

    85b5d125be build: fix rxjs 7 build failure (#47548)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-next.3+sha-f9e21d3";
+  var SW_VERSION = "15.0.0-next.3+sha-85b5d12";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
