078d06c2b92a77e5b3db192fbc0420acad172c01
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:41:48 +0000

    b618b5aa86 fix(zone.js): cancel tasks only when they are scheduled or running (#46435)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-next.5+sha-96b7fe9";
+  var SW_VERSION = "15.0.0-next.5+sha-b618b5a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
