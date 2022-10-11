f85efcb7ffb35e38fbafc7c0aa107a41218e2200
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 16:17:53 +0000

    a41ff9413d docs(core): Improved README.md (#47700)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-next.5+sha-1b602fa";
+  var SW_VERSION = "15.0.0-next.5+sha-a41ff94";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
