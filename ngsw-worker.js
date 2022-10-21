455822e18c7479229c4527d593c7f87570284fd4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 15:53:21 +0000

    a42c8cfc67 docs: fix minor typo in hierarchical-dependency-injection.md (#47812)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.2.7+sha-4d18ba1";
+  var SW_VERSION = "14.2.7+sha-a42c8cf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
