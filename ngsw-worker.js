afb5e05397032660cb2dab9a003dd64027dc5515
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:13:10 +0000

    9d12a072b8 Revert "build: add atscott to unavailable list for pullapprove (#47658)" (#47877)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "14.2.7+sha-a478ab1";
+  var SW_VERSION = "14.2.7+sha-9d12a07";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
