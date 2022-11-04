b24e85386e921407f534a08b0807366e6b9814f2
Andrew Kushnir <akushnir@google.com>
2022-11-04 00:22:01 +0000

    173dde7681 Revert "fix(core): hardening rules related to the attribute order on iframe elements (#47935)" (#47961)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-rc.1+sha-d4b3c0b";
+  var SW_VERSION = "15.0.0-rc.1+sha-173dde7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
