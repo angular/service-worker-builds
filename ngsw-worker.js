89a0bbe2d30425460dcab59c004b4f16969bd84d
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-10 17:54:45 +0000

    1d1e33e8d0 fix(common): Add fetchpriority to ngOptimizedImage preloads (#48010)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.0+sha-4a05cfd";
+  var SW_VERSION = "15.1.0-next.0+sha-1d1e33e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
