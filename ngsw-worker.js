55f0c83bf6a209334e9b1c103ad77c5f6546159a
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-11 19:47:32 +0000

    154ca82f7b docs(core): add an error details page for unsafe <iframe> bindings (#48027)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.0-rc.3+sha-0c0550b";
+  var SW_VERSION = "15.0.0-rc.3+sha-154ca82";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
