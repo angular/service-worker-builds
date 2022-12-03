f97aa4a2c5fb71138d9938b4a57dc35785e14a4c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-03 16:38:54 +0000

    09fe75d0e9 build: update actions/checkout action to v3 (#48337)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.1+sha-d1a501e";
+  var SW_VERSION = "15.1.0-next.1+sha-09fe75d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
