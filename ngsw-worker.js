2fc83a4755716bee9f5a283cc766659770c77308
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-05 12:00:16 +0000

    d2dcf95467 build(bazel): set ci steps that build aio to 2xlarge+ (#48342)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.1+sha-192df71";
+  var SW_VERSION = "15.1.0-next.1+sha-d2dcf95";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
