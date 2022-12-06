64998d97f7570b87723d3fc36a7b2e205aebed50
Andrew Kushnir <akushnir@google.com>
2022-12-06 18:38:22 +0000

    8656ac0df3 build: update dependency karma-jasmine-html-reporter to v2 (#48369)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.1+sha-4c02395";
+  var SW_VERSION = "15.1.0-next.1+sha-8656ac0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
