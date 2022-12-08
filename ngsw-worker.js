c11c9c0c6b3418b96d094ee91270f27914220053
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 23:30:37 +0000

    83f694752a refactor(router): use `RegExp` to check if a URL is absolute (#48406)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.2+sha-a6849f2";
+  var SW_VERSION = "15.1.0-next.2+sha-83f6947";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
