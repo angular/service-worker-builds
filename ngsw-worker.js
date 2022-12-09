3aab8ea015bcb49b6e57979adcd8c28a78c5a314
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 23:25:35 +0000

    c4d17b92f9 docs: Document extracted from the original Introduction document, which is to be retired. (#47913)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.3+sha-2925c6a";
+  var SW_VERSION = "15.0.3+sha-c4d17b9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
