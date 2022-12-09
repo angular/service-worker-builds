cfe53c63ecf58d9a50d56faa3814a4554de5d654
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 21:37:17 +0000

    126e798edf docs(common): Add note to location about investigating base href handling (#48422)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.3+sha-85d3f6f";
+  var SW_VERSION = "15.0.3+sha-126e798";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
