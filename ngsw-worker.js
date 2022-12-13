64a5f151087f47e12decfe1d31eac813b6a46d97
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 21:41:17 +0000

    2e65a2bd84 fix(devtools): Replace material imports and styles (#48420)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.2+sha-b05113c";
+  var SW_VERSION = "15.1.0-next.2+sha-2e65a2b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
