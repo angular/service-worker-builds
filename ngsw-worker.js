aecefc7c3695cbc708484131e313ac19652cdb0c
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 21:57:56 +0000

    279e7d4465 refactor: cleanup polyfills by importing zone.js directly in angular.json (#48380)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.2+sha-2e65a2b";
+  var SW_VERSION = "15.1.0-next.2+sha-279e7d4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
