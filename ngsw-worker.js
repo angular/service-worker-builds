33961bced94a5fd84f0f08d445411bbe234fbab0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 21:30:52 +0000

    28ea114d54 release: cut the v15.1.0-rc.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-next.3+sha-5d1fbbb";
+  var SW_VERSION = "15.1.0-rc.0+sha-28ea114";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
