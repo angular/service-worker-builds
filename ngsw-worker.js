29a18e3806e8ff34ef9f9eaf94f086ba2a10a9d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-06 00:45:52 +0000

    8aa8b4b77c fix(forms): Form provider FormsModule.withConfig return a FormsModule (#48526)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.0-rc.0+sha-61fc474";
+  var SW_VERSION = "15.1.0-rc.0+sha-8aa8b4b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
