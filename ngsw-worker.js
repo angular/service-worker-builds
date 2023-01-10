685d5ecc64f43f0258295ccec75f83871abb154e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 17:29:39 +0000

    ae34e6cabd fix(language-service): expose `package.json` for vscode extension resolution (#48678)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.4+sha-aa31d61";
+  var SW_VERSION = "15.0.4+sha-ae34e6c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
