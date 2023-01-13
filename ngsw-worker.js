67ff06fd2ff5bb42da85da1ec774f8696132fff5
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:47:41 +0000

    5b1c7c10a0 build: update dependency @rollup/plugin-commonjs to v24 (#48530)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.0-next.0+sha-d47bd48";
+  var SW_VERSION = "15.2.0-next.0+sha-5b1c7c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
