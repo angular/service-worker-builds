9fa12221eb02aaaa94b5e03ad491da1cf6e2760f
Andrew Scott <atscott@google.com>
2023-02-06 20:58:23 +0000

    03a946dda3 docs: Fixing typo error (#48891)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.1.3+sha-21b15e8";
+  var SW_VERSION = "15.1.3+sha-03a946d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
