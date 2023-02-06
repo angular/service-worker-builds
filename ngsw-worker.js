98a74a635980cd00a9a977b38183a5abd76a9859
Andrew Scott <atscott@google.com>
2023-02-06 20:51:51 +0000

    a47721c2f1 refactor(migrations): add API to remap generated import names (#48974)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.0-next.3+sha-5a886c0";
+  var SW_VERSION = "15.2.0-next.3+sha-a47721c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
