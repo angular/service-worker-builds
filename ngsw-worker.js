a8a36ee7be89785d5b8298327ecf1a0d4def9955
Andrew Scott <atscott@google.com>
2023-02-06 20:56:16 +0000

    431ec6c8be refactor(forms): removing a workaround comment (#48904)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.0-next.3+sha-c4e5840";
+  var SW_VERSION = "15.2.0-next.3+sha-431ec6c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
