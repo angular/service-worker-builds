576b553c3df522f49173e76d689cd49ce1e1e919
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 13:53:09 +0000

    64f3684f8a build: update io_bazel_rules_sass digest to b603e6f (#49053)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.0-next.4+sha-3ec2e2d";
+  var SW_VERSION = "15.2.0-next.4+sha-64f3684";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
