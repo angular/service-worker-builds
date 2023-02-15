98e8766fe7b7d5d8c8f63b7131399c5a406c5037
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-15 18:01:08 +0000

    440b51b7af release: cut the v15.2.0-rc.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.0-next.4+sha-44f13e2";
+  var SW_VERSION = "15.2.0-rc.0+sha-440b51b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
