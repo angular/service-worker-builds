ef7a84cf7cefd2d16567cd7f6d67c3dbeacc6bb5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:09:40 +0000

    918e6f09b9 docs: fix typo in router-tutorial-toh.md (#49095)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.0+sha-6b8636f";
+  var SW_VERSION = "16.0.0-next.0+sha-918e6f0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
