8f9df18cf50f5a77bdf1d7c556dc4f460e17b301
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-03-08 18:59:31 +0000

    75d5bfda1e release: cut the v15.2.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.1+sha-9937508";
+  var SW_VERSION = "15.2.2+sha-75d5bfd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
