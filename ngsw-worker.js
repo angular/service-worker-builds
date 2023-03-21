2deed420f6649848ed2f7bf74d24f8b21af1ff00
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 15:50:06 +0000

    f0f1e7dfa7 build: update scorecard action dependencies (#49508)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.3+sha-66f8ed1";
+  var SW_VERSION = "16.0.0-next.3+sha-f0f1e7d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
