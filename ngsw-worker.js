6b7ac740b40c1f55fba5122d15d62ef2dadbcf04
Andrew Scott <atscott01@gmail.com>
2023-03-22 20:33:27 +0000

    c6d8e5fef4 docs: release notes for the v15.2.4 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.3+sha-0851bd5";
+  var SW_VERSION = "16.0.0-next.3+sha-c6d8e5f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
