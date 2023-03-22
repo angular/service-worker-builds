49bd66e40df30c303bcc9ad9b067ac5ca5a685db
Andrew Scott <atscott01@gmail.com>
2023-03-22 17:21:15 +0000

    8ea1fb7396 refactor(core): exclude disconnected nodes from hydration (#49471)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.3+sha-ff9d3b0";
+  var SW_VERSION = "16.0.0-next.3+sha-8ea1fb7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
