008cf0b0d48568363a6ad662ed292c2ed2363b0e
Andrew Scott <atscott01@gmail.com>
2023-03-29 16:32:41 +0000

    e99460865e refactor(platform-server): deprecate `useAbsoluteUrl` and `baseUrl` (#49546)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.4+sha-ef149de";
+  var SW_VERSION = "16.0.0-next.4+sha-e994608";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
