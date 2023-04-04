940d38b4b9f4d9d53452fb53512488c14c0097fe
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 18:39:57 +0000

    edc3bb180f Revert "feat(forms): Improve typings form (async)Validators" (#49706)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.6+sha-761e02d";
+  var SW_VERSION = "16.0.0-next.6+sha-edc3bb1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
