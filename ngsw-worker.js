be9edbe9a55852fbf10354ccc4efa6dead2c5b1a
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:57:39 +0000

    fd13e11e75 test: improve testing of HTTP and transfer state cache (#49810)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-next.7+sha-15c91a5";
+  var SW_VERSION = "16.0.0-next.7+sha-fd13e11";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
