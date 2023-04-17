f03bacc9fbfae7866fd7f23863ce67c3c335fa7f
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:54:24 +0000

    d994f8520c fix(core): include inner ViewContainerRef anchor nodes into ViewRef.rootNodes output (#49867)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.0+sha-96a4e74";
+  var SW_VERSION = "16.1.0-next.0+sha-d994f85";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
