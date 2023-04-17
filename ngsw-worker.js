15bbd50465b2d4dbf4e7a54b254501b15300b4da
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:43:17 +0000

    a63d9a30d8 build: update actions/checkout digest to 8e5e7e5 (#49815)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.7+sha-09a42d9";
+  var SW_VERSION = "15.2.7+sha-a63d9a3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
