976d73894f726c49231b84af547bce630b4369ba
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:42:13 +0000

    5a13eb8da0 docs: add a dot (#49434)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.7+sha-bce7f6b";
+  var SW_VERSION = "15.2.7+sha-5a13eb8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
