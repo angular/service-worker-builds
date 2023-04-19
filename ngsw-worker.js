d1a6040c016352669d5b37cc649cc6b6a15bf170
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 20:11:15 +0000

    9180f98f0e fix(core): ensure takeUntilDestroyed unregisters onDestroy listener on unsubscribe (#49901)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0-rc.2+sha-2f4b61e";
+  var SW_VERSION = "16.0.0-rc.2+sha-9180f98";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
