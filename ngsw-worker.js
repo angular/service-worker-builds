ce92e3fe6ebdde86dbcc74d3fb75d81f79791601
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 17:12:02 +0000

    f33abc805d ci: do not install Java in the test execturor (#49975)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.0+sha-be104ec";
+  var SW_VERSION = "16.1.0-next.0+sha-f33abc8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
