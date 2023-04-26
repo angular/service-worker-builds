6aa705c10b76629bd542693e941cecb4c7c554c1
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:43:21 +0000

    dd4fb18169 fix(docs-infra): add `work-break` to `a` tags in `.cli-option` (#50012)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.8+sha-bdf2aa1";
+  var SW_VERSION = "15.2.8+sha-dd4fb18";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
