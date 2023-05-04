1e978746f7c5fe9a59d1a38c8d56f8ce8650911e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:13:42 +0000

    cb8cdadd3b refactor(compiler): reflect arrow function definition (#50084)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.0+sha-2180a69";
+  var SW_VERSION = "16.1.0-next.0+sha-cb8cdad";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
