905cf384f11be9c9a078c9eb62e1d10a135f8ac6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 17:05:08 +0000

    da7ff37dc9 Revert "docs(docs-infra): Remove unused annotation template (#50114)" (#50206)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.0+sha-2f03eef";
+  var SW_VERSION = "16.0.0+sha-da7ff37";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
