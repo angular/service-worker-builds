7043d2fedc18b2a7d6b1da6b6625bfdd8d7f2c3e
Andrew Kushnir <akushnir@google.com>
2023-05-10 21:40:36 +0000

    143b85184d refactor: remove benchpres tests for other frameworks (#50108)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.0+sha-cd5946b";
+  var SW_VERSION = "16.1.0-next.0+sha-143b851";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
