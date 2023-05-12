ede579cdc90d4bc91c936b2ded7bd4ac38e1019d
Andrew Kushnir <akushnir@google.com>
2023-05-12 20:13:02 +0000

    ee081217de build: update all non-major dependencies (#50217)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.1+sha-21199f8";
+  var SW_VERSION = "16.0.1+sha-ee08121";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
