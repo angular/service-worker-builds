9c2b2922c9762c7d6761e26022bd68adc4576a5b
Andrew Kushnir <akushnir@google.com>
2023-05-15 17:11:47 +0000

    2d87aeecc1 docs: Glossary link to N (#50294)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.1+sha-37acd37";
+  var SW_VERSION = "16.0.1+sha-2d87aee";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
