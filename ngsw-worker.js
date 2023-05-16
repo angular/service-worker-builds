77ad38a0c782fd02510609c4b20c5fe70351d81a
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:39:17 +0000

    165b8b647c fix(core): allow passing value of any type to `isSignal` function (#50035)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.0+sha-fab7f39";
+  var SW_VERSION = "16.1.0-next.0+sha-165b8b6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
