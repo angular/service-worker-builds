5333a412e6e33ee8a747d610f38b1f7a79b01501
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-05-17 18:54:37 +0000

    70099a393c release: cut the v16.1.0-next.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.0+sha-d3eda34";
+  var SW_VERSION = "16.1.0-next.1+sha-70099a3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
