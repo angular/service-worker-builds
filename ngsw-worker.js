b3cba9fe1a1540e8050fd3c9bc0f63d1e122b508
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-18 14:22:53 +0000

    2d3dc4a848 docs(docs-infra): Drop `ngcli-compat` (#50345)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.2+sha-c92220a";
+  var SW_VERSION = "16.0.2+sha-2d3dc4a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
