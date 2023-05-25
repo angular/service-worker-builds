882138cddd4f9d3fd9f3df92592b63c3c4f8b265
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:57:33 +0000

    721bc72649 fix(compiler): resolve deprecation warning with TypeScript 5.1 (#50460)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.0-next.2+sha-04553b9";
+  var SW_VERSION = "16.1.0-next.2+sha-721bc72";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
