a118ed058af43e907a66d0c21a5c5aade3a00781
Dylan Hunn <dylhunn@gmail.com>
2023-05-25 18:59:03 +0000

    df65c4fc8f fix(animations): Trigger leave animation when ViewContainerRef is injected (#48705)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.0.3+sha-fc6b10c";
+  var SW_VERSION = "16.0.3+sha-df65c4f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
