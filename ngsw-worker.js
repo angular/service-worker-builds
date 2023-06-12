8adab715e9b8f146ddedb0a0f49da8faa08d600e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:19:01 +0000

    58549e7f18 docs: added lang attributes in html files (#50588)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-next.0+sha-24c8e4e";
+  var SW_VERSION = "16.2.0-next.0+sha-58549e7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
