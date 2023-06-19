44ba46f4f9a18588748ab5464421690643512b15
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-19 13:15:14 +0000

    619c318dcb build: fix benchmark sha resolution breaking when compare ref is abbreviated (#50767)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-next.0+sha-17a95da";
+  var SW_VERSION = "16.2.0-next.0+sha-619c318";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
