d8612ff423405323c530427f839ad3fedb256c02
Andrew Kushnir <akushnir@google.com>
2023-06-20 20:20:35 +0000

    64745a89b2 refactor(compiler-cli): remove unused HandlerFlags enum (#50604)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-next.0+sha-1221b37";
+  var SW_VERSION = "16.2.0-next.0+sha-64745a8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
