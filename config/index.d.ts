cd4f0b089994698a7d7b329185d22d1e7ed868bc
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:51:01 +0000

    b09119b14b docs: Updating index.md to remove alert message which no longer applicable after version 15 (#50832)

--- config/index.d.ts
+++ config/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.2.0-next.0+sha-e699f1a
+ * @license Angular v16.2.0-next.0+sha-b09119b
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-next.0+sha-e699f1a";
+  var SW_VERSION = "16.2.0-next.0+sha-b09119b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
