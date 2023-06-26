5b6231214dbedb53057a8fe29a28efac751c137f
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:53:05 +0000

    ec1f6e3a5e build: do not attempt deflaking locally (#50852)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.2+sha-a844249";
+  var SW_VERSION = "16.1.2+sha-ec1f6e3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
