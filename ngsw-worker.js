f2134308c1c86fd11a5d15a97ccd8c2d986b1bea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 19:02:20 +0000

    0bfd3f537c docs: Use id instead of name for anchor targets (#50885)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-next.1+sha-4782336";
+  var SW_VERSION = "16.2.0-next.1+sha-0bfd3f5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
