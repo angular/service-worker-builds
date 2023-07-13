5f96a3c18f3d45d569484d98ae38cee5bc1a74ab
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:52:59 +0000

    d0a9f029d9 build: update dependency prettier to v3 (#50945)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.4+sha-52749db";
+  var SW_VERSION = "16.1.4+sha-d0a9f02";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
