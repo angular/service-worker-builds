7072ab47b4614678032039bf3e45a8f84ab45b50
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:34:24 +0000

    b5cf5d22e5 docs: remove orphan migration docs (#51147)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-next.3+sha-8b32f89";
+  var SW_VERSION = "16.2.0-next.3+sha-b5cf5d2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
