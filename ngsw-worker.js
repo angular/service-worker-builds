645f77fbe181b6b9dad5ccacd89e470db97e0401
Andrew Kushnir <akushnir@google.com>
2023-07-25 17:40:02 +0000

    03f9c9305b docs: updated the tour of heroes doc file (#51151)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.6+sha-de0d3d7";
+  var SW_VERSION = "16.1.6+sha-03f9c93";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
