aced7f3a9c2a1ccde78751b44cf8e0db4f136544
Andrew Kushnir <akushnir@google.com>
2023-07-26 18:31:33 +0000

    21299d95f5 fix(devtools): ensure that inspected component label is always in the viewport (#50656)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.7+sha-07969d1";
+  var SW_VERSION = "16.1.7+sha-21299d9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
