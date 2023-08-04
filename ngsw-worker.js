0c1f9eb78be46a646182d5d279a3ff4b546a5809
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 20:17:10 +0000

    e01549b26b refactor(core): introduce LView and LContainer utility functions (#51191)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.0-rc.0+sha-cf62063";
+  var SW_VERSION = "16.2.0-rc.0+sha-e01549b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
