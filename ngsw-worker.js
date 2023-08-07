cd21b3a85cabfc4f7cffc4ca283ab7de9d6a11da
Dylan Hunn <dylhunn@gmail.com>
2023-08-07 16:45:35 +0000

    4a8efbe287 refactor(animations): remove unecessary interface (#50662)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.1.8+sha-d4ffef2";
+  var SW_VERSION = "16.1.8+sha-4a8efbe";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
