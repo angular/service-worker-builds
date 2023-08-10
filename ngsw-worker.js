40c5281db2adf0fdd2e465aaa77711d22baa21c6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 23:52:25 +0000

    464b458ef8 build: update all non-major dependencies (#51161)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.3.0-next.0+sha-36b180a";
+  var SW_VERSION = "16.3.0-next.0+sha-464b458";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
