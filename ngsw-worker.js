20aa282a1070e484b5bd4955992879e91693a762
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 00:26:56 +0000

    93675dc797 feat(core): conditional built-in control flow (#51346)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.0+sha-6a60a29";
+  var SW_VERSION = "17.0.0-next.0+sha-93675dc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
