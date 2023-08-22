4b2869f7be07126e0206f3cbb27462d08ce165cc
Andrew Kushnir <akushnir@google.com>
2023-08-22 18:52:08 +0000

    c9ce89ee5c ci: correctly check aio job results (#51464)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.0+sha-36663e6";
+  var SW_VERSION = "17.0.0-next.0+sha-c9ce89e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
