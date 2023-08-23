15b2100e1a9f894edae10ea91bdc199a12402377
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-08-23 17:48:40 +0000

    189bb74205 docs: release notes for the v16.2.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.0+sha-c9ce89e";
+  var SW_VERSION = "17.0.0-next.0+sha-189bb74";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
