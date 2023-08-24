6f6e78c9381bc9fd01e2c6c56a55aecfe44184da
Joey Perrott <josephperrott@gmail.com>
2023-08-24 20:46:11 +0000

    c3349fb2a1 ci: fix escaping for determing current stable version (#51496)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.1+sha-6d29ab7";
+  var SW_VERSION = "17.0.0-next.1+sha-c3349fb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
