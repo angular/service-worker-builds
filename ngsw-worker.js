97457c9ee9dd3c1fa260b7330a1292dcfdcd461d
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 18:34:17 +0000

    0c7c852ee7 fix(core): run afterRender callbacks outside of the Angular zone (#51551)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.2+sha-20d6260";
+  var SW_VERSION = "16.2.2+sha-0c7c852";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
