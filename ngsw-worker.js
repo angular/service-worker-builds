cf34e9f37db80af02b4934c52125a696b575f086
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:29:01 +0000

    b9ba6e6a16 refactor(core): remove duplicated access to TView (#51610)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.2+sha-bb47f81";
+  var SW_VERSION = "17.0.0-next.2+sha-b9ba6e6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
