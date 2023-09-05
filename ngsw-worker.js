afbd8c55315848209cdc6a351c760d8fc24f3782
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 14:29:34 +0000

    75ab0bdf45 refactor(compiler): type check deferred when and prefetch when triggers (#51570)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.2+sha-80de250";
+  var SW_VERSION = "17.0.0-next.2+sha-75ab0bd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
