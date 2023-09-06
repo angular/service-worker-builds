b9df927308b2a42808342a5dba9b19aca6f420c1
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:42:51 +0000

    8ad21ccfb8 docs: Add info about hydration caching. (#51648)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.2+sha-970d68f";
+  var SW_VERSION = "17.0.0-next.2+sha-8ad21cc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
