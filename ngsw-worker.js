327af34908f1e84feb844db65e9f07ed01339b37
Andrew Kushnir <akushnir@google.com>
2023-09-07 17:14:33 +0000

    3e0e6ed60f docs: update Dynamic Component loader docs to rely on `ngComponentOutlet` (#49915)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.3+sha-3f2d783";
+  var SW_VERSION = "17.0.0-next.3+sha-3e0e6ed";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
