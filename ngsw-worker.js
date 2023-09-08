a5ad7c011ea511e675fed69f687a89a1234a9086
Andrew Kushnir <akushnir@google.com>
2023-09-08 17:40:47 +0000

    5277cb408b refactor(core): delegate afterRender errors to an ErrorHandler (#51662)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.3+sha-e866d85";
+  var SW_VERSION = "17.0.0-next.3+sha-5277cb4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
