572f1ff97bd9b48dd4b6f7e53644eac60f711016
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:36:19 +0000

    f6f3783a3d docs: Fix misspelling in note of `git commit --all` (#51735)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.4+sha-38c6fa1";
+  var SW_VERSION = "16.2.4+sha-f6f3783";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
