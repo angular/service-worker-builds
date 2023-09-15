743b50785d4b3ae2b3556e3daec6b280185b3c5c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 14:03:52 +0000

    3b46066ff0 refactor: ensure top-level symbols are marked as side-effect free (#51776)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.5+sha-b0b004e";
+  var SW_VERSION = "16.2.5+sha-3b46066";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
