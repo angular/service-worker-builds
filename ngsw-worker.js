12a78de28fffbeb9c738a0aa8dd90d768a89b4b0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:33:49 +0000

    20cdb7730e build: update dependency puppeteer-core to v21 (#51802)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.4+sha-8a2dc0f";
+  var SW_VERSION = "17.0.0-next.4+sha-20cdb77";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
