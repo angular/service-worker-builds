fba84ec4b506a3295b8ac4a5eeb9cd0b0628ba80
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:01:53 +0000

    82e8e5e043 ci: move permissions for google-internal-tests into job definition (#51847)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1022,7 +1022,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.7+sha-a16e3b8";
+  var SW_VERSION = "16.2.7+sha-82e8e5e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
