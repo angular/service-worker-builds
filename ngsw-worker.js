ece3a27b691c41b6f720469a589d38f4a6a8f8be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 15:51:53 +0000

    5c70148 docs: add Nicolas Frizzarin to GDE resources (#52003)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.8+sha-65b4604";
+  var SW_VERSION = "17.0.0-next.8+sha-5c70148";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
