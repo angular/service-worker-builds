5e22833036079f55b5639c6cc4dfd5881e0e23c0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 13:52:07 +0000

    65b4604 fix(common): missing space in ngSwitch equality warning (#52180)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.8+sha-2e5ee1c";
+  var SW_VERSION = "17.0.0-next.8+sha-65b4604";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
