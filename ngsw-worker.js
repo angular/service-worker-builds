3f81369294e12621d6f6cc7f8e95e04b5e0597d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 14:35:20 +0000

    81018d7562 docs: add missing next steps section (#52220)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.9+sha-6997c52";
+  var SW_VERSION = "16.2.9+sha-81018d7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
