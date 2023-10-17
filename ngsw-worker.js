0f40b17fa52211c675659e4367cd36081293a757
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-17 16:18:09 +0000

    d811d82 build: update babel dependencies to v7.23.2 (#52236)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.8+sha-a318e6a";
+  var SW_VERSION = "17.0.0-next.8+sha-d811d82";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
