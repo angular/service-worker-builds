7eb43c23567e91b95123a37a47e181b01bf59da4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 09:36:53 +0000

    06ce49aeeb docs: fix typo (#52247)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.9+sha-d6070af";
+  var SW_VERSION = "16.2.9+sha-06ce49a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
