e005211d4da316e342ef926c9fd2c692d2f37539
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 16:29:38 +0000

    1091239 refactor(core): remove developer preview note from hydration stats note (#52197)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-next.8+sha-37768da";
+  var SW_VERSION = "17.0.0-next.8+sha-1091239";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
