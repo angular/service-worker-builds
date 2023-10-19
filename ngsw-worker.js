a10ac730ebfc3156b178629b77d04933e7e91f8c
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:35:08 +0000

    dcf9086 refactor(core): check "skip hydration" condition only when hydration info is present (#52221)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.0-next.0+sha-43115da";
+  var SW_VERSION = "17.1.0-next.0+sha-dcf9086";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
