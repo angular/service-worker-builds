580086364868e081f7fb65611788cca71b914659
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 21:46:57 +0000

    40f7536 release: cut the zone.js-0.14.1 release (#52374)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-rc.0+sha-6858149";
+  var SW_VERSION = "17.0.0-rc.1+sha-40f7536";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
