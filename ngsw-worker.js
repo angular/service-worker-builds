49f3e9b0b7c728d882abb3808265c20710fd2df8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:56:32 +0000

    a51bb6927d docs(docs-infra): fix typo in tap into the observable tutorial example (#52359)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.11+sha-5ff99f2";
+  var SW_VERSION = "16.2.11+sha-a51bb69";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
