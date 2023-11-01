8418ecf16968321a976a4364ca0b3a6aad6e01f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-01 15:13:34 +0000

    ee9605f fix(core): effects wait for ngOnInit for their first run (#52473)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.0-next.0+sha-de802f0";
+  var SW_VERSION = "17.1.0-next.0+sha-ee9605f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
