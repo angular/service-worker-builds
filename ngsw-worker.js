9a0ae58ab7b2849ef8d8e1a62bc1d3701001f714
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 20:29:49 +0000

    32f417f refactor(core): Do not refresh view if producers did not actually change (#52476)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.0-rc.1+sha-0d88e25";
+  var SW_VERSION = "17.0.0-rc.1+sha-32f417f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
