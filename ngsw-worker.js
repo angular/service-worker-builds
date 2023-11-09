42c96cb262d2bf4a92418155807ff9963b51d93c
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 20:04:29 +0000

    b16fc26 fix(core): limit rate of markers invocations (#52742)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.1+sha-cfb42ee";
+  var SW_VERSION = "17.0.1+sha-b16fc26";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
