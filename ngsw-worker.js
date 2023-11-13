151c67761c0fd62c65f1527dc2978888b71a8341
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:36:07 +0000

    5ee11a7 fix(animations): prevent the AsyncAnimationRenderer from calling the delegate when there is no element. (#52570)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.0-next.0+sha-cf86ae5";
+  var SW_VERSION = "17.1.0-next.0+sha-5ee11a7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
