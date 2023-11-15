05f138acc62b24631935b3dc6793b19641897b88
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 21:21:17 +0000

    98376f2 fix(compiler): changed after checked error in for loops (#52935)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.3+sha-f5307bf";
+  var SW_VERSION = "17.0.3+sha-98376f2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
