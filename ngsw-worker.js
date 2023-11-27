b5d294c823d57ac38fb3fed1478c0a362a52b768
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 10:05:00 +0000

    4604be2 refactor(platform-browser): clean up legacy way of getting a relative path (#53097)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.4+sha-6d61f2d";
+  var SW_VERSION = "17.0.4+sha-4604be2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
