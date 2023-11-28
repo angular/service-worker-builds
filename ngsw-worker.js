821899168b1e99dc0b569aae65e01f4e96ddad4d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 10:12:23 +0000

    e8ad51a test: refactor platform-server integration tests to use application builder (#53205)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.0-next.1+sha-b76b3c6";
+  var SW_VERSION = "17.1.0-next.1+sha-e8ad51a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
