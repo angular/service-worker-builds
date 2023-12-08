ee8197516ec427348cd52d663070501fd6cc134f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 23:06:08 +0000

    044cb55 perf(core): optimize memory allocation when reconcilling lists (#52245)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.6+sha-f88b77b";
+  var SW_VERSION = "17.0.6+sha-044cb55";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
