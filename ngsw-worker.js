2241f3daf70db26834623a22eae5a883961ee80b
Definitely not me <something@something.com>
2023-12-11 21:40:55 +0000

    cc75fab build: drop rxjs_umd_modules as it is unused (#53506)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.0-next.3+sha-0ea8864";
+  var SW_VERSION = "17.1.0-next.3+sha-cc75fab";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
