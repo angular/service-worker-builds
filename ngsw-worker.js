a8fe3cfb80c8faf5698b475ff6ca97208e4a3e8c
Andrew Scott <atscott01@gmail.com>
2023-12-19 17:26:12 +0000

    0696ab6 fix(router): Should not freeze original object used for route data (#53635)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.7+sha-b131c3b";
+  var SW_VERSION = "17.0.7+sha-0696ab6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
