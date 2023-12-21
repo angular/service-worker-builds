6575b3cfb5383611e29716e11c81355a54772fe5
Andrew Scott <atscott01@gmail.com>
2023-12-21 14:31:00 +0000

    b87324d release: cut the v17.0.8 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.7+sha-257536e";
+  var SW_VERSION = "17.0.8+sha-b87324d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
