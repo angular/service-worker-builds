d30fd3010a08de52f1d94d1d88217387b4aa40dc
Andrew Scott <atscott@google.com>
2024-01-03 19:34:56 +0000

    7bf11ea refactor(core): Use NoopNgZone in componentFixture (#53670)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.8+sha-7c19dc2";
+  var SW_VERSION = "17.0.8+sha-7bf11ea";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
