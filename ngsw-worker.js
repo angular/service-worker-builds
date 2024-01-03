365bc7c34665d4b018faf8e64a31cdbba91768d7
Andrew Scott <atscott@google.com>
2024-01-03 18:39:56 +0000

    7c19dc2 docs: fix incorrect glossary entry (#53609)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.8+sha-bd9f89d";
+  var SW_VERSION = "17.0.8+sha-7c19dc2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
