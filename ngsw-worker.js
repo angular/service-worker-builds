d8457c29555c04df4ee4fb642ad32195717cda21
Andrew Scott <atscott@google.com>
2024-01-04 22:04:05 +0000

    8fa5ebb refactor(router): replace `last` helper with native `Array.at(-1)` (#53749)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.8+sha-91cb16f";
+  var SW_VERSION = "17.0.8+sha-8fa5ebb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
