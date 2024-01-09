a1b2bf537a5239e6985c5364a5dc40d143feaa77
Andrew Scott <atscott@google.com>
2024-01-09 20:33:43 +0000

    92fd6cc fix(compiler): generate less code for advance instructions (#53845)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.8+sha-c4b266c";
+  var SW_VERSION = "17.0.8+sha-92fd6cc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
