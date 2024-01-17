15608c911c72e6f8618f417a6398aae5ee5099d6
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 22:13:15 +0000

    3c3a947 Revert "refactor(core): improve forwardRef typings"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.9+sha-8ef626e";
+  var SW_VERSION = "17.0.9+sha-3c3a947";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
