146aba216262374a6dd25832e78e0fee18aba595
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 16:41:24 +0000

    c0f0baa refactor(devtools): improving type safety (#53436)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.0+sha-0fe9331";
+  var SW_VERSION = "17.1.0+sha-c0f0baa";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
