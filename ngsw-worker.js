be6eec6f3c12605c842725df4a23199b20c1edc8
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:53:58 +0000

    dfc6c8d fix: cta clickability issue in adev homepage. (#52905) PR Close #52905

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-next.0+sha-c9c22fa";
+  var SW_VERSION = "17.2.0-next.0+sha-dfc6c8d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
