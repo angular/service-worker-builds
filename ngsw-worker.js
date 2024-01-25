998f3554c458ac2ac67681caa3096e46b001f739
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:22:55 +0000

    293dc3b docs: fix reference to forwardRef in ForwardRefFn JSdoc (#53556)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.1+sha-8dca5a1";
+  var SW_VERSION = "17.1.1+sha-293dc3b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
