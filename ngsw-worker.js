692cb37d8f67859544afa6b9f81f584c68737590
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:03:34 +0000

    89ab1c1 refactor(common): Use Nullish coalescing assignment when possible. (#53931)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-next.0+sha-271755d";
+  var SW_VERSION = "17.2.0-next.0+sha-89ab1c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
