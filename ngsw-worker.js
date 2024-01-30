eef63bfa4dc4c56c2e1ee838357e00e4506646ad
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 16:14:46 +0000

    4c2b0d9 refactor: migrate common to prettier formatting (#54159)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.1+sha-57174bb";
+  var SW_VERSION = "17.1.1+sha-4c2b0d9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
