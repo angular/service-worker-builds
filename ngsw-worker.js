9beaccce6ce6b0ca1cc05021149cfc836ec6c0ca
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 19:00:36 +0000

    bc81440 docs(docs-infra): Remove the edit button from AIO (#54204)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-next.1+sha-5d98a02";
+  var SW_VERSION = "17.2.0-next.1+sha-bc81440";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
