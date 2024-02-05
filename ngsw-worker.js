6fb721fea9e2530c4fe4e56e03822fe118f4ee78
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-05 16:46:19 +0000

    04a1d50 docs: add Jason Warner to GDE resources (#54237)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.1.2+sha-fc4d8bb";
+  var SW_VERSION = "17.1.2+sha-04a1d50";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
