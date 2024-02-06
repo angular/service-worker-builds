2351b9a6482b98735b8f0687e76d692030124afa
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 17:59:49 +0000

    0763fba build: update to latest @angular/docs (#54282)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-next.1+sha-6ef60c0";
+  var SW_VERSION = "17.2.0-next.1+sha-0763fba";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
