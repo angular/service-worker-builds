d7bfe5ccee0a802994eac0bf0e46c3c175e6b94e
Andrew Kushnir <akushnir@google.com>
2024-02-12 23:43:59 +0000

    501c159 build: update node (#54399)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-rc.1+sha-277540c";
+  var SW_VERSION = "17.2.0-rc.1+sha-501c159";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
