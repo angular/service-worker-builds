44babf8d71e99c1794b78addb0e83c93c0102801
Andrew Kushnir <akushnir@google.com>
2024-02-12 22:37:23 +0000

    277540c build: reuse fake core in type checking tests (#54344) (#54398)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-rc.1+sha-6d00115";
+  var SW_VERSION = "17.2.0-rc.1+sha-277540c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
