fe22df5e32999d2767f36fc4a409d7f403746784
Andrew Kushnir <akushnir@google.com>
2024-02-12 16:56:07 +0000

    4d722a2 fix(zone.js): patch form-associated custom element callbacks (#50686)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.0-rc.1+sha-deb9ea0";
+  var SW_VERSION = "17.2.0-rc.1+sha-4d722a2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
