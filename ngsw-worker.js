b9012a331ce467a9be5103171432d5d4792138b1
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:38:59 +0000

    e5e8be4 ci: remove feature-request bot (#54442)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.1+sha-44a0ea4";
+  var SW_VERSION = "17.2.1+sha-e5e8be4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
