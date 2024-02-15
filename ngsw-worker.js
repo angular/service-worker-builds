803e2c2ee43baab81bfdf553a54b8b270c05b5f8
Andrew Kushnir <akushnir@google.com>
2024-02-15 18:39:12 +0000

    03acdbe fix(docs-infra): process mermaid code blocks (#54462)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.0-next.0+sha-6c2d4b6";
+  var SW_VERSION = "17.3.0-next.0+sha-03acdbe";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
