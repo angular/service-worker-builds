aacae1874176990c0092bb4b73d39418ed747e05
Andrew Kushnir <akushnir@google.com>
2024-02-15 01:20:22 +0000

    a7e4b99 refactor(devtools): implement multiframe support in devtools page (#53934)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.1+sha-fba7fe1";
+  var SW_VERSION = "17.2.1+sha-a7e4b99";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
