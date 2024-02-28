4b89491c1c5e4489a3d9899fca1097f135d264cc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 16:20:25 +0000

    c19c44d docs: fix broken link in AIO (#54649)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.3+sha-b0f434a";
+  var SW_VERSION = "17.2.3+sha-c19c44d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
