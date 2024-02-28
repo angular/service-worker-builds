c5aa4abf062e7a4b2d2fe1eee6cfc44df98fe4ba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 14:21:24 +0000

    b0f434a docs: fix broken markdown syntax (#53567)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.3+sha-c05bef0";
+  var SW_VERSION = "17.2.3+sha-b0f434a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
