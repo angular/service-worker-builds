63f6bc2243fe58c64697be484a23bdd2e090531d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 10:42:53 +0000

    7ead0fc docs: revise documentation for @for control flow, 'track $index' performance issues explanation, and confusing 'trackBy' mention (#53806)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.2.3+sha-6ca8eca";
+  var SW_VERSION = "17.2.3+sha-7ead0fc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
