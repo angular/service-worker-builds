92c5bc6bb7d8988df0570c698a557c07780bdfbf
GitHub <noreply@github.com>
2024-03-06 21:31:18 +0000

    fc0f47c release: bump the next branch to v18.0.0-next.0 (#54737)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.4.0-next.0+sha-781dbe2";
+  var SW_VERSION = "18.0.0-next.0+sha-fc0f47c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
