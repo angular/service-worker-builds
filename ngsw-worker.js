1e860b9e734760390fa415b2b67a720dc719bf38
Joey Perrott <josephperrott@gmail.com>
2024-03-15 16:48:20 +0000

    a5cc82e docs: update events (#54749)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.0+sha-c22c5c4";
+  var SW_VERSION = "17.3.0+sha-a5cc82e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
