66026ef7070cf40c3b1aceb07e100a6738e7979d
Dylan Hunn <dylhunn@gmail.com>
2024-03-26 16:15:25 +0000

    e1650e3 fix(compiler): throw error if item name and context variables conflict (#55045)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-next.1+sha-ee3bb81";
+  var SW_VERSION = "18.0.0-next.1+sha-e1650e3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
