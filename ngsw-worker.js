47bdce0d06120deb83de195247491e01ea8bf9fc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-04 18:19:09 +0000

    39624c6 fix(compiler): output input flags as a literal (#55215)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-next.3+sha-9ad4ed1";
+  var SW_VERSION = "18.0.0-next.3+sha-39624c6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
