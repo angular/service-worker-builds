c02b61b3df05552b67a6198cbca68c226a68c0b8
Andrew Scott <atscott@google.com>
2024-04-08 18:41:56 +0000

    f8d5921 docs(docs-infra): move CLI docs to ADEV (#55242)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-next.3+sha-3583d67";
+  var SW_VERSION = "18.0.0-next.3+sha-f8d5921";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
