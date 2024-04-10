9fe98363b73e0271486971ad682c23cd5deaf165
Andrew Scott <atscott@google.com>
2024-04-10 20:50:36 +0000

    19a238d Revert "refactor(core): warn about duplicated keys when using built-in @for (#55243)" (#55293)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-next.3+sha-92debf4";
+  var SW_VERSION = "18.0.0-next.3+sha-19a238d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
