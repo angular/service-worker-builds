a3ff77437763d7f088b8d0beef0a26ed84923735
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 18:55:11 +0000

    051c140 build(http): Mark the `http` tests as flaky because of one particular transferCache test. (#55255)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.4+sha-c77ebb6";
+  var SW_VERSION = "17.3.4+sha-051c140";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
