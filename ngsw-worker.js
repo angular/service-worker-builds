3705d8d9a5f69f73d3749878f0c2baea267b571a
Andrew Scott <atscott@google.com>
2024-04-12 17:19:14 +0000

    a3b1dc0 docs: fix repeated note in "Add an Input to the component" section (#55306)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.4+sha-8a670e6";
+  var SW_VERSION = "17.3.4+sha-a3b1dc0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
