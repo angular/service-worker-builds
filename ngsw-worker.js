f9de72c9b71fd557e37f053672a427d9f25dd20a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-16 08:30:11 +0000

    7d5bc1c fix(compiler): remove container index from conditional instruction (#55190)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-next.4+sha-9afa2ea";
+  var SW_VERSION = "18.0.0-next.4+sha-7d5bc1c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
