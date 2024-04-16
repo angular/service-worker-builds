25b24925d82e055f4e828fc9e0b7048e5af12824
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-16 23:36:06 +0000

    e6425f7 refactor(core): use patched timers in root zone for zoneless scheduler (#55367)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-next.4+sha-f09c5a7";
+  var SW_VERSION = "18.0.0-next.4+sha-e6425f7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
