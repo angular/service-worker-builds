3a282d96e7726535d8e1bdd5e895fd2786db14dd
Andrew Kushnir <akushnir@google.com>
2024-04-25 15:55:04 +0000

    56e085f3f8 build: remove Windows CI check (#52140) (#55527)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.12+sha-499f09a";
+  var SW_VERSION = "16.2.12+sha-56e085f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
