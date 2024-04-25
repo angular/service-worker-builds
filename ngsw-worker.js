21dff1f73aa0ee2d5dc307987e71cd38384efafc
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-04-25 15:30:48 +0000

    544aede release: cut the v17.3.6 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.5+sha-add11c6";
+  var SW_VERSION = "17.3.6+sha-544aede";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
