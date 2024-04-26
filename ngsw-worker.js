0812ab9e9b5bb4cc4e8209b4ed7dfbfbec2a65f2
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:37:20 +0000

    91b1f24 fix(migrations): resolve multiple structural issues with HttpClient migration (#55557)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.1.0-next.0+sha-5b4970b";
+  var SW_VERSION = "18.1.0-next.0+sha-91b1f24";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
