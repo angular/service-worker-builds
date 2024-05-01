556855ea66fe726226435f15f59a5b200e0857c1
Andrew Kushnir <akushnir@google.com>
2024-05-01 19:00:58 +0000

    b9a997c refactor(core): Remove STOP_PROPAGATION define. (#55582)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.1.0-next.0+sha-874d332";
+  var SW_VERSION = "18.1.0-next.0+sha-b9a997c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
