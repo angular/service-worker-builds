3c6ddf875fe16ef4813a3ecefeda2def6f16bf02
Andrew Kushnir <akushnir@google.com>
2024-05-03 18:26:24 +0000

    0a8f6a7 ci: use application builder for integration tests (#55660)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-rc.0+sha-a97ac32";
+  var SW_VERSION = "18.0.0-rc.0+sha-0a8f6a7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
