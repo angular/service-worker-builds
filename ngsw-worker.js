8d68d1a8c317607d4b2c4f62c00b1f9dc08376b3
Andrew Scott <atscott@google.com>
2024-05-13 16:42:23 +0000

    eb31f2c refactor(core): Remove custom event and replay behavior. (#55695)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.0-rc.1+sha-7c9d37d";
+  var SW_VERSION = "18.0.0-rc.1+sha-eb31f2c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
