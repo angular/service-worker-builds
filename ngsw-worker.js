99610fc68df9d51f344abdb44d3a33380641676f
Andrew Scott <atscott@google.com>
2024-05-13 18:26:24 +0000

    71f8bca build: update io_bazel_rules_sass digest to fcce061 (#55755)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.3.8+sha-84f70cc";
+  var SW_VERSION = "17.3.8+sha-71f8bca";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
