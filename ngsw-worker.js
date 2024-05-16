961681a426790a270356644273b80ea3ef4cc385
Andrew Scott <atscott@google.com>
2024-05-16 16:26:36 +0000

    20a530a fix(zone.js): correctly bundle `zone-patch-rxjs` (#55826)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.1.0-next.0+sha-f3b0e2f";
+  var SW_VERSION = "18.1.0-next.0+sha-20a530a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
