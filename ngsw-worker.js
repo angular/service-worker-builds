7a3db4941bce8f59ac7ed41d42a6609a6ed1c052
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:29:45 +0000

    c698668 docs: language-service - Fix quick info URLs (#56416)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.1.0-next.2+sha-ef2ad1b";
+  var SW_VERSION = "18.1.0-next.2+sha-c698668";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
