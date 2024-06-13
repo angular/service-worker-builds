9daed70fbd04c6740491f7e06a2e2a64b3b84c08
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:05:44 +0000

    7c76eb5 refactor: add select scenario to the js-web-frameworks benchmark (#56362)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.0.3+sha-be09f0b";
+  var SW_VERSION = "18.0.3+sha-7c76eb5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
