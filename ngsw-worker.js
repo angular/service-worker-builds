077e69f2d6424caa87cd8f8b7483aebe6092959c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 16:09:56 +0000

    d5ce323 docs(docs-infra): add missing context with link (#56896)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.0-next.0+sha-f370f64";
+  var SW_VERSION = "18.2.0-next.0+sha-d5ce323";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
