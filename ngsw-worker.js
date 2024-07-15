3690c868915a1ab7ebdddc293c32bd753e93d6d0
Andrew Scott <atscott@google.com>
2024-07-15 18:13:56 +0000

    38b9320 refactor(compiler-cli): Extract call signatures from interfaces. (#56973)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.0-next.0+sha-8f08d04";
+  var SW_VERSION = "18.2.0-next.0+sha-38b9320";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
