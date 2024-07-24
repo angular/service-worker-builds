6ae9cfb8a31a0bd2067474eb4dd3669c06edd85c
Andrew Scott <atscott@google.com>
2024-07-24 17:33:46 +0000

    3d0ee73 docs(docs-infra): Use shiki for code highlighting (#57059)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.1.1+sha-8bb5937";
+  var SW_VERSION = "18.1.1+sha-3d0ee73";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
