296d0f61ed56fc81027bbad2b2c44d4846dc5324
Andrew Scott <atscott@google.com>
2024-07-24 17:23:52 +0000

    d5fbe11 docs(docs-infra): disable experimental decorators (#56257)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.0-next.1+sha-0e439d7";
+  var SW_VERSION = "18.2.0-next.1+sha-d5fbe11";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
