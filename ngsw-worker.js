72ebebca821716b6810d8450c24e19b0cdf9958a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 15:17:05 +0000

    6c76c91 feat(router): Add defaultQueryParamsHandling to router configuration (#57198)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.0-next.3+sha-2a915d1";
+  var SW_VERSION = "18.2.0-next.3+sha-6c76c91";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
