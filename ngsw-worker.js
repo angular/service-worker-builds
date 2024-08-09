2b12d0fc80faa50bcbceccb25bb3a2864d899dc3
Andrew Kushnir <akushnir@google.com>
2024-08-09 15:14:43 +0000

    f271021 feat(router): Add `routerOutletData` input to `RouterOutlet` directive (#57051)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.3.0-next.0+sha-769b6e1";
+  var SW_VERSION = "19.0.0-next.0+sha-f271021";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
