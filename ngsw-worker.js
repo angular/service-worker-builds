6c87e085c1dc9fd2540c08431d4647a834331704
Andrew Kushnir <akushnir@google.com>
2024-08-13 19:16:50 +0000

    e30c60e refactor(core): Add experimental support to have one event contract when there are multiple apps on the page. (#57355)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.0-rc.0+sha-b1a9d0f";
+  var SW_VERSION = "18.2.0-rc.0+sha-e30c60e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
