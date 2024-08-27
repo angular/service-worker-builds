05439a5039e7541b96be09307abb3fe34b53f046
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 20:04:01 +0000

    3067633 docs(docs-infra): Add query as an url query param to the api reference (#57062)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.1+sha-03b3527";
+  var SW_VERSION = "18.2.1+sha-3067633";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
