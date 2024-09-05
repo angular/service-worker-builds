792fb327d4b9d7fc67b1a4de91eacb8d0e371c30
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 18:03:36 +0000

    af4e39a docs: update template in-depth guide (#57676)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.3+sha-3bce676";
+  var SW_VERSION = "18.2.3+sha-af4e39a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
