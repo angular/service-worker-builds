d74de393f4ab45f69f64d9f55237c6b8b7425324
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-12 08:59:19 +0000

    6c8fd30 docs: add section on removing Zone.js imports/dependency from the build (#57784)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.5+sha-2b3e7f8";
+  var SW_VERSION = "19.0.0-next.5+sha-6c8fd30";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
