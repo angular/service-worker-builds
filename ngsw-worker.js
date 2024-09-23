8a1016e1330d779f47326116edd57a5976d86532
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:15:33 +0000

    3cc40f3 refactor(devtools): provide user with suggestions on why their angular application is not being detected in dev mode (#57861)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.5+sha-6092015";
+  var SW_VERSION = "18.2.5+sha-3cc40f3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
