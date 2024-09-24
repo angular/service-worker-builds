4afd2f77b379dab5a06d40a88b84cc02af384d7b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 13:39:14 +0000

    9dbe6fc refactor: update license text to point to angular.dev (#57901)

--- ngsw-config.js
+++ ngsw-config.js
@@ -162,6 +162,6 @@ var gen = new Generator(filesystem, baseHref);
  * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
+ * found in the LICENSE file at https://angular.dev/license
  */
 //# sourceMappingURL=ngsw_config.js.map
