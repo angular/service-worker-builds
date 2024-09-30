a8bd310d4afddc6a78c06093339473b7448b5740
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-30 19:58:07 +0000

    add5c25 docs: set expiry date to the developer survey (#57981)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.6+sha-69b7cf5";
+  var SW_VERSION = "18.2.6+sha-add5c25";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
