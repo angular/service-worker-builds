f440ba37c5c484fc5f09fc87970696f8c9489104
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 15:47:18 +0000

    2d11314 refactor(migrations): improve statistic tracking of signal input migration (#58137)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.8+sha-e00775a";
+  var SW_VERSION = "19.0.0-next.8+sha-2d11314";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
