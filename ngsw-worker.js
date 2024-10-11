e6e66863f95c20d08550a193f8002019f540183c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 10:44:20 +0000

    b198593 build: update io_bazel_rules_sass digest to 583fc8c (#58155)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.9+sha-fc6c76a";
+  var SW_VERSION = "19.0.0-next.9+sha-b198593";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
