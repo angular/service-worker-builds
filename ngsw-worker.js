6b7c4a42e41753b7cc06aaf0b50cee62401bc16c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 10:21:01 +0000

    d1cfbb1 docs: removed typos of withHttpTransferCacheOptions description (#58244)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1029,7 +1029,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "18.2.8+sha-b0ab653";
+  var SW_VERSION = "18.2.8+sha-d1cfbb1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
