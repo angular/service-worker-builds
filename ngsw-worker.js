238f2270ecbc159767e73ae675c3cd954cc164be
Andrew Kushnir <akushnir@google.com>
2024-10-21 19:11:17 +0000

    9544930 refactor(core): add incremental hydration infrastructure (#58193)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.10+sha-2e3e9b1";
+  var SW_VERSION = "19.0.0-next.10+sha-9544930";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
