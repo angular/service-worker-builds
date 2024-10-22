5e0063c08f1850468d91a538457226d8b8781a6d
Andrew Kushnir <akushnir@google.com>
2024-10-22 21:10:52 +0000

    1f45338 Revert "fix(forms): fix FormRecord type inference (#50750)" (#58315)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.10+sha-888657a";
+  var SW_VERSION = "19.0.0-next.10+sha-1f45338";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
