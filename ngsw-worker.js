e7d807ba7707cb2bbeb298547d0546204695ed21
Andrew Kushnir <akushnir@google.com>
2024-10-22 19:34:06 +0000

    888657a Revert "refactor(migrations): support parallel tsurge metadata merging (#58280)" (#58313)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.10+sha-1e9328e";
+  var SW_VERSION = "19.0.0-next.10+sha-888657a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
