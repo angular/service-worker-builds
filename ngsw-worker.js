bf42c1b2256d69e2a768a8a588676232e4cca5e6
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-10-23 20:27:51 +0000

    f3a028c docs: release notes for the v19.0.0-next.11 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.10+sha-2d8ae84";
+  var SW_VERSION = "19.1.0-next.0+sha-f3a028c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
