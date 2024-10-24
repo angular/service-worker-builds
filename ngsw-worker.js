7ddfe4eb7823e812db7753390e44f57a14ab27d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:30:58 +0000

    486c5a9 Update queries.md (#58330)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.11+sha-aadcfda";
+  var SW_VERSION = "19.0.0-next.11+sha-486c5a9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
