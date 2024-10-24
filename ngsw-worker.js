142083ca5a4b23b8729e15f7a6cef5ebf60297ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 19:51:09 +0000

    aadcfda refactor(language-service): support standalone by default (#58238)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.11+sha-f418b49";
+  var SW_VERSION = "19.0.0-next.11+sha-aadcfda";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
