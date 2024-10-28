45e58c7ac9318a9ae294824d61b86db57a0ee35f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 19:58:31 +0000

    7d9b38e docs: update Angular CLI help [main] (#58396)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.0+sha-db467e1";
+  var SW_VERSION = "19.1.0-next.0+sha-7d9b38e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
