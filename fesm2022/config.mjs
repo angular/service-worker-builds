a7fc7b9288fca6644e557cb2479a9865bdcfc9b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 19:47:23 +0000

    f815d7b refactor(core): Additional cleanup for incremental hydration (#58394)

--- fesm2022/config.mjs
+++ fesm2022/config.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.0.0-next.11+sha-395cb34
+ * @license Angular v19.0.0-next.11+sha-f815d7b
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -90,6 +90,8 @@ const DEFAULT_NAVIGATION_URLS = [
  * @publicApi
  */
 class Generator {
+    fs;
+    baseHref;
     constructor(fs, baseHref) {
         this.fs = fs;
         this.baseHref = baseHref;
