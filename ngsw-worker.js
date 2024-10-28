b0602581845143832ea45225344edc22e6244a0d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-28 16:33:24 +0000

    0f2f7ec refactor(core): cleanup incremental hydration code (#58363)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1023,7 +1023,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.0+sha-35d7ca5";
+  var SW_VERSION = "19.1.0-next.0+sha-0f2f7ec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
