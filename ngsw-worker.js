548a2066a3add5fe0bbe9236140d627a58081a86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 20:26:52 +0000

    ee1d06c release: cut the v19.0.0-rc.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-next.11+sha-dcd16b4";
+  var SW_VERSION = "19.0.0-rc.0+sha-ee1d06c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
