41876c30b10a198b620f471764e94ac2af9927ec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-03 18:13:16 +0000

    6819d6a fix(migrations): flip the default standalone flag in route-lazy-loading migration (#58474)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.0+sha-7d0ba0c";
+  var SW_VERSION = "19.1.0-next.0+sha-6819d6a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
