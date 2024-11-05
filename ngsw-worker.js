430cf97d1c84c7c1640f174d3aecf81715bf7ba4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-05 10:39:47 +0000

    c36a1c0 fix(platform-browser): correctly add external stylesheets to ShadowDOM components (#58482)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-rc.0+sha-90509f8";
+  var SW_VERSION = "19.0.0-rc.0+sha-c36a1c0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
