befd45ffbf6863a153430c9d851abfebab0a1e02
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 14:57:37 +0000

    64661dd ci: re-enable adev tests (#58574)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-rc.1+sha-b6a998b";
+  var SW_VERSION = "19.0.0-rc.1+sha-64661dd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
