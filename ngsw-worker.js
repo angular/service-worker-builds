ed0eaf1ce78e456d60b40ba06eb4220ecb9c4529
Andrew Kushnir <akushnir@google.com>
2024-11-15 17:57:00 +0000

    497fe97 docs: add incremental hydration guide (#58671)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-rc.3+sha-36a5712";
+  var SW_VERSION = "19.0.0-rc.3+sha-497fe97";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
