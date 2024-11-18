9e0699595a009319c1528295a6c3c45563340b3b
Andrew Kushnir <akushnir@google.com>
2024-11-18 20:56:33 +0000

    2c05f4c docs: add v19 developer event banner (#58721)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-rc.3+sha-805d8f1";
+  var SW_VERSION = "19.0.0-rc.3+sha-2c05f4c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
