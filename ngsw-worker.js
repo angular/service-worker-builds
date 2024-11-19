6199cc2bf628498c7d8733bd71e592f214d40f9a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-19 14:29:07 +0000

    e7566c3 release: cut the v19.0.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-rc.3+sha-b2802e5";
+  var SW_VERSION = "19.0.0+sha-e7566c3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
