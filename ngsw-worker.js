9edd81ef49d0e7f2d6b45aefec3e7d4c32061b8c
Andrew Kushnir <akushnir@google.com>
2024-11-19 00:09:43 +0000

    02b258a docs: update the angular roadmap for v19 (#58549)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.0-rc.3+sha-f696ecb";
+  var SW_VERSION = "19.0.0-rc.3+sha-02b258a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
