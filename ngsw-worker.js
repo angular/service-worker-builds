3272ac108452c07382e59bd6d83f4e80c12b5c36
Andrew Kushnir <akushnir@google.com>
2024-11-19 18:02:12 +0000

    b69b16c docs: update standalone migration guide (#58703)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.0+sha-284771d";
+  var SW_VERSION = "19.1.0-next.0+sha-b69b16c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
