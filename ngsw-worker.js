b58c25ae148747aebf9a7d09905b3b7713a8de7c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 15:03:31 +0000

    7fd034e build: remove unused patch (#58821)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.0+sha-aca8cc0";
+  var SW_VERSION = "19.1.0-next.0+sha-7fd034e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
