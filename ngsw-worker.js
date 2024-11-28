36baa847a03bd1831bc302eaf996d167eb170088
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 14:24:49 +0000

    e86b179 refactor(core): preventing resolving renderer factory every tick (#58618)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.1+sha-293dce9";
+  var SW_VERSION = "19.0.1+sha-e86b179";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
