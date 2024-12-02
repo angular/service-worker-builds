22569e88657a77cd823e1fe757a5b036ef5d42aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 15:23:49 +0000

    9a87aa3 docs(docs-infra): read jsdoctags from function overloads (#58994)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.1+sha-f72a1b1";
+  var SW_VERSION = "19.0.1+sha-9a87aa3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
