b2c064b8b0aaa16740b0eeb287719f012d94fdf5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 19:29:01 +0000

    acf9ab2 docs: release notes for the v19.0.3 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.1+sha-04e074b";
+  var SW_VERSION = "19.1.0-next.1+sha-acf9ab2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
