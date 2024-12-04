ab42d46ff6240d3bfc8aea05fa1b1410ab7f9eb8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 14:33:13 +0000

    7d2308f feat(docs-infra): move members descriptions section to the center of the page (#58610)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.1+sha-0df7b1e";
+  var SW_VERSION = "19.0.1+sha-7d2308f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
