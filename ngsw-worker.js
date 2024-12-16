a6e77ecf9a5885e1963cd97aea14a0bcf7a2f855
Andrew Kushnir <akushnir@google.com>
2024-12-16 18:33:18 +0000

    4d74f86 refactor(core): add skipped defer blocks count to ngDevMode (#59188)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.4+sha-3793218";
+  var SW_VERSION = "19.0.4+sha-4d74f86";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
