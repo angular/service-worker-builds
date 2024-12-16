8a742d1856bc8cca99617a9b8176dd7c275a267b
Andrew Kushnir <akushnir@google.com>
2024-12-16 23:32:47 +0000

    b476537 build: lock file maintenance (#59202)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.4+sha-b74e06e";
+  var SW_VERSION = "19.0.4+sha-b476537";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
