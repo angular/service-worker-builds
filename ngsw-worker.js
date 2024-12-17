cb927ff72f4a1e3244f8fecac04c508b6428a2da
Andrew Kushnir <akushnir@google.com>
2024-12-17 22:59:22 +0000

    c181903 refactor(core): reduce generated code (#59220)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.3+sha-8333ef7";
+  var SW_VERSION = "19.1.0-next.3+sha-c181903";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
