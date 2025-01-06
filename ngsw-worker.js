76f11898aeb23f8626d8d74d7986f6e14dfcef80
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 17:36:50 +0000

    cc7634f fix(bazel): handle module names and ES output in hybrid `ts_project` mode (#59325)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-next.4+sha-0bf6f76";
+  var SW_VERSION = "19.1.0-next.4+sha-cc7634f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
