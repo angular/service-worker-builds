66c4b5bd01317cec2b397a6f419ba9c07122a05c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 16:15:14 +0000

    8b73fc2 refactor: remove unnecessary `// tslint:disable` rule flags in devtools (#59365)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.5+sha-6d00efd";
+  var SW_VERSION = "19.0.5+sha-8b73fc2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
