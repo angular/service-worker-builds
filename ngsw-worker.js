ebb6cccb92a5b5025f80c082c87a6592482f5a10
kirjs <kirjs@users.noreply.github.com>
2025-01-09 18:21:08 +0000

    bfaeefe refactor(animations): drop warning functions in production (#59408)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-rc.0+sha-4491b5b";
+  var SW_VERSION = "19.1.0-rc.0+sha-bfaeefe";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
