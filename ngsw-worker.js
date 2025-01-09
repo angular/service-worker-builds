ec8c546e67b40aadddab749f7bf0d86260f5783f
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:38:42 +0000

    ac0a655 fix(devtools): fixing dark mode colors for devtools and router tree (#59329)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-rc.0+sha-a6a4347";
+  var SW_VERSION = "19.1.0-rc.0+sha-ac0a655";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
