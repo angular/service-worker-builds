dcde65ca041834d7d6ca31ed07852533c09a402b
kirjs <kirjs@users.noreply.github.com>
2025-01-13 16:07:50 +0000

    4866cbd refactor(compiler): fix typo in method name (#59479)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.0.6+sha-64f1bd7";
+  var SW_VERSION = "19.0.6+sha-4866cbd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
