5dbf5a8568b9eea4e7845182859225a9d94cfde5
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:34:35 +0000

    b44c020 docs: fix property wrong usage inside afterNextRender in lifecycle.md (#59521)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-rc.0+sha-0130298";
+  var SW_VERSION = "19.1.0-rc.0+sha-b44c020";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
