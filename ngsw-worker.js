245b2191468dd40e01de5ee1d458525225b96c8f
kirjs <kirjs@users.noreply.github.com>
2025-01-14 14:55:03 +0000

    f68e9cd docs: update component HMR to fully stable (#59503)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0-rc.0+sha-f1aba53";
+  var SW_VERSION = "19.1.0-rc.0+sha-f68e9cd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
