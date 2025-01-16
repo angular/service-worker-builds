51cbbf4f3698297696e09ecbe17aa26c676ddcdc
Andrew Kushnir <akushnir@google.com>
2025-01-16 18:47:11 +0000

    884e0b4 build: update cross-repo angular dependencies to v19.2.0-next.0 (#59571)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0-next.0+sha-01fffdb";
+  var SW_VERSION = "19.2.0-next.0+sha-884e0b4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
