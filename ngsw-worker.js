c6c94d7112e39e30beaaba3a90e91d197f611a7a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 10:19:11 +0000

    74daef1 refactor(core): move logic out of shared instructions code (#59453)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.0+sha-0d019e9";
+  var SW_VERSION = "19.1.0+sha-74daef1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
