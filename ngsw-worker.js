2a2e377af6af3254fd973d945fb8700355b97cc2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-17 09:51:34 +0000

    4e6017a test(platform-server): fix flaky incremental hydration test (#59584)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0-next.0+sha-8ea691a";
+  var SW_VERSION = "19.2.0-next.0+sha-4e6017a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
