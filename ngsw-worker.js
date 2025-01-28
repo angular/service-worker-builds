1f4c516a4c7102f6569a336fd350a5d609e41b64
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 08:46:50 +0000

    873870f refactor(common): drop error messages in production (#59545)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.3+sha-d0dc8f7";
+  var SW_VERSION = "19.1.3+sha-873870f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
