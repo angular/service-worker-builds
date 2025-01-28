29db58683d822eb20a628fdc0e206c4f58e3e0ac
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 12:00:40 +0000

    f3d6c49 ci: configure merge tool to enforce mandatory PR statuses (#59749)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.3+sha-3c28301";
+  var SW_VERSION = "19.1.3+sha-f3d6c49";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
