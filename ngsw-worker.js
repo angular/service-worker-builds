368ee7cc324a76d5eda4ede20708412ddbbdf56e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 23:17:57 +0000

    eb9dca3 docs: release notes for the v19.1.4 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0-next.0+sha-b5f9018";
+  var SW_VERSION = "19.2.0-next.0+sha-eb9dca3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
