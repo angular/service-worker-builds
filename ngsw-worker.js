1f885390118e6be6e2c718dff964453be371becb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-19 19:09:46 +0000

    11ea0a0 release: cut the v19.2.0-rc.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0-next.3+sha-497028c";
+  var SW_VERSION = "19.2.0-rc.0+sha-11ea0a0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
