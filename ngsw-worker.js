1849c08a469e5e09df8b80915fb40d017db0e1c5
kirjs <kirjs@users.noreply.github.com>
2025-02-20 17:43:21 +0000

    20f806a refactor(core): drop platform check in `ImagePerformanceWarning` (#59809)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.7+sha-1dc5c39";
+  var SW_VERSION = "19.1.7+sha-20f806a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
