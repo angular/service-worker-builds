4ec59283fd2e61f1d21b9740d59e5f766c2764d6
kirjs <kirjs@users.noreply.github.com>
2025-02-20 17:03:05 +0000

    c513e5d refactor(devtools): fix component inspector highlighting (#59995)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.3.0-next.0+sha-7a8e8f3";
+  var SW_VERSION = "19.3.0-next.0+sha-c513e5d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
