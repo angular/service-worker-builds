c5935a34dd83bce1d479f8df43ac118387f3fd7d
kirjs <kirjs@users.noreply.github.com>
2025-02-25 16:06:21 +0000

    ef17371 refactor(core): add infrastructure for setting inputs on specific directives (#60075)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0-rc.0+sha-d5f467e";
+  var SW_VERSION = "19.2.0-rc.0+sha-ef17371";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
