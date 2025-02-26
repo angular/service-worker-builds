18d41b2c4a0259f5b70586bb3c54879d46611906
kirjs <kirjs@users.noreply.github.com>
2025-02-26 21:25:52 +0000

    e012cf0 build: add ignore revs file for git blame (#60117)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0+sha-8e9463b";
+  var SW_VERSION = "19.2.0+sha-e012cf0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
