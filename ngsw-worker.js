2dcc1c8bb48e8615aac1fdd19953a17ad09eec25
Kirill Cherkashin <kirill@kcherkashin.org>
2025-02-26 19:18:52 +0000

    a8a3534 release: cut the v19.1.8 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.7+sha-41ef1cb";
+  var SW_VERSION = "19.1.8+sha-a8a3534";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
