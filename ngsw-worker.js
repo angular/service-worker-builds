7d320366d22656a60962334d5cf613a77544f001
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-03-05 10:11:32 +0000

    a2db87e docs(docs-infra): fix secondary menu (#60212)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.0+sha-a64a575";
+  var SW_VERSION = "19.2.0+sha-a2db87e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
