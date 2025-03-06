e5ede1d29233953284e106ad7dd7de572a64932b
Andrew Kushnir <akushnir@google.com>
2025-03-06 18:48:45 +0000

    48dc0d6 refactor(language-service): rename internal symbols to accommodate type checking outside a template (#60191)

--- fesm2022/config.mjs
+++ fesm2022/config.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.1+sha-dad02c6
+ * @license Angular v19.2.1+sha-48dc0d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -238,12 +238,6 @@ function buildCacheQueryOptions(inOptions) {
         ...inOptions,
     };
 }
-
-// This file is not used to build this module. It is only used during editing
-
-/**
- * Generated bundle index. Do not edit.
- */
 
 export { Generator };
 //# sourceMappingURL=config.mjs.map
