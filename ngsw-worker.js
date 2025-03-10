c253e6e9a2589449a73cec08847fdde071624fe0
Andrew Kushnir <akushnir@google.com>
2025-03-10 17:07:57 +0000

    23ca885 fix(compiler-cli): handle transformed classes when generating HMR code (#60298)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.1+sha-44155c9";
+  var SW_VERSION = "19.2.1+sha-23ca885";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
