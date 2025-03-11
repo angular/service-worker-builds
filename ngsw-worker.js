c3f8688889f0abd00ddd94ecc30ba95ddc3818b9
Andrew Kushnir <akushnir@google.com>
2025-03-11 21:33:57 +0000

    705fb0d docs: add mention of GH issue for devPreview feedback (#60010)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.1+sha-b6f22a7";
+  var SW_VERSION = "19.2.1+sha-705fb0d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
