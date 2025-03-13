02d52e144385ff8fb150b4bff3d81442fea11592
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 14:58:33 +0000

    8b888b2 docs: note that CSS is also taken into account in the max bundle size (#60362)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.2+sha-4a30a4e";
+  var SW_VERSION = "19.2.2+sha-8b888b2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
