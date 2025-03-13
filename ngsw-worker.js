fcf8e012e24ab5f5b2225316f6710dda669e032d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 16:11:19 +0000

    55ba1ed docs: added short explanation about event reply (#60349)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.2+sha-7a39585";
+  var SW_VERSION = "20.0.0-next.2+sha-55ba1ed";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
