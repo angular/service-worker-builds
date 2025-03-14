8d8b7163fcc53adc6164cafb0d0d996973095c97
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 10:24:33 +0000

    f030409 refactor(router): Clean up the transition subject (#60357)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.2+sha-8551cab";
+  var SW_VERSION = "19.2.2+sha-f030409";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
