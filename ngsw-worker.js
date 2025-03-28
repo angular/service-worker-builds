ddfd4e0273352bd4eafe40afad642f98aa61b947
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 15:12:05 +0000

    152261c refactor(core): produce a message about `@defer` behavior when HMR is enabled (#60533)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.4+sha-b21c6e5";
+  var SW_VERSION = "20.0.0-next.4+sha-152261c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
