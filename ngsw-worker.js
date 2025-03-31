bbf60a66450389f950fb3d083cfc98fe988fbe8e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 19:29:15 +0000

    be7c8d4 refactor(forms): convert scripts within `packages/forms` to relative imports (#60624)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.4+sha-3767429";
+  var SW_VERSION = "20.0.0-next.4+sha-be7c8d4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
