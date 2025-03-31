6614974bde888fb47a4469b351d5292431a1c243
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:54:28 +0000

    4387b37 refactor(router): drop special handling of the OutletInjector (#58351)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.4+sha-3855730";
+  var SW_VERSION = "19.2.4+sha-4387b37";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
