3ba2393eb5c34c5d1dd022d198ac36c0ad4dd8b8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 16:40:27 +0000

    84ca0ef refactor: added typing for Response (#60541)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.4+sha-4f86f3f";
+  var SW_VERSION = "20.0.0-next.4+sha-84ca0ef";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
