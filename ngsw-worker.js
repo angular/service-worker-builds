a4255d059b841d8fdf6078d99bbd9fd12b54f83a
Andrew Scott <atscott@google.com>
2025-04-07 17:37:33 +0000

    ae2fc18 docs: fix typo in documentation for pendingUntilEvent (#60756)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.5+sha-00bbd9b";
+  var SW_VERSION = "19.2.5+sha-ae2fc18";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
