d855f3919bdea6b969ee59fbbcc2b29b723847a5
Andrew Scott <atscott01@gmail.com>
2025-04-09 20:08:59 +0000

    ea58402 release: cut the v20.0.0-next.6 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.5+sha-c3113b9";
+  var SW_VERSION = "20.0.0-next.6+sha-ea58402";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
