272623beaf9282507bcf97db1d0653d07d03bcda
kirjs <kirjs@users.noreply.github.com>
2025-04-14 13:28:46 +0000

    c5ca597 refactor(devtools): switch to components tab whenever the inspector is used (#60694)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.6+sha-c0ef923";
+  var SW_VERSION = "20.0.0-next.6+sha-c5ca597";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
