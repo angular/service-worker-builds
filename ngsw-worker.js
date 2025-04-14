07eb21882b7aa91f4473c19aa1f4690e51c2959b
kirjs <kirjs@users.noreply.github.com>
2025-04-14 15:08:26 +0000

    c80f24d docs: fix missing card-title tag in example of fallback-content section (#60855)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.6+sha-c0e4ff1";
+  var SW_VERSION = "19.2.6+sha-c80f24d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
