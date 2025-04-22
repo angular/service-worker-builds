96b3e2218d45ea6ad9c5112d230f28df4c94a673
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 19:52:50 +0000

    3824519 docs: replace EventEmitter with output and self-closing tags in templates (#60910)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.7+sha-adac95c";
+  var SW_VERSION = "19.2.7+sha-3824519";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
