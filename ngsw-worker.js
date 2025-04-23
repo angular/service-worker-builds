5d23347e1eb4ebb8c00e524c93639ede042e2b4c
Miles Malerba <mmalerba@google.com>
2025-04-23 17:56:18 +0000

    57794f0 Revert "refactor(router): produce error message when `canMatch` is used with `redirectTo` (#60958)" (#60989)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.8+sha-3331b10";
+  var SW_VERSION = "20.0.0-next.8+sha-57794f0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
