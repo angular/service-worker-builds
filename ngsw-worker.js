bb1572eed47e3b5d50898503db8ed34213a466c9
Miles Malerba <mmalerba@google.com>
2025-04-24 16:51:43 +0000

    e6c14f9 docs: remove old pipes docs. (#60996)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.8+sha-c6b41e1";
+  var SW_VERSION = "20.0.0-next.8+sha-e6c14f9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
