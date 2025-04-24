701332a3242434dc53a601caf4ac4bb3c17e6917
Miles Malerba <mmalerba@google.com>
2025-04-24 16:40:00 +0000

    9e3665a docs(docs-infra): fix github links for for previews (#60994)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.8+sha-5b0ec4a";
+  var SW_VERSION = "19.2.8+sha-9e3665a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
