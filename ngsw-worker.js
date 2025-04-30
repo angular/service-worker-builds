06755d897b97c9e327fb6787620f0ff7c1c052c9
Andrew Kushnir <akushnir@google.com>
2025-04-30 19:50:20 +0000

    9b96fcc refactor(service-worker): drop error messages in production (#59702)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.1.0-next.0+sha-c1e46c5";
+  var SW_VERSION = "20.1.0-next.0+sha-9b96fcc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
