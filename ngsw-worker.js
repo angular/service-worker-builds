5ef8b2e9ea98e2f2955f05d2e0a4fb704ff1ecf7
Miles Malerba <mmalerba@google.com>
2025-04-30 17:38:31 +0000

    829379e docs: release notes for the v19.2.9 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.0.0-next.8+sha-bb00b93";
+  var SW_VERSION = "20.0.0-next.8+sha-829379e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
