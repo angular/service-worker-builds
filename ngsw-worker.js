65aa46242218c3701ab452b1326bd4edd7d6a106
Andrew Kushnir <akushnir@google.com>
2025-05-06 18:13:52 +0000

    c39e799 Revert "docs(docs-infra): fix first-app intro example (#61042)" (#61145)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.1.0-next.0+sha-c3ff01b";
+  var SW_VERSION = "20.1.0-next.0+sha-c39e799";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
