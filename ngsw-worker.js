ba7dc3847dbabbfba24df94583f4ea34c218ff87
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-05-07 16:31:46 +0000

    4282488 release: cut the v19.2.10 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.2.9+sha-c081e04";
+  var SW_VERSION = "19.2.10+sha-4282488";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
