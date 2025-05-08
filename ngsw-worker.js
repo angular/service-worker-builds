761f93a9997580bc52e2f679f3629e4b0c7559d1
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:37:40 +0000

    50993be ci: add workflow to sync non-default branches (#61201)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "20.1.0-next.0+sha-0558575";
+  var SW_VERSION = "20.1.0-next.0+sha-50993be";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
