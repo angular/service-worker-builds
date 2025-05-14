357fc6f3b94831b9f782d15eae31348bc65717aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 21:15:43 +0000

    e62fb35 feat(compiler-cli): add experimental support for fast type declaration emission (#61334)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.0+sha-68d774f";
+  var SW_VERSION = "20.1.0-next.0+sha-e62fb35";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
