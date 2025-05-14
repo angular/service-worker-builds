15459b2ea1b41509055f7aa8407d9d79768c1591
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 00:23:55 +0000

    d4cb444 build: update cross-repo angular dependencies (#61287)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.0-rc.0+sha-41db650";
+  var SW_VERSION = "20.0.0-rc.0+sha-d4cb444";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
