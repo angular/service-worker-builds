db0d2aee6eeb04dd80948404ce2de96f33363816
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:23:14 +0000

    85e0baf docs: fix Routed components example (#61278)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "19.2.11+sha-8edafd0";
+  var SW_VERSION = "19.2.11+sha-85e0baf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
