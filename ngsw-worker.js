dd87e674ed78c1908cc6aecd00b5f867cc369d38
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:17:17 +0000

    8f9b05e fix(core): cleanup testability subscriptions (#61261)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "19.2.11+sha-0104a69";
+  var SW_VERSION = "19.2.11+sha-8f9b05e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
