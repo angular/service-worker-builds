7b472484947bcc71dda55b7b3c14404967e1c222
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:15:14 +0000

    3a8bbe9 build: migrate language service to use rules_js (#61578)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "19.2.11+sha-fc2483e";
+  var SW_VERSION = "19.2.11+sha-3a8bbe9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
