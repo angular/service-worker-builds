cb7ea09bccea613929b4dcf5c8b8e500d9958ebc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 10:16:56 +0000

    ef01d3c refactor(core): Merge R3TemplateRef implementation and TemplateRef interface (#61455)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.0+sha-7f6bb91";
+  var SW_VERSION = "20.1.0-next.0+sha-ef01d3c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
