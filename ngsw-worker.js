37560b7975ed47378459945c15c935f501dcfab4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 14:00:10 +0000

    1123b31 refactor(core): consolidate TNode creation checks (#61563)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.0+sha-e7656b8";
+  var SW_VERSION = "20.1.0-next.0+sha-1123b31";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
