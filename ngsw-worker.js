2352aaccfa47ca813d7a63035ec4a647b6bfc2ff
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 09:25:17 +0000

    3cc0910 build: update compiler-cli to not be stamped when used for the compiler in ng_project (#61535)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.0-rc.1+sha-0a82e58";
+  var SW_VERSION = "20.0.0-rc.1+sha-3cc0910";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
