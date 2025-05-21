6fc00ec4c2c6141c81df549c42e6cb740130a71b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:47:00 +0000

    b6d7b4b build: remove the "restamping" substitutions from adev shared-docs package (#61585)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "19.2.11+sha-6f6e91d";
+  var SW_VERSION = "19.2.11+sha-b6d7b4b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
