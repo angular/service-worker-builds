56bad17813ee0685024ad18ba0a675e6b7651e35
Andrew Scott <atscott@google.com>
2025-05-22 03:51:38 +0000

    5f1bd07 fix(zone.js): classes that extend Error should retain cause property (#61599)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "19.2.12+sha-2f5912e";
+  var SW_VERSION = "19.2.12+sha-5f1bd07";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
