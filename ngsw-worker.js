3de5df281acc1f2768c2a77d611d3936c5287ef2
Andrew Scott <atscott@google.com>
2025-05-27 18:41:04 +0000

    c77e198 docs(docs-infra): limit file interactions on tutorials. (#61696)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.0+sha-c11527f";
+  var SW_VERSION = "20.1.0-next.0+sha-c77e198";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
