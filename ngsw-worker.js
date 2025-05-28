2bd74f6921f29420a3a931bf7e3aec3225763e3e
Andrew Scott <atscott01@gmail.com>
2025-05-28 14:00:46 +0000

    40e2b5b release: cut the v20.0.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.0-rc.3+sha-0502cf2";
+  var SW_VERSION = "20.0.0+sha-40e2b5b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
