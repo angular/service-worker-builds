32c7d8df155ee06faf2da49e6e4e35ccd0f72c3d
Andrew Scott <atscott@google.com>
2025-05-28 19:35:46 +0000

    a0d599e docs: link to v20 blog post in changelog (#61734)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.0+sha-aed8a77";
+  var SW_VERSION = "20.0.0+sha-a0d599e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
