86ed3dc6be30fa8f1e2b2eae6d648018a412f11b
Andrew Scott <atscott@google.com>
2025-05-28 18:19:06 +0000

    2a290ab docs: update versions.json with v20 (#61732)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.0+sha-449c540";
+  var SW_VERSION = "20.1.0-next.0+sha-2a290ab";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
