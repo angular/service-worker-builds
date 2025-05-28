356b1b1fb32eaf7c1be29148affb65f1d1454d04
Andrew Scott <atscott@google.com>
2025-05-28 17:11:31 +0000

    55306bd docs: update tutorials and playground to v20 (#61728)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2d99d9656325/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.0+sha-40e2b5b";
+  var SW_VERSION = "20.0.0+sha-55306bd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
