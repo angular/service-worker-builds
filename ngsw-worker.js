0a9f6177a937c7f752e4c81b8dbe66e264514ff7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 12:58:45 +0000

    1e8158b fix(core): components marked for traversal resets reactive context (#61663)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.1+sha-fdcb14d";
+  var SW_VERSION = "20.0.1+sha-1e8158b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
