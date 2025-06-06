e7d67482a3fb00c8e362169996c5fe60b463d4a8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 11:28:47 +0000

    0d198f2 docs: update first-app tutorial to signal inputs (#61876)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.1+sha-a6a193f";
+  var SW_VERSION = "20.0.1+sha-0d198f2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
