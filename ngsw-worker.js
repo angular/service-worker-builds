dce59357c27055e2d89f8b09559041449649817e
Andrew Kushnir <akushnir@google.com>
2025-06-10 21:51:46 +0000

    b9141da refactor(devtools): use import type to improve performance (#61966)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.0+sha-4f0221e";
+  var SW_VERSION = "20.1.0-next.0+sha-b9141da";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
