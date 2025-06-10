4555901d6de32996537afd40576518b4bee1dee7
Andrew Kushnir <akushnir@google.com>
2025-06-10 23:02:59 +0000

    eef6faf ci: update to latest version of dev-infra actions (#61990)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.2+sha-9d6db41";
+  var SW_VERSION = "20.0.2+sha-eef6faf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
