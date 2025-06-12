fcb6ec546d064c815ce8636e1e9878089e478beb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 11:05:59 +0000

    8424b3b fix(core): Fixes template outlet hydration (#61989)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.1+sha-61fb6e7";
+  var SW_VERSION = "20.1.0-next.1+sha-8424b3b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
