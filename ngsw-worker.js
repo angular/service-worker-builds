af915703929409ab2caab381112a1ad163ba6c72
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-13 10:45:03 +0000

    9b33089 docs: remove developer preview hint for withI18nSupport (#62039)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.3+sha-67f657e";
+  var SW_VERSION = "20.0.3+sha-9b33089";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
