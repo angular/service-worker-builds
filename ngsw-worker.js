a643570366e5298a7d042e24e5a71814ff265be1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-16 09:40:14 +0000

    deaa292 build: lock file maintenance (#62063)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.3+sha-8403bc7";
+  var SW_VERSION = "20.0.3+sha-deaa292";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
