e9d1d2602c63048dcca68d0c973505741bfe29b6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 17:03:25 +0000

    18a7ce8 build: migrate back to using the @npm// sourced benchmark_test macro (#62101)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.1+sha-2e0c98b";
+  var SW_VERSION = "20.1.0-next.1+sha-18a7ce8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
