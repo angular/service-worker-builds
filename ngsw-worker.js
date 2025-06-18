06e0313514951212a2f10077ca1f609721f83825
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 07:22:35 +0000

    24b4484 build: update cross-repo angular dependencies (#62080)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.3+sha-04e0d4f";
+  var SW_VERSION = "20.0.3+sha-24b4484";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
