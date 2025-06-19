84c4a1e1c392c41bbf496f5582c0f1118c4e4bd7
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 11:22:45 +0000

    8752014 docs: Update define-routes.md (#62069)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.4+sha-f042cb3";
+  var SW_VERSION = "20.0.4+sha-8752014";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
