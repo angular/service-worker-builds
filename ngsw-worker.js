d9835f8acc5ff0f776532c0267ad1ea312ac3809
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 08:19:48 +0000

    288700c docs(docs-infra): fix spelling mistake (#62126)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.4+sha-127120a";
+  var SW_VERSION = "20.0.4+sha-288700c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
