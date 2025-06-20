1fab31c65cdead9787fa01fc558b10feb3df7950
Andrew Kushnir <akushnir@google.com>
2025-06-20 20:17:07 +0000

    f11b3f9 docs: update Angular CLI help (#62166)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.2+sha-f8a90c9";
+  var SW_VERSION = "20.1.0-next.2+sha-f11b3f9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
