dd0e2ba99b2badc4c12e5d484be2b11df7ee5212
Andrew Kushnir <akushnir@google.com>
2025-06-22 16:25:49 +0000

    4c14503 Revert "docs: embeded best practices in page (#62173)" (#62193)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.2+sha-152ff0c";
+  var SW_VERSION = "20.1.0-next.2+sha-4c14503";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
