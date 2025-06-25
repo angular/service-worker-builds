6a978a3d5832b621c59c6410a5161fab98f0b79a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 13:32:45 +0000

    36a1931 fix(core): allow to set a resource in an error state (#62253)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.2+sha-1bf1fb2";
+  var SW_VERSION = "20.1.0-next.2+sha-36a1931";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
