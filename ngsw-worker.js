b4b2f79f4c895f3e704b92d8035b8cd67d786672
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 17:33:05 +0000

    31e3eb2 build: migrate to use web test runner rules (#62294)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.5+sha-8955ed2";
+  var SW_VERSION = "20.0.5+sha-31e3eb2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
