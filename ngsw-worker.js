30a69df80819cd3de52623b45aeb4d196ca0e1cb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 09:10:52 +0000

    19eaf71 docs: Update documentation httpResource with keepalive feature (#62278)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.5+sha-c39b80f";
+  var SW_VERSION = "20.0.5+sha-19eaf71";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
