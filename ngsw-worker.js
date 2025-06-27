8e26e2ca2467e80191c59593c02bfe2ae9e94eb0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:00:05 +0000

    6ec2900 docs: Update documentation httpResource with cache & priority feature (#62334)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-next.3+sha-7077520";
+  var SW_VERSION = "20.1.0-next.3+sha-6ec2900";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
