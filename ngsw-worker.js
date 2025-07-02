d138b3d8c9368a5b01531730d907bc3bc7596e88
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 16:18:18 +0000

    85b3ae2 build: migrate devtools to use packaged version of @angular/* packages (#62413)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.0+sha-0cdb02a";
+  var SW_VERSION = "20.2.0-next.0+sha-85b3ae2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
