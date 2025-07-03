802f838aff393e041ac620deaa5b904bd06c106d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 12:00:19 +0000

    2943591 docs: improve support for decorator APIs (#60411)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-rc.0+sha-fca058a";
+  var SW_VERSION = "20.1.0-rc.0+sha-2943591";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
