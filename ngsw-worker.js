1005de23fdb5c62038c7c5e044758b273be66e80
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 18:37:54 +0000

    2f45910 build: disable firefox tests in devtools (#62504)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-rc.0+sha-bf23414";
+  var SW_VERSION = "20.1.0-rc.0+sha-2f45910";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
