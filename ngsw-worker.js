3fab2040ea13e28ca90abe08e7c6afbb51e13410
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-07-07 11:10:03 +0000

    5fca07c refactor(compiler-cli): ensure mock file system can be loaded in the browser (#62493)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.0.6+sha-434508f";
+  var SW_VERSION = "20.0.6+sha-5fca07c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
