411e60ac6dd0262d87bfae89cf53f2fbc3264ee7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-07-08 11:39:27 +0000

    1a8297b refactor(compiler): fix import manager to be closure property renaming-compatible (#62517)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild/bin/packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0-rc.0+sha-efc6405";
+  var SW_VERSION = "20.1.0-rc.0+sha-1a8297b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
