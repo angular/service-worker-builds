cba1eec68919b081d285d96d8c274dc4c992a10b
Andrew Kushnir <akushnir@google.com>
2025-07-11 17:53:36 +0000

    02dce8e ci: add to public-api approvers list (#62591)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0+sha-4f1e88e";
+  var SW_VERSION = "20.1.0+sha-02dce8e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
