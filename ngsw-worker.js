5a3b16ec6495abe6016db9f9bd158140ea6d5efc
Andrew Kushnir <akushnir@google.com>
2025-07-14 23:01:32 +0000

    e8c5603 build: move all rule/macro loading into adev/defaults.bzl and fix intellisense loading (#62627)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.0+sha-eca2334";
+  var SW_VERSION = "20.2.0-next.0+sha-e8c5603";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
