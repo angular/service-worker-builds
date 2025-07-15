c8b6db7d1f0213f00bfb405c1d11d3308e760c5a
Andrew Kushnir <akushnir@google.com>
2025-07-15 16:44:16 +0000

    acf8b74 build: update all non-major dependencies (#62643)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0+sha-72605a4";
+  var SW_VERSION = "20.1.0+sha-acf8b74";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
