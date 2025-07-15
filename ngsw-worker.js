28fea128946687b76c08568e3659cda24fb91e85
Andrew Kushnir <akushnir@google.com>
2025-07-15 15:09:05 +0000

    72605a4 docs: fix missing 'of' in Style Guide sentence (#62628)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.0+sha-a81f0fa";
+  var SW_VERSION = "20.1.0+sha-72605a4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
