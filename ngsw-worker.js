4f51184badf7d5c6aadbddccaa6f348ce669277b
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:20:58 +0000

    f5f8f76 release: bump Angular DevTools version to 1.0.39 (#62670)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-a484a11";
+  var SW_VERSION = "20.2.0-next.1+sha-f5f8f76";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
