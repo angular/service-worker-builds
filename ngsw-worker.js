9581cee199903debfa3d156412250d14e6972e00
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:48:48 +0000

    0db3613 build: remove rules_nodejs based protractor tooling (#62649)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.1+sha-4b9385f";
+  var SW_VERSION = "20.1.1+sha-0db3613";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
