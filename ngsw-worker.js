53239a56eb6a0f269f693cb7556cd638554b59d3
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-07-16 18:19:27 +0000

    a484a11 release: cut the v20.2.0-next.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.0+sha-193257b";
+  var SW_VERSION = "20.2.0-next.1+sha-a484a11";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
