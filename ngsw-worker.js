f13fc5a3d72705149fee2ca0cbecbd0a6f6b032e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-16 10:49:20 +0000

    527bd77 fix(compiler-cli): infer type of event target for void elements (#62648)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.0+sha-18a6750";
+  var SW_VERSION = "20.2.0-next.0+sha-527bd77";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
