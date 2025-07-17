eb70a01b6a3ee020bc14425c42b3d4bae1c911ec
Kirill Cherkashin <kirill@kcherkashin.org>
2025-07-17 21:57:33 +0000

    6f52e97 docs: release notes for the v20.1.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-16a9bea";
+  var SW_VERSION = "20.2.0-next.1+sha-6f52e97";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
