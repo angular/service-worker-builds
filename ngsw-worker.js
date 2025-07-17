9810083b96ed8a26822644833acec92210c10a27
Kirill Cherkashin <kirjs@google.com>
2025-07-17 17:37:00 +0000

    2e6c2ff refactor(devtools): disable view source button if source function is not available (#62678)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-fc8247d";
+  var SW_VERSION = "20.2.0-next.1+sha-2e6c2ff";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
