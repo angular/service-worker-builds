c41a3306fffd8874101d598a7ec5e7406ec84b23
Kirill Cherkashin <kirjs@google.com>
2025-07-18 14:28:23 +0000

    c36de9e build: update cross-repo angular dependencies (#62651)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-409287a";
+  var SW_VERSION = "20.1.2+sha-c36de9e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
