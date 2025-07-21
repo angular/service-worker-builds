ef8ad38ef4b2b5dac23cddc4aeef294ec182422f
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:30:37 +0000

    a1a78b2 build: update all non-major dependencies (#62716)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-cf48b13";
+  var SW_VERSION = "20.1.2+sha-a1a78b2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
