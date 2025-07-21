cd66d7a3e3cdf8f65df253dd646b51d99460891e
Kirill Cherkashin <kirjs@google.com>
2025-07-21 17:47:15 +0000

    74e4778 docs: fix minor typos in styleguide (#62706) PR Close #62706

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-b388f1e";
+  var SW_VERSION = "20.1.2+sha-74e4778";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
