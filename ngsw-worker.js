1d51866c596cac55106adfed3f8aea7b5a28c490
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:26:09 +0000

    aee5260 fix(devtools): stop reseting currentlyMatchedIndex when a node is selected in the component explorer (#62727)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-70d0c10";
+  var SW_VERSION = "20.1.2+sha-aee5260";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
