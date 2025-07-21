9fd43921c91613e61c1dc09b525109a5b76e9882
Kirill Cherkashin <kirjs@google.com>
2025-07-21 13:45:46 +0000

    cf48b13 docs: fix code snippet (#62720)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-c36de9e";
+  var SW_VERSION = "20.1.2+sha-cf48b13";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
