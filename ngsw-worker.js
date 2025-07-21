6d94a86175da5393bc3768235a4c02fee4155907
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:16:36 +0000

    a6b2d7d fix(devtools): add event tagging to prevent DDOS (#62645)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-97b1bd1";
+  var SW_VERSION = "20.2.0-next.1+sha-a6b2d7d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
