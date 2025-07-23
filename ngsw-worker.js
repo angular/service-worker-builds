32710db42074ac3fdecc14be661940a1dcc8ce7b
Jan Martin <jan.krems@gmail.com>
2025-07-23 16:32:51 +0000

    a4a4f7f release: bump Angular DevTools version to 1.0.40 (#62767)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-7767aa6";
+  var SW_VERSION = "20.2.0-next.1+sha-a4a4f7f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
