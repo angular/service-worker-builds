31b913e9ab5113a82c6d96d6ac21f27c2613d17d
Kirill Cherkashin <kirjs@google.com>
2025-07-23 15:16:09 +0000

    2fd1f7b fix(platform-browser): resolve component resources before bootstrapping in JIT mode (#62758)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-b7c745b";
+  var SW_VERSION = "20.1.2+sha-2fd1f7b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
