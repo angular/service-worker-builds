9cb6093455e63483c449a36b9d5d331c33bc2ac8
Kirill Cherkashin <kirjs@google.com>
2025-07-23 11:33:11 +0000

    b7c745b docs: create MCP server setup guide (#62746)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.2+sha-91bdd17";
+  var SW_VERSION = "20.1.2+sha-b7c745b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
