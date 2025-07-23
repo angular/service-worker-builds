279fa7fbcb72b4daad82f7d917abae8bef3f3702
Kirill Cherkashin <kirjs@google.com>
2025-07-23 14:13:43 +0000

    882522c refactor(compiler): Tree shake Element Registry (#62682)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-cec91c0";
+  var SW_VERSION = "20.2.0-next.1+sha-882522c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
