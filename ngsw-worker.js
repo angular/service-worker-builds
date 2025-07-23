f547012632d603b3b6f97c777aca1a078c987ae0
Kirill Cherkashin <kirjs@google.com>
2025-07-23 18:08:57 +0000

    9562b63 docs: guard a resource error with `hasValue()` (#62708)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.1+sha-593cc8a";
+  var SW_VERSION = "20.2.0-next.1+sha-9562b63";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
