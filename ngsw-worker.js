39e31bcf2632c2cec7ca8a57a313c340a324203d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 16:22:07 +0000

    82862de docs(docs-infra): correct typos in adev and wording in resource api (#62801) PR Close #62801

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-da0fb33";
+  var SW_VERSION = "20.1.3+sha-82862de";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
