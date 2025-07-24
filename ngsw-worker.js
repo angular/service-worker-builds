6af6491ec793a53ba7051511e300395803500704
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 13:33:41 +0000

    206d153 refactor(forms): Removes redundant destroy ref usage in value accessor in select control (#62738)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-5aaf726";
+  var SW_VERSION = "20.1.3+sha-206d153";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
