78edfc2442fa5e45514cf2f308f0a75ba0a8d82e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 10:13:09 +0000

    92b0802 test(router): Use autoTick feature in router tests (#62776)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.2+sha-9d8c48d";
+  var SW_VERSION = "20.2.0-next.2+sha-92b0802";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
