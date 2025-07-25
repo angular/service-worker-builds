17f1dc4527e746b4c4a4cb509d309a0fc695bc3a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 15:03:36 +0000

    9a8b5c7 build: remove local zone.js build from usage in integration tests (#62800)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-c11f7b9";
+  var SW_VERSION = "20.1.3+sha-9a8b5c7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
