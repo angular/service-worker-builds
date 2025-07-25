d0699d4bb67357ef782a237efc237ef6854b7a31
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-25 08:16:56 +0000

    dce0609 build: use ts_config rule from rules_ts only and always load from common location (#62808)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-82862de";
+  var SW_VERSION = "20.1.3+sha-dce0609";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
