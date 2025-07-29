1b3d7593c58605ea0c4150058e234ac75a2e39ba
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 12:24:33 +0000

    0b9f53a docs: fix ActivateRoute example (#62855)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-f0fca61";
+  var SW_VERSION = "20.1.3+sha-0b9f53a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
