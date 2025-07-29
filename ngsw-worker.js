88938791bbd816e850de981a990ad41fdbbb5688
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 17:21:12 +0000

    c9fb4e3 build: use ts_project based extract_types (#62875)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-ea1afec";
+  var SW_VERSION = "20.1.3+sha-c9fb4e3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
