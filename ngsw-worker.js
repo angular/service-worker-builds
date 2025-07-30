56feece07a03ad70941c5a7efeb4e462bea7ef20
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 09:44:21 +0000

    a2e3bfc build: update all non-major dependencies (#62892)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-25c6b15";
+  var SW_VERSION = "20.1.3+sha-a2e3bfc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
