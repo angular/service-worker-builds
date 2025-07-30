d79fe8ae85dd16437dde230275138f0723d66afd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 16:01:01 +0000

    280f1f5 test(language-service): update language-service legacy tests (#62900)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.3+sha-82fcc9c";
+  var SW_VERSION = "20.1.3+sha-280f1f5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
