10603ac98aba5d81d97214e1fea4495327847ec6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 14:14:06 +0000

    245494a fix(http): add missing http options allowed in fetch API (#62881)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.2+sha-320de4e";
+  var SW_VERSION = "20.2.0-next.2+sha-245494a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
