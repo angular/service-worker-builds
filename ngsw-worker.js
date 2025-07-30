c269c6f2bc8f2398ae886506758b97f0307de415
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 17:43:07 +0000

    e7d748e build: remove @bazel/esbuild (#62906)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.2+sha-335cdc7";
+  var SW_VERSION = "20.2.0-next.2+sha-e7d748e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
