61934842e381860ea30843135f5e06cf72cb1762
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 08:18:12 +0000

    e5d6fb5 fix(http): Reset headers, progress, and statusCode when using `set()` in `HttpResource` (#62873)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.3+sha-d05138a";
+  var SW_VERSION = "20.2.0-next.3+sha-e5d6fb5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
