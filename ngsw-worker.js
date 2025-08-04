b0460c333d294b387f6f9bdebde14af7d827f606
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 09:49:39 +0000

    e0d436a docs: Adds docs on handling redirect info in interceptors (#62968)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.4+sha-c2b48be";
+  var SW_VERSION = "20.1.4+sha-e0d436a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
