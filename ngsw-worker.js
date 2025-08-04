4e2b04530a748dc5939559f9c0985a0a88d99b99
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 10:29:30 +0000

    6597ac0 fix(core): fix support for space separated strings in leave animations (#62979)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.3+sha-cbd300d";
+  var SW_VERSION = "20.2.0-next.3+sha-6597ac0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
