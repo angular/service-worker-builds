6eb7be59676df263bb0edc1a0ffdcbaa0b154cc6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-05 16:08:10 +0000

    ddf779e Revert "feat(router): add a `currentNavigation` signal to the `Router` service. (#62971)" (#63010)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.3+sha-5802f80";
+  var SW_VERSION = "20.2.0-next.3+sha-ddf779e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
