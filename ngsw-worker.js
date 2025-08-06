57e9180dd9c130160c6bdcfd60a0b8de28a8041e
Andrew Scott <atscott@google.com>
2025-08-06 16:59:34 +0000

    687c374 feat(router): add a currentNavigation signal to the Router service. (#63011) This new signal property is convenient to derive a `isNavigating` state.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.4+sha-d24d574";
+  var SW_VERSION = "20.2.0-next.4+sha-687c374";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
