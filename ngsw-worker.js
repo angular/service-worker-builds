9cd1d92896a93b7cbcf31b6bbc3596d64c12723f
Doug Parker <dgp1130@users.noreply.github.com>
2025-08-06 22:01:03 +0000

    3a320bd release: bump Angular DevTools version to 1.0.42 (#63036)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-next.5+sha-90b0227";
+  var SW_VERSION = "20.2.0-next.5+sha-3a320bd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
