7a3cef68954c01de33f15274b2d0007c168a6158
Andrew Scott <atscott@google.com>
2025-08-06 18:49:40 +0000

    847de40 build: explicitly include types for locales in @angular/common/locales (#63030)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.5+sha-6a3d2a0";
+  var SW_VERSION = "20.1.5+sha-847de40";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
