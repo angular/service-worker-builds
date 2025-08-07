7047712ac8685d7fb27227d5c10080a94a628962
Andrew Scott <atscott@google.com>
2025-08-07 16:36:38 +0000

    bba7c6f build: remove unused old toolchain dependencies (#63039)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.6+sha-8f234ff";
+  var SW_VERSION = "20.1.6+sha-bba7c6f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
