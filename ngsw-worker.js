14877cc6b85771260c640c3cc500718dd19eaa9f
Andrew Scott <atscott@google.com>
2025-08-07 16:16:33 +0000

    8f234ff docs(devtools): update `pnpm` install command (#63040)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.6+sha-0430a56";
+  var SW_VERSION = "20.1.6+sha-8f234ff";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
