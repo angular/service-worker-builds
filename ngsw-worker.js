ffcec8a3244c4761ce853e9b63adcd5ca2ae0f76
Andrew Scott <atscott@google.com>
2025-08-11 18:09:37 +0000

    06f5879 ci: several fixes for renovate (#63083)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-c4dcfdf";
+  var SW_VERSION = "20.3.0-next.0+sha-06f5879";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
