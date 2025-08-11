41dd2df6dd36cedbb490023b0d71b6a9971d32ac
Andrew Scott <atscott@google.com>
2025-08-11 19:03:40 +0000

    d17555a fix(compiler): incorrect spans for AST inside input value with leading space (#63082)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-06f5879";
+  var SW_VERSION = "20.3.0-next.0+sha-d17555a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
