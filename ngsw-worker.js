5968a8e8e5e196567d975790fe1fac490ad1dd61
Andrew Scott <atscott01@gmail.com>
2025-08-12 20:35:43 +0000

    5e9893d refactor(compiler-cli): ensure JIT transforms with queries work with closure property renaming (#63123)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.6+sha-bb5a69c";
+  var SW_VERSION = "20.1.6+sha-5e9893d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
