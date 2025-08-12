d9756f2f03bca01f6fe6b04692003dddc587f9a1
Andrew Scott <atscott01@gmail.com>
2025-08-12 23:58:06 +0000

    d7a5ead build: update pnpm to v10 (#63087)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-ea78278";
+  var SW_VERSION = "20.3.0-next.0+sha-d7a5ead";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
