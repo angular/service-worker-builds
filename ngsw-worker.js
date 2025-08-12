b66209a1c921b24bda986924cbd5612e31627e8e
Andrew Scott <atscott@google.com>
2025-08-12 18:09:55 +0000

    bb5a69c fix(docs-infra): put content in `<main>` element (#63121)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.6+sha-f251c7d";
+  var SW_VERSION = "20.1.6+sha-bb5a69c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
