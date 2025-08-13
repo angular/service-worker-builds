e1d76b512f40471679e80efc752e3ac8869fb435
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:34:58 +0000

    55cd8c2 docs: add rxjs-interop section for `rxResource` (#63125)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-32ba8b5";
+  var SW_VERSION = "21.0.0-next.0+sha-55cd8c2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
