a7bb1cf87cfaeb491a1d1c0648fa5d7703dec942
Andrew Scott <atscott01@gmail.com>
2025-08-13 20:33:22 +0000

    e7cc89e docs: release notes for the v20.2.0-rc.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-74e1796";
+  var SW_VERSION = "21.0.0-next.0+sha-e7cc89e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
