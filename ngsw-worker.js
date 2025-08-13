b92af2c8174f6cdf7be0f0128b3e39d8e5fa31d2
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:20:23 +0000

    32ba8b5 docs: add link to ai-config command (#63139)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-91b3466";
+  var SW_VERSION = "21.0.0-next.0+sha-32ba8b5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
