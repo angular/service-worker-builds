a81ebc0df0e3ff45f194a1a142ee42448b611f29
Andrew Scott <atscott01@gmail.com>
2025-08-13 01:46:12 +0000

    4d9385b docs: add topic for `takeUntilDestroyed` (#63122)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-d7a5ead";
+  var SW_VERSION = "20.3.0-next.0+sha-4d9385b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
