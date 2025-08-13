b68eb375838d9b8d8f845b3223cebafb99201139
Andrew Scott <atscott01@gmail.com>
2025-08-13 20:16:11 +0000

    b16de05 release: cut the v20.1.7 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.6+sha-774b225";
+  var SW_VERSION = "20.1.7+sha-b16de05";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
