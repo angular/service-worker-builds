ae402b064aa50289b7b71131f80dc9a8cc107e51
Andrew Scott <atscott01@gmail.com>
2025-08-13 20:31:43 +0000

    1cf4d34 release: cut the v20.2.0-rc.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-rc.0+sha-ced0357";
+  var SW_VERSION = "20.2.0-rc.1+sha-1cf4d34";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
