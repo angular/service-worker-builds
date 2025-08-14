1edcc1c835b2d2acd3b34d4f28a78ebfd2b334ad
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 11:17:10 +0000

    4bed062 feat(http): Provide http services in root (#56212)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-e7cc89e";
+  var SW_VERSION = "21.0.0-next.0+sha-4bed062";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
