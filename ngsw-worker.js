f8affecab5afda33671b706d7a3d1a23e20f5729
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:20:01 +0000

    5129dae ci: redirect adev to `index.csr.html` (#63168)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-d733c69";
+  var SW_VERSION = "21.0.0-next.0+sha-5129dae";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
