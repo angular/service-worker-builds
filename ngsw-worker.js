5ca4ada4d48684914662f4e299135157537bcf48
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 07:59:55 +0000

    691f5ed fix(compiler): error when ng-content fallback has translated children (#63156)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.7+sha-39e4f93";
+  var SW_VERSION = "20.1.7+sha-691f5ed";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
