8cbccc75e00551fc3b1f0475264b96ee914c2e1e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-15 11:54:59 +0000

    3f88fbd docs(docs-infra): replace deprecated router method (#63183)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-6f91972";
+  var SW_VERSION = "21.0.0-next.0+sha-3f88fbd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
