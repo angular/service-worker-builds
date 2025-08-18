d10000bd552db2da72801a5f67995e510559cd9d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:47:18 +0000

    5115050 fix(devtools): DOM traversal bug (#62719)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-d0cf624";
+  var SW_VERSION = "21.0.0-next.0+sha-5115050";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
