695d9e6291c0ec2bf11e20ef196f844fa5fe423e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 08:11:46 +0000

    5d31e20 docs(docs-infra): remove @adev/constant path (#63213)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.7+sha-a9bab9e";
+  var SW_VERSION = "20.1.7+sha-5d31e20";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
