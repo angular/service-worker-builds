622efa9fda81bcdc76f83eb3a4ce45081e426f50
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 15:13:52 +0000

    b413b62 build: update the failure message with the correct command (#63219)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.7+sha-bc3798d";
+  var SW_VERSION = "20.1.7+sha-b413b62";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
