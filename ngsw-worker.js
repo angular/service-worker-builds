c2c4e3ec4564ffba1d787eec4faff9b3cc44a767
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-18 12:46:36 +0000

    a7cfb42 fix(core): ensure animate events do not have duplicate elements (#63216)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-6c8d000";
+  var SW_VERSION = "21.0.0-next.0+sha-a7cfb42";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
