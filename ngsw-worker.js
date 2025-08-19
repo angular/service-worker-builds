543ad64c4bbff8b170a30cd704f40100c788e726
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:05:41 +0000

    a8c5baf docs: fix typo in SSR section (#63224) PR Close #63224

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-rc.1+sha-7b51728";
+  var SW_VERSION = "20.2.0-rc.1+sha-a8c5baf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
