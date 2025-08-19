e211b2ff38ae32e67f68f96090979860f88d23ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 12:40:49 +0000

    683ae03 docs: use correct closing quote in `input` and `model` import usage notes (#63237)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.7+sha-9e26158";
+  var SW_VERSION = "20.1.7+sha-683ae03";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
