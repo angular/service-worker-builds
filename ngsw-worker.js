868527f841153d92d58780d1ea5a9d7bea98e809
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:34:20 +0000

    9e26158 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.7+sha-d81baf6";
+  var SW_VERSION = "20.1.7+sha-9e26158";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
