f6ed9cb59c53ca2d21f6737c9e77f67f7dbe73d6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:09:29 +0000

    d5a337d build: update github/codeql-action action to v3.29.10 (#63230)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1282,7 +1282,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-cab3adf";
+  var SW_VERSION = "21.0.0-next.0+sha-d5a337d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
