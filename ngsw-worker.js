1d243e8d8940a0edb45f603a4a001e04553b0b0b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:37:21 +0000

    6a6cb01 docs: update `NG_VALIDATORS` examples to use `forwardRef` (#63247)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-85468ef";
+  var SW_VERSION = "21.0.0-next.0+sha-6a6cb01";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
