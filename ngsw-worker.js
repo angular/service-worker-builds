bc27d17939ec9b066f51d879b540e7ebe3c60b16
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:09:55 +0000

    39febe1 docs: rewording heading for `define` option (#63009)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1261,7 +1261,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.1.7+sha-c935d42";
+  var SW_VERSION = "20.1.7+sha-39febe1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
