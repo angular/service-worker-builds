3d4d360101f5159b4674ea672635bba5f58146e9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:20:03 +0000

    9655ea7 build: update dependency playwright-core to v1.55.0 (#63273)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.0-rc.1+sha-d76a911";
+  var SW_VERSION = "20.2.0-rc.1+sha-9655ea7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
