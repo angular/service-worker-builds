2a52e1b8ee632093b876ff1a59e8a27506f56f61
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:04:51 +0000

    1c5fb1e build: enforce frozen lockfile mode (#63260)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-d8bdf56";
+  var SW_VERSION = "21.0.0-next.0+sha-1c5fb1e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
