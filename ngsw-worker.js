1036df9d95cc22cb0129949ca2316228f86cea48
Andrew Kushnir <akushnir@google.com>
2025-08-21 17:21:09 +0000

    49e00b3 docs(docs-infra): fix mermaid rendering (#63311)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.1+sha-1c27fa9";
+  var SW_VERSION = "20.2.1+sha-49e00b3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
