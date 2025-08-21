d485c171ba63430945b41708d139557712c371e6
Andrew Kushnir <akushnir@google.com>
2025-08-21 16:52:38 +0000

    0dd95c5 feat(forms): Add FormArrayDirective (#55880)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-1b0c84a";
+  var SW_VERSION = "21.0.0-next.0+sha-0dd95c5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
