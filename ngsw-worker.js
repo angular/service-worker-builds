ed440639f0c3f6f4ba68432489ece3c9745742ca
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:32:26 +0000

    e1481d8 ci: add myself to review fw-general (#63337)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.1+sha-e8aa7d3";
+  var SW_VERSION = "20.2.1+sha-e1481d8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
