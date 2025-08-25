3aff4757b13ad81393a25644e4578d37af4d6c81
Andrew Kushnir <akushnir@google.com>
2025-08-25 15:42:38 +0000

    f881082 build: update bazel dependencies (#63345)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.1+sha-10b0927";
+  var SW_VERSION = "20.2.1+sha-f881082";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
