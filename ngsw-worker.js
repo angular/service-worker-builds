a35b4bf5f4f3c08c6d93a87d93cac5ada204d8e8
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:55:10 +0000

    e220a61 docs(docs-infra): patch port changes from `main` (#63394)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.1+sha-c9b0f45";
+  var SW_VERSION = "20.2.1+sha-e220a61";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
