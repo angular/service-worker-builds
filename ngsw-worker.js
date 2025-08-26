66d1241b960cd51c589b1acc505d24ef49fe6a0d
Andrew Kushnir <akushnir@google.com>
2025-08-26 18:50:15 +0000

    dfa2044 build: fix strict deps failure (#63403)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.0+sha-a038840";
+  var SW_VERSION = "21.0.0-next.0+sha-dfa2044";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
