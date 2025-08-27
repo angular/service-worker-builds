efff682ab589916115599a7e54ea08718722604b
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:46:51 +0000

    1bb30c7 docs(docs-infra): remove dependency on `html-entities` (#63416)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-3a3bd36";
+  var SW_VERSION = "21.0.0-next.1+sha-1bb30c7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
