7ed6c994a0ad56cf8678178b7efdf343e950c985
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:16:19 +0000

    f868465 fix(upgrade): Remove deprecated UpgradeAdapter (#61659)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-478eebe";
+  var SW_VERSION = "21.0.0-next.1+sha-f868465";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
