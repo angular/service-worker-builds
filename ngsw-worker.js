f7942453c7801e8a526038d1ba8bb355c2838bda
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-08-27 17:57:15 +0000

    6e0b66d release: cut the v20.2.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.1+sha-47caed9";
+  var SW_VERSION = "20.2.2+sha-6e0b66d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
