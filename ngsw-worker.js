697c563f781941d5c2c44fec78da5a7e782d6b54
Andrew Kushnir <akushnir@google.com>
2025-08-27 14:40:49 +0000

    47caed9 ci: pin create-pull-request version to sha (#63409)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.1+sha-23d1ace";
+  var SW_VERSION = "20.2.1+sha-47caed9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
