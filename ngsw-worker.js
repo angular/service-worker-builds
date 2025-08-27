d928b356c8896f4459fcbdb61f8c992df11b209c
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:44:54 +0000

    fce5602 refactor(core): mark `VERSION` as `@__PURE__` for better tree-shaking (#63400)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.2+sha-06c5367";
+  var SW_VERSION = "20.2.2+sha-fce5602";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
