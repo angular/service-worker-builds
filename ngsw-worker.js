d01c95acfdc828cf37b79734e8c606df1a5e4b07
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:17:06 +0000

    114906d fix(core): Fix cancellation of animation enter classes (#63442)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.2+sha-690405b";
+  var SW_VERSION = "20.2.2+sha-114906d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
