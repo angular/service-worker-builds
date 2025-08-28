c9124c1b3016e9272f2910c8b3cab1b276e4defc
Miles Malerba <mmalerba@google.com>
2025-08-28 00:02:39 +0000

    b2ed21b build: remove unused deps (#63417)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-0d57dfb";
+  var SW_VERSION = "21.0.0-next.1+sha-b2ed21b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
