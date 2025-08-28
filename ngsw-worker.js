9cffe855bf8f2d103b131255c0ab852b81dcacb1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:07:05 +0000

    421edfe build: update @bazel/ibazel to `0.26.10` (#63435)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.2+sha-87bfe16";
+  var SW_VERSION = "20.2.2+sha-421edfe";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
