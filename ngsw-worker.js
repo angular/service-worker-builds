a9d4921348d6eb65bea2f0722541b408f452a884
Miles Malerba <mmalerba@google.com>
2025-08-29 16:03:07 +0000

    6e1599d refactor(router): Add internal flag to control async vs rxjs recognize (#62994)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-dcd81d9";
+  var SW_VERSION = "21.0.0-next.1+sha-6e1599d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
