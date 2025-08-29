e7b6a04f540338a1a63b634c3fea3f8707d60286
Miles Malerba <mmalerba@google.com>
2025-08-29 22:15:31 +0000

    16fa983 ci: add more team members as approvers (#63495)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-db95a5c";
+  var SW_VERSION = "21.0.0-next.1+sha-16fa983";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
