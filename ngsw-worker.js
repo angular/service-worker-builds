39d84f345d4ddbe94f361d132d701a5c91e02c2d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:49:30 +0000

    dcd81d9 build: update cross-repo angular dependencies to 06d3af5 (#63475)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-3fb02ed";
+  var SW_VERSION = "21.0.0-next.1+sha-dcd81d9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
