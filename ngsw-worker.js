6c763365c8bf11d5bc64fa42aaed255288cf2a85
Miles Malerba <mmalerba@google.com>
2025-09-02 16:47:43 +0000

    e63608d docs(docs-infra): signal property should be readonly (#63166)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-1541606";
+  var SW_VERSION = "21.0.0-next.1+sha-e63608d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
