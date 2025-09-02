3fe9f19689133da7dd99f4de68f02bdef4213562
Miles Malerba <mmalerba@google.com>
2025-09-02 16:41:46 +0000

    3b42314 docs(docs-infra): export class (#63506)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.3+sha-dddce83";
+  var SW_VERSION = "20.2.3+sha-3b42314";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
