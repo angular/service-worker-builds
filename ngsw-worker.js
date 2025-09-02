dbb805ede133f1db4c42d9f9d95354c3a9af5b82
Miles Malerba <mmalerba@google.com>
2025-09-02 19:43:16 +0000

    4fb8c46 docs: update Angular CLI help (#63552)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-4c36659";
+  var SW_VERSION = "21.0.0-next.1+sha-4fb8c46";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
