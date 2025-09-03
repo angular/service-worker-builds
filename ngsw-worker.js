95224b15388447890cdbc6413e0f7249c9f6cea1
Miles Malerba <mmalerba@google.com>
2025-09-03 16:37:46 +0000

    8738b81 build: use common jasmine rule from dev-infra (#63568)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-49d5de4";
+  var SW_VERSION = "21.0.0-next.1+sha-8738b81";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
