743f5da6c53b2f472594b2264f33826eee917119
Miles Malerba <mmalerba@google.com>
2025-09-03 16:20:41 +0000

    0c2255b refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.3+sha-f4a2d0a";
+  var SW_VERSION = "20.2.3+sha-0c2255b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
