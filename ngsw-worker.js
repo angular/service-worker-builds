1a12d6e292987a1fadd585dc6eea6686b64ceb56
Miles Malerba <mmalerba@google.com>
2025-09-03 03:56:38 +0000

    475ed59 refactor(core): export zoneless provider token privately (#63558)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-6148a40";
+  var SW_VERSION = "21.0.0-next.1+sha-475ed59";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
