552ceaa219beeb10e22db4bc7f45060c283b8ef5
Miles Malerba <mmalerba@google.com>
2025-09-03 19:41:30 +0000

    c918f61 docs: release notes for the v20.2.4 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.1+sha-8738b81";
+  var SW_VERSION = "21.0.0-next.1+sha-c918f61";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
