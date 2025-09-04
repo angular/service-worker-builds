070a87fa1fcc27195a90f6648f37b92421ec7998
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-04 11:51:31 +0000

    948b811 release: bump Angular DevTools version to 1.1.44 (#63596)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.2+sha-835aad7";
+  var SW_VERSION = "21.0.0-next.2+sha-948b811";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
