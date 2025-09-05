e17292fd5af6f052825beeafa12e58e07ceab483
Andrew Scott <atscott@google.com>
2025-09-05 20:58:57 +0000

    c0791e1 fix(core): drop support for TypeScript 5.8 (#63589)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.2+sha-af55552";
+  var SW_VERSION = "21.0.0-next.2+sha-c0791e1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
