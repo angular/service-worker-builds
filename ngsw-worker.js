b66bb6ed62315be07704a5aaee1f0bfe31c86050
Andrew Scott <atscott@google.com>
2025-09-08 22:25:33 +0000

    2fd0b57 build: lock file maintenance (#63634)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.2+sha-efb9474";
+  var SW_VERSION = "21.0.0-next.2+sha-2fd0b57";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
