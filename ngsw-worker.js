b1c8d46099a0dc3ae54734129d7b780eb0c11348
Andrew Scott <atscott@google.com>
2025-09-08 17:19:15 +0000

    f2ec6fb docs(docs-infra): quote title attributes in links (#63578)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.4+sha-f18980c";
+  var SW_VERSION = "20.2.4+sha-f2ec6fb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
