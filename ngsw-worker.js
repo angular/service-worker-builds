9f09b483fd76cd6fb4b7ede52153f0029f6996f2
Andrew Scott <atscott@google.com>
2025-09-08 23:35:29 +0000

    17d5479 docs: Update Language Service IDE list (#63647)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.2+sha-2fd0b57";
+  var SW_VERSION = "21.0.0-next.2+sha-17d5479";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
