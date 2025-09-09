0c7e9046cf6bf2fc9ab50f684cbda38c1ff93ff0
Andrew Scott <atscott@google.com>
2025-09-09 21:40:56 +0000

    0571b33 feat(compiler-cli): enable type checking of host bindings by default (#63654)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.2+sha-1e614ec";
+  var SW_VERSION = "21.0.0-next.2+sha-0571b33";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
