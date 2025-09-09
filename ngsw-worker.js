82e76a41c65aa90400753d00b3b403500df91659
Andrew Scott <atscott@google.com>
2025-09-09 19:15:06 +0000

    0c8c855 docs: add missing drag & drop docs (#63619)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.2.4+sha-e097269";
+  var SW_VERSION = "20.2.4+sha-0c8c855";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
