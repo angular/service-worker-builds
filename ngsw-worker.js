aeb4ca6ce4ff3d8ab74e909dae0bdaaa332fe439
Andrew Scott <atscott@google.com>
2025-09-09 21:33:32 +0000

    650cd6b docs: add missing component harness docs (#63620)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-d0f0c08";
+  var SW_VERSION = "20.3.0-next.0+sha-650cd6b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
