63cb3980fa3c4a2238ac888d9cfa8eb649929ded
Andrew Scott <atscott@google.com>
2025-09-09 19:38:31 +0000

    749a71c ci: update runs-on to ubuntu-latest (#63656)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-3fb7a12";
+  var SW_VERSION = "20.3.0-next.0+sha-749a71c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
