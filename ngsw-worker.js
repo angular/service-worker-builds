dd2b1d014de307a89eec93b1a5f9b4787ccb829e
Andrew Scott <atscott@google.com>
2025-09-09 17:37:02 +0000

    90e798b build: prepare exceptional minor branch: 20.3.x

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-11a54d1";
+  var SW_VERSION = "20.3.0-next.0+sha-90e798b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
