bb2685c1bd53473ff58e32a67521a3a493a1b763
Andrew Scott <atscott@google.com>
2025-09-09 20:01:06 +0000

    d0f0c08 docs: add signals tutorial (#62750)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0-next.0+sha-749a71c";
+  var SW_VERSION = "20.3.0-next.0+sha-d0f0c08";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
