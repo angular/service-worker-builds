7a91d8967aa31640feae516032350350722c2ebb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 22:04:07 +0000

    39337cf docs: update supported Angular versions to include 20.3.x (#63679)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-fb3082f";
+  var SW_VERSION = "21.0.0-next.3+sha-39337cf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
