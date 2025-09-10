453627f962fc2f6fdf0850e9eab46c8a7a35ac83
Andrew Scott <atscott@google.com>
2025-09-10 17:35:03 +0000

    9487dc6 build: update `@angular/ng-dev` to latest SHA (#63696)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-48fee39";
+  var SW_VERSION = "21.0.0-next.3+sha-9487dc6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
