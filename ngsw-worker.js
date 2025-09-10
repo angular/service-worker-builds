af4869c1797f4bba4f27f5951d910a93e3837d28
Andrew Scott <atscott@google.com>
2025-09-10 17:13:43 +0000

    802dbcc fix(core): prevent animation events from being cleaned up on destroy (#63414)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-9096d45";
+  var SW_VERSION = "20.3.0+sha-802dbcc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
