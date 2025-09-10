395d5190e0b198ef1127214386e1959a77c90418
Andrew Scott <atscott@google.com>
2025-09-10 21:19:27 +0000

    69b61cb build: update symbol goldens (#63711)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-8266790";
+  var SW_VERSION = "20.3.0+sha-69b61cb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
