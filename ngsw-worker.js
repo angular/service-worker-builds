0210354741a5fdf533e5dbd202ab1ef54ab29dcf
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 22:35:47 +0000

    5f5828f refactor(core): dispatch enter and leave animations at the right times (#63710)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-8dc3496";
+  var SW_VERSION = "20.3.0+sha-5f5828f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
