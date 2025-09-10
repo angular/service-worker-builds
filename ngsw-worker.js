ac6c254c068ac7db2ed72e19a69e8b028450cd79
Andrew Scott <atscott@google.com>
2025-09-10 20:34:35 +0000

    fb3082f build: fix symbol tests (#63709)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-f642fcf";
+  var SW_VERSION = "21.0.0-next.3+sha-fb3082f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
