223157c225271cde90859bf782fd1b768deff5c2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 21:43:23 +0000

    e5de410 build: update cross-repo angular dependencies to v20.2.3 (#63741)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-00b87a7";
+  var SW_VERSION = "20.3.0+sha-e5de410";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
