8d84e7900fcc9dbfdf6ac741020a2fbd6c1d03b8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 21:22:05 +0000

    00b87a7 build: automatically select a random available port when running adev locally (#63740)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-50b3e9f";
+  var SW_VERSION = "20.3.0+sha-00b87a7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
