7519e93112e95912ba213020bc29fa93ca30934e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 17:44:51 +0000

    50b3e9f docs: Adds guide for extending Angular service worker (#63629)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-b62cadb";
+  var SW_VERSION = "20.3.0+sha-50b3e9f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
