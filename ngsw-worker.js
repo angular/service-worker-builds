0d9f15f87e1cb996203b7e30ba74de0165cd840d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:55:21 +0000

    895f0fb docs: update Creating service with CLI (#63714)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-3ec8a5c";
+  var SW_VERSION = "20.3.0+sha-895f0fb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
