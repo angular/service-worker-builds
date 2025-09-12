5a27c28569421dae9298bddafe613a2c8ea42053
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-12 07:55:29 +0000

    185ac24 docs: fix links and remove second banner (#63755)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-e5de410";
+  var SW_VERSION = "20.3.0+sha-185ac24";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
