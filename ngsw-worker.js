02571c1a1e571ab76de4d643fb4cc9a94dbafadc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 15:28:05 +0000

    3ec8a5c fix(core): Prevent leave animations on a move operation (#63745)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-4ee3993";
+  var SW_VERSION = "20.3.0+sha-3ec8a5c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
