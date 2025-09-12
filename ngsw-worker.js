acdcd83b7fe01a2b9b411aa72202582091a3873c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:09:04 +0000

    9de50b1 docs(docs-infra): move the navigation related files (#63729)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-895f0fb";
+  var SW_VERSION = "20.3.0+sha-9de50b1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
