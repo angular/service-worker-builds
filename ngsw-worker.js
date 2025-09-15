bf6b61cf3bce9f183f622992f7282e72da372f8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:13:17 +0000

    a2ea2ec docs: Update outputs.md to demonstrate how to use event data (#63732) PR Close #63732

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-cd36189";
+  var SW_VERSION = "20.3.0+sha-a2ea2ec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
