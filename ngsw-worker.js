ce3b24ccb2d3381c6212bd023661dfe1dc377eb4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 21:16:56 +0000

    4a23f6e refactor(core): wrap `checkNoChanges` body in `if (ngDevMode)` for better tree-shaking (#63387)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-19462fc";
+  var SW_VERSION = "20.3.0+sha-4a23f6e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
