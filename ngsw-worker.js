e77af3cd967bc76e6fb7685f3f835d68b9dbe2c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:08:20 +0000

    de8be97 ci: update pullapprove public-api rules (#63737)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-d201cd2";
+  var SW_VERSION = "21.0.0-next.3+sha-de8be97";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
