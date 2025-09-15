11645867e6f965711ab59b1d87373ad59fc82a82
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:07:49 +0000

    c536035 docs: correct typos in AI design patterns guide (#63788)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-d191926";
+  var SW_VERSION = "20.3.0+sha-c536035";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
