53ba51124d79f4f4fa3382ddbde3451666ab633e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 20:20:50 +0000

    8dc8914 feat(migrations): add migration to convert ngStyle to use style (#63517)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-8b43fce";
+  var SW_VERSION = "21.0.0-next.3+sha-8dc8914";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
