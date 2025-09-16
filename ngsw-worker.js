fac3a9b258a46214dbf6b0ff413c95c4d322e59f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 14:51:56 +0000

    9707ac1 build: update dependency aspect_rules_js to v2.6.0 (#63821)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-7e0cf41";
+  var SW_VERSION = "21.0.0-next.3+sha-9707ac1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
