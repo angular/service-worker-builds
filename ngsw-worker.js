072b8d4d2beb0b41e8be31b5326c7200f8c86b50
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 14:58:42 +0000

    1004418 docs: remove leading `$` from script examples (#63795)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-9707ac1";
+  var SW_VERSION = "21.0.0-next.3+sha-1004418";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
