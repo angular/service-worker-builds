4511b7319322b91e9771dfedf806f50afbd3dc6e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:59:14 +0000

    4133b08 fix(migrations): fix route-lazy-loading migration (#63818)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-e43d9b9";
+  var SW_VERSION = "21.0.0-next.3+sha-4133b08";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
