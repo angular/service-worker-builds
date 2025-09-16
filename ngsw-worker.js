90668c8ed0fc422181a49f5b639d89e468c56e16
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 19:01:42 +0000

    d5e00f9 refactor(core): Make the zoneless-by-default change a flag flip (#63382)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-d9369d1";
+  var SW_VERSION = "21.0.0-next.3+sha-d5e00f9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
