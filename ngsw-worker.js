ccbac4d05b2d3a2994a4cb6dd192b2fabc729543
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:22:27 +0000

    e43d9b9 docs(docs-infra): Use port 4201 for adev (#63834)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-a943e7c";
+  var SW_VERSION = "21.0.0-next.3+sha-e43d9b9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
