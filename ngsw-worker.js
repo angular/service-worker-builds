86e54d26f665c58a07c403a88a6faec0af021106
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 20:53:03 +0000

    4cd56c2 build: remove bazel flag usage for adev (#63845)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-dd6e1c3";
+  var SW_VERSION = "20.3.0+sha-4cd56c2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
