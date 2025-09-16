8868374b4a98d080eb5c42c7b9eb2a9253409f30
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:14:22 +0000

    27b2a4a refactor(core): Update async-test.ts to use globalThis polyfill (#57505)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-c80d9ba";
+  var SW_VERSION = "20.3.0+sha-27b2a4a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
