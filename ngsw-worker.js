a4cadeb7441ad0797cb666674cf7d4c897992ffa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 17:46:09 +0000

    dee6ef8 refactor(core): tree-shake `HOST_TAG_NAME` in prod (#63861)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-32c98e0";
+  var SW_VERSION = "21.0.0-next.3+sha-dee6ef8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
