d3f9035d9d1e0c21f81b0f978c3b77ed92fa22ad
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:30:34 +0000

    dcdca98 refactor(core): remove usages of Promise.withResolvers (#63856)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.0+sha-423cc17";
+  var SW_VERSION = "20.3.0+sha-dcdca98";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
