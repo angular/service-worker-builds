24fbaff54c59bf87dc6c9d089cc4faf010b719ef
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:27:57 +0000

    af33647 refactor(core): remove usages of Promise.withResolvers (#63856)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.3+sha-e523384";
+  var SW_VERSION = "21.0.0-next.3+sha-af33647";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
