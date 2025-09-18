743e8b835ebb871c14a6a595047486745abf23ee
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 20:22:53 +0000

    307e4ea refactor(migrations): remove common module if there are no references (#63892)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-e426302";
+  var SW_VERSION = "21.0.0-next.4+sha-307e4ea";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
