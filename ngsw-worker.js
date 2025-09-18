5793bed71dfcef5f069ea87098c1498c42876552
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 16:09:58 +0000

    96a070e refactor(core): Remove duplicate code getSelectedTNode (#63897)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-ccdb340";
+  var SW_VERSION = "20.3.1+sha-96a070e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
