5c27617f2f825c6d05803414296128c62a01250a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 21:39:40 +0000

    ba40153 fix(compiler-cli): capture metadata for undecorated fields (#63904)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-d40867c";
+  var SW_VERSION = "20.3.1+sha-ba40153";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
