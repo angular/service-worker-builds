10fe2c9e1f51b39bb9ac309fd913218a10222903
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 20:31:41 +0000

    56cb093 refactor(core): drop the usage of `PromiseSettledResult<void>` (#63955)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-e835c35";
+  var SW_VERSION = "21.0.0-next.4+sha-56cb093";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
