e39cd0b94ca58595d71470ac312376bdc2b10f6a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 21:39:35 +0000

    40ae5af docs: callout for HMR+Defer (#63958)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-56cb093";
+  var SW_VERSION = "21.0.0-next.4+sha-40ae5af";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
