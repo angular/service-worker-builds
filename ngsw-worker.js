42d1a2ee24432e9fd4efddfdbf03d5da5c546979
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 22:18:01 +0000

    923c81c docs: remove api grouping toggle (#63961)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-dbb8dbc";
+  var SW_VERSION = "20.3.1+sha-923c81c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
