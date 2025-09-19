83071a5a1137f6fe07a052fb0a56b3ed68ab42aa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 17:11:28 +0000

    150ed2b docs: add documentation for NG0401 error (#63947)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-d0c7ea8";
+  var SW_VERSION = "20.3.1+sha-150ed2b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
