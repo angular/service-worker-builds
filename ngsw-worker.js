e95306590f47103964c6891c5ac65ebd1d659f25
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 22:52:00 +0000

    3d1e8fa docs: add types category for signal forms API entries (#63967)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-304d91f";
+  var SW_VERSION = "21.0.0-next.4+sha-3d1e8fa";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
