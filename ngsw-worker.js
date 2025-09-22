8c9a2acf36d2b33db7a9870ea6171cbb64a704f9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:45:53 +0000

    a2d98cc docs: add missing `signal` function (#63974)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-6782f71";
+  var SW_VERSION = "21.0.0-next.4+sha-a2d98cc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
