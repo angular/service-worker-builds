bb6180fedbd6634527c96b144e577931105ff7df
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 21:46:20 +0000

    71955dc build: update pnpm to v10.17.1 (#64016)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-bc2b819";
+  var SW_VERSION = "20.3.1+sha-71955dc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
