2cb3d1d50eb4827e0743e43ae7ee0047e2f51687
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 14:27:04 +0000

    9515a70 fix(core): fix narrowing of `Resource.hasValue()` (#63994)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-2d07610";
+  var SW_VERSION = "20.3.1+sha-9515a70";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
