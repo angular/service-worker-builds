3db523f735076121526525362283641370f5ebdc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:53:30 +0000

    1fd8d5d fix(forms): Emit `FormResetEvent` when resetting control (#64034)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-3ebaecc";
+  var SW_VERSION = "20.3.1+sha-1fd8d5d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
