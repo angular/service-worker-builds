8c31e5c1c9d0e5c8a8064bd3d0b8963fc7c7f678
Doug Parker <dgp1130@users.noreply.github.com>
2025-09-24 20:57:59 +0000

    e8beaa4 release: bump Angular DevTools version to 1.3.0 (#64044)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-863c7ea";
+  var SW_VERSION = "21.0.0-next.4+sha-e8beaa4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
