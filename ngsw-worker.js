2c0b9a22191f2f616b9cb8a595aae4877dad84d3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:54:19 +0000

    057eff6 build: update dependency prettier to v3 (#64053)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-090c0bb";
+  var SW_VERSION = "21.0.0-next.4+sha-057eff6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
