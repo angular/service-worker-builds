270688b37b7a55821270567dd68e6a50ece7cded
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 20:29:21 +0000

    863c7ea build: migrate vscode extension into repo (#63924)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.4+sha-0b2018d";
+  var SW_VERSION = "21.0.0-next.4+sha-863c7ea";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
