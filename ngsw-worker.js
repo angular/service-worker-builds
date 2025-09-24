bab03df01c1814e63f031fd9edc0dda2cfad8daf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-24 23:27:01 +0000

    31f2030 release: cut the v20.3.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.1+sha-c525a5a";
+  var SW_VERSION = "20.3.2+sha-31f2030";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
