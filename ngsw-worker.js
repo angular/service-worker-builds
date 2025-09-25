7d6af7c92fe3eaa8953d80dce4802cc7efe5ffb8
kirjs <kirts@google.com>
2025-09-25 14:47:23 +0000

    9fd8d40 ci: authenticate to Bazel RBE (#64070)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-45e3f77";
+  var SW_VERSION = "21.0.0-next.5+sha-9fd8d40";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
