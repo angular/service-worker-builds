42da6f7488c1fa44f03e06869b68700ad84be89d
kirjs <kirts@google.com>
2025-09-25 14:41:35 +0000

    076ccbf refactor(devtools): remove hard newlines from reviewer note (#64062)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-31f2030";
+  var SW_VERSION = "20.3.2+sha-076ccbf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
