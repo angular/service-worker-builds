2c9169d21718730eda74a6ac93f5046b236cd8b2
kirjs <kirts@google.com>
2025-09-25 18:32:44 +0000

    a74d74b docs: fix homepage width (#64087)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-076ccbf";
+  var SW_VERSION = "20.3.2+sha-a74d74b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
