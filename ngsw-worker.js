a9f35f7cac4b32f3c83ce941ada4199f9a8f02b3
kirjs <kirts@google.com>
2025-09-25 18:55:35 +0000

    542cd00 fix(core): do not rename ARIA property bindings to attributes (#64089)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-a74d74b";
+  var SW_VERSION = "20.3.2+sha-542cd00";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
