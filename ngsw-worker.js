6fbf3938a479ae0133f5fcc84e7304cd22fd4e45
kirjs <kirts@google.com>
2025-09-25 20:34:28 +0000

    b12e5e6 docs: add warning to `AbstractControl.setErrors` (#64063)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-7d720d5";
+  var SW_VERSION = "20.3.2+sha-b12e5e6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
