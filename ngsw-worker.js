867854dd6e90c6091ad37bff784e5d204b67c908
kirjs <kirts@google.com>
2025-09-26 16:06:51 +0000

    8cf95ce build: update cross-repo angular dependencies (#64094)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-e02985a";
+  var SW_VERSION = "20.3.2+sha-8cf95ce";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
