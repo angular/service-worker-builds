4f1f54e92878d13f48fe2cfd98af2ed6e9c2c4e0
kirjs <kirts@google.com>
2025-09-29 20:52:54 +0000

    9a913dc docs(docs-infra): clean up code and use inferred types where possible (#64139)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-04462ed";
+  var SW_VERSION = "21.0.0-next.5+sha-9a913dc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
