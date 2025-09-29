b0682e3f3eed0152122532a0b7ceeb1c9b3242ae
kirjs <kirts@google.com>
2025-09-29 15:54:17 +0000

    633db24 build: update bazel dependencies (#64100)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-e6c0d7e";
+  var SW_VERSION = "20.3.2+sha-633db24";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
