c21dfe5dce92e5606dd04e0dc1129422c29a9e91
kirjs <kirts@google.com>
2025-09-30 17:40:56 +0000

    899c25f docs: add mention of `host-context()` being spec deprecated but still supported (#64153)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-20bb459";
+  var SW_VERSION = "21.0.0-next.5+sha-899c25f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
