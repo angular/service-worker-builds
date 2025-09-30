9c4a939c8840960a76dbda36e8b22203b2c69320
kirjs <kirts@google.com>
2025-09-30 17:54:14 +0000

    2aca6da refactor(core): Add custom formatters for Signals (#64000)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-899c25f";
+  var SW_VERSION = "21.0.0-next.5+sha-2aca6da";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
