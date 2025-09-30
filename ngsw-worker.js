481b43dc97cc9aa4f72dbd6045b437bf3a2535cb
kirjs <kirts@google.com>
2025-09-30 19:01:49 +0000

    612ce99 refactor(compiler-cli): include linkedSignal in the signal debugName transformer (#64106)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-8e80083";
+  var SW_VERSION = "20.3.2+sha-612ce99";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
