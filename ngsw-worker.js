bcf35c7af8e521fde6235361a1376fb4923cd142
kirjs <kirts@google.com>
2025-09-30 15:10:11 +0000

    3db8ef0 docs(docs-infra): fix app-scroller offset. (#64144)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-3598855";
+  var SW_VERSION = "20.3.2+sha-3db8ef0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
