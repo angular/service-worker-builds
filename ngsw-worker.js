4b602acf22128feeb8ace490e199e18d9bde0d0d
kirjs <kirts@google.com>
2025-10-01 16:49:32 +0000

    0537083 Revert "feat(common):  Support of optional keys for the KeyValue pipe (#48814)" (#64179)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-1b102c2";
+  var SW_VERSION = "21.0.0-next.5+sha-0537083";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
