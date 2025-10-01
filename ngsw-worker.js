daa448d8b9bad424b2e91a1088ccfe406aa35046
kirjs <kirts@google.com>
2025-10-01 17:57:29 +0000

    be0455a fix(elements): return value on signal input getter (#62113)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-c0e1c41";
+  var SW_VERSION = "21.0.0-next.5+sha-be0455a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
