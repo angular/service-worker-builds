868fae34d7f7ebd1dc52f642f24165651cdc054a
kirjs <kirts@google.com>
2025-10-01 17:27:23 +0000

    94c9003 refactor(common): update NgIf, ngComponentOutlet and ngTemplateOutlet examples and remove redundant standalone flag (#64155)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-cc8f5e1";
+  var SW_VERSION = "20.3.2+sha-94c9003";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
