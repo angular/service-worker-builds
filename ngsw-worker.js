2b38d299088fcdbe6250b2bd61e44d9bcfffb26d
kirjs <kirts@google.com>
2025-10-01 14:46:16 +0000

    51a0b59 fix(migrations): handle shorthand property declarations in NgModule (#64160)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-5319177";
+  var SW_VERSION = "21.0.0-next.5+sha-51a0b59";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
