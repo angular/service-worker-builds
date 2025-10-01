ecbccfc87da94d0429832791fcc353ebf8c83063
kirjs <kirts@google.com>
2025-10-01 13:07:41 +0000

    24c979d docs: add allowed-host mention (#64145)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-f51ab32";
+  var SW_VERSION = "20.3.2+sha-24c979d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
