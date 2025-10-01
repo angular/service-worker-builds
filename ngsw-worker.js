eaeacc05ad77868a4bad74e5e6bdc72bf6602741
kirjs <kirts@google.com>
2025-10-01 17:18:10 +0000

    ee7d49a docs: update Angular CLI help (#64174)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-0537083";
+  var SW_VERSION = "21.0.0-next.5+sha-ee7d49a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
