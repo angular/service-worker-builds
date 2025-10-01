82c3cfb0373e3814e786bd90c03f19b0ea475bb4
kirjs <kirts@google.com>
2025-10-01 13:46:15 +0000

    3bd4362 docs: update APF docs for new `.d.ts` layout (#64168)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-1cb16fd";
+  var SW_VERSION = "21.0.0-next.5+sha-3bd4362";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
