5e71c429f4a1adac3f8f0f4940de9bee9a726017
kirjs <kirts@google.com>
2025-10-01 17:49:48 +0000

    c0e1c41 build: update ossf/scorecard-action action to v2.4.3 (#64164)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-851c07b";
+  var SW_VERSION = "21.0.0-next.5+sha-c0e1c41";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
