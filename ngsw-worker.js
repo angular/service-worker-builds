890062245d121fb72e02acffd26086b50f134b2b
Miles Malerba <mmalerba@google.com>
2025-10-01 22:47:55 +0000

    a453b1d Revert "fix(core): Fixes animations in conjunction with content projection (#63776)" (#64189)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-952e3b1";
+  var SW_VERSION = "20.3.2+sha-a453b1d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
