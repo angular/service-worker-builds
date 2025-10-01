2d47ae92342f4dbb2c9bf775de52dae0139f5bc5
kirjs <kirts@google.com>
2025-10-01 13:15:49 +0000

    0e928fb fix(core): Fixes animations in conjunction with content projection (#63776)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.2+sha-24c979d";
+  var SW_VERSION = "20.3.2+sha-0e928fb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
