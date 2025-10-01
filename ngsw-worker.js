94d889f2958a32e997bcf9c743be7186c01968f1
kirjs <kirts@google.com>
2025-10-01 17:31:26 +0000

    851c07b build: update all non-major dependencies (#64148)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-61624fd";
+  var SW_VERSION = "21.0.0-next.5+sha-851c07b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
