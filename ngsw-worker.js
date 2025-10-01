1b5ed63623df1288acc170a1a61c91f474bc6122
Miles Malerba <mmalerba@google.com>
2025-10-01 22:52:50 +0000

    7bc4ec4 build: update cross-repo angular dependencies to v21.0.0-next.6 (#64187)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.5+sha-d892aab";
+  var SW_VERSION = "21.0.0-next.5+sha-7bc4ec4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
