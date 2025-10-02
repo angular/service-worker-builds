20e8eaeb45fd41efa650621c52ea96467105ec24
Miles Malerba <mmalerba@google.com>
2025-10-02 21:00:15 +0000

    d3e76b1 fix(migrations): preserve component imports when pruning NgModules in standalone migration (#64186)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.6+sha-e23815b";
+  var SW_VERSION = "21.0.0-next.6+sha-d3e76b1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
