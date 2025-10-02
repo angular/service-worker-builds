7d9579fca660473772b64a22ed3065935d4f0cff
Miles Malerba <mmalerba@google.com>
2025-10-02 17:06:09 +0000

    9eac43c feat(common): Support of optional keys for the KeyValue pipe (#48814)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.6+sha-cb56fbf";
+  var SW_VERSION = "21.0.0-next.6+sha-9eac43c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
