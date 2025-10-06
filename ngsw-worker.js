254de27e5d00fc8d0e9d9605dcd90cb3463e9efa
Miles Malerba <mmalerba@google.com>
2025-10-06 19:41:42 +0000

    3b95910 fix(core): prevent early exit from leave animations when multiple transitions are present (#64225)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.3+sha-9be76ad";
+  var SW_VERSION = "20.3.3+sha-3b95910";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
