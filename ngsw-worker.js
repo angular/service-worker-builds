8c383d2d8d41a43d91669a092e9486da08d7ff0b
Andrew Kushnir <akushnir@google.com>
2025-10-09 18:40:29 +0000

    d73d9ac build: fix broken test (#64319)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.7+sha-e1da41f";
+  var SW_VERSION = "21.0.0-next.7+sha-d73d9ac";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
