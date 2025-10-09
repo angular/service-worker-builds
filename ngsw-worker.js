cdbf06a5365ad607327affad5c667f951a8957ef
Andrew Kushnir <akushnir@google.com>
2025-10-09 12:25:21 +0000

    e8de1b4 refactor(docs-infra): replace @Input with signal input for ignoredElementsIds in ClickOutside directive (#64302)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.4+sha-28b0d53";
+  var SW_VERSION = "20.3.4+sha-e8de1b4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
