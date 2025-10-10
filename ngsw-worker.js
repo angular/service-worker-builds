de78ad4f2bae2b1efd29c8425e816bcd341195ef
Andrew Kushnir <akushnir@google.com>
2025-10-10 13:58:06 +0000

    249fbb3 docs: add example for assertInInjectionContext (#64325)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.7+sha-d73d9ac";
+  var SW_VERSION = "21.0.0-next.7+sha-249fbb3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
