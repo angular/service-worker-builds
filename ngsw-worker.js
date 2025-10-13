8dd374c351051f79985217fd3b57280eba53ca3b
Andrew Kushnir <akushnir@google.com>
2025-10-13 18:17:47 +0000

    f23df71 build: update symbol extractor tests to stabalize the order of the symbols listed (#64389)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.4+sha-e54f41e";
+  var SW_VERSION = "20.3.4+sha-f23df71";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
