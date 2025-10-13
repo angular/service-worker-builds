278eff3a7a619311cc6244b8811f9a92ef682826
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:32:59 +0000

    09a927a ci: remove `cache-node-modules` from `checkout-and-setup-node` (#64387)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.7+sha-62cda78";
+  var SW_VERSION = "21.0.0-next.7+sha-09a927a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
