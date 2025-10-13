a9773bedc47272ba446f3f05f588e3210d138bfb
Andrew Kushnir <akushnir@google.com>
2025-10-13 22:15:41 +0000

    2a7a5de feat(core): Allow passing application providers in `bootstrapModule` options (#64354)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.7+sha-32f86d3";
+  var SW_VERSION = "21.0.0-next.7+sha-2a7a5de";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
