fe7f51eea1bdfeb4844b80fcf939c6bcbd158392
Andrew Kushnir <akushnir@google.com>
2025-10-13 20:09:53 +0000

    32f86d3 perf(forms): optimize `[field]` binding instructions (#64351)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.7+sha-6a96844";
+  var SW_VERSION = "21.0.0-next.7+sha-32f86d3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
