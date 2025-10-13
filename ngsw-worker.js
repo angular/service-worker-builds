7459000a4d9084cbdf32b47121781f0e6d9471ae
Andrew Kushnir <akushnir@google.com>
2025-10-13 23:02:57 +0000

    fdd0856 docs: Removes standalone flag from drag-drop examples (#64395)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.4+sha-0c58b5f";
+  var SW_VERSION = "20.3.4+sha-fdd0856";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
