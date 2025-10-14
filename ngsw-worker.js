ac51bd49be5c0efd005a9578a8b77401fc48c2a3
Andrew Kushnir <akushnir@google.com>
2025-10-14 19:27:56 +0000

    285dad4 build: lock file maintenance (#64377)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.4+sha-5b0363c";
+  var SW_VERSION = "20.3.4+sha-285dad4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
