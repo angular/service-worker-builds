12d0dd6a776be6d0815396081885a9ff89d1826c
Andrew Kushnir <akushnir@google.com>
2025-10-14 16:42:43 +0000

    e941e6b refactor(compiler-cli): add a resource debugName transform (#64172)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.7+sha-c0510cd";
+  var SW_VERSION = "21.0.0-next.7+sha-e941e6b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
