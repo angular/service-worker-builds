376622f08a2c3ee9a52d29169d55b2c1e8e2d54d
Andrew Kushnir <akushnir@google.com>
2025-10-14 20:58:56 +0000

    c2e817b perf(compiler-cli): fix performance of "interpolated signal not invoked" check (#64410)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.4+sha-285dad4";
+  var SW_VERSION = "20.3.4+sha-c2e817b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
