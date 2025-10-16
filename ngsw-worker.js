7464bea69556ed655bf52d8d5d7f20bff2065362
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 17:48:40 +0000

    aab7367 fix(core): update animation scheduling (#64441)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-2e46596";
+  var SW_VERSION = "21.0.0-next.8+sha-aab7367";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
