0d904278455c17fa7f3873aa6927883a1f5ccad4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 20:02:38 +0000

    c1d870b fix(compiler-cli): missingStructuralDirective diagnostic produces false negatives (#64470)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-9a7529d";
+  var SW_VERSION = "21.0.0-next.8+sha-c1d870b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
