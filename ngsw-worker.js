cf93dc1f94e9e15ed0395123e8f486e58fe97e0b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 17:34:55 +0000

    46ae034 fix(router): Respect custom `UrlSerializer` handling of query parameters (#64449)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-1d40465";
+  var SW_VERSION = "21.0.0-next.8+sha-46ae034";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
