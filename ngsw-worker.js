4b8fffbc0e46afb1f163678b18fd19ff0ea37d35
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 18:21:20 +0000

    062a696 refactor(platform-server): use URL constructor for robust parsing (#64494)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-cc5d698";
+  var SW_VERSION = "21.0.0-next.8+sha-062a696";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
