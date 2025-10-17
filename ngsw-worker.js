7010d1663118e0cac7a438290bc97516f13f5bb1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:50:21 +0000

    7d7b4be docs: add matrix parameters docs and examples (#64459)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-10aa4f9";
+  var SW_VERSION = "21.0.0-next.8+sha-7d7b4be";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
