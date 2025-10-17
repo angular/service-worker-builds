f32e741c138fba059f133a498ee3fefd3ccd1564
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:58:05 +0000

    56afa3d docs(docs-infra): Corrects repo name typo in entry parsing logic (#64487)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.6+sha-ccaa9e5";
+  var SW_VERSION = "20.3.6+sha-56afa3d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
