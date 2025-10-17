642664e474cedb6798144ce2e0a2bda263807921
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:36:58 +0000

    3cfa6e8 build: remove unused commit message scopes (#64492)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-c241038";
+  var SW_VERSION = "21.0.0-next.8+sha-3cfa6e8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
