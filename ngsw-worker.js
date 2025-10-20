4882e689bdb5a1ade320ad81bac09bef065476d1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 16:08:33 +0000

    e4e6b92 build: lock file maintenance (#64534)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.6+sha-4073dc5";
+  var SW_VERSION = "20.3.6+sha-e4e6b92";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
