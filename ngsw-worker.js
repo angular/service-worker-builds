c5662be74d5098397163e98142abe9e2c7b6c9e8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 20:25:04 +0000

    9b7a79a refactor(devtools): update debug signal node type to match core type (#64127)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-f6a73f1";
+  var SW_VERSION = "21.0.0-next.8+sha-9b7a79a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
