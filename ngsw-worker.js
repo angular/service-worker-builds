3c8c9285bdc0e5c4a703dfb7814134a2944fc4c5
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 16:48:25 +0000

    50e2c54 build: update rules_browsers digest to 6a699bf (#64559)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.6+sha-e568ddb";
+  var SW_VERSION = "20.3.6+sha-50e2c54";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
