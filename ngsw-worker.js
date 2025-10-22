de5f7bddf00ec01a77814c761451fc1fead3876e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 14:40:25 +0000

    9126d0d docs(docs-infra): fix example viewer (#64580)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-584dd4a";
+  var SW_VERSION = "21.0.0-next.8+sha-9126d0d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
