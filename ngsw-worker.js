e1dee2ce71702c80b263e49d4110c8ca78b745f3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 18:32:44 +0000

    a3e91b2 refactor(forms): Add a test better documenting behavior of the error function in built-in validators (#63993)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-e039c6b";
+  var SW_VERSION = "21.0.0-next.8+sha-a3e91b2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
