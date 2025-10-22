18ae06c97d3e96aaa925a50aa28405f51498d654
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 19:11:52 +0000

    81bd671 fix(core): prevent duplicate nodes from being retained with fast `animate.leave`` calls (#64592)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.8+sha-a3e91b2";
+  var SW_VERSION = "21.0.0-next.8+sha-81bd671";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
