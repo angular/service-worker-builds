8173aca910bb39a71b89149a8adb03d250547580
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 16:17:40 +0000

    02f7263 refactor(forms): rename files related to metadata (#64603)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.9+sha-0385c81";
+  var SW_VERSION = "21.0.0-next.9+sha-02f7263";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
