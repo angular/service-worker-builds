72a86645bd84bb8f468f91054ce19261352b5d54
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 16:58:57 +0000

    5099ed4 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.9+sha-d430d99";
+  var SW_VERSION = "21.0.0-next.9+sha-5099ed4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
