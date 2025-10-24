6f9b34e89ed809c09736750c810f1ae4ad78da3d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 13:30:02 +0000

    b97a677 docs: update workspace configuration link in security documentation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.7+sha-50d2670";
+  var SW_VERSION = "20.3.7+sha-b97a677";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
