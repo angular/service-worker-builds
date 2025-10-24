7feccf551b14817653148ee4aec33f19e486e5d6
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-24 17:07:10 +0000

    620e2c4 Revert "build: refactor location of best-practices.md"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-d7e5aea";
+  var SW_VERSION = "21.1.0-next.0+sha-620e2c4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
