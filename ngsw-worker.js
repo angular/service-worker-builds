a6a6554d48cecd20ecdddd7b428fec9e0030ef2f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 12:48:36 +0000

    04dd75b fix(compiler): support arbitrary nesting in :host-context()

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.9+sha-a1b4e38";
+  var SW_VERSION = "21.0.0-next.9+sha-04dd75b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
