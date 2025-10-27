0a5508560ee6520ac0497f223bd1c43c054b8b97
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 08:34:29 +0000

    14b5590 fix(docs-infra): preserve shell class for multifile code blocks

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.7+sha-6daabb0";
+  var SW_VERSION = "20.3.7+sha-14b5590";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
