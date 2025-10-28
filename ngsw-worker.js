be29c58ebb09a8c1d24bc709c0b836574720eded
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 16:00:28 +0000

    adae565 docs(docs-infra): allow cors for json files

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.7+sha-766559e";
+  var SW_VERSION = "20.3.7+sha-adae565";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
