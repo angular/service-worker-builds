d334a2922b104dd406d9911725360053450900ff
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 20:02:52 +0000

    f80b51a refactor(forms): track arrays in a parent array by index

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-5a93eeb";
+  var SW_VERSION = "21.1.0-next.0+sha-f80b51a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
