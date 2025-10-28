8f8c5739bfc79be9d83e58fc9b4f001cfecd7536
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 15:09:29 +0000

    4f21ea7 refactor(compiler-cli): Remove deep imports of `compiler-cli` in angular/core

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.9+sha-3fbaaa0";
+  var SW_VERSION = "21.0.0-next.9+sha-4f21ea7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
