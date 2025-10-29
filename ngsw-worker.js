85c166aa3690ee61b0a123b30a08938aded3bdbd
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 09:55:47 +0000

    95344c1 fix(migrations): Do not remove a template if it is referenced even with a trailing semilocon

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.9+sha-6e004ca";
+  var SW_VERSION = "21.0.0-next.9+sha-95344c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
