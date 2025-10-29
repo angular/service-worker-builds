551c5532e3cdec7275a8265d84ad89bb7f22b579
Andrew Scott <atscott@google.com>
2025-10-29 20:43:22 +0000

    81b18fc ci: add workflow to copy and generate aria api pages

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-20b4b2c";
+  var SW_VERSION = "21.0.0-rc.0+sha-81b18fc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
