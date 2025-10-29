2f2eb776f1846ffd9854673ec08b9bc58ec207f9
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 13:16:17 +0000

    9123e3d fix(docs-infra): fix broken adev tests (#64761)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.7+sha-2edc82a";
+  var SW_VERSION = "20.3.7+sha-9123e3d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
