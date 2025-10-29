1981cb2ff0d4c0fde460d3e7375e21090d7b980b
Andrew Scott <atscott@google.com>
2025-10-29 20:30:30 +0000

    981fabc feat(docs-infra): add support for namespaces

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-2a06e2b";
+  var SW_VERSION = "21.0.0-rc.0+sha-981fabc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
