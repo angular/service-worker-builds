5287aa4a12a41b85ce11a9fbf9a2dbe84dabdfa3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 17:54:36 +0000

    2a06e2b docs: Rename TodoListItem to UserProfile and fix usage of `isValidUserId`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-next.9+sha-95344c1";
+  var SW_VERSION = "21.0.0-rc.0+sha-2a06e2b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
