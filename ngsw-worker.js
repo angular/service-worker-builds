4419d0667820bc63729e240cedc77befd4b7096e
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:55:08 +0000

    d23bb3c docs(docs-infra): allow cors for json files (#64783)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-58f921d";
+  var SW_VERSION = "21.0.0-rc.0+sha-d23bb3c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
