4d396430a22ad4b42eacd5ec04459f4e2f828e07
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:27:23 +0000

    a832b1c docs: update dependency injection guides (part 1) (#63652)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-a3b08ea";
+  var SW_VERSION = "20.3.9+sha-a832b1c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
