8954fcad6e64c322c01c683be5be7ffd937adfc6
Andrew Scott <atscott01@gmail.com>
2025-10-30 18:21:13 +0000

    29d6eba docs: update Angular Aria apis (#64782)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-56f26f1";
+  var SW_VERSION = "21.1.0-next.0+sha-29d6eba";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
