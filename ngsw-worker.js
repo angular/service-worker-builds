6cc6368dac3709dfb8c23bfa78dabe3048dbf647
Andrew Scott <atscott01@gmail.com>
2025-10-30 15:49:04 +0000

    c84d372 feat(router): Support wildcard params with segments trailing (#64737)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-b0c9c63";
+  var SW_VERSION = "21.1.0-next.0+sha-c84d372";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
