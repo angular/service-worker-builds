490177e7f347c6c9557ab9ba02df7eb7d76672e5
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:47:35 +0000

    debc41f ci: fix typings on exit

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-a832b1c";
+  var SW_VERSION = "20.3.9+sha-debc41f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
