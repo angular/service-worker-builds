7b2684f834602ef2462675c3f47b6f9efece42a0
Andrew Scott <atscott@google.com>
2025-11-03 21:06:41 +0000

    dc99f4c docs(docs-infra): ensure all redirections are absolute

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-32a166b";
+  var SW_VERSION = "21.0.0-rc.0+sha-dc99f4c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
