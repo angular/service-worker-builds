9563b6baec43fd41bd22ccba2f330c64d4ab0c13
Andrew Scott <atscott01@gmail.com>
2025-11-03 23:34:50 +0000

    6b3fdf3 docs: remove feature from changelog.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-d66384d";
+  var SW_VERSION = "21.0.0-rc.0+sha-6b3fdf3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
