4c01f872383c12be75f0b3d243775f10b981e4ac
Andrew Scott <atscott01@gmail.com>
2025-11-03 20:24:06 +0000

    32a166b build: update bazel dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-5d75d0e";
+  var SW_VERSION = "21.0.0-rc.0+sha-32a166b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
