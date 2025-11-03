157532b0646d1ba95d79e19838f61b8cf7c24a41
Andrew Scott <atscott@google.com>
2025-11-03 20:13:10 +0000

    1572baf refactor(core): Replace usages of the `Function` type for animations

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-debc41f";
+  var SW_VERSION = "20.3.9+sha-1572baf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
