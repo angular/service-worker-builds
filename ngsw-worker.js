c0769af1e1c94165e12c6b905ce5079e7771a716
Andrew Scott <atscott01@gmail.com>
2025-11-04 18:11:05 +0000

    dd7cb3d docs: update Angular CLI help

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-1b6d0cf";
+  var SW_VERSION = "20.3.9+sha-dd7cb3d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
