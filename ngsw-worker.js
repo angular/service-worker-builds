c6924e639681cea63d6a728dee81d2bcd2affba4
Andrew Scott <atscott01@gmail.com>
2025-11-04 22:54:32 +0000

    43f7f41 fix(docs-infra): properly join url parts

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-ec9b074";
+  var SW_VERSION = "20.3.9+sha-43f7f41";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
