f02c066468e198dd365b00e143c1b9aad10e17bd
Andrew Scott <atscott@google.com>
2025-11-04 23:03:05 +0000

    c374e80 docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-43f7f41";
+  var SW_VERSION = "20.3.9+sha-c374e80";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
