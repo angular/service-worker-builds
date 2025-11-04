91c1f2046a12bc4c972ab91ae00b806ae5e6d961
Andrew Scott <atscott@google.com>
2025-11-04 00:31:31 +0000

    34f53aa docs: remove standalone: true since it's now the default

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-a702c87";
+  var SW_VERSION = "21.0.0-rc.0+sha-34f53aa";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
