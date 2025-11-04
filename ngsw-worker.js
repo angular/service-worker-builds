39ea48787c99785bd25798dfcdebaa520d9e883e
Andrew Scott <atscott@google.com>
2025-11-04 23:30:23 +0000

    a45e6b2 fix(migrations): Prevent removal of templates referenced with preceding whitespace characters

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-c374e80";
+  var SW_VERSION = "20.3.9+sha-a45e6b2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
