f38dace6ee4afc71d8bbcf5d904feca52cdfc3e4
Andrew Scott <atscott@google.com>
2025-11-04 04:27:02 +0000

    36f4c9b refactor(core): Removes unused flag for onDestroy

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.0+sha-3140b87";
+  var SW_VERSION = "21.0.0-rc.0+sha-36f4c9b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
