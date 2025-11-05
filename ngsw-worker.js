88d766942c32ee650e0a99211c52b5acd85bdbb2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-05 23:23:08 +0000

    086ce4b build: update all github actions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-4bdd317";
+  var SW_VERSION = "21.0.0-rc.1+sha-086ce4b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
