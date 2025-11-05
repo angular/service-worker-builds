b8b27b3f501e876a981fdd235c59385464a842ae
Andrew Scott <atscott@google.com>
2025-11-05 19:52:09 +0000

    ab94cd2 docs: Add the missing function call to use the signal's actual value

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.10+sha-098ed8c";
+  var SW_VERSION = "20.3.10+sha-ab94cd2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
