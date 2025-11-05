058e19cca40b08ca0c392c5e4da81fa23114b03d
Andrew Scott <atscott01@gmail.com>
2025-11-05 17:17:47 +0000

    7c49c53 refactor(common): remove redundant `providedIn: 'root'` from injection tokens

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-1cd5b44";
+  var SW_VERSION = "21.1.0-next.0+sha-7c49c53";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
