0b96addde4dd1bee766479fd72c865b28d8d0df4
Andrew Scott <atscott01@gmail.com>
2025-11-05 18:40:50 +0000

    098ed8c release: cut the v20.3.10 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.9+sha-22d190c";
+  var SW_VERSION = "20.3.10+sha-098ed8c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
