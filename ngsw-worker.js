cac0283f6cbff5880ee2abf03b3c68fdaeba62ab
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 15:58:43 +0000

    26995e2 fix(docs-infra): properly expose the prerendered routes for sitemap generation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-f0ffd9b";
+  var SW_VERSION = "21.0.0-rc.1+sha-26995e2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
