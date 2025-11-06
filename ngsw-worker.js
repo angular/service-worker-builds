41b5dcdabbe6815ac0b4f8d5e088bb51f5e5ae4a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 17:51:09 +0000

    dcdd1bc fix(core): skip leave animations on view swaps

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.10+sha-d6ef181";
+  var SW_VERSION = "20.3.10+sha-dcdd1bc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
