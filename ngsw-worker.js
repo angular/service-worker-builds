2a10271d165c8b2dcc01b42176f2f31988ec7bfa
GitHub <noreply@github.com>
2025-11-06 18:14:58 +0000

    ee578d3 build: format md files

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.10+sha-dcdd1bc";
+  var SW_VERSION = "20.3.10+sha-ee578d3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
