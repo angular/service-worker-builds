99e29b5725500867cc20637dc2ebaad4ee05a92b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 17:47:30 +0000

    08a84e0 refactor(router): simplify imports and improve option access in RouterScroller tests

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-25e13e9";
+  var SW_VERSION = "21.1.0-next.0+sha-08a84e0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
