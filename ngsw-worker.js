1edd3a9d7ca7b378a736aecfc085bda2e5f948ea
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:51:47 +0000

    f54cc4f fix(compiler): support commas in :host() argument

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-0552e72";
+  var SW_VERSION = "21.0.0-rc.1+sha-f54cc4f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
