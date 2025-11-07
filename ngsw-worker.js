583a79d4a0666e58c80a35ee73509633c829d291
Andrew Kushnir <akushnir@google.com>
2025-11-07 23:33:41 +0000

    590244a docs(core): add a11y considerations related to `@defer()`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-647e640";
+  var SW_VERSION = "21.0.0-rc.1+sha-590244a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
