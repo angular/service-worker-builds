f1a339c3c0871133c5ce242b4816ca15ec751437
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:19:09 +0000

    46c1a80 docs(docs-infra): refactor update to use host property and refactor isWindows

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-a3cb1df";
+  var SW_VERSION = "21.0.0-rc.1+sha-46c1a80";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
