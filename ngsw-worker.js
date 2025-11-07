0739f5a6e0a5d2dad20469f782b4b11698a45580
Andrew Kushnir <akushnir@google.com>
2025-11-07 19:41:13 +0000

    0f55443 refactor: exclude tsconfig.json from VSIX package contents (#64991)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-680c3c7";
+  var SW_VERSION = "21.1.0-next.0+sha-0f55443";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
