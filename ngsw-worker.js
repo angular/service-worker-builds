7a7a1721f663b9cb1812652c9dd6480c43a24c5d
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 19:49:10 +0000

    aada582 build: update `rules_angular` to latest

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-1e53f4e";
+  var SW_VERSION = "21.0.0-rc.1+sha-aada582";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
