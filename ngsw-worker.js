f40600b74cb956ae78102a6f0c83b079256b8b9c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-07 15:49:08 +0000

    b8972da refactor(devtools): replace HostListener with host metadata for keydown handling

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.10+sha-41466a7";
+  var SW_VERSION = "20.3.10+sha-b8972da";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
