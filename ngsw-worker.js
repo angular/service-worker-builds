ae6f7a66358730c5cb715cbcda508ac0acac7376
Andrew Kushnir <akushnir@google.com>
2025-11-07 21:03:16 +0000

    647e640 refactor(core): Extract NavigateEvent cancel steps based on recent spec update

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-781a329";
+  var SW_VERSION = "21.0.0-rc.1+sha-647e640";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
