1f498c310f88acf0a88ae522daab58ae23b9bf49
Andrew Kushnir <akushnir@google.com>
2025-11-10 17:59:52 +0000

    650af71 refactor(http): migrate XSRF classes to use inject() function

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-afa2ac2";
+  var SW_VERSION = "21.0.0-rc.1+sha-650af71";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
