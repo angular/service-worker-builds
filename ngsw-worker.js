2471eb7b61dbd1575c01ca4e42299ec99358f048
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:07:34 +0000

    5092a89 docs: add shell language to CLI installation examples

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.1+sha-590244a";
+  var SW_VERSION = "21.0.0-rc.1+sha-5092a89";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
