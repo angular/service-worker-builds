52d3d135a29759b0126c39b3c97721edc81a8bd1
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-10 16:07:38 +0000

    2db8f2e refactor(router): Add handling of abort event on signal of NavigateEvent

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-d2b854b";
+  var SW_VERSION = "21.1.0-next.0+sha-2db8f2e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
