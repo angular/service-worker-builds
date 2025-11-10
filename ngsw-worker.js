4a879286594dbd62b13d19075b5bf4a843148eec
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-10 22:23:01 +0000

    ec9dc94 feat(platform-browser): add `context` to `createApplication`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-feb86e3";
+  var SW_VERSION = "21.1.0-next.0+sha-ec9dc94";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
