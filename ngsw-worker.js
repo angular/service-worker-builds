a572273ff6b6c0dadeb3750f783e82b24c62ae72
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 18:24:38 +0000

    3b1fbf0 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-75dd600";
+  var SW_VERSION = "21.1.0-next.0+sha-3b1fbf0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
