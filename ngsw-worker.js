fc3059ad1be777a27fac344c04f02d59c416aea6
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 22:16:40 +0000

    c3e82fb refactor(core): render additional WAI-ARIA 1.3 property bindings as attributes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-4ed8781";
+  var SW_VERSION = "21.1.0-next.0+sha-c3e82fb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
