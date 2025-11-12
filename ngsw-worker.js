21caa109753da2bae3871d8eccaabcd43e620cf8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 21:18:00 +0000

    1c4fb90 docs: update tutorials with self-closing tags, standalone router directives, and fix syntax highlighting

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.11+sha-2e7623c";
+  var SW_VERSION = "20.3.11+sha-1c4fb90";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
