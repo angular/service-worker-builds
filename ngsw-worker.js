3d9a65425220710feaea0be514a241c863e0d256
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-12 17:48:29 +0000

    18cc260 build: update bazel dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.2+sha-909588e";
+  var SW_VERSION = "21.0.0-rc.2+sha-18cc260";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
