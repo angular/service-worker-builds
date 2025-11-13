57c8d4d5b1c92e5c74251ea7cabd71e303f5a2a7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:05:27 +0000

    fdd267a ci: use hardcoded global approvers group

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.11+sha-70da36f";
+  var SW_VERSION = "20.3.11+sha-fdd267a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
