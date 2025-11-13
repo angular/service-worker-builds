d1e8110e19f2a3eb9775aa38b2319f889b3b831f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:10:02 +0000

    482e137 docs(docs-infra): improve theme picker accessibility and add animations

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.2+sha-16d51f6";
+  var SW_VERSION = "21.0.0-rc.2+sha-482e137";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
