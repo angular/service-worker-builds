2e567d6593ca98c4d29048152b5c9784c1492a5a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:46:18 +0000

    750da17 docs: add signal forms models guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.2+sha-d7101de";
+  var SW_VERSION = "21.0.0-rc.2+sha-750da17";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
