2a9670da5ba216ff4965daab922b8d312198189a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:59:23 +0000

    911999e docs: update cross-repo adev docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.2+sha-f550f4a";
+  var SW_VERSION = "21.0.0-rc.2+sha-911999e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
