9dca32938395ba251fda12e7ff6932c2f561a99c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 20:28:54 +0000

    3228cce docs(docs-infra): enable tabs layout

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.2+sha-8a5b284";
+  var SW_VERSION = "21.0.0-rc.2+sha-3228cce";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
