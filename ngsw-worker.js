a0a74065da43382c6278297d9ba36eed64a95f7a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 16:54:49 +0000

    0432e76 docs: Adds links to relevant guides for APIs in core package

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-599b72e";
+  var SW_VERSION = "21.1.0-next.0+sha-0432e76";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
