69202844dd61955e13bc17ec0352b938e3a053b6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:13:47 +0000

    68c5556 docs(router): update navigation event example to use event.code

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-f7fcef1";
+  var SW_VERSION = "20.3.12+sha-68c5556";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
