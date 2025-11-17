3e45486f0047f2b6fbfbbb800f2b8bc8a70cf601
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 23:13:21 +0000

    72b19d8 docs: replace @HostListener with host event bindings in attribute directives

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-81b6e45";
+  var SW_VERSION = "20.3.12+sha-72b19d8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
