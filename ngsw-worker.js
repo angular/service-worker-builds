f3afe4f42819a7f4fced4450e46319743e07843c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 18:41:18 +0000

    85777ef Revert "docs: replace legacy <docs-code> with fenced code blocks with highlight metadata"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-820bb39";
+  var SW_VERSION = "20.3.12+sha-85777ef";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
