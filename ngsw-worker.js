db0a48c771ea657cf542afd038c3a22ec4f515d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:34:18 +0000

    4a1a703 docs(docs-infra): set max-height to code example

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-daae263";
+  var SW_VERSION = "20.3.12+sha-4a1a703";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
