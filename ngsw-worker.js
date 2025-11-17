1e5950f4b4dc5cb9902dee9c6a97632354efef3f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:05:25 +0000

    f7fcef1 docs(docs-infra): simplify file headers in <docs-code> blocks

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-e0a6dad";
+  var SW_VERSION = "20.3.12+sha-f7fcef1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
