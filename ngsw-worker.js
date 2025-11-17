c18e25bc4334ead9dbbf585f004f52c288f5b45c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 16:54:49 +0000

    a7dddcc docs: Adds links to relevant guides for APIs in core package

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.2+sha-59543d6";
+  var SW_VERSION = "21.0.0-rc.2+sha-a7dddcc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
