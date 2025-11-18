268308133545713e19c94ee6a13294a091f9aff1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 16:20:19 +0000

    3686987 docs: simplify docs-code header by removing paths and keeping only file name and info

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-72b19d8";
+  var SW_VERSION = "20.3.12+sha-3686987";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
