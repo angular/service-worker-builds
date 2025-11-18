30fd125d3d86757b65fefd22c12051d500bec9ad
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 22:17:57 +0000

    73e9700 docs: format examples with replacing <docs-code> typescript examples with fenced ts code blocks

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.3+sha-e43b4da";
+  var SW_VERSION = "21.0.0-rc.3+sha-73e9700";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
