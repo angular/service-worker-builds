a1903de0f652ed345d20839e6eb3ea0886f8dbf7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 23:30:24 +0000

    2bf0c52 refactor(core): export profile event as enum and move profile_types.ts and framework to shared devtools folder

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-e0026c8";
+  var SW_VERSION = "21.0.0+sha-2bf0c52";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
