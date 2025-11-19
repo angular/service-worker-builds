2d6e820db89c2ef1fba68ab3e561798ca14176cd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 22:19:28 +0000

    d9017a4 docs: update aria overview page

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-4e14a53";
+  var SW_VERSION = "21.0.0+sha-d9017a4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
