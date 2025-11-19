058c9992ce4614d5216fb89d25af05f7b69cc3a5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:18:33 +0000

    d2fe867 docs: update aria overview page

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-c231794";
+  var SW_VERSION = "21.1.0-next.0+sha-d2fe867";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
