3b699605237241629a4d33e60ac7481c66612c0c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:24:39 +0000

    e69855a docs: aria grid replace overview example with a playful one

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.3+sha-28c1a2d";
+  var SW_VERSION = "21.0.0-rc.3+sha-e69855a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
