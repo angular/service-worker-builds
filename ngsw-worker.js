cc7aae56a7c4a38b921946ea14534f4a960d8000
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 01:04:53 +0000

    330e046 docs: fix docs heading alignment

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-5aea52a";
+  var SW_VERSION = "20.3.12+sha-330e046";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
