8c06118d08450eef11abb991409a902449dae5bd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 01:07:05 +0000

    28c1a2d docs: add signal forms comparison guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0-rc.3+sha-012326e";
+  var SW_VERSION = "21.0.0-rc.3+sha-28c1a2d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
