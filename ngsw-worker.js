ba808a53094f9b338e4910b94a4374f915ea836f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:30:35 +0000

    24c999a fix(docs-infra): add moduleResolution to TypeScript compiler options for playground

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.12+sha-96a8d58";
+  var SW_VERSION = "20.3.12+sha-24c999a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
