6e32bc372379a7a7f905b8defd2396277025f0a7
kirjs <kirts@google.com>
2025-11-20 18:32:47 +0000

    7219759 docs: add v21 event game

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-cd86de1";
+  var SW_VERSION = "21.0.0+sha-7219759";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
