cf95d2f02d88009e001f4c26f80c94744bac30f7
kirjs <kirts@google.com>
2025-11-20 15:40:09 +0000

    cf7579f docs: add signal forms custom controls guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-f31fbdd";
+  var SW_VERSION = "21.0.0+sha-cf7579f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
