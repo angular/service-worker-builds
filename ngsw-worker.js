b05becca7c063d5f17323be57f584aef1fd8416a
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 18:32:15 +0000

    b8fa9cc docs: add v21 event game

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-be6f2f0";
+  var SW_VERSION = "21.1.0-next.0+sha-b8fa9cc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
