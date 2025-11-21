70f92e1d8c6863bd0aad3003a72ab977888b9c06
kirjs <kirts@google.com>
2025-11-21 21:39:38 +0000

    a70a191 docs: Adds signal type checking documentation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-b8da1d4";
+  var SW_VERSION = "21.0.0+sha-a70a191";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
