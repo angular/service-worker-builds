ac69782aac7f412e7aec4c9788b33d290dd19ec2
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 16:00:43 +0000

    68f5cee docs: fix incorrect Signal Forms link

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-b7d91a7";
+  var SW_VERSION = "21.1.0-next.0+sha-68f5cee";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
