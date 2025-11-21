0c5068d3c6a553fb8c9b608dc72be2b07d8d7b7c
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 21:38:59 +0000

    38a354f docs: Adds signal type checking documentation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-f884c18";
+  var SW_VERSION = "21.1.0-next.0+sha-38a354f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
