4f41675d868aa600703571925515dc197084fcb7
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-21 21:27:57 +0000

    f884c18 docs: format JSON examples and replace <docs-code> with fenced code blocks

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-68aa83e";
+  var SW_VERSION = "21.1.0-next.0+sha-f884c18";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
