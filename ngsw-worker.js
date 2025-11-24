042ee3d6afd782f7c0f2393dbdcf4a01d409211e
kirjs <kirts@google.com>
2025-11-24 18:34:26 +0000

    534cc3d docs: standardize aria code examples to not use component suffix

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-a62162d";
+  var SW_VERSION = "21.0.0+sha-534cc3d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
