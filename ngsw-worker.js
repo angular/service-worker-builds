fd834b14c8992d92a0a58a1003c70a7b05686bbd
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:45:07 +0000

    a4f50bd fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-b26fe73";
+  var SW_VERSION = "21.1.0-next.0+sha-a4f50bd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
