22f46427ddec5bf0f0c1fa36ddedc18271bfaa45
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 18:54:30 +0000

    f97a1d4 refactor(forms): support custom control directives

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-d442c72";
+  var SW_VERSION = "21.1.0-next.0+sha-f97a1d4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
