8a0899a307d57cf4cab382687b50a18d6f2b88a2
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:23:47 +0000

    9dd7cc1 ci: adding myself as optional to the compiler scope

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-0f4b11c";
+  var SW_VERSION = "21.1.0-next.0+sha-9dd7cc1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
