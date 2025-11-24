422c8f0a8e8d00419aa966bea9d511149115e662
kirjs <kirts@google.com>
2025-11-24 22:04:29 +0000

    0e458c7 refactor(core): Remove toggles used for zoneless by default migration

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-70507b8";
+  var SW_VERSION = "21.0.0+sha-0e458c7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
