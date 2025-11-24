01f0092331b152049934d3212c984657ae83be6a
kirjs <kirts@google.com>
2025-11-24 16:37:03 +0000

    9852033 refactor(compiler-cli): add a resource debugName transform (#64172)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-102cb87";
+  var SW_VERSION = "21.0.0+sha-9852033";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
