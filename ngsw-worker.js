e6c4db037f1cf57a5a5fc9be7067fc5d307c179a
kirjs <kirts@google.com>
2025-11-24 14:13:07 +0000

    92f0b66 docs: fix v21 event header image

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-696aaa1";
+  var SW_VERSION = "21.0.0+sha-92f0b66";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
