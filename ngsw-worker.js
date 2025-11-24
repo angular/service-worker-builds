bf404845b34b0d1aa0acb90a333f0b580f1651e9
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 16:16:32 +0000

    b5ae62c docs: update devtools guide - splits the injector inspector into its own guide - update the screenshots - add defer block section - add hydration section

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-0203dad";
+  var SW_VERSION = "21.1.0-next.0+sha-b5ae62c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
