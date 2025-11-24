f37a25e0c6500540a043cc7a61d1cf259ba029d7
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 18:24:16 +0000

    2ac8268 docs: Update router docs to add references and components input fixed syntaxis

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-a4f50bd";
+  var SW_VERSION = "21.1.0-next.0+sha-2ac8268";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
