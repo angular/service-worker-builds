f11aa3fde45228b1d9ef7beff4d8062c591cc6d9
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 14:21:00 +0000

    3a1eb07 fix(forms): allow dynamic `type` bindings on signal form controls

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-a294e3b";
+  var SW_VERSION = "21.1.0-next.0+sha-3a1eb07";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
