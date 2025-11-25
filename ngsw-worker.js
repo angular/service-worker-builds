68305483675c4fd23a7499cbb9b30334c8ccd9fb
kirjs <kirts@google.com>
2025-11-25 14:22:51 +0000

    8acf5d2 fix(forms): allow dynamic `type` bindings on signal form controls

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-d5b339c";
+  var SW_VERSION = "21.0.0+sha-8acf5d2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
