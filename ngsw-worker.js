e0acf99e40ec2d9b14fd0dfd04bb79c14640dc82
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 20:10:55 +0000

    6b1fb02 docs: release notes for the v21.0.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-40790ef";
+  var SW_VERSION = "21.1.0-next.0+sha-6b1fb02";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
