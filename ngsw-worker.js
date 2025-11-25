7b6f26bf8cf7fc0e77c027b38a66f36261309196
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 16:44:30 +0000

    3b1fa82 fix(core): use injected `DOCUMENT` for `CSP_NONCE`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-5bfa027";
+  var SW_VERSION = "21.1.0-next.0+sha-3b1fa82";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
