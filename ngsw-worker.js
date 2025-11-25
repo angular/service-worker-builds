3d9e2154ddf0538ecccf9fa1ab1874f47020c104
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-25 15:24:35 +0000

    67d41e9 docs: improve core package API documentation with additional reference links

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-3a1eb07";
+  var SW_VERSION = "21.1.0-next.0+sha-67d41e9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
