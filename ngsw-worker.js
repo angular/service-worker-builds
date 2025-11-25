8994785c55b47f1af42aeceb95180026c6c2a120
kirjs <kirts@google.com>
2025-11-25 12:49:03 +0000

    d5b339c docs: fix example imports and missing event URL reference

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.0+sha-0e458c7";
+  var SW_VERSION = "21.0.0+sha-d5b339c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
