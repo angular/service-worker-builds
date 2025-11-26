aac7bff79492c7b31a54b5673757e390039e138d
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-26 18:18:05 +0000

    a0930e1 refactor(devtools): Use the same error message as the signal custom formatter for signal errors

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-add8c41";
+  var SW_VERSION = "21.1.0-next.0+sha-a0930e1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
