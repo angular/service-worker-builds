d12313226140e15e2f6e2fe6b034b207d9d091e5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:46:21 +0000

    ee53e6f docs: release notes for the v19.2.17 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-1c6b070";
+  var SW_VERSION = "21.1.0-next.0+sha-ee53e6f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
