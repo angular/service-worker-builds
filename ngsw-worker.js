5d610dab584bae4df30d80b522b3eb8d98d81b8c
GitHub <noreply@github.com>
2025-12-01 09:31:40 +0000

    1c6b070 fix(compiler): prevent XSS via SVG animation `attributeName` and MathML/SVG URLs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-8f3fdc3";
+  var SW_VERSION = "21.1.0-next.0+sha-1c6b070";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
