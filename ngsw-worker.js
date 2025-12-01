62790e7890ef972caccfa8f0c51e2f07f9129e07
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:25:50 +0000

    8ab7594 docs: format typescript examples and replace <docs-code> with fenced code blocks

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.2+sha-ed32e8c";
+  var SW_VERSION = "21.0.2+sha-8ab7594";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
