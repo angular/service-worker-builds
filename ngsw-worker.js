f22cd53cbc0877cdd2e6bb563ae275e87fc55333
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-01 09:38:21 +0000

    78fd159 fix(compiler): prevent XSS via SVG animation `attributeName` and MathML/SVG URLs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.1+sha-fec1f1a";
+  var SW_VERSION = "21.0.1+sha-78fd159";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
