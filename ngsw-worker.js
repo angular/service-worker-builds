34529ae6f1deeb9a438efa33ded4a560620463e9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:51:04 +0000

    4755bbd release: cut the v20.3.15 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.14+sha-d1ca8ae";
+  var SW_VERSION = "20.3.15+sha-4755bbd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
