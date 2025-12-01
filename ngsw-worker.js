be44b585f7321646dd3fe1453f3964f71322012b
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:56:12 +0000

    8c4a152 release: cut the v21.0.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.1+sha-78fd159";
+  var SW_VERSION = "21.0.2+sha-8c4a152";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
