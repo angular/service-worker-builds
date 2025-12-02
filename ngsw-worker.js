c07e09597226dea07217c3dc04bfac0cf724a9f8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 11:11:34 +0000

    c0e0010 fix(devtools): hide profiler node details when the frame is changed

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.2+sha-2935fef";
+  var SW_VERSION = "21.0.2+sha-c0e0010";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
