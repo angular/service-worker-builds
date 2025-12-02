90fcaa6d95655b717998a565a9c5907eaeab427c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 12:09:32 +0000

    f35b2ef refactor(compiler): Generate the `controlCreate` instruction after the native element has been created

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-a784995";
+  var SW_VERSION = "21.1.0-next.0+sha-f35b2ef";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
