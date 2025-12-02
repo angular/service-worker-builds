405395ef5a0d45d3a0ef8af98ec1644f7020093d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 15:50:55 +0000

    a0ad5d4 test(router): Add tests for support of path params before/after a wildcard

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-e30e61b";
+  var SW_VERSION = "21.1.0-next.0+sha-a0ad5d4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
