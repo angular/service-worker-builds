8a0233d228d19ee6d2287985d329e67674fa9a49
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 14:17:01 +0000

    e30e61b fix(compiler-cli): avoid allocating an object for signals in production mode

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-f35b2ef";
+  var SW_VERSION = "21.1.0-next.0+sha-e30e61b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
