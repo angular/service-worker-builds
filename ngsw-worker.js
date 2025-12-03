a372137889f34fc00a8ee281b1eea057fbe2b113
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 20:56:49 +0000

    b96f65a fix(forms): memoize reads of child fields in signal forms (#65802)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.1+sha-fc23fcd";
+  var SW_VERSION = "21.1.0-next.1+sha-b96f65a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
