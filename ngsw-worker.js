ff945706bb4acc40a61da2444156b9274b960354
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 19:30:34 +0000

    eebcaf4 ci: use defined ng-dev config for deploying docs site

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.1+sha-7f8ffa3";
+  var SW_VERSION = "21.1.0-next.1+sha-eebcaf4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
