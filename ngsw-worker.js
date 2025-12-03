71c2c83d9f1f7fa368f91b12e295a3d0d6df574e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 10:58:54 +0000

    819b640 docs: prevent showing typescript diagnostic errors until the node runtime reaches a ready state

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.0+sha-13353bf";
+  var SW_VERSION = "21.1.0-next.0+sha-819b640";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
