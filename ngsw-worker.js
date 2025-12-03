dd5e9732ebfd0e08aae670d589e7bafa284bf70e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:35:42 +0000

    2e5d187 fix(docs-infra): prevent readonly TypeScript keyword from being linked

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-c355ffd";
+  var SW_VERSION = "21.0.3+sha-2e5d187";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
