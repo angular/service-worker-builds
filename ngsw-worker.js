75a2c9ab025d616108027cbad9087cb4016f29b9
GitHub <noreply@github.com>
2025-12-04 19:39:16 +0000

    5be3304 refactor(forms): Break logic.ts into separate files

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.1+sha-b96f65a";
+  var SW_VERSION = "21.1.0-next.1+sha-5be3304";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
