5bd878cce7fe5e7b55ed97d151838dd2e83b6708
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-04 19:51:39 +0000

    28e7546 feat(docs-infra): forward preview error locations

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-c3bd22d";
+  var SW_VERSION = "21.0.3+sha-28e7546";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
