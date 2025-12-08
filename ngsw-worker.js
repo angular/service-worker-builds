139aa540511ec8e66c3befb29c507e29fcfa68b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 16:57:19 +0000

    28017c1 docs: add browser-mode option description to Vitest migration guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.1+sha-789f91b";
+  var SW_VERSION = "21.1.0-next.1+sha-28017c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
