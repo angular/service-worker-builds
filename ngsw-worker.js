ada47fda5d6f93dea2a2fd2ccc2490f251924195
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 18:10:29 +0000

    253dc95 feat(docs-infra): add complexity level badge

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.1+sha-dd47c57";
+  var SW_VERSION = "21.1.0-next.1+sha-253dc95";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
