a557738a1d3f06c2394a5d6482bab4a34d5e1473
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-09 21:50:14 +0000

    96bb4c6 build: add the ignore all rc files flag when running bazel info

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-7f96799";
+  var SW_VERSION = "21.0.3+sha-96bb4c6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
