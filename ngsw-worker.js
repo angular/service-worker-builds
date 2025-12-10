fad2161b613e0281248648e8e789b4b5f47ba98a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 18:25:55 +0000

    497c0e3 refactor(core): Add transition.to property to FakeNavigation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-2dd971b";
+  var SW_VERSION = "21.0.3+sha-497c0e3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
