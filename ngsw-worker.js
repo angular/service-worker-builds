9c408142885f9efd4990b14f1b748d36292156cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:38:19 +0000

    af6c01f build: update pnpm to v10.25.0

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.1+sha-954c546";
+  var SW_VERSION = "21.1.0-next.1+sha-af6c01f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
