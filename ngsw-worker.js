13b66dd77428f0d49a4642e1226a44798b06e055
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 18:14:41 +0000

    2dd971b docs: hide menu icons from screen readers in aria example

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-834593b";
+  var SW_VERSION = "21.0.3+sha-2dd971b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
