3b5c251d026227b1619762eb5a4d995edfbf2250
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:54:16 +0000

    fa4e6e6 release: cut the v21.0.4 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-c937da4";
+  var SW_VERSION = "21.0.4+sha-fa4e6e6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
