a2201b5468c6f1e1c9b9ce7297ea2ea58543c37a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 19:29:13 +0000

    69d243a fix(core): avoid false-positive deprecation when using `InjectionToken` with factory only

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.4+sha-fa4e6e6";
+  var SW_VERSION = "21.0.4+sha-69d243a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
