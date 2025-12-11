d8bc1cd45bccabebc579322a788df8645fd484d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:38:05 +0000

    d0097f7 fix(forms): fix signal forms type error

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.2+sha-4f6014a";
+  var SW_VERSION = "21.1.0-next.2+sha-d0097f7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
