fb0d7b3f92835b5bef7c002539b9df0d3dc38d79
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:48:00 +0000

    c937da4 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.3+sha-67a5a65";
+  var SW_VERSION = "21.0.3+sha-c937da4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
