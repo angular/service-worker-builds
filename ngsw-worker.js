4a0fa09a63b0736e7fecfa372b732edbfd451ced
Andrew Kushnir <akushnir@google.com>
2025-12-15 18:05:57 +0000

    15fd24d docs: replace `@debugging` tag with proper Markdown syntax

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.5+sha-fe59d9a";
+  var SW_VERSION = "21.0.5+sha-15fd24d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
