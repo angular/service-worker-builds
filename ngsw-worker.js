7947d526ff46b17b7a384731c3d6c9ff9aea0e4d
Andrew Kushnir <akushnir@google.com>
2025-12-16 23:04:18 +0000

    feed89e ci: reformat files

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.5+sha-729b964";
+  var SW_VERSION = "21.0.5+sha-feed89e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
