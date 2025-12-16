020ad2be0509edb93656f99885c2bfe9e8869a0f
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 21:40:05 +0000

    06be803 fix(core): Microtask scheduling should be used after any application synchronization

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.3+sha-ae0c590";
+  var SW_VERSION = "21.1.0-next.3+sha-06be803";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
