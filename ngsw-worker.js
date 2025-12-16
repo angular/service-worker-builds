b8d44557d53bc8a3cb07c2f242181ab3d1d21dc8
Andrew Kushnir <akushnir@google.com>
2025-12-16 01:49:03 +0000

    c250bd0 refactor(docs-infra): remove deprecated h.JSX.HTMLAttributes usage

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.5+sha-ca167b9";
+  var SW_VERSION = "21.0.5+sha-c250bd0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
