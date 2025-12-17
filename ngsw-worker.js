b4c499fef3edc274c93caae7e47e2d95549e73a8
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 17:45:24 +0000

    d23ce76 docs(docs-infra): leverage the search on 404

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.3+sha-9fa77af";
+  var SW_VERSION = "21.1.0-next.3+sha-d23ce76";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
