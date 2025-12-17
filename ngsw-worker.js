83a22edede803c2297d8b305bcba872d8385f03d
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 21:24:04 +0000

    664fa39 release: cut the v21.1.0-next.4 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.3+sha-868cb7c";
+  var SW_VERSION = "21.1.0-next.4+sha-664fa39";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
