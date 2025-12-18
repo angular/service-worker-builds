f722039607e9bf547b45b58aac5fdad4ac0cf1ac
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-18 01:02:27 +0000

    069974a build: update dependency @angular/aria to v21.0.4

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-59b3790";
+  var SW_VERSION = "21.0.6+sha-069974a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
