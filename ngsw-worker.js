e032b5d9aec42df94235845a36064d92f2d7e8b4
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 07:43:29 +0000

    d868a5b build: update bazel dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.4+sha-5a146b3";
+  var SW_VERSION = "21.1.0-next.4+sha-d868a5b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
