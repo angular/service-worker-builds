63220cf0a87d5b1889182fec057aeeb650522500
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 22:40:41 +0000

    9184146 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.4+sha-df2aaaa";
+  var SW_VERSION = "21.1.0-next.4+sha-9184146";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
