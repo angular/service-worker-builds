382845185d57539afbb6a65cbf547063c1a07a8c
kirjs <kirts@google.com>
2026-01-06 17:28:37 +0000

    8e80874 fix(compiler): better types for a few expression AST nodes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-45bfd82";
+  var SW_VERSION = "21.0.6+sha-8e80874";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
