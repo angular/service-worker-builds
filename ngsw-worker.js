5ddf05b066749550b66acdf41e77a028ac84f07d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 15:25:43 +0000

    9d33cf3 build: lock file maintenance

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-a119427";
+  var SW_VERSION = "21.0.6+sha-9d33cf3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
