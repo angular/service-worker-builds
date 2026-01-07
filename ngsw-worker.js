8a8a36255d2045155e0cd9aed71ae041c7f0a494
kirjs <kirts@google.com>
2026-01-07 14:16:12 +0000

    0ff2d5a docs: fixed incorrect link in migrating-to-vitest.md

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-4161161";
+  var SW_VERSION = "21.0.6+sha-0ff2d5a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
