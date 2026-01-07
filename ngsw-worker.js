858a97771b715a2b128ddedd48da1163a5d42c29
kirjs <kirts@google.com>
2026-01-07 19:25:09 +0000

    c3adc5a docs(docs-infra): Fix combobox dialog styles for dark theme

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-61370dc";
+  var SW_VERSION = "21.0.6+sha-c3adc5a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
