6bdc02150d39b84d5d75b816b3c67f1f09e7f591
kirjs <kirts@google.com>
2026-01-07 14:40:59 +0000

    7738eb9 docs(docs-infra): Use category for signal forms guides

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-c1e9627";
+  var SW_VERSION = "21.0.6+sha-7738eb9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
