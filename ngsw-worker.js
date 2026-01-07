e453886f55c89e0493d1578a2ba4167d8287cd31
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-07 00:34:17 +0000

    174d2da Revert "fix(router): Ensure createUrlTree does not reuse segments of input"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.4+sha-2d85ae5";
+  var SW_VERSION = "21.1.0-next.4+sha-174d2da";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
