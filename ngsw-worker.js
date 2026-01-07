da9ece309d3888e9474fbe11b56f078c7d40078c
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 22:25:11 +0000

    f76d891 release: cut the v20.3.16 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.15+sha-c2c2b4a";
+  var SW_VERSION = "20.3.16+sha-f76d891";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
