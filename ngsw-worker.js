d53216ddaa351322b1e4988f8f171a9032f97f7d
kirjs <kirts@google.com>
2026-01-07 14:28:46 +0000

    c1e9627 docs(core): fix Ivy view data docs tables

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.6+sha-0ff2d5a";
+  var SW_VERSION = "21.0.6+sha-c1e9627";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
