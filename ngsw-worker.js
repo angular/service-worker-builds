67494cb0e6f39d2e1e7b0f2794a54c5633f623d1
GitHub <noreply@github.com>
2026-01-07 22:39:41 +0000

    5c75d29 refactor(core): Improve tree-shakable `RuntimeError` error for `INVALID_FIELD_DIRECTIVE_HOST`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.4+sha-af4e521";
+  var SW_VERSION = "21.1.0-next.4+sha-5c75d29";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
