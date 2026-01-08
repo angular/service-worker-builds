6d86a81d78ddc0a5ef01e1da48c99d8beff20f7f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 16:41:35 +0000

    1488c2e docs: fix broken anchor link for Angular versioning section

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.7+sha-f5bad8e";
+  var SW_VERSION = "21.0.7+sha-1488c2e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
