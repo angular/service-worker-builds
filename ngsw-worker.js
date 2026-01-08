b3e1d5f8f467aaaf2630af601a5ecac07eeaa222
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 21:34:40 +0000

    4929577 refactor(core): Use the provided Document value rather than global in FakeNavigation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-9529362";
+  var SW_VERSION = "21.2.0-next.0+sha-4929577";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
