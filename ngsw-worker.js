f0f9f647057ba690beb2b0735a43d8e326ddd5bf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 20:49:39 +0000

    3b8c4fc docs: release notes for the v21.0.8 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-next.4+sha-a2b9429";
+  var SW_VERSION = "21.1.0-next.4+sha-3b8c4fc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
