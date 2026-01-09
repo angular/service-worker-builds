582e0bf8ab40d468bc839af1428729c292253a8a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 16:28:33 +0000

    f41d721 refactor(devtools): style the profiler dialogs to match the current design

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-rc.0+sha-5dd2f2b";
+  var SW_VERSION = "21.1.0-rc.0+sha-f41d721";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
