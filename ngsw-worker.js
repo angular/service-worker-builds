cd6d36869b3762e7c508489ae07904b16a7998f0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 17:43:26 +0000

    94b7079 docs(docs-infra): avoid persistent AbortSignal listener in wait debounce in search

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-e3b3290";
+  var SW_VERSION = "21.2.0-next.0+sha-94b7079";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
