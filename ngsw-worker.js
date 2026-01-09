e3ee9971bb72c806369dc56884aca408800ced48
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 22:45:21 +0000

    615a77b refactor(compiler-cli): update error message links to versioned docs (#66374)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-5bc072e";
+  var SW_VERSION = "21.0.8+sha-615a77b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
