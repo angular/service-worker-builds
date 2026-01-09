e5e58ecf439d961c181c4ec41f348e5393b91219
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 15:53:01 +0000

    baee12d Revert "docs: remove incorrectly listed operators in "Supported operators" table (Expression Syntax Guide) (#62092)"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-696adbc";
+  var SW_VERSION = "21.0.8+sha-baee12d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
