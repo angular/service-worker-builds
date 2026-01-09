77a11abff850c5d42a65d537fbe2b2864491714e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 16:03:49 +0000

    491a156 docs(docs-infra): improve label fallback logic in search results

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-baee12d";
+  var SW_VERSION = "21.0.8+sha-491a156";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
