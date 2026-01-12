69a56215330a61b5bb6673137af7d91d1fac251b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 17:47:13 +0000

    a056413 docs(docs-infra): Show API decorators docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-ad699ce";
+  var SW_VERSION = "21.2.0-next.0+sha-a056413";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
