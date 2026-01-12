74d86c566391dd75fa72167f8d9898aaa29f8a16
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:24:20 +0000

    5b0e705 docs: improve separation between enter and leave example

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-615a77b";
+  var SW_VERSION = "21.0.8+sha-5b0e705";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
