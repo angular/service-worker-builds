db07a6a4a3c6096d60597436504aec2374546252
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:55:30 +0000

    481557e docs(docs-infra): prevent from showing duplicate usage blocks in functions API docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-rc.0+sha-13a7ee8";
+  var SW_VERSION = "21.1.0-rc.0+sha-481557e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
