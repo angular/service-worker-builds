ec32ff71c440a952e8862dc6e021506c108b4d50
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:55:50 +0000

    49e18e5 fix(docs-infra): improve symbolic green contrast for WCAG compliance

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-dfd8c52";
+  var SW_VERSION = "21.0.8+sha-49e18e5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
