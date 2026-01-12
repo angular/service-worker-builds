1929c313bd4f48c52a87b58aa6cdb900d00db809
GitHub <noreply@github.com>
2026-01-12 18:21:24 +0000

    4d983e9 build(docs-infra): Update dependency @xterm/xterm to v6

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-511d9ca";
+  var SW_VERSION = "21.2.0-next.0+sha-4d983e9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
