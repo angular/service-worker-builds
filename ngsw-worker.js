00e748e15d912637840255b91e4066f815a8c2ca
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 21:14:08 +0000

    6d3b093 build: Update vscode release script

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-rc.0+sha-9a764e0";
+  var SW_VERSION = "21.1.0-rc.0+sha-6d3b093";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
