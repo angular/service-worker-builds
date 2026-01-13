1e79d14860f383646af57054098c179589f079dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 16:38:04 +0000

    33de839 docs: update hierarchical-dependency-injection.md

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-6e8f142";
+  var SW_VERSION = "21.0.8+sha-33de839";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
