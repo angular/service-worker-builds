159a35a6e0b41d877cf3d0e8c81b6ec432047bcc
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 00:11:59 +0000

    6e8f142 build: improve changelog generation and tag fetching in release script

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-d5caac6";
+  var SW_VERSION = "21.0.8+sha-6e8f142";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
