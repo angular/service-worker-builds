a9bead942bd49c320276b6dd6ecaca8be07f0d9f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:31:10 +0000

    d710ebe docs(docs-infra): Exempts animation-related symbols from linking

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-b0b341f";
+  var SW_VERSION = "21.0.8+sha-d710ebe";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
