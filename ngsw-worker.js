e7c6f1c3d9bc0adb1a4e1dfe0d215318b943c56e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 17:26:06 +0000

    79273c1 Revert "docs(docs-infra): use Signals Forms focusBoundControl"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-d710ebe";
+  var SW_VERSION = "21.0.8+sha-79273c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
