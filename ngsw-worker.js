73d298547aa28f77bd2ea7406827142b9d75491e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:06:46 +0000

    79e6dfd docs(docs-infra): use Signals Forms focusBoundControl

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-de9277b";
+  var SW_VERSION = "21.0.8+sha-79e6dfd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
