8a6cee4e5d6ad6e2039e8d50093c4df0a1b7176c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:49:56 +0000

    6027c91 release: cut the v21.1.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0-rc.0+sha-b108ece";
+  var SW_VERSION = "21.1.0+sha-6027c91";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
