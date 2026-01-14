44c53c8936393851f98fe0b51dff7d71b9eb2ab7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:17:52 +0000

    b0b341f docs: add IMPORTANT prefix to pipe usage warning and

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.0.8+sha-79e6dfd";
+  var SW_VERSION = "21.0.8+sha-b0b341f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
