4542d003e37d5f0272a4ab9205b91eb94acf1864
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-15 17:04:41 +0000

    a792315 docs(docs-infra): prevent heading from linking symbols

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-61614f6";
+  var SW_VERSION = "21.2.0-next.0+sha-a792315";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
