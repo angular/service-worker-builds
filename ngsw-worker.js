19c61276490a9b412a698fd68895e41ab4cf639b
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 18:33:05 +0000

    ac50dbc build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0+sha-32ad1e0";
+  var SW_VERSION = "21.1.0+sha-ac50dbc-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
