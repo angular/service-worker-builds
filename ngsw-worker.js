1cd3842d6e5b62e43179d106be30c7906cf6e236
Alon Mishne <amishne@users.noreply.github.com>
2026-01-16 21:31:51 +0000

    6b34e5f docs: Add section on `animate.leave` for clarity

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0+sha-ac50dbc-with-local-changes";
+  var SW_VERSION = "21.1.0+sha-6b34e5f-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
