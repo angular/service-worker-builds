afe25a9ce2e808c9b89be1434775653306918a2e
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 17:42:18 +0000

    82593de fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0+sha-6b34e5f-with-local-changes";
+  var SW_VERSION = "21.1.0+sha-82593de";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
