13db6dc026a2e01f05d165ca6e5c5d2b00555b99
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 22:06:51 +0000

    f577f8c release: cut the v21.1.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0+sha-643f0f4";
+  var SW_VERSION = "21.1.1+sha-f577f8c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
