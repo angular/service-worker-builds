8ec1a971d28ced81ebecc7b742f3ce94e9cf6d95
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 18:32:59 +0000

    b9c995e refactor: Delete counterproductive aria-label

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0+sha-6a56204";
+  var SW_VERSION = "21.1.0+sha-b9c995e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
