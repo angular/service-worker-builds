c1dc4789b1ecc3c51d9d27853f35e714b6915ee5
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 00:27:03 +0000

    3c8d697 fix(docs-infra): don't use private symbol

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.0+sha-e664249";
+  var SW_VERSION = "21.1.0+sha-3c8d697";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
