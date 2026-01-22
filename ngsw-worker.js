5a2b84d8cea172a687667b8bca02d885991edd9c
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:11:56 +0000

    c9043fb refactor(compiler-cli): remove deprecated signature usage

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.1+sha-61dbdee";
+  var SW_VERSION = "21.1.1+sha-c9043fb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
