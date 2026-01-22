5befda057ad98801e996b9272e397c277e54d132
Andrew Scott <atscott@google.com>
2026-01-22 23:07:23 +0000

    458e21e docs(docs-infra): Exempts more symbols from automatic linking

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.1+sha-e4c9ff7";
+  var SW_VERSION = "21.1.1+sha-458e21e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
