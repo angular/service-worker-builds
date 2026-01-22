bf3ca08bd80d5f607185fb0b4171e3da12b2dfb0
Andrew Scott <atscott01@gmail.com>
2026-01-22 22:25:16 +0000

    ebae211 feat(forms): introduce parse errors in signal forms

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-145e77d";
+  var SW_VERSION = "21.2.0-next.0+sha-ebae211";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
