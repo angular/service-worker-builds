32133dbe11d771941b3728a1dab0f118bf9e66cd
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-22 00:48:54 +0000

    bfc2afd release: cut the v21.2.0-next.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-e35eea6";
+  var SW_VERSION = "21.2.0-next.0+sha-bfc2afd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
