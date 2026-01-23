81ffe60cbbdf8164f4861c56808c856ba93dd8e7
Andrew Scott <atscott@google.com>
2026-01-23 19:55:25 +0000

    e6ea328 docs: update `bootstrapApplication` docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.1+sha-b203ab0";
+  var SW_VERSION = "21.1.1+sha-e6ea328";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
