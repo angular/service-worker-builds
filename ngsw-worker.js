80c3e724d527e608c0d1b5f02f3655c63efdb0a2
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:38:28 +0000

    8efee1c docs: apply prefer/avoid blocks in UI with comments

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-d306737";
+  var SW_VERSION = "21.2.0-next.0+sha-8efee1c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
