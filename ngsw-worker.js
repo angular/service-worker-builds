70323301852677a6bc12b67fda3fe76ac2f0507f
Andrew Scott <atscott01@gmail.com>
2026-01-26 21:04:46 +0000

    21ecdc0 fix(router): Do not intercept reload events with Navigation integration

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.1+sha-2499238";
+  var SW_VERSION = "21.1.1+sha-21ecdc0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
