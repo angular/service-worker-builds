c8ce4f73bb2ea19a01bc42861bbae8c0e8b69d7d
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:42:25 +0000

    4f0d29d release: cut the v21.1.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.1+sha-290b281";
+  var SW_VERSION = "21.1.2+sha-4f0d29d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
