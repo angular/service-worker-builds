bc5d62a7dbfca91350f42478da42de6d01d47908
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:54:49 +0000

    5a6fefb release: cut the v21.2.0-next.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-571747d";
+  var SW_VERSION = "21.2.0-next.1+sha-5a6fefb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
