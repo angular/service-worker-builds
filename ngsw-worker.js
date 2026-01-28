2daeac4ff938dc0fabacd27691f44b42c20acbeb
Andrew Scott <atscott01@gmail.com>
2026-01-28 21:02:23 +0000

    cfe7340 refactor(core): remove unused restriction parameter

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.2+sha-4f0d29d";
+  var SW_VERSION = "21.1.2+sha-cfe7340";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
