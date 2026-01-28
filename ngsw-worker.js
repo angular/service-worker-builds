f4fd54812e89dd0d2b6d1aae5fc16523d96f04ae
Andrew Scott <atscott01@gmail.com>
2026-01-28 00:21:34 +0000

    31e6f0d test(forms): submit behavior while validation is pending

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.1+sha-6601f06";
+  var SW_VERSION = "21.1.1+sha-31e6f0d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
