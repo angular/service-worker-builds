be38365294fd32d5b20781e7b0f0837326c09a28
GitHub <noreply@github.com>
2026-01-28 00:14:02 +0000

    4448356 test(common): enables zoneless change detection in tests

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.0+sha-960de62";
+  var SW_VERSION = "21.2.0-next.0+sha-4448356";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
