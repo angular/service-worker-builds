205361c7d001bcc936115f3b61ead26e98a747b1
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:13:36 +0000

    0a4c647 refactor(core): Remove unsued properties in differ factories

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.2+sha-cfe7340";
+  var SW_VERSION = "21.1.2+sha-0a4c647";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
