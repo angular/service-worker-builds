f06c4ce691d5d597ccf68df0a511b47754ba2816
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:32:49 +0000

    6990f88 feat(docs-infra): increase table-of-contents width for large screens

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.1+sha-0e04233";
+  var SW_VERSION = "21.2.0-next.1+sha-6990f88";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
