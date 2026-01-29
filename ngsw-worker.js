9db428f42979482f735c0fc9fa53ac764c50be94
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 21:38:07 +0000

    74e7681 docs: update cross-repo adev docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.1+sha-a67e007";
+  var SW_VERSION = "21.2.0-next.1+sha-74e7681";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
