e180d97cf189150ee5054f67fc99bf7c51ee08d9
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:23:16 +0000

    a64a90d docs(animations): fix small typo in animations migrations docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.1+sha-8d5210c";
+  var SW_VERSION = "21.2.0-next.1+sha-a64a90d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
