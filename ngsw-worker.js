8e14bc2b6aea2aa8a3d3d4f79040fd7c3b98cd66
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 23:08:49 +0000

    68ba9c4 test(router): remove provider zoneless from tests

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.1+sha-8ab433a";
+  var SW_VERSION = "21.2.0-next.1+sha-68ba9c4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
