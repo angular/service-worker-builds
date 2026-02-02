5c3b895f50ae4611a4d5eb55b5ad21e6eaee4ede
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 19:18:52 +0000

    e5110b4 fix(core): export DirectiveWithBindings

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.2+sha-575f8a3";
+  var SW_VERSION = "21.1.2+sha-e5110b4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
