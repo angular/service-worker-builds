0e2bffa4fa2490333e3d505bf158475661ba1588
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:05:10 +0000

    eb28032 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.2+sha-6f75b6e-with-local-changes";
+  var SW_VERSION = "21.1.2+sha-eb28032";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
