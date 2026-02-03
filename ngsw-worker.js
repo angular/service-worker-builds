85ae188a1fce6fa780741534871f8d918321ca62
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:38:38 +0000

    43d61ec docs(docs-infra): extract call signature return type.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.1+sha-5a0f272";
+  var SW_VERSION = "21.2.0-next.1+sha-43d61ec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
