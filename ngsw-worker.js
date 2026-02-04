0df560a01ed8501fde2331841b44639d71c3f1e0
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 23:50:49 +0000

    8c21866 feat(language-service): add linked editing ranges for HTML tag synchronization

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.1+sha-da1f61c";
+  var SW_VERSION = "21.2.0-next.1+sha-8c21866";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
