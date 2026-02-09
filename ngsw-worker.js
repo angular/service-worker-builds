ed16fc2176b9ea54af64203e02587b9c9560092b
Andrew Kushnir <akushnir@google.com>
2026-02-09 22:53:45 +0000

    19542a3 test(common): remove zone-based testing utilities Removes usages of zone-based helpers such as `fakeAsync` , `tick` `waitForAsync` as part of the migration to zoneless tests.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.3+sha-d6aeac5";
+  var SW_VERSION = "21.1.3+sha-19542a3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
