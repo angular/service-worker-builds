8f58dce3b767aa5f7cd99c48b11c5ff89b67ff80
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 16:03:40 +0000

    4b3b149 refactor(benchpress): remove zone-based testing utilities

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.2+sha-63d828a";
+  var SW_VERSION = "21.2.0-next.2+sha-4b3b149";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
