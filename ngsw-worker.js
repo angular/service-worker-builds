810bbe84121c3f3dc2b56e57efc1213572b25dd8
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 00:12:44 +0000

    760910e Revert "refactor(service-worker): remove zone-based testing utilities"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.2+sha-51cc914";
+  var SW_VERSION = "21.2.0-next.2+sha-760910e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
