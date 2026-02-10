c3ffce6ead8630721d54ffd7ee59ba112910fa21
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 17:49:33 +0000

    18003a3 feat(common): add an 'outlet' injector option for ngTemplateOutlet

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.2+sha-0f47fda";
+  var SW_VERSION = "21.2.0-next.2+sha-18003a3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
