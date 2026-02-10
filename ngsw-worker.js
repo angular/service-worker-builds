71516ad4e71ba816160eb8a8af732154e1df7f1c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 15:53:44 +0000

    0f47fda test(router): move timeout and autoTick helpers to shared testing utilities

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.2+sha-760910e";
+  var SW_VERSION = "21.2.0-next.2+sha-0f47fda";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
