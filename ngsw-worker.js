08b5974fb2b35e6297fa2c0718e908391bbdbf55
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 00:03:44 +0000

    c6bbf40 docs(forms): replace 'legacy' with 'existing' in migration guide and navigation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.3+sha-19542a3";
+  var SW_VERSION = "21.1.3+sha-c6bbf40";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
