982f559c6c21793ceb1a68ae5654cac4f6b336c4
Matthew Beck <me@mattrb.com>
2026-02-11 22:08:33 +0000

    aa03843 ci: update pullapprove reviewer list

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-8868130";
+  var SW_VERSION = "21.1.4+sha-aa03843";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
