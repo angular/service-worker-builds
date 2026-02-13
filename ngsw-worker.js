f1ba5ab30e254ed6f7b6aeb1ffd350923e91a2ec
Matthew Beck <me@mattrb.com>
2026-02-13 18:13:25 +0000

    5a86528 docs: Adds new agent skills to Angular

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-23c0858";
+  var SW_VERSION = "21.1.4+sha-5a86528";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
