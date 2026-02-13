5ddefd300a431a8e3fbd435d21c733e80b45a9d7
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:27:10 +0000

    50c5353 build: update pnpm to v10.29.3

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-55b501a";
+  var SW_VERSION = "21.1.4+sha-50c5353";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
