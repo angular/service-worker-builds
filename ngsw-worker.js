2ade516ca049e7bc7d455e344ef90bd225d90866
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:50:08 +0000

    e10a634 refactor(compiler-cli): use phaseModifier for type-only import detection

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.3+sha-dfd5e3a";
+  var SW_VERSION = "21.2.0-next.3+sha-e10a634";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
