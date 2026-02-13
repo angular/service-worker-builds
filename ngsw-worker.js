521b8633ab65dbbca21054f36e9aeba2b0a4e86b
Matthew Beck <me@mattrb.com>
2026-02-13 01:00:10 +0000

    55b501a refactor(common): improve image directive typings

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-5cf357b";
+  var SW_VERSION = "21.1.4+sha-55b501a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
