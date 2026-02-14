8d6094e625460dbdb3a51fe31d72b0bf28004931
Matt Beck <mattrb@google.com>
2026-02-14 00:57:12 +0000

    a3588a0 refactor(compiler-cli): Add skeleton tests around source->source compiler transform mode

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-670dee9";
+  var SW_VERSION = "21.1.4+sha-a3588a0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
