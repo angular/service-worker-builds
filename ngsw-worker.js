012aeac7234e81a1522aa7616c9db82afdb6eca7
Matthew Beck (Berry) <me@mattrb.com>
2026-02-17 19:38:22 +0000

    fc557f0 fix(zone.js): support passthrough of Promise.try API

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.3+sha-95b3f37";
+  var SW_VERSION = "21.2.0-next.3+sha-fc557f0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
