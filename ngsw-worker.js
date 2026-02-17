612cafb384dbd199a120c56d8fa9732258d1a374
Matthew Beck <me@mattrb.com>
2026-02-17 21:41:20 +0000

    4953622 docs: fix enableProfiling note

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-7f5a728";
+  var SW_VERSION = "21.1.4+sha-4953622";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
