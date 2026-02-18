7299f76a64406ba8e13d20cf9c067d349c4e8512
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 17:00:29 +0000

    3c494b4 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.4+sha-25ebceb";
+  var SW_VERSION = "21.1.4+sha-3c494b4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
