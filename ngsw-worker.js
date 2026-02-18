27aa2f061f95f82b8eab105f7a28bc93f80d4034
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 15:22:34 +0000

    680e1c0 docs: fix custom control metadata example in signal forms guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.3+sha-8de0d25";
+  var SW_VERSION = "21.2.0-next.3+sha-680e1c0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
