728a6bee1163a3ae442e10ecf6498cbc3002d2b5
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 22:31:57 +0000

    cb8e974 docs: release notes for the v21.1.5 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-next.3+sha-567f292";
+  var SW_VERSION = "21.2.0-next.3+sha-cb8e974";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
