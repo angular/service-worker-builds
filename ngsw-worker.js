f2bd1647f2f5de763946403606e5e71ee811210d
GitHub <noreply@github.com>
2026-02-18 23:20:31 +0000

    dd630ef build: update version target to prep for 21.2.0 release for vscode extension (#67132)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-rc.0+sha-13fd2b1";
+  var SW_VERSION = "21.2.0-rc.0+sha-dd630ef";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
