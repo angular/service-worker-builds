7aee9b6688dfeaba0bc2d2b04bacb4d4c895782d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 20:46:48 +0000

    eb7c593 ci: add `zone.js` to `minimumReleaseAgeExclude`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-rc.0+sha-5cea357";
+  var SW_VERSION = "21.2.0-rc.0+sha-eb7c593";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
