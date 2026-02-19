da4426ca5292cd942731580f05b069efca3434fe
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 16:51:13 +0000

    6f922c1 fix(devtools): fix messaging bus

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.3.0-next.0+sha-ac8b5ff";
+  var SW_VERSION = "21.3.0-next.0+sha-6f922c1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
