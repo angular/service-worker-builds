ae984fc5c1a58760eda46e15b8e15254e57af304
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 17:00:18 +0000

    4ea3caf docs(docs-infra): fixed missing gray color palette definitions for light and dark modes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-rc.0+sha-f58f9c8";
+  var SW_VERSION = "21.2.0-rc.0+sha-4ea3caf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
