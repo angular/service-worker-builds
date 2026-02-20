f319a26ca5ec6fc7b779fb87eb307f11126e98c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:08:26 +0000

    390efd5 docs: add new debugging and troubleshooting di guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-rc.0+sha-c2cedd1";
+  var SW_VERSION = "21.2.0-rc.0+sha-390efd5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
