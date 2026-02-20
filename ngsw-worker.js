ff0b1f3be94f396bf514113a5e902600d2c0c073
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 18:12:07 +0000

    b9b5c27 refactor(core): enhance AnimationCallbackEvent.animationComplete signature

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.0+sha-66b472e";
+  var SW_VERSION = "22.0.0-next.0+sha-b9b5c27";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
