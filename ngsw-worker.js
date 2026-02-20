dabe54cbe512dc6ed98a4469bc34b93057967ede
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:23:05 +0000

    3b64e7d refactor(common): log a warning when a JsonPipe receives a signal

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.5+sha-24b578c";
+  var SW_VERSION = "21.1.5+sha-3b64e7d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
