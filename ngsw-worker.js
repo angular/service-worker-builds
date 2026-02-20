00a8c54009976bf3c8eca8fce5118b0324dd8e39
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 17:11:26 +0000

    f90e556 fix(compiler-cli): detect uninvoked functions in defer trigger expressions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.0+sha-cfdbb7c";
+  var SW_VERSION = "22.0.0-next.0+sha-f90e556";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
