713242b43e0689504e13d8a639feb6bd79eee701
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 21:33:55 +0000

    31d3d56 fix(common): fix LCP image detection with duplicate URLs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.5+sha-0f4ba4a";
+  var SW_VERSION = "21.1.5+sha-31d3d56";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
