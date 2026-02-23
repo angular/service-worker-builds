d8b42de1d359c948be0d723105dc9b653cd2bfa9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:17:36 +0000

    44b9f46 docs: Add a compiler entry to the roadmap

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.5+sha-d3479f1";
+  var SW_VERSION = "21.1.5+sha-44b9f46";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
