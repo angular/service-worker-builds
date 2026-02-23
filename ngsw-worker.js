bc9ac31e707f02b6fb1ac1f7e15b6ddd7c7731b4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 23:22:20 +0000

    30e486a fix(docs-infra): use shared heading ID generation logic

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0-rc.0+sha-a73d543";
+  var SW_VERSION = "21.2.0-rc.0+sha-30e486a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
