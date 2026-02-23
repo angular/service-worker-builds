a7377994b6d53e88db0725f74499c59d61708226
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 17:19:37 +0000

    0596a5b docs: add Route Loading Strategies section and update navigation links

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.5+sha-31d3d56";
+  var SW_VERSION = "21.1.5+sha-0596a5b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
