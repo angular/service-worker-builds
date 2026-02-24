2c905fd72ff52fc84bfe6affe1d1b0072284472f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 19:30:09 +0000

    db11e74 refactor(vscode-extension): Add support for exhaustive type check in the syntax

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.0+sha-9dc3ca4";
+  var SW_VERSION = "22.0.0-next.0+sha-db11e74";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
