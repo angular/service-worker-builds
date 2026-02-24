c454e3389bf258e417a43396ffb658e246a5a53a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 22:30:26 +0000

    24b39ab docs: update README to clarify app setup confirmation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.5+sha-0101922";
+  var SW_VERSION = "21.1.5+sha-24b39ab";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
