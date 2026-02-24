ff333463138209f1283bdcde05bbb1779f3c0b8f
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 22:17:42 +0000

    bb2745d build: update bazel dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.0+sha-db11e74";
+  var SW_VERSION = "22.0.0-next.0+sha-bb2745d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
