52f9c8b891917a0b02a036238e097c7a945b13b1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 16:27:16 +0000

    7f9de3c fix(core): block creation of sensitive URI attributes from ICU messages

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.16+sha-f76d891";
+  var SW_VERSION = "20.3.16+sha-7f9de3c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
