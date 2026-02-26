0a014f93c1aff426f104ace54061941276e7fec9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 18:04:16 +0000

    3d3e6cb build: update repository locations for bazel rules

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-2158bf8";
+  var SW_VERSION = "21.2.0+sha-3d3e6cb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
