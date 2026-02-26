da384b1e24d80a9a78e879bfac057fa23c95b235
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 18:11:18 +0000

    9441818 ci: set up linting for upcoming agent skills

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-3d3e6cb";
+  var SW_VERSION = "21.2.0+sha-9441818";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
