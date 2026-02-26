0e7579c8cbcb48bd00a1a852fd885e2a71da67f7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 21:48:12 +0000

    54a0541 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-9c0587f";
+  var SW_VERSION = "21.2.0+sha-54a0541";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
