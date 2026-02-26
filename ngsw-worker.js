2ed7ea51dd618a341725a8901572bd9e1faa228b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 20:28:46 +0000

    385b948 build: update to latest sha of dev-infra repo

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.0+sha-6585e4f";
+  var SW_VERSION = "22.0.0-next.0+sha-385b948";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
