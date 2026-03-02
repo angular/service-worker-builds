5bb44a8591c68ef3c6f18fe1330e3d89ddd3eb3d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-02 19:49:21 +0000

    9758ea9 refactor(core): ignore infinite animations in animate api

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.0+sha-a2d5048";
+  var SW_VERSION = "22.0.0-next.0+sha-9758ea9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
