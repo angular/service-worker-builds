5db324277a65effec9f382a2d090e204398fbc39
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 16:54:17 +0000

    23cb9a4 refactor(forms): address feedback

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-498d9cd";
+  var SW_VERSION = "21.2.0+sha-23cb9a4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
