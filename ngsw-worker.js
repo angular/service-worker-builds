98bfad2e90b32c00c01ee4f41246e59a07f15ee4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-03 22:55:36 +0000

    657d532 build: update dependency preact-render-to-string to v6.6.6

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-796b9a7";
+  var SW_VERSION = "21.2.0+sha-657d532";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
