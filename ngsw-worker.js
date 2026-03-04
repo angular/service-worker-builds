79af0ccb0775351bcb2067bb887980c8a9e49b2a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 16:28:23 +0000

    b4ec3cc fix(core): prevent child animation elements from being orphaned

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-84e79f5";
+  var SW_VERSION = "21.2.0+sha-b4ec3cc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
