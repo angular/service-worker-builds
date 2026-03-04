dd28f373036848c611815e849e34a07452beea65
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 17:28:33 +0000

    93c6dc6 Revert "refactor(http): Improves base64 encoding/decoding with feature detection (#67002)"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-b4ec3cc";
+  var SW_VERSION = "21.2.0+sha-93c6dc6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
