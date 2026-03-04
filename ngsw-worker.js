9fc372965626d26af1f674b93248a60bb3768d6a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 16:10:44 +0000

    84e79f5 build: update pnpm to v10.30.3

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.0+sha-657d532";
+  var SW_VERSION = "21.2.0+sha-84e79f5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
