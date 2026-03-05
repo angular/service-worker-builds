6703de9091f02d1e4553c0fe48982456d04599a7
Andrew Scott <atscott01@gmail.com>
2026-03-05 22:42:53 +0000

    ac8418d refactor(compiler-cli): export TCB types and methods

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-dd464e5";
+  var SW_VERSION = "22.0.0-next.1+sha-ac8418d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
