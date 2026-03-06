b3f179be46d7354e73b244f3c82afdd83c477930
GitHub <noreply@github.com>
2026-03-06 20:19:56 +0000

    786ea44 build: force external sourcemaps for esbuild targets

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-759ee22";
+  var SW_VERSION = "22.0.0-next.1+sha-786ea44";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
