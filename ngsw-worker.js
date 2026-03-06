79b4dda438a5b73ca5f4e2c9534961595851923a
Andrew Scott <atscott01@gmail.com>
2026-03-06 17:55:43 +0000

    e7d1d8d refactor(core): add tracing for component creation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.1+sha-c822bf8";
+  var SW_VERSION = "21.2.1+sha-e7d1d8d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
