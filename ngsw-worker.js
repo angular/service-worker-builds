471f168ae8047e123e573b8073b5f3bc193742d6
Andrew Scott <atscott@google.com>
2026-03-09 17:29:10 +0000

    79ac570 refactor(devtools): fix browser-specific styles infra

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.1+sha-276c12a";
+  var SW_VERSION = "21.2.1+sha-79ac570";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
