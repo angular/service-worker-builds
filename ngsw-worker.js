e6711143f2c045b96ee94c12638bb25d79ccd6ff
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:35:55 +0000

    c499c57 ci: fix exclusion of manual_api_docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.2+sha-0588133";
+  var SW_VERSION = "21.2.2+sha-c499c57";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
