e1df1eb5fd7f8d5bc1562593770d287a1802b9c2
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:34:25 +0000

    09638ec docs(core): clarify provideZoneChangeDetection usage in v21+

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.2+sha-ab35383";
+  var SW_VERSION = "21.2.2+sha-09638ec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
