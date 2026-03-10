9187a6445b75ee3febb05c8d4f3b6e775c37ba79
Andrew Scott <atscott01@gmail.com>
2026-03-10 18:28:24 +0000

    8630319 fix(core): sanitize translated attribute bindings with interpolations

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-1866bf5";
+  var SW_VERSION = "22.0.0-next.1+sha-8630319";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
