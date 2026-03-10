62d78a3d4aa6823362aad91e6c1ab95b4b320c5b
Andrew Scott <atscott01@gmail.com>
2026-03-10 17:11:56 +0000

    ab35383 docs: fix first-app code getHousingLocationById housing.service.ts

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.2+sha-106cf4e";
+  var SW_VERSION = "21.2.2+sha-ab35383";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
