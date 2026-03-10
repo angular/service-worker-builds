3567b2aab5b349b167e9c7a6ac046ea9cfea3b59
Andrew Scott <atscott01@gmail.com>
2026-03-10 20:16:23 +0000

    02a617e build: update dependency @rollup/plugin-babel to v7

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-a675950";
+  var SW_VERSION = "22.0.0-next.1+sha-02a617e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
