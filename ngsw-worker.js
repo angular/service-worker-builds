ae150cea1a7cfd2884a5a9b3cce88f1ddcf48f9a
Alan Agius <alan.agius4@gmail.com>
2026-03-11 13:28:31 +0000

    71c9c6d release: bump Angular DevTools version to 1.13.0

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-57ba621";
+  var SW_VERSION = "22.0.0-next.1+sha-71c9c6d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
