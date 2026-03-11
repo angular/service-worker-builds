478d482b2b1b4e7dbad3c32e804b21e3393388f0
Andrew Scott <atscott01@gmail.com>
2026-03-11 19:17:11 +0000

    dea3241 docs: release notes for the v21.2.3 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-3c2d95a";
+  var SW_VERSION = "22.0.0-next.1+sha-dea3241";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
