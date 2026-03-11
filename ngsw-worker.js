2c075a3d9d5e96eb7dc7ff34b33ee05a0730b1dc
Andrew Scott <atscott@google.com>
2026-03-11 21:20:10 +0000

    74698d5 docs(forms): clarify zoneless change detection for reactive FormArray updates

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.3+sha-7907e98";
+  var SW_VERSION = "21.2.3+sha-74698d5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
