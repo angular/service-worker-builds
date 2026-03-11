cde6d067057eba10bef651cf184be4296ce894be
Andrew Scott <atscott01@gmail.com>
2026-03-11 21:38:29 +0000

    8ba2844 docs: document coverageExclude option for excluding files from coverage

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.3+sha-74698d5";
+  var SW_VERSION = "21.2.3+sha-8ba2844";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
