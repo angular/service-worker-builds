4a581e5d0cad7afd7ed51426f61040e63553b4ee
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:01:48 +0000

    6c73aac refactor(common): Removes unused generic type parameters from `KeyValueDiffers`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.2+sha-d45fced";
+  var SW_VERSION = "21.2.2+sha-6c73aac";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
