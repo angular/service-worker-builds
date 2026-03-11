c85ab7454dd6a8145a05d28fd507d18a8e12ab30
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:10:40 +0000

    d45fced docs: remove duplicate aria-hidden attribute in angular aria ngTree example

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.2+sha-ed48c41";
+  var SW_VERSION = "21.2.2+sha-d45fced";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
