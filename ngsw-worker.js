8f8107a4c8552a4ff27c3bc4b5fa7a80f1d4bb57
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:21:05 +0000

    4c532bd build: lock file maintenance

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.1+sha-66e72ef";
+  var SW_VERSION = "22.0.0-next.1+sha-4c532bd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
