21afc8e73e7ce32dbb51261e3757dc1684202212
Matthew Beck <me@mattrb.com>
2026-03-12 21:06:52 +0000

    5d755be refactor: ensure modules with 'declare global' are correctly handled by Rollup

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.4+sha-8017617";
+  var SW_VERSION = "21.2.4+sha-5d755be";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
