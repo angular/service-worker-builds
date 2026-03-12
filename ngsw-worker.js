ca368b5281a3e56f2d436f8bb9ddd496f4ca1dbf
Matthew Beck <me@mattrb.com>
2026-03-12 23:46:20 +0000

    6f41efc refactor(core): Update determineLongestAnimation to account for playback rate

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.4+sha-bfb8b17";
+  var SW_VERSION = "21.2.4+sha-6f41efc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
