374fc1542fc0770b926ad9069f69502e1f56cf34
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 19:00:24 +0000

    334ae10 fix(compiler): ensure generated code compiles

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.4+sha-6f41efc";
+  var SW_VERSION = "21.2.4+sha-334ae10";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
