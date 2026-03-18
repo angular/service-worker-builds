359a8a32ff571c2d1284ab09547f08cb45df3ed4
Matthew Beck (Berry) <me@mattrb.com>
2026-03-18 23:41:51 +0000

    57ce2b6 release: cut the v21.2.5 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.4+sha-5393d8e";
+  var SW_VERSION = "21.2.5+sha-57ce2b6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
