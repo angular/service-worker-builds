a1a35113594129fdeb5c53833f76092a7f590849
Matthew Beck <me@mattrb.com>
2026-03-18 00:09:06 +0000

    076d41c fix(migrations): prevent trailing comma syntax errors after removing NgStyle

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.4+sha-a8f80c1";
+  var SW_VERSION = "21.2.4+sha-076d41c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
