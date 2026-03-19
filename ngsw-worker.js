873de589ac5cb2af1078a6a42707f7e136497ab3
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:00:43 +0000

    ad0156e fix(core): fixes a regression with animate.leave and reordering

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.5+sha-aa3220a";
+  var SW_VERSION = "21.2.5+sha-ad0156e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
