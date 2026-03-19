efa957e06833d5b382634d70966da4a1de07a775
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:21:18 +0000

    831746e docs: Add callout about the Component interface.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.5+sha-9182b62";
+  var SW_VERSION = "21.2.5+sha-831746e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
