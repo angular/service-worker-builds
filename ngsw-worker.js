e6398aa8509c2dea64c22ff6adfc5006006e6777
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:43:29 +0000

    f46e8bd refactor(compiler): remove `fullTemplateTypeCheck` compiler option.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.4+sha-2615f35";
+  var SW_VERSION = "22.0.0-next.4+sha-f46e8bd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
