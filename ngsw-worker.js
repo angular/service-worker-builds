1745161a473cda500f1fd8c61cf0b0cdeadf6e0a
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-24 21:56:49 +0000

    e731bbd docs: clarify size budget configuration

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.5+sha-c7a1892";
+  var SW_VERSION = "21.2.5+sha-e731bbd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
