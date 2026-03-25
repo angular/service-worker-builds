dbb4c211d95fe8de35fd94334d5a60e8fa9ca508
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-03-25 09:59:17 +0000

    2c7d3b5 build: re-sync file

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.5+sha-6bd2121";
+  var SW_VERSION = "21.2.5+sha-2c7d3b5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
