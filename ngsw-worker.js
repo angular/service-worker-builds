f293347294e6ce79ac36fc33356b23dd890c1429
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 20:16:41 +0000

    eecfa4c refactor(language-service): Export template target from API

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.4+sha-ad57c9d";
+  var SW_VERSION = "22.0.0-next.4+sha-eecfa4c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
