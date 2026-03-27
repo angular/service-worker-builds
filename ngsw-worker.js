3538426ad0a6f39af6f220ba8cae530205afce33
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 13:09:08 +0000

    c8902de build: update all github actions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.6+sha-b453c0c";
+  var SW_VERSION = "21.2.6+sha-c8902de";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
