1128a161c535463c1e19f74acf981fb687d964fd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 15:16:58 +0000

    fcd0bb0 fix(compiler-cli): prevent recursive scope checks for invalid NgModule imports

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.5+sha-12b3a1a";
+  var SW_VERSION = "22.0.0-next.5+sha-fcd0bb0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
