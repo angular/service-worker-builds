f429b82d5696166e58c4d6fc957521d64abbac02
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 12:00:48 +0000

    895c576 refactor(forms): use strict equality for pending status getter

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.6+sha-5fd4f0f";
+  var SW_VERSION = "21.2.6+sha-895c576";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
