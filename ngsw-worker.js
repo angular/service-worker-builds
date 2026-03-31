77bca29a007ba845625c64bdb37c42b7bc665944
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 10:02:17 +0000

    f22d9a7 docs: modernize CLI reference for build and test

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.6+sha-cf7846b";
+  var SW_VERSION = "21.2.6+sha-f22d9a7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
