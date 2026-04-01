276857805f4eae7e4f1f2242c746e2b1b1ee5e06
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 11:27:56 +0000

    f363ab4 build: update cross-repo angular dependencies to 616a50d

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.6+sha-0960592";
+  var SW_VERSION = "21.2.6+sha-f363ab4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
