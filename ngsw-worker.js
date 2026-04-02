c504dab8f4510211a8a4b690f951daa00c2698a7
Andrew Scott <atscott01@gmail.com>
2026-04-02 22:56:59 +0000

    4441a95 build: update cross-repo angular dependencies to v21.2.5

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.7+sha-2ae0912";
+  var SW_VERSION = "21.2.7+sha-4441a95";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
