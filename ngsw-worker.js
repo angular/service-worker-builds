fc8b31f1af13131ac7f8ea5ee5628f004af37e74
Andrew Scott <atscott01@gmail.com>
2026-04-03 17:25:39 +0000

    394ad0c fix(forms): allow late-bound input types for signals forms

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.6+sha-9c55fcb";
+  var SW_VERSION = "22.0.0-next.6+sha-394ad0c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
