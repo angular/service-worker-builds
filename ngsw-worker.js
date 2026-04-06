878aef119892a0d4e5dfce0d4adb62e0dbb01c29
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:41:28 +0000

    2ec9ff0 docs: update cross-repo adev docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.7+sha-8102331-with-local-changes";
+  var SW_VERSION = "21.2.7+sha-2ec9ff0-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
