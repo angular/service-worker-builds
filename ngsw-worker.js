808318d5e7eea6d1da44f9a29330d888cfb7e0fe
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:39:59 +0000

    057cc6d fix(core): remove obsolete iOS cursor pointer hack in event delegation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.7+sha-dea76c7";
+  var SW_VERSION = "21.2.7+sha-057cc6d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
