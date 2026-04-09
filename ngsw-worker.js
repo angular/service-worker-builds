42910e2dddcac0246eb5c84e2f3afef11461189f
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 12:47:22 +0000

    db9f9a8 build: update all non-major dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.7+sha-5516769";
+  var SW_VERSION = "22.0.0-next.7+sha-db9f9a8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
