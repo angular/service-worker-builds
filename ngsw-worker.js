553328fd5cb960ea23ba7e8fe6103c93c66b3bb4
kirjs <kirts@google.com>
2026-04-10 08:04:45 +0000

    dc95814 docs: add documentation for NG1002

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.8+sha-05d9b97";
+  var SW_VERSION = "21.2.8+sha-dc95814";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
