022afd2e0746548a3341bb24da4fe16476a4eb2c
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 13:54:40 +0000

    337e6e7 refactor: add flaky tests workflow

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.7+sha-0304228";
+  var SW_VERSION = "22.0.0-next.7+sha-337e6e7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
