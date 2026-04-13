83e8e2bf26bb66b99f89472389a2ba29de7bd47b
kirjs <kirts@google.com>
2026-04-13 10:31:49 +0000

    b72b6b4 docs(forms): update signal forms migration guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.8+sha-3ed14c6";
+  var SW_VERSION = "21.2.8+sha-b72b6b4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
