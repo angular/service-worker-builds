052cb952b61ca7a073c0c18b8588a71ac1461c4f
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 07:56:35 +0000

    fda8d20 docs(docs-infra): fix essentials next-step navigation pills

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.7+sha-a89b565";
+  var SW_VERSION = "22.0.0-next.7+sha-fda8d20";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
