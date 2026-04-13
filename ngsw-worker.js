854ecac2054fbb1919ac8e4f41dd7f43b71a92b6
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:15:25 +0000

    c3d4be4 docs(docs-infra): fix card container overflow on mobile viewports

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.7+sha-fda8d20";
+  var SW_VERSION = "22.0.0-next.7+sha-c3d4be4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
