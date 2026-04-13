acaab5f5aba866e7e9d5977dab54e8a18b8fe281
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:41:52 +0000

    5f74e7e docs: update cross-repo adev docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.7+sha-973ede6";
+  var SW_VERSION = "22.0.0-next.7+sha-5f74e7e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
