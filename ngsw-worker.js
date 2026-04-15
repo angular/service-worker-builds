051e7963f0ec2814eb499db2b3ed0962209676b7
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 16:54:20 +0000

    303d4cd fix(platform-server): prevent SSRF bypasses via protocol-relative and backslash URLs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.18+sha-cef3164";
+  var SW_VERSION = "20.3.18+sha-303d4cd";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
