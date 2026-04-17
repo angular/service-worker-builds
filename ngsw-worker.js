283008d5ba87a90a615ec27081bbb2050bde71d1
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-17 18:06:27 +0000

    c738d45 build: update all github actions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.9+sha-e14d5ea";
+  var SW_VERSION = "21.2.9+sha-c738d45";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
