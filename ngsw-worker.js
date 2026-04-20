62ebd82c2c28f689913da0fe02b86fe661231b5d
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:04:02 +0000

    c9215b3 Revert "refactor(core): complete removal of deprecated `createNgModuleRef` alias"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.9+sha-e5b93ea";
+  var SW_VERSION = "21.2.9+sha-c9215b3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
