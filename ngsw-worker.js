79ef55e5f592e802944376132cbba7327fca71af
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:39:47 +0000

    a9ddc5d docs: warn against storing secrets in environment files

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.9+sha-c9215b3";
+  var SW_VERSION = "21.2.9+sha-a9ddc5d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
