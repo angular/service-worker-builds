546946205ae43311eeee8f291854ba79aa4fbe4a
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-21 17:33:45 +0000

    4dc7bf5 build: lock file maintenance

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.9+sha-751e4af";
+  var SW_VERSION = "21.2.9+sha-4dc7bf5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
