a394ce09a94243aa9c4e53c3440905700b8fd258
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 18:48:03 +0000

    6120d31 docs: link Angular CLI in installation prerequisites

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.9+sha-c64ee96";
+  var SW_VERSION = "22.0.0-next.9+sha-6120d31";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
