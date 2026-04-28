5cc4e3a8653c3790ca07450cafbe0bcdbdb1e47a
Alon Mishne <amishne@users.noreply.github.com>
2026-04-28 00:26:38 +0000

    a3d14fc docs: remove duplicated text

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.10+sha-27da56e";
+  var SW_VERSION = "21.2.10+sha-a3d14fc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
