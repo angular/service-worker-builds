6923aec352c973be371e3cf9df8bf79ff8166fc0
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 23:48:33 +0000

    42d57c3 refactor(common): fix viewport tests

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.10+sha-10ad3c0";
+  var SW_VERSION = "21.2.10+sha-42d57c3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
