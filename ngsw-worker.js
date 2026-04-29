6e2128f541496d9a1ccce18e14b6554cb799e0ba
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 17:11:23 +0000

    37ec63e refactor(compiler): introduce default constant for legacyOptionalChaining flag

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.9+sha-2896c93";
+  var SW_VERSION = "22.0.0-next.9+sha-37ec63e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
