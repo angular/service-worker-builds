47aae16e35314d4b408872815c5bc05d9703caa0
GitHub <noreply@github.com>
2026-05-05 23:04:52 +0000

    f81fa6e docs: add documentation for lazy loading services and update navigation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.10+sha-8a7f955";
+  var SW_VERSION = "22.0.0-next.10+sha-f81fa6e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
