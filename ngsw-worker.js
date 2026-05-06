dd96081e585c8af23c3a718111b1d360b8b69d75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 22:40:29 +0000

    a02ac76 docs: release notes for the v20.3.20 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.10+sha-3b0ae5f";
+  var SW_VERSION = "22.0.0-next.10+sha-a02ac76";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
