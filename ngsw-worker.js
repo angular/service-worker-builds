873b99e8d8cd08867687320717b8adf869c8ad91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 17:57:29 +0000

    849dba6 fix(forms): implement custom control reset propagation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.10+sha-5a7c1e6";
+  var SW_VERSION = "22.0.0-next.10+sha-849dba6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
