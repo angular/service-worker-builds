157346f0a2b5e01e21a06168cebfd057f15ec3b7
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:52:33 +0000

    6ed6498 docs: update cross-repo adev docs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.12+sha-629905d";
+  var SW_VERSION = "21.2.12+sha-6ed6498";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
