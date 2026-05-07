32ec53e2ff5c87ca8d026a698053b0bc581c6580
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:42:30 +0000

    a3d4631 build: update bazel dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.11+sha-fd9dda5";
+  var SW_VERSION = "22.0.0-next.11+sha-a3d4631";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
