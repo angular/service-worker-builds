c91a113b4fb4893d813311ae3795207429268c2c
Matthew Beck (Berry) <me@mattrb.com>
2026-05-08 15:30:40 +0000

    38b2bed docs: release notes for the v22.0.0-next.12 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-c72ebcb";
+  var SW_VERSION = "22.1.0-next.0+sha-38b2bed";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
