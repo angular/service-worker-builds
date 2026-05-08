5da196727e62bd7b9bfd223cc80eadaa494742a1
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-05-08 08:24:09 +0000

    c72ebcb fix(core): allow service with factory on abstract classes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-7b4791f";
+  var SW_VERSION = "22.1.0-next.0+sha-c72ebcb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
