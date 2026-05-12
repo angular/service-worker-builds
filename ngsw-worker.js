690c5254dbfe2d1a3dc97abd4966616761693d6a
Matthew Beck <me@mattrb.com>
2026-05-12 17:56:45 +0000

    7ab78d5 ci: mark devversion as unavailable

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.12+sha-49a133a";
+  var SW_VERSION = "21.2.12+sha-7ab78d5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
