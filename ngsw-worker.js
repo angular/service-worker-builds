0ba1ddeba0b5e50d2b66892c519610c5c2425dce
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 17:59:42 +0000

    a617967 refactor(http): update HTTP resource options APIs to stable

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-62710ad";
+  var SW_VERSION = "22.1.0-next.0+sha-a617967";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
