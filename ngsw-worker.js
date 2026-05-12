a356b59171755691c85062aa90db3d22d7875d90
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 20:31:47 +0000

    e0593ec refactor(compiler-cli): drop @ts-ignore around fs.readFileSync

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-cdef73a";
+  var SW_VERSION = "22.1.0-next.0+sha-e0593ec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
