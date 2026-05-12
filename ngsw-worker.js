3cf54aa97449108bd25f3f639b5222fa1932b47a
Matthew Beck <me@mattrb.com>
2026-05-12 01:41:20 +0000

    e9d1c7e refactor(router): Move target RouterState creation before 'blocking' stage

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.12+sha-8391cf0";
+  var SW_VERSION = "22.0.0-next.12+sha-e9d1c7e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
