f4fa51060026f80d89bb5aef3c690a6248b38b93
Matthew Beck <me@mattrb.com>
2026-05-13 22:45:47 +0000

    639a785 docs: add version 22 to the browser support reference table

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-next.12+sha-1f0c1ab";
+  var SW_VERSION = "22.0.0-next.12+sha-639a785";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
