9296c433fb9a616ebadc8806e1e423cbd610bd31
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 18:40:40 +0000

    06f6dec fix(compiler): type check invalid for loops

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-739bd37";
+  var SW_VERSION = "22.1.0-next.0+sha-06f6dec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
