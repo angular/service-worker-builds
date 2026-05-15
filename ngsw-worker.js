aa6d31547020e8982c077272597c080c44374aea
leonsenft <leonsenft@users.noreply.github.com>
2026-05-15 17:53:59 +0000

    4dd9839 docs: move Angular 19 to unsupported versions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.0+sha-5ca0c5a";
+  var SW_VERSION = "22.0.0-rc.0+sha-4dd9839";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
