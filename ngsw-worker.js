36111db736b5e80b8bc4f40f69f4d2441f2db2c3
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 22:52:32 +0000

    d8a848b release: cut the v22.0.0-rc.1 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.0+sha-9966a80";
+  var SW_VERSION = "22.0.0-rc.1+sha-d8a848b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
