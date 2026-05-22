b0bd780dda6181ec4a88979a926fe24b6712fa9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:07:36 +0000

    c08f22a refactor(vscode-extension): Remove effectively dead code

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.1+sha-3b4ef1e";
+  var SW_VERSION = "22.0.0-rc.1+sha-c08f22a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
