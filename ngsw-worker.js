1945f707bc5c839ad214d2c1b8a89c1ec6a59391
GitHub <noreply@github.com>
2026-05-27 17:39:02 +0000

    140c4d0 fix(platform-server): prevent SSRF bypasses via backslash URLs in HttpClient

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1288,7 +1288,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-4f9ee3c";
+  var SW_VERSION = "22.1.0-next.0+sha-140c4d0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
