0c7f04fd6dba81572c0ceda7be356fa9d6646f0e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:54:49 +0000

    7b4b067 docs: fix grammar issues in resource guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.1+sha-d88b796";
+  var SW_VERSION = "22.0.0-rc.1+sha-7b4b067";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
