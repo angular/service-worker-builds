e4ea33737b38e4b9a143921df3c952e7bf9d69e7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:24:43 +0000

    80d6a3b build: update bazel dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-4f79cfb";
+  var SW_VERSION = "22.1.0-next.0+sha-80d6a3b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
