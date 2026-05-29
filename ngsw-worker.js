7e0dcdae23c5734415c8a5af55b1909cc356c852
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 13:07:33 +0000

    7e38336 fix(core): use Object.create(null) for LOCALE_DATA as a hardening measure

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.15+sha-f6d8e64";
+  var SW_VERSION = "21.2.15+sha-7e38336";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
