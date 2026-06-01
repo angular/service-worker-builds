d18d90a489a26a0eec903134d481f3210e41bc1c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:42:45 +0000

    2891f7e fix(compiler): move projection attributes into constants

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.2+sha-15f7f8a";
+  var SW_VERSION = "22.0.0-rc.2+sha-2891f7e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
