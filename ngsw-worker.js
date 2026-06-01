eaeef130971fad7f8473736146c89d82945d11db
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 18:16:28 +0000

    7b91309 fix(platform-server): throw on suspicious URLs and restrict protocol-relative URLs

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.2+sha-ae12a09";
+  var SW_VERSION = "22.0.0-rc.2+sha-7b91309";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
