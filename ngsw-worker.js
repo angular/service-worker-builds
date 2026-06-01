5854abaca594c83754bb38388035ec15ea6334a5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 20:15:11 +0000

    3cc9e2b docs: release notes for the v22.0.0-rc.3 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-0b19c92";
+  var SW_VERSION = "22.1.0-next.0+sha-3cc9e2b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
