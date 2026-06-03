49a480aa65f24e5d9650433ce429d77c019e3f9e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 16:05:00 +0000

    4e42420 docs: update landing page for v22

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0+sha-24416ce";
+  var SW_VERSION = "22.0.0+sha-4e42420";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
