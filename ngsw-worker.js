3193b225bc9092d0352feeca4a264876735a8438
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 15:19:44 +0000

    595660d docs: update tutorials and playground to v22.0.0

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-09c03a2";
+  var SW_VERSION = "22.1.0-next.0+sha-595660d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
