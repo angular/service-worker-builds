f868ad442729266cbb7fb53cec16522bfdea9a87
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:40:06 +0000

    8e4fd59 build: deprecate @angular/animations and @angular/platform-browser-dynamic

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0+sha-3d9ca2f";
+  var SW_VERSION = "22.0.0+sha-8e4fd59";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
