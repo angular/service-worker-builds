bc005c6d6d23273a70dc1eae24d51acaf6c356d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 16:23:21 +0000

    c1b2c58 docs: align dev skills examples with style guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-2052495";
+  var SW_VERSION = "22.1.0-next.0+sha-c1b2c58";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
