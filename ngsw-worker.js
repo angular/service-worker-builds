dce1d7b8594813f3a335a05a863e5f92dbb9eef9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 09:12:48 +0000

    e10647b docs: update signal forms conditional rules

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0-rc.3+sha-f3c9249";
+  var SW_VERSION = "22.0.0-rc.3+sha-e10647b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
