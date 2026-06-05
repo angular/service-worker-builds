fb6e112f097d2e504975c9dbd180c0f51f24c4f5
GitHub <noreply@github.com>
2026-06-05 21:33:49 +0000

    a810a31 fix(http): Rejects non-HTTP(S) URLs in JSONP requests

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.16+sha-3551074";
+  var SW_VERSION = "21.2.16+sha-a810a31";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
