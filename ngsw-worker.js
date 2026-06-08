3c9034181125bcc7e0133764a4af11ad5638190d
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:26:50 +0000

    393b84c fix(compiler): sanitize two-way properties

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0+sha-60d88b1";
+  var SW_VERSION = "22.0.0+sha-393b84c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
