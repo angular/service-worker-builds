a7d7997e6b33b126da40527c3248536dfbe28853
Andrew Scott <atscott01@gmail.com>
2026-06-08 22:49:44 +0000

    06be298 fix(http): preserve empty referrer option in HttpRequest

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.24+sha-9f443bc";
+  var SW_VERSION = "20.3.24+sha-06be298";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
