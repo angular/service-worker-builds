e62ea78b703fcc69f552e9189c43b51a53d165ce
Andrew Scott <atscott01@gmail.com>
2026-06-08 19:54:47 +0000

    06db706 feat(devtools): improve transfer state tab

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1299,7 +1299,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-fe50a7e";
+  var SW_VERSION = "22.1.0-next.0+sha-06db706";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
