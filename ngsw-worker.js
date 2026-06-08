46d04118791b6b9abe1f4119a5e136ce73324534
Andrew Scott <atscott01@gmail.com>
2026-06-08 22:00:44 +0000

    9f443bc fix(common): Limits date format string length

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.24+sha-fa940e1";
+  var SW_VERSION = "20.3.24+sha-9f443bc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
