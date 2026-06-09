20cd2b9d5ba9064279ef36d7adca2a3bd52c3bc9
Andrew Scott <atscott01@gmail.com>
2026-06-09 16:54:17 +0000

    547d85a ci: run benchmark comparison in isolated worktree and harden security

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-e6ae250";
+  var SW_VERSION = "22.1.0-next.0+sha-547d85a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
