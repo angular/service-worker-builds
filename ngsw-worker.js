ed9cc1fec1ffe5966b6d2589987e9e94fec3e7eb
Andrew Scott <atscott01@gmail.com>
2026-06-09 18:30:22 +0000

    390af25 build: update cross-repo angular dependencies

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-fe72186";
+  var SW_VERSION = "22.1.0-next.0+sha-390af25";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
