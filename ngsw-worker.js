d628ac8875fa1ea6b0afae2299ed9c6d4c2189dd
Andrew Scott <atscott01@gmail.com>
2026-06-09 19:31:40 +0000

    0e16bb7 fix(core): Handle synchronous errors in PendingTasks.run function

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-390af25";
+  var SW_VERSION = "22.1.0-next.0+sha-0e16bb7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
