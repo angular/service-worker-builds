654ba90ba952e6ecbc47ed6d7c9c1b09f8cdf86c
GitHub <noreply@github.com>
2026-06-09 17:12:30 +0000

    d55c94a refactor(platform-server): deprecate ServerXhr (#69256)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.24+sha-06be298";
+  var SW_VERSION = "20.3.24+sha-d55c94a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
