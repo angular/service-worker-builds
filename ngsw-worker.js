6b12070fa0ad29482cbba6f1b0ab0fe002fe8dea
GitHub <noreply@github.com>
2026-06-10 16:57:53 +0000

    88832c8 fix(core): validate lowercase SVG animation attribute names (#69269)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.16+sha-13fb0af";
+  var SW_VERSION = "21.2.16+sha-88832c8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
