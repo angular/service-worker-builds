44430c2e2eae77bcc49ec0f094d09e92f76598dc
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:19:05 +0000

    f6695b0 fix(vscode-extension): resolve relative workspace tsdk paths to absolute

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.0+sha-4645850";
+  var SW_VERSION = "22.0.0+sha-f6695b0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
