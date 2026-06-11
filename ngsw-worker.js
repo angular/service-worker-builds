8cf8decb72d1bba0b84a7fda12e81667133b94c3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-11 16:52:29 +0000

    fd7c2da fix(zone.js): harden zoneSymbolEventNames against __proto__ key (defense-in-depth)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-71bb19d";
+  var SW_VERSION = "22.1.0-next.0+sha-fd7c2da";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
