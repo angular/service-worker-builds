3a06c84c9346e6f9716af0029095716b2c8d8901
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-11 17:09:17 +0000

    a6c7fc5 fix(http): distinguish repeated transfer cache params

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-fd7c2da";
+  var SW_VERSION = "22.1.0-next.0+sha-a6c7fc5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
