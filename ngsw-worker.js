479037ddb2fd6df2efad6734d3211a52af5b81aa
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-11 17:53:20 +0000

    3a48abc fix(core): preserve leave animation for sibling instances sharing a TNode

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.1+sha-6867f77";
+  var SW_VERSION = "22.0.1+sha-3a48abc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
