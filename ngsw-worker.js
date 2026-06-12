8fe68e713324d9c1785237518bb65b55c4bbfd7d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 17:24:32 +0000

    1d97355 docs: clarify value attribute on radio/checkbox inputs is allowed with formField

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.1+sha-15314c1";
+  var SW_VERSION = "22.0.1+sha-1d97355";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
