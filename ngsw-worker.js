d1c5739f3b8b3d72486b8b07ed15e3964e8cd251
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 17:36:04 +0000

    86ade07 refactor(compiler): add support for @Input transforms under isolatedDeclarations

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-0152e3c";
+  var SW_VERSION = "22.1.0-next.0+sha-86ade07";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
