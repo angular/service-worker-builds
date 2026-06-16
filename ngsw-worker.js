ad4561a0d22e836796f8c0e25ea0edfa49a6fc1a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 16:16:22 +0000

    0152e3c fix(core): treat iframe credentialless as security-sensitive

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-cbccd36";
+  var SW_VERSION = "22.1.0-next.0+sha-0152e3c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
