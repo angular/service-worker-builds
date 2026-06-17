5f4529ce91e6050063b5c5407198ac6a58748283
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 17:08:48 +0000

    a89ab78 docs(docs-infra): update environment configuration examples and remove production flag

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.0+sha-2d2d5fc";
+  var SW_VERSION = "22.1.0-next.0+sha-a89ab78";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
