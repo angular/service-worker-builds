feeec986ad0ac872b1908cefa43de75b4338401d
kirjs <kirts@google.com>
2026-06-18 16:40:42 +0000

    3e7c83b Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.2+sha-73880c5";
+  var SW_VERSION = "22.0.2+sha-3e7c83b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
