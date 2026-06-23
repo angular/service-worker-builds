bc6a52a2657b0ff6c099416db96542bd6cba2a1b
kirjs <kirts@google.com>
2026-06-23 15:37:46 +0000

    f598dcf docs: add error guide for NG05101

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.2+sha-7766dc0";
+  var SW_VERSION = "22.0.2+sha-f598dcf";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
