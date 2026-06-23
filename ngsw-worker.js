87e70532e324a6fbf44916b34e901909d57007b1
kirjs <kirts@google.com>
2026-06-23 17:07:30 +0000

    2799304 fix(core): avoid uncaught promise errors in injectAsync prefetching

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.2+sha-f598dcf";
+  var SW_VERSION = "22.0.2+sha-2799304";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
