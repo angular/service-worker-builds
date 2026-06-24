6936184072c102b8ae81ff939979fe57db9c6a45
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 15:44:29 +0000

    12fcec8 docs(forms): clarify debounce('blur') usage with custom FormValueControl

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.1+sha-edea40b";
+  var SW_VERSION = "22.1.0-next.1+sha-12fcec8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
