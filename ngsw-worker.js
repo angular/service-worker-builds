af1eb8710aad9d7a440e2725ae02f7aefd729c9d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 15:34:30 +0000

    edea40b docs: add doc to focusBoundControl feature

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.1+sha-ea17725";
+  var SW_VERSION = "22.1.0-next.1+sha-edea40b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
