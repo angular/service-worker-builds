5047e667af02fc832766bd44934acfb31b496907
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-25 15:40:13 +0000

    28a90e3 release: cut the v22.1.0-next.2 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.1+sha-7f2d34a";
+  var SW_VERSION = "22.1.0-next.2+sha-28a90e3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
