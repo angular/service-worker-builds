dd82da6dd58b7b2349aa3dd804488846c60ad206
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 16:25:05 +0000

    26b0c71 fix(migrations): resolve migration failure when tsconfig specifies rootDir

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.2+sha-28a90e3";
+  var SW_VERSION = "22.1.0-next.2+sha-26b0c71";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
