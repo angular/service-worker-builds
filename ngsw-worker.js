413bd4b779c7954efdd9cdbbf9526b741b3e0622
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 23:12:20 +0000

    74803c7 refactor(compiler): remove `visitAttributeComment`

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.3+sha-e653a7b";
+  var SW_VERSION = "22.1.0-next.3+sha-74803c7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
