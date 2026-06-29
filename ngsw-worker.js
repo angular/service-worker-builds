71cba98c1679c57188df155ad7b859530e6612e4
GitHub <noreply@github.com>
2026-06-29 21:39:44 +0000

    1659952 fix(compiler-cli): include toSignal in debugName transform

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.3+sha-d109cc5";
+  var SW_VERSION = "22.1.0-next.3+sha-1659952";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
