2a62b7528c8b7a2df667bc876902ecbf9bb29c54
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-30 20:31:17 +0000

    a5f1b20 Revert "fix(compiler-cli): include toSignal in debugName transform"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.3+sha-a720094";
+  var SW_VERSION = "22.1.0-next.3+sha-a5f1b20";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
