57ae82172f619d00a1afce460d7f5ceae933a5b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 01:08:22 +0000

    c4cb66e docs: improve introductory resource example

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.3+sha-d7f7061";
+  var SW_VERSION = "22.1.0-next.3+sha-c4cb66e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
