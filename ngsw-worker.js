287f704461de0b8cdec0865828d0f0d37a52e600
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 21:15:39 +0000

    b126dc9 release: cut the v22.1.0-next.4 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.3+sha-c4fa24a";
+  var SW_VERSION = "22.1.0-next.4+sha-b126dc9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
