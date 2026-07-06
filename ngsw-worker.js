cf41e905b9b976d2d696a671c13c2ec30a894725
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-06 21:09:09 +0000

    8d22cc9 fix(compiler-cli): update babel dependencies to latest v7

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1310,7 +1310,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.17+sha-5a693ba";
+  var SW_VERSION = "21.2.17+sha-8d22cc9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
