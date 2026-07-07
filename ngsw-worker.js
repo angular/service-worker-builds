15092b1933aacc51f8a39b8757cec20d89cf179b
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 18:12:45 +0000

    eb2a8ff fix(compiler-cli): apply debugName transform to required signal queries

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.4+sha-2f8134b";
+  var SW_VERSION = "22.1.0-next.4+sha-eb2a8ff";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
