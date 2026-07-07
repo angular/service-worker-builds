5d87ced308db27fd06c7f31a8250493cd3bd5a6f
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 23:52:14 +0000

    95b7385 docs: fix import of `AdminPage` in Angular routing documentation. #69670

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.4+sha-eb2a8ff";
+  var SW_VERSION = "22.1.0-next.4+sha-95b7385";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
