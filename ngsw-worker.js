820ad8977899609ed683a38b555a04d5cc193add
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:00:43 +0000

    5ec0b16 fix(docs-infra): disable nav-list tooltip on mobile

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.4+sha-e91046f";
+  var SW_VERSION = "22.1.0-next.4+sha-5ec0b16";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
