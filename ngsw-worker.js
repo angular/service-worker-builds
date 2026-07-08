c900403e5a1b5691ceab0db4e3e2f8a3c60f6a6c
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 22:10:37 +0000

    5d00aa2 release: cut the v21.2.18 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.17+sha-e86c31b";
+  var SW_VERSION = "21.2.18+sha-5d00aa2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
