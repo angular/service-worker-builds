df18347deb8eb68baeb7354afc070d163be7b25f
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 17:52:19 +0000

    39ea1c5 docs: fix grammar and punctuation issues in guides

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.4+sha-c1829f6";
+  var SW_VERSION = "22.1.0-next.4+sha-39ea1c5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
