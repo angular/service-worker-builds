aa4d79b6f080a86b2a7ba46e6a7d7068b64139c1
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 16:00:15 +0000

    42862f7 docs: update formatting

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.4+sha-95b7385";
+  var SW_VERSION = "22.1.0-next.4+sha-42862f7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
