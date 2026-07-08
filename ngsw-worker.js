7fb1be10ccbadfb11a4ad475fc9881a92826623c
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 17:36:26 +0000

    748faa4 docs(docs-infra): Add build-time validation for API and guide links using route manifest

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.5+sha-bec1413";
+  var SW_VERSION = "22.0.5+sha-748faa4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
