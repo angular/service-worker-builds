4417d3edbbd35f08ec2d1ffc0bec4eb92229d82c
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 22:20:25 +0000

    e3ac727 docs: release notes for the v20.3.26 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.5+sha-57616af";
+  var SW_VERSION = "22.1.0-next.5+sha-e3ac727";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
