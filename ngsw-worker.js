f4618dd4941abc81b1edf9f84d21fa014013bfbd
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 16:00:49 +0000

    bec1413 docs: update formatting

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.5+sha-4314f72";
+  var SW_VERSION = "22.0.5+sha-bec1413";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
