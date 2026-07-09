c2ab4a0845f3f9642cb01a6cc774328a39287ca5
Matthew Beck <me@mattrb.com>
2026-07-09 17:08:32 +0000

    aa03cb9 test(language-service): empty template literal interpolation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.6+sha-89e2c77";
+  var SW_VERSION = "22.0.6+sha-aa03cb9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
