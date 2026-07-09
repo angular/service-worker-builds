235c89380fa0001aa800385338bbb5c25a9c7310
Matthew Beck <me@mattrb.com>
2026-07-09 17:06:28 +0000

    53ca8c2 test(language-service): empty template literal interpolation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.5+sha-5bd00ad";
+  var SW_VERSION = "22.1.0-next.5+sha-53ca8c2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
