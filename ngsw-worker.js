7ac55557de5630229c54bb9f0e7426932e9b52a7
Matthew Beck <me@mattrb.com>
2026-07-10 01:05:20 +0000

    1fb4678 Revert "fix(core): allow static attributes for explicit input transforms"

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.5+sha-b178e83";
+  var SW_VERSION = "22.1.0-next.5+sha-1fb4678";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
