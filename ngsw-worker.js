68cb2bd6c6ad496ce116483a090118aaea22bf9c
Matthew Beck <me@mattrb.com>
2026-07-10 18:20:22 +0000

    72cc548 feat(docs-infra): add Angie to the embedded editor error state

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.5+sha-1fb4678";
+  var SW_VERSION = "22.1.0-next.5+sha-72cc548";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
