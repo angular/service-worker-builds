253cf444f68db4f8d5d233e1ffa3b77df2247b86
Matthew Beck <me@mattrb.com>
2026-07-13 19:00:52 +0000

    ac5f476 build: update pnpm to v10.34.5

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.6+sha-cfb77d6";
+  var SW_VERSION = "22.0.6+sha-ac5f476";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
