a5a797c2f22ce7e73eee9a6337363ec4875ae68b
Matthew Beck <me@mattrb.com>
2026-07-14 16:28:48 +0000

    20b7dc3 fix(http): prevent interceptor signal reads from leaking into calling reactive contexts

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.6+sha-2695290";
+  var SW_VERSION = "22.0.6+sha-20b7dc3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
