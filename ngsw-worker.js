4eb24b0c7605b1107893657421c9f13e0d5fbdf2
Matthew Beck <me@mattrb.com>
2026-07-16 04:35:11 +0000

    f8c9e9d release: cut the v22.1.0-next.6 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.5+sha-d43acaf";
+  var SW_VERSION = "22.1.0-next.6+sha-f8c9e9d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
