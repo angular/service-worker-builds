6e30f8bad0a0300de60f75382ea8e8befccd42de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:47:08 +0000

    3192dcc fix(http): prevent transfer cache key collisions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.6+sha-00699f3";
+  var SW_VERSION = "22.1.0-next.6+sha-3192dcc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
