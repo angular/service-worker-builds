6fa33e12d325fb9547c040797c22dc03aa9031a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 09:17:10 +0000

    7eec520 test(compiler-cli): cover DOM-only vs full instruction set across compilation modes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.7+sha-f542a09";
+  var SW_VERSION = "22.0.7+sha-7eec520";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
