4fef81bd913eb6c8ae5108f1182ffef8f84c4e3a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 10:14:06 +0000

    2bc2146 build: update pnpm to v11.15.1

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.6+sha-eb3e480";
+  var SW_VERSION = "22.1.0-next.6+sha-2bc2146";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
