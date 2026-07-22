5a0638a86a7c30fffc82caee242acebc282b291c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:40:47 +0000

    548ef47 release: cut the v22.0.8 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.7+sha-45eef01";
+  var SW_VERSION = "22.0.8+sha-548ef47";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
