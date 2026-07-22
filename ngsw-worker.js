1dc7f0342e034144b675ea81c2b942241151488c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 12:32:16 +0000

    ab52df4 fix(docs-infra): fail the guide build when a markdown file starts with a BOM

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-next.6+sha-9a1e620";
+  var SW_VERSION = "22.1.0-next.6+sha-ab52df4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
