33357b8976a4cf1c7702aaec1cf6edbdf2e20c18
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 09:58:49 +0000

    66f27a4 docs: add skills for Angular pipes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.0.7+sha-388daea";
+  var SW_VERSION = "22.0.7+sha-66f27a4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
