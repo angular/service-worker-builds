5048c1c7345db3cf89c20b5638d88b1d63196898
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:34:28 +0000

    125a794 docs: use https for external links in adev

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0-rc.0+sha-1b85b3c";
+  var SW_VERSION = "22.1.0-rc.0+sha-125a794";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
