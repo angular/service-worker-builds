fe40b01141dcd2d657bf067cf151c74dba254739
GitHub <noreply@github.com>
2026-07-24 20:30:37 +0000

    9949dcc fix(http): enable xsrf for root-provided HttpClient

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.18+sha-5d00aa2";
+  var SW_VERSION = "21.2.18+sha-9949dcc";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
