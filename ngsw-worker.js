3d84e7a50020ec2c61d8f45ef3ef4f3364847eac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:42:23 +0000

    1784530 docs: modernize directives guides (#69822)

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.0+sha-28d59e8";
+  var SW_VERSION = "22.2.0-next.0+sha-1784530";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
