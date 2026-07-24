6ca9f65e582f6c5b405c6b5cd58b04aa7e28e0c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 20:53:09 +0000

    e606a02 fix(language-service): account for strictTemplates being enabled by default

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.0+sha-1784530";
+  var SW_VERSION = "22.2.0-next.0+sha-e606a02";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
