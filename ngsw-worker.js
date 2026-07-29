ef415ca742556f5330ead830b62deb9a386f42a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:00:28 +0000

    db0d4a1 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.26+sha-4d62760";
+  var SW_VERSION = "20.3.26+sha-db0d4a1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
