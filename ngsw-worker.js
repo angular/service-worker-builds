db1afa291aecd024e3fa2e3248185643e8eb9ada
GitHub <noreply@github.com>
2026-07-30 15:36:51 +0000

    6afe6fa fix(core): sanitize host bindings on concrete hosts

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.19+sha-07cb840";
+  var SW_VERSION = "21.2.19+sha-6afe6fa";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
