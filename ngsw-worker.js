cae408bc70c4d7d263dec8b600741489addac89e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 19:38:20 +0000

    aa6d318 fix(core): initialize hydration triggers after late runtime activation

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0+sha-d6ec7c7";
+  var SW_VERSION = "22.1.0+sha-aa6d318";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
