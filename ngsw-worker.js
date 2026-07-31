97203450e2c8655a9d59490470953c17a1099a1c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 22:41:57 +0000

    3d0067a refactor(animations): remove outdated comment

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0+sha-aa6d318";
+  var SW_VERSION = "22.1.0+sha-3d0067a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
