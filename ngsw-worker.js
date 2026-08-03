ca8662807900184ebb157337348031176cd3c0e0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-03 16:52:31 +0000

    337053e refactor(compiler): remove unused code and exports

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.0+sha-fb698d1";
+  var SW_VERSION = "22.2.0-next.0+sha-337053e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
