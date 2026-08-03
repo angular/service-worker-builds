9578b3428927ae4e44d5806ebb931b1ce5259456
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-03 16:05:52 +0000

    79a8193 docs: fix roadmap link text and preview-site link

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.0+sha-3d0067a";
+  var SW_VERSION = "22.1.0+sha-79a8193";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
