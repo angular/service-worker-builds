7cebc964e7be0977d9d9d304ac2c9bfe4e34266b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-03 16:36:40 +0000

    fb698d1 test(platform-server): add SSR integration test for null and normal input values

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.0+sha-efe1aae";
+  var SW_VERSION = "22.2.0-next.0+sha-fb698d1";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
