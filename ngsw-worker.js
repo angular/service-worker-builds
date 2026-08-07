b709c6c92021d028fd4c06276a32bcdb0843a8c7
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 17:38:06 +0000

    cf1844b refactor(devtools): use JavaScriptTransformer for Angular optimization esbuild plugin

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.1+sha-8a40fbe";
+  var SW_VERSION = "22.2.0-next.1+sha-cf1844b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
