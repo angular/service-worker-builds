b05d7dbccd8ba2f81997a20869fa6a658a267861
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 16:51:20 +0000

    4fc6e31 build: update dependency @types/jsdom to v30

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.1+sha-768c8c2";
+  var SW_VERSION = "22.2.0-next.1+sha-4fc6e31";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
