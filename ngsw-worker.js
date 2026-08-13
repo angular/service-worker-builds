e61ee5b8a007acb140c652feac22f360c88f704b
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 00:13:55 +0000

    dca0a77 release: cut the v21.2.20 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.2.19+sha-caf6166";
+  var SW_VERSION = "21.2.20+sha-dca0a77";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
