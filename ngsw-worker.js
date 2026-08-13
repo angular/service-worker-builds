104e5e3d4cfb8c5727ce29ce11d86bfb18d2b171
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 16:05:19 +0000

    647bf8e docs: note SafeResourceUrl audio src change

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.1+sha-6f848db";
+  var SW_VERSION = "22.2.0-next.1+sha-647bf8e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
