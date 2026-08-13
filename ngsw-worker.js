ada8ec96cf65382052b9b4936c208bfdda54ae7a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 15:48:35 +0000

    6f848db build(vscode-extension): dynamically resolve upstream remote and use token for https pushes

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.1+sha-c9990c4";
+  var SW_VERSION = "22.2.0-next.1+sha-6f848db";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
