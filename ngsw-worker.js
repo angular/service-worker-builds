1ee0009c8c4bdfcfa3c25479f5759609e08863ab
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 17:15:07 +0000

    1d576ee release: cut the v20.3.28 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.27+sha-e4c416c";
+  var SW_VERSION = "20.3.28+sha-1d576ee";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
