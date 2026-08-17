9eb3f739b2d63170ecc14a1030593cbf490547fb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-17 20:51:59 +0000

    7538744 fix(platform-browser): disallow event handler attributes in Meta

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.28+sha-1d576ee";
+  var SW_VERSION = "20.3.28+sha-7538744";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
