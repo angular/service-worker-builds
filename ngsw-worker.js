4bbddfe51d2ad54ddfef6345e28411324953f006
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-17 21:14:40 +0000

    5cb4ea7 fix(forms): warn in dev mode when ngModel cannot reach parent NgForm across component boundary

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.2+sha-0849fac";
+  var SW_VERSION = "22.1.2+sha-5cb4ea7";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
