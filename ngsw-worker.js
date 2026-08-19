0bcbd71c032b6610ab946349a0336a20d932527d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 00:18:25 +0000

    83450d2 fix(forms): report forbidden 2way bindings on when `FormField` is applied

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.2+sha-94f0b9a";
+  var SW_VERSION = "22.1.2+sha-83450d2";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
