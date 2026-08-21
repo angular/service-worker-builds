bc20337a8c5bebafdea2841cc0ac90d094e8bb07
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-21 19:45:46 +0000

    51cb07e docs: point the LLM indexes at current pages, not redirects

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.3+sha-238d8bf";
+  var SW_VERSION = "22.2.0-next.3+sha-51cb07e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
