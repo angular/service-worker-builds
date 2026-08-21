c0f71088b008575cdb5e583263f146402244ae60
leonsenft <leonsenft@users.noreply.github.com>
2026-08-21 19:16:07 +0000

    ee63c0a refactor(docs-infra): hold editor diagnostics in a signal

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.3+sha-004cf3a";
+  var SW_VERSION = "22.1.3+sha-ee63c0a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
