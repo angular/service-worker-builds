5110c0563bba1c88381fc038a88bb41de591dba4
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 17:51:01 +0000

    89b2056 fix(platform-server): update domino to latest version

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.29+sha-77f7b60";
+  var SW_VERSION = "20.3.29+sha-89b2056";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
