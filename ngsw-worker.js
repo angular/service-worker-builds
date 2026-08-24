e488763dd66e048df4d85338e3f20c2adc058c65
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 22:43:26 +0000

    82a7675 feat(devtools): add "Watch signal" button to signal-details component

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.3+sha-671d8d3";
+  var SW_VERSION = "22.2.0-next.3+sha-82a7675";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
