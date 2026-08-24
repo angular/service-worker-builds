2ed6d8de82c1dc33dbe166b705a0f3b391bcfb7b
leonsenft <leonsenft@users.noreply.github.com>
2026-08-24 22:40:27 +0000

    7c752d4 fix(core): prevent TransferState prototype pollution

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.3+sha-ba3bc47";
+  var SW_VERSION = "22.1.3+sha-7c752d4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
