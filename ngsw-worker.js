1949039180cb507bfb2a83cd855adb2520797555
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 17:32:49 +0000

    745cc30 refactor(docs-infra): remove example code that nothing references

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.3+sha-7c752d4";
+  var SW_VERSION = "22.1.3+sha-745cc30";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
