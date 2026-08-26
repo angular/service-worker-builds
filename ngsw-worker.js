3d1d4bb554b7995d41578d031a80b6f79cfc00d3
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-26 20:56:59 +0000

    66d505e refactor(core): Decouple ɵɵFactoryDeclaration and ɵɵInjectableDeclaration from type parameter T.

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.3+sha-1d0945f";
+  var SW_VERSION = "22.2.0-next.3+sha-66d505e";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
