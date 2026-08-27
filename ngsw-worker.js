f6a910877a68d76a84e629cdae4b36403226a02b
Matthew Beck <me@mattrb.com>
2026-08-27 15:12:40 +0000

    6f51be6 build: pin `vscode-languageserver-textdocument` version

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.4+sha-8983809";
+  var SW_VERSION = "22.1.4+sha-6f51be6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
