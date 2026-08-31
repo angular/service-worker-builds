e24225fe32a16c1ca9b0782e770fdab2fbc36a37
Matthew Beck <me@mattrb.com>
2026-08-31 20:37:15 +0000

    3b8723c fix(platform-server): resolve HTTP(S) URLs without authority as relative during SSR

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.4+sha-341bc0d";
+  var SW_VERSION = "22.1.4+sha-3b8723c";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
