485037973454269b0db32f6d9094775e1875d9f3
Matthew Beck <me@mattrb.com>
2026-08-31 22:44:56 +0000

    87d38ec build: lock file maintenance

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.4+sha-4c4a705";
+  var SW_VERSION = "22.2.0-next.4+sha-87d38ec";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
