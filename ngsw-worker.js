ff3e25a97d3901e301c5109403bf75810aa6751d
Matthew Beck <me@mattrb.com>
2026-09-02 23:18:08 +0000

    731b959 fix(vscode-extension): prevent JSDoc link provider injection

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.4+sha-94b1d3d";
+  var SW_VERSION = "22.1.4+sha-731b959";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
