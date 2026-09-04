10cf3c3be416a59753c970e202a7a5405ffe0450
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:56:59 +0000

    ef48630 test(core): replace act with actAsync

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.5+sha-6f5406c";
+  var SW_VERSION = "22.1.5+sha-ef48630-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
