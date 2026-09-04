dadfa3b125d810b9e55be1cc65e3d4ef8bdc5d74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:22:05 +0000

    74c1716 refactor(compiler-cli): index pipes in template expressions

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.5+sha-81c3f3a";
+  var SW_VERSION = "22.2.0-next.5+sha-74c1716";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
