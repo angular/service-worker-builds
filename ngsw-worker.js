64dc685f118b86de4d92fa2e82783124a95c73da
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-09-09 20:42:54 +0000

    da8dac6 fix(docs-infra): highlight home page code for the resolved theme

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.6+sha-356adf7";
+  var SW_VERSION = "22.1.6+sha-da8dac6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
