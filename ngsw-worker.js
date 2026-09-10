d4f02a620ebfc692aa6b9ea7ad3129cdc8a8882f
Andrew Scott <atscott01@gmail.com>
2026-09-10 23:24:52 +0000

    ae33a5f test(core): fix view injector integration spec

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-e205075-with-local-changes";
+  var SW_VERSION = "22.2.0-next.7+sha-ae33a5f-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
