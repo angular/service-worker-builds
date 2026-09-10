0da15c1bbc782a8ac395993b67522f60ff9baffb
Andrew Scott <atscott01@gmail.com>
2026-09-10 20:33:29 +0000

    14dbbf9 fix(compiler): wrap `@for` collection expression before appending non-null assertion

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-8ca879e";
+  var SW_VERSION = "22.2.0-next.7+sha-14dbbf9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
