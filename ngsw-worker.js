712e65b61c7bf09829ab9de12dd8b561299cb256
Andrew Scott <atscott01@gmail.com>
2026-09-10 22:43:34 +0000

    e205075 docs: add error boundaries entries

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-29f9dd5";
+  var SW_VERSION = "22.2.0-next.7+sha-e205075-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
