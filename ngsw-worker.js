ecf30f6595d1c8fd200ce0e492bc64a3e4b18663
Andrew Scott <atscott01@gmail.com>
2026-09-11 17:50:29 +0000

    01d091b fix(docs-infra): raise SSR fetch limit so the menubar guide prerenders

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-37ca679-with-local-changes";
+  var SW_VERSION = "22.2.0-next.7+sha-01d091b";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
