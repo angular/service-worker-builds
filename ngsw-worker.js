aebeb4e1d1bb8dc848e42eeaf2a9a0fbe8824436
Andrew Scott <atscott01@gmail.com>
2026-09-11 21:47:17 +0000

    2dcdf9a fix(router): mark router_resource module-level symbols as side-effect free

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-c097c01";
+  var SW_VERSION = "22.2.0-next.7+sha-2dcdf9a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
