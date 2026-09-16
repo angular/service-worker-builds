c41b1edf9a4bcec0045757664e418b70a6c381be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 13:54:00 +0000

    430a8eb docs: add the missing heading to the effects guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-0de811d";
+  var SW_VERSION = "22.2.0-next.7+sha-430a8eb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
