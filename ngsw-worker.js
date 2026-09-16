d9668cb12a5a51ff241ab32cbed7f47156aeb0f6
GitHub <noreply@github.com>
2026-09-16 12:18:51 +0000

    fe94aae refactor(devtools): introduce a global backend config

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.2.0-next.7+sha-c60c41e";
+  var SW_VERSION = "22.2.0-next.7+sha-fe94aae";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
