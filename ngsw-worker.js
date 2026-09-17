b1df0d9f6579908a0860ed05bbbf625e7fdb229c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 17:35:36 +0000

    9ba5faa docs(core): restore the PipeTransform code examples

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-e623982";
+  var SW_VERSION = "22.3.0-next.0+sha-9ba5faa";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
