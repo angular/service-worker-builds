133734f878d04aeb249e26d203de6a575ae6d90a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 17:54:29 +0000

    6e3ce79 docs: use the remote-cache config in the bazel caching guide

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-9ba5faa";
+  var SW_VERSION = "22.3.0-next.0+sha-6e3ce79";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
