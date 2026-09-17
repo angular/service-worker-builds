10547d629ddcbfd85fe82493bd94b36e6b4abaa7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 21:26:00 +0000

    0b632de refactor(platform-browser): type shadow DOM renderer fields

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-03754b3";
+  var SW_VERSION = "22.1.7+sha-0b632de";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
