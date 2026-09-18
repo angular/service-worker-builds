70d34a6671dce54610568f2ae29395a833a2bb2e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-18 16:54:07 +0000

    7569a02 docs(core): add error guide for NG0600

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-1151eb9";
+  var SW_VERSION = "22.3.0-next.0+sha-7569a02";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
