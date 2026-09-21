063501529e80fcb24c2ce7c9e84c105996fd620d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:18:06 +0000

    aa2cabb refactor(compiler): add method to emitter context

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-f39db77";
+  var SW_VERSION = "22.1.7+sha-aa2cabb";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
