062b6d622f62b78b45301e0328bc87185ab14bc4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:37:03 +0000

    78c7ab3 fix(forms): create controls before listeners

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-aa2cabb";
+  var SW_VERSION = "22.1.7+sha-78c7ab3";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
