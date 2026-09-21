3f1123f07fe9616e5455b1c3a4090b9909143d94
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 19:16:27 +0000

    6647a07 docs: update `disabledReasons` and `transformedValue` examples

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-b0d4010-with-local-changes";
+  var SW_VERSION = "22.3.0-next.0+sha-6647a07";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
