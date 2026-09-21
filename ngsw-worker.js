64dec904db95d07e91ec6145f3878afd149ca87e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-21 18:39:27 +0000

    b0d4010 test(router): Fix broken test

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-6688a3d-with-local-changes";
+  var SW_VERSION = "22.3.0-next.0+sha-b0d4010-with-local-changes";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
