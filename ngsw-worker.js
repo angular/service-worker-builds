e9b7a94825bb9c1d24314445875edc233ed4a51b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:50:58 +0000

    71f84a8 fix(vscode-extension): provide folding ranges for templates with interpolated strings

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-d7d5401";
+  var SW_VERSION = "22.1.7+sha-71f84a8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
