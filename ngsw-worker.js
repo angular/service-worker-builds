37788ea6b9ae18ba4c18da96bd8ce4a5d01a7168
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:56:07 +0000

    59e2c2f docs(service-worker): render the SwPush and SwRegistrationOptions examples

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-726368b";
+  var SW_VERSION = "22.1.7+sha-59e2c2f";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
