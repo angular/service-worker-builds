10685719d6bd6b4db04d2e391d1019107acd05f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:26:22 +0000

    d7d5401 fix(core): don't use plain objects as maps for jsaction data

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-8db8817";
+  var SW_VERSION = "22.1.7+sha-d7d5401";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
