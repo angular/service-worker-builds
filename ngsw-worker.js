902a33b18e039a66f89f42636020a2e654dfdf13
Andrew Scott <atscott01@gmail.com>
2026-09-23 20:09:31 +0000

    28ad39a release: bump Angular DevTools version to 1.22.0

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-0737c4e";
+  var SW_VERSION = "22.3.0-next.0+sha-28ad39a";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
