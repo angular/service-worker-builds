bb65b8563820ecb816883fb054ee36d356765eb6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 16:02:56 +0000

    2cab6f5 fix(router): avoid dense elements allocation for numeric URL keys

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.1.7+sha-a2fedc1";
+  var SW_VERSION = "22.1.7+sha-2cab6f5";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
