fd8c6c44c75185e2df99bc0041ef6f3f95ebbed3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 18:04:49 +0000

    4d3f502 docs: release notes for the v22.2.0 release

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-123729b";
+  var SW_VERSION = "22.3.0-next.0+sha-4d3f502";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
