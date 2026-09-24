ea1b66ba7af64fe66ba747ac7cd88750501f2250
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:44:54 +0000

    846c73d fix(core): add debugName on afterRenderEffect

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-5b52d19";
+  var SW_VERSION = "22.3.0-next.0+sha-846c73d";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
