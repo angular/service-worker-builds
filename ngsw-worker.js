0fddd718ed9e51d9a128ec840218a235d49481a6
Andrew Scott <atscott01@gmail.com>
2026-09-25 22:50:20 +0000

    3c5ed06 docs: update releases.md for v22.2

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-d3eaf9a";
+  var SW_VERSION = "22.3.0-next.0+sha-3c5ed06";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
