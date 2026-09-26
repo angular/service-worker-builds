3574e4b3980ed0065556b7279cabc511ea1149d1
Andrew Scott <atscott01@gmail.com>
2026-09-26 00:03:36 +0000

    319a3c4 perf(router): do not retain UrlTree instances in RouterLink

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1312,7 +1312,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "22.3.0-next.0+sha-deade10";
+  var SW_VERSION = "22.3.0-next.0+sha-319a3c4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
